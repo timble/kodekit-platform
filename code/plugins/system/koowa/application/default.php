195f88952dc6b2f59305921f719508c489620fa4
Johan Janssens <johan@timble.net>
2008-08-16 01:09:59 +0000

    Initial import

---
+++ code/plugins/system/koowa/application/default.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* @version $Id: $
+* @package Koowa_Application
+* @copyright Copyright (C) 2007 - 2008 Joomlatools. All rights reserved.
+* @license GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+* @link http://www.koowa.org
+*/
+ 
+/**
+* Default Document Class
+*
+* @author Johan Janssens <johan@joomlatools.org>
+* @package Koowa_Application
+*/
+class KApplicationDefault extends KApplicationAbstract
+{
+ 
+}
