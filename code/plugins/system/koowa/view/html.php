2e19d72dd4065a506ee0070be930910a70681aee
Mathias Verraes <mathias@verraes.net>
2009-06-29 19:26:06 +0000

    componentname.css is no longer loaded automatically

--- code/plugins/system/koowa/view/html.php
+++ code/plugins/system/koowa/view/html.php
@@ -22,32 +22,29 @@ class KViewHtml extends KViewAbstract
 	public function __construct($options = array())
 	{
 		$options = $this->_initialize($options);
-		
+
 		// Add a rule to the template for form handling and secrity tokens
 		KTemplate::addRules(array(KFactory::get('lib.koowa.template.filter.form')));
-		
+
 		// Set a base path for use by the view
 		$this->assign('baseurl', $options['base_url']);
 
 		parent::__construct($options);
 	}
-	
+
 	public function display()
 	{
 		$prefix = $this->getClassName('prefix');
 		$suffix = $this->getClassName('suffix');
 
 		$app = KFactory::get('lib.joomla.application')->getName();
-		
-		//Set the main stylesheet for the component
-		KTemplate::loadHelper('stylesheet', $prefix.'.css', 'media/com_'.$prefix.'/css/');
-		
+
 		//Push the toolbar output into the document buffer
 		$this->_document->setBuffer(
-			KFactory::get($app.'::com.'.$prefix.'.toolbar.'.$suffix)->render(), 
-			'modules', 
+			KFactory::get($app.'::com.'.$prefix.'.toolbar.'.$suffix)->render(),
+			'modules',
 			'toolbar'
-		);	
+		);
 
 		parent::display();
 	}
