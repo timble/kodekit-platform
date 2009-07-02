ed7c08a27bd4f90249c3cde83f96633d3a751b06
Tom Janssens <tom@timble.net>
2009-07-02 20:35:04 +0000

    Whoeps, forgot to commit the dispatcher

---
+++ code/administrator/components/com_beer/beer.php
@@ -0,0 +1,13 @@
+<?php
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+// Check if Koowa is active
+if(!defined('KOOWA')) {
+    JError::raiseWarning(0, JText::_("Koowa wasn't found. Please install the Koowa plugin and enable it."));
+    return;
+}
+
+// Create the component dispatcher
+$dispatcher = KFactory::get('admin::com.beer.dispatcher', array('default_view' => 'people'));
+$dispatcher->dispatch();
