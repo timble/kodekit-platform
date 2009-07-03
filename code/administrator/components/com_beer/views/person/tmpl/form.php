9d56999aa3897645768f146eb7a0f17e44c67cfe
Shayne Bartlett <shayne@timble.net>
2009-07-03 14:49:41 +0000

    Replaced definition list with more semantically correct CSS based layout. Minor accessibility changes.

--- code/administrator/components/com_beer/views/person/tmpl/form.php
+++ code/administrator/components/com_beer/views/person/tmpl/form.php
@@ -20,57 +20,23 @@
 	}
 </script>
 <form action="<?= @route('&id='. @$person->id)?>" method="post" class="adminform" name="adminForm">
-	<div style="width:100%; float: left">
+	<div style="width:100%; float: left"  id="mainform">
 		<fieldset>
 			<legend><?= @text('Details'); ?></legend>
-			<dl class="list">
-				<dt><label><?= @text('Firstname'); ?></label></dt>
-				<dd>
-					<input id="firstname_field" type="text" name="firstname" value="<?= @$person->firstname; ?>" />
-				</dd>
-				<dt><label><?= @text('Middlename'); ?></label></dt>
-				<dd>
-					<input id="middlename_field" type="text" name="middlename" value="<?= @$person->middlename; ?>" />
-				</dd>
-				<dt><label><?= @text('Lastname'); ?></label></dt>
-				<dd>
-					<input id="lastname_field" type="text" name="lastname" value="<?= @$person->lastname; ?>" />
-				</dd>
-				<dt><label><?= @text('Office'); ?></label></dt>
-				<dd>
-					<?=@helper('admin::com.beer.helper.select.offices', @$person->beer_office_id, 'beer_office_id', '', '', true) ?>
-				</dd>
-				<dt><label><?= @text('Department'); ?></label></dt>
-				<dd>
-					<?=@helper('admin::com.beer.helper.select.departments', @$person->beer_department_id, 'beer_department_id', '', '', true) ?>
-				</dd>
-				<dt><label><?= @text('Position'); ?></label></dt>
-				<dd>
-					<input id="position_field" type="text" name="position" value="<?= @$person->position; ?>" />
-				</dd>
-				<dt><label><?= @text('Birthday'); ?></label></dt>
-				<dd>
-					<input id="birthday_field" type="text" name="birthday" value="<?= @$person->birthday; ?>" />
-				</dd>
-				<dt><label><?= @text('Gender'); ?></label></dt>
-				<dd>
-					<input id="gender_field" type="text" name="gender" value="<?= @$person->gender; ?>" />
-				</dd>
-				<dt><label><?= @text('Mobile'); ?></label></dt>
-				<dd>
-					<input id="mobile_field" type="text" name="mobile" value="<?= @$person->mobile; ?>" />
-				</dd>
-				<dt><label><?= @text('Email'); ?></label></dt>
-				<dd>
-					<input id="email_field" type="text" name="email" value="<?= @$person->email; ?>" />
-				</dd>
-				<dt><label><?= @text('Published'); ?></label></dt>
-				<dd>
-					<?= @helper('select.booleanlist', 'enabled', null, @$person->enabled, 'yes', 'no', 'enabled'); ?>
+				<label for="firstname" id="mainlabel"><?= @text('Firstname'); ?></label><input id="firstname" type="text" name="firstname" value="<?= @$person->firstname; ?>" /><br />
+				<label for="middlename" id="mainlabel"><?= @text('Middlename'); ?></label><input id="middlename" type="text" name="middlename" value="<?= @$person->middlename; ?>" /><br />
+				<label for="lastname" id="mainlabel"><?= @text('Lastname'); ?></label><input id="lastname" type="text" name="lastname" value="<?= @$person->lastname; ?>" /><br />
+				<label for="office" id="mainlabel"><?= @text('Office'); ?></label><?=@helper('admin::com.beer.helper.select.offices', @$person->beer_office_id, 'beer_office_id', '', '', true) ?><br />
+				<label for="departmen" id="mainlabel"><?= @text('Department'); ?></label><?=@helper('admin::com.beer.helper.select.departments', @$person->beer_department_id, 'beer_department_id', '', '', true) ?><br />
+				<label for="position" id="mainlabel"><?= @text('Position'); ?></label><input id="position" type="text" name="position" value="<?= @$person->position; ?>" /><br />
+				<label for="birthday" id="mainlabel"><?= @text('Birthday'); ?></label><input id="birthday" type="text" name="birthday" value="<?= @$person->birthday; ?>" /><br />
+				<label for="gender" id="mainlabel"><?= @text('Gender'); ?></label><input id="gender" type="text" name="gender" value="<?= @$person->gender; ?>" /><br />
+				<label for="mobile" id="mainlabel"><?= @text('Mobile'); ?></label><input id="mobile" type="text" name="mobile" value="<?= @$person->mobile; ?>" /><br />
+				<label for="email" id="mainlabel"><?= @text('Email'); ?></label><input id="email" type="text" name="email" value="<?= @$person->email; ?>" /><br />
+				<label for="enabled" id="mainlabel"><?= @text('Published'); ?></label><?= @helper('select.booleanlist', 'enabled', null, @$person->enabled, 'yes', 'no', 'enabled'); ?><br />
 				</dd>
 			</dl>
 		</fieldset>
-
 	</div>
 	<input type="hidden" name="id" value="<?= @$person->id ?>" />
 </form>
