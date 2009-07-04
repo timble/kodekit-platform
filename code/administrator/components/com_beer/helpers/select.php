3fa9008043d321e0dafcf3f6c0a78ed15d23b0ca
Tom Janssens <tom@timble.net>
2009-07-04 14:19:22 +0000

    Changed select helpers, allowAny is now default true

--- code/administrator/components/com_beer/helpers/select.php
+++ code/administrator/components/com_beer/helpers/select.php
@@ -4,7 +4,7 @@
  */
 class BeerHelperSelect extends KTemplateHelperSelect
 {
-	public static function offices($selected, $name = 'filter_office_id', $attribs = array('class' => 'inputbox', 'size' => '1'), $idtag = null, $allowAny = false)
+	public static function offices($selected, $name = 'filter_office_id', $attribs = array('class' => 'inputbox', 'size' => '1'), $idtag = null, $allowAny = true)
  	{
 		$items = KFactory::get('admin::com.beer.model.offices')->getAll();
 
@@ -21,7 +21,7 @@ class BeerHelperSelect extends KTemplateHelperSelect
 		return self::genericlist($list, $name, $attribs, 'beer_office_id', 'title', $selected, $idtag );
  	}
  	
-	public static function departments($selected, $name = 'filter_department_id', $attribs = array('class' => 'inputbox', 'size' => '1'), $idtag = null, $allowAny = false)
+	public static function departments($selected, $name = 'filter_department_id', $attribs = array('class' => 'inputbox', 'size' => '1'), $idtag = null, $allowAny = true)
  	{
 		$items = KFactory::get('admin::com.beer.model.departments')->getAll();
 
