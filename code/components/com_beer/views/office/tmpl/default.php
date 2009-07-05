99fab1d1b2c75d4147b612bcc7acf8b6b5a807c9
Shayne Bartlett <shayne@timble.net>
2009-07-05 17:26:01 +0000

    Google Maps

--- code/components/com_beer/views/office/tmpl/default.php
+++ code/components/com_beer/views/office/tmpl/default.php
@@ -1,17 +1,24 @@
 <? /** $Id$ */ ?>
 <? defined('KOOWA') or die('Restricted access'); ?>
 <? @style(@$mediaurl.'/com_beer/css/default.css'); ?>
+<? @script('http://maps.google.com/maps?file=api&amp;v=2.x&amp;key=ABQIAAAAnMnp44FFgiYJ2JttXUUpNxT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQGCyA8pHIjpDhN93FhoORdP8TN1A'); ?>
+<? @script(@$mediaurl.'/com_beer/js/maps.js'); ?>
 
 <h1 class="componentheading"><?= @$office->title; ?></h1>
 <div id="beer_info">
-	<img src="<?= @$mediaurl;?>/com_beer/images/flags/<?= strtolower(@$office->country);?>.png" alt="<?= @$office->country;?> flag"/><?= nl2br(@$office->address)?>
+	<img src="<?= @$mediaurl;?>/com_beer/images/flags/<?= strtolower(@$office->country);?>.png" alt="<?= @$office->country;?> flag"/><?= @$office->address1 . ' ' . @$office->address2 . ' ' . @$office->city . ' ' . @$office->state . ' ' . @$office->postcode; ?>
 	<span class="phone"><?= @$office->phone; ?></spam>
 	<span class="fax"><?= @$office->fax; ?></span>
-	<span class="people"><?= @$office->people; ?></span>
-	<span class="map"><?= @text('Map'); ?></span>
+	<span class="people"><?= @$office->people; ?><a href="<?=@route('option=com_beer&view=people&filter[office]='.$person->beer_office_id) ?>" /></a></span>
+	<span class="map"><a href="#" onclick="showAddress(<?= @$office->address1 . ' ' . @$office->address2 . ', ' . @$office->city . ', ' . @$office->state . ', ' . @$office->postcode . ', ' . @$office->country; ?>); return false"><?= @text('Map'); ?></a></span>
 	</div>
 <div id="beer_desc">
 	<h2><?= @text('Information'); ?></h2>
 	<?= @$office->description; ?>
 </div>
+<div id="map_canvas" style="width: 500px; height: 300px"></div>
+
+	
+	
+
 
---
+++ code/media/com_beer/js/maps.js
@@ -0,0 +1,28 @@
+    var map = null;
+    var geocoder = null;
+
+    function initialize() {
+      if (GBrowserIsCompatible()) {
+        map = new GMap2(document.getElementById("map_canvas"));
+        map.setCenter(new GLatLng(37.4419, -122.1419), 13);
+        geocoder = new GClientGeocoder();
+      }
+    }
+
+    function showAddress( address ) {
+      if (geocoder) {
+        geocoder.getLatLng(
+          address,
+          function(point) {
+            if (!point) {
+              alert(address + " not found");
+            } else {
+              map.setCenter(point, 13);
+              var marker = new GMarker(point);
+              map.addOverlay(marker);
+              marker.openInfoWindowHtml(address);
+            }
+          }
+        );
+      }
+    }
