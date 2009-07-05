9cce3ed0f1ae71d754c53e8a229001e0595c3e40
Shayne Bartlett <shayne@timble.net>
2009-07-05 08:46:07 +0000

    Added Gravatar Support and more details icons

--- code/components/com_beer/views/person/tmpl/default.php
+++ code/components/com_beer/views/person/tmpl/default.php
@@ -1,10 +1,17 @@
 <? /** $Id$ */ ?>
 <? @style(@$mediaurl.'/com_beer/css/default.css'); ?>
-
-<h1 class="componentheading"><?= @$person->firstname . ' ' .  @$person->middlename . ' ' . @$person->lastname; ?></h1>
+<div id="beer_header">
+	<div id="gravatar"><img src="http://www.gravatar.com/avatar.php?gravatar_id=<?= md5( strtolower(@$person->email) ); ?>&size=50" alt="Gravatar" /></div> 
+	<h1 class="componentheading"><?= @$person->firstname . ' ' .  @$person->middlename . ' ' . @$person->lastname; ?></h1>
+	<h2 id="beer_position"><?= @$person->position; ?></h2>
+	<div class="clr"></div>
+</div>
 <div id="beer_info">
 	<span class="mobile"><?= @$person->mobile; ?></spam>
-	<span class="email"><?= @$office->email; ?></span>
+	<span class="email"><?= @$person->email; ?></span>
+	<span class="dob"><?= @$person->dob; ?></span>
+	<span class="gender_<?= @$person->gender; ?>"><?= @$person->gender == "1" ? @text('Male') : @text('Female'); ?></span>
+	<span class="vcard"><?= @text('VCard'); ?></span>
 	</div>
 <div id="beer_desc">
 	<h2><?= @text('Bio'); ?></h2>
