2d12724d251fe1892576dfa0fc3bd7885865a271
Tom Janssens <tom@timble.net>
2009-07-05 21:41:14 +0000

    Added sample data

--- code/administrator/components/com_beer/install/install.sql
+++ code/administrator/components/com_beer/install/install.sql
@@ -94,3 +94,95 @@ SELECT o.*,
 FROM #__beer_offices AS o
 LEFT JOIN #__beer_people AS p ON p.beer_office_id = o.beer_office_id AND p.enabled > 0
 GROUP BY o.beer_office_id;
+
+-- --------------------------------------------------------
+
+--
+-- Dumping data for table `#__beer_departments`
+--
+
+INSERT INTO `#__beer_departments` (`beer_department_id`, `title`, `alias`, `description`, `created`, `created_by`, `modified`, `modified_by`, `enabled`) VALUES
+(1, 'Marketing', 'marketing', 'The world-class marketing team at Showdown is focused on leading-edge hardware and software that define the solutions that customers want, prompting the competition to emulate us. As the only company that designs the hardware, the software, and the operating system, we stand alone in our ability to innovate beyond the status quo. Part of what drives this innovation is our challenging and creative environment and the fierce dedication and talent of our team. In marketing, you have the unique opportunity to work on revolutionary products from concept to launch with the best creative minds in the industry.', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(2, 'Sales', 'sales', 'Showdown is committed to delivering the finest and most innovative computing solutions to students, educators, consumers, businesses, and creative professionals around the world. On the Sales team, our primary focus is to drive revenue for hardware, software, and professional services. One of the benefits of selling Showdown products is that they are completely integrated platforms. We focus on selling the value inherent in the complete product, rather than just individual boxes, and giving our customers a solution that address their needs. Our high-performance sales teams constantly strive to increase customer satisfaction and grow our market share.', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(3, 'Finance', 'finance', 'The Finance department is an integral part of Showdown\\''s success, supporting the growth and change of all functional areas of the company with flexibility and integrity. Having a team of talented thinkers who can balance a detail-oriented and quantifiable function within a dynamic, forward-thinking organization enables Showdown to create products that defy the status quo. The Finance department at Showdown offers opportunities for career development and growth as varied and engaging as the products we build.', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(4, 'Applications', 'applications', 'The Applications division develops simply awesome consumer and professional applications for digital media and personal productivity. Our people are both super-technical and abundantly creative. They\\''re engineers and marketers but they\\''re also musicians, filmmakers, photographers, composers and digital artists.', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(5, 'Information Systems & Technology', 'information-systems--technology', 'Information Systems & Technology (IS&T) drives Showdown\\''s corporate systems, retail systems and other key infrastructure. IS&T offers you tremendous opportunities to work on a wide variety of projects, from developing new tools, to implement standards-based applications, to managing Showdown\\''s email and telephone systems. The team uses industry-standard technologies such as SAP, PeopleSoft, Oracle and AIX, but also creates customized programs when off-the-shelf products don\\''t meet Showdown\\''s needs. If a global IT/MIS shop like no other is the challenge you\\''re looking for, we\\''re looking for you.', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1);
+
+
+-- --------------------------------------------------------
+
+--
+-- Dumping data for table `jos_beer_people`
+--
+
+INSERT INTO `#__beer_people` (`beer_person_id`, `beer_department_id`, `beer_office_id`, `firstname`, `middlename`, `lastname`, `alias`, `position`, `birthday`, `gender`, `mobile`, `email`, `bio`, `created`, `created_by`, `modified`, `modified_by`, `enabled`) VALUES
+(2, 1, 1, 'Eberhardt', '', 'Terkki', 'eberhardt_terkki', 'Employee', '1964-06-02', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(3, 1, 2, 'Bamford', '', 'Parto', 'bamford_parto', 'Employee', '1959-11-30', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:13', 62, '1970-01-01 01:00:00', 0, 1),
+(4, 1, 3, 'Chirstian', '', 'Koblick', 'chirstian_koblick', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:18', 62, '1970-01-01 01:00:00', 0, 1),
+(5, 1, 4, 'Kyoichi', '', 'Maliniak', 'kyoichi_maliniak', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:22', 62, '1970-01-01 01:00:00', 0, 1),
+(6, 1, 5, 'Tzvetan', '', 'Zielinski', 'tzvetan_zielinski', 'Employee', '1953-04-20', 1, '147258369', 'info@show.down', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(7, 1, 6, 'Saniya', '', 'Kalloufi', 'saniya_kalloufi', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(8, 1, 7, 'Sumant', '', 'Peac', 'sumant_peac', 'Employee', '1954-05-01', 2, '147258369', 'info@down.show', '', '2009-07-05 23:24:51', 62, '1970-01-01 01:00:00', 0, 1),
+(9, 1, 8, 'Duangkaew', '', 'Piveteau', 'duangkaew_piveteau', 'Employee', '1959-12-05', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:26', 62, '1970-01-01 01:00:00', 0, 1),
+(10, 1, 9, 'Mary', '', 'Sluis', 'mary_sluis', 'Employee', '1986-01-08', 2, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(11, 1, 10, 'Patricio', '', 'Bridgland', 'patricio_bridgland', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(12, 2, 1, 'Bezalel', '', 'Simmel', 'bezalel_simmel', 'Employee', '1964-06-02', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(13, 2, 2, 'Berni', '', 'Genin', 'berni_genin', 'Employee', '1959-11-30', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:31', 62, '1970-01-01 01:00:00', 0, 1),
+(14, 2, 3, 'Guoxiang', '', 'Nooteboom', 'guoxiang_nooteboom', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:36', 62, '1970-01-01 01:00:00', 0, 1),
+(15, 2, 4, 'Cristinel', '', 'Bouloucos', 'cristinel_bouloucos', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:43', 62, '1970-01-01 01:00:00', 0, 1),
+(16, 2, 5, 'Kazuhide', '', 'Peha', 'kazuhide_peha', 'Employee', '1953-04-20', 1, '147258369', 'info@show.down', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(17, 2, 6, 'Lillian', '', 'Haddadi', 'lillian_haddadi', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(18, 2, 7, 'Mayuko', '', 'Warwick', 'mayuko_warwick', 'Employee', '1954-05-01', 2, '147258369', 'info@down.show', '', '2009-07-05 23:24:59', 62, '1970-01-01 01:00:00', 0, 1),
+(19, 2, 8, 'Ramzi', '', 'Erde', 'ramzi_erde', 'Employee', '1959-12-05', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:49', 62, '1970-01-01 01:00:00', 0, 1),
+(20, 2, 9, 'Shahaf', '', 'Famili', 'shahaf_famili', 'Employee', '1986-01-08', 2, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(21, 2, 10, 'Bojan', '', 'Montemayor', 'bojan_montemayor', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(22, 3, 1, 'Suzette', '', 'Pettey', 'suzette_pettey', 'Employee', '1964-06-02', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(23, 3, 2, 'Prasadram', '', 'Heyers', 'prasadram_heyers', 'Employee', '1959-11-30', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:54', 62, '1970-01-01 01:00:00', 0, 1),
+(24, 3, 3, 'Yongqiao', '', 'Berztiss', 'yongqiao_berztiss', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '2009-07-05 23:33:06', 62, '1970-01-01 01:00:00', 0, 1),
+(25, 3, 4, 'Divier', '', 'Reistad', 'divier_reistad', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:59', 62, '1970-01-01 01:00:00', 0, 1),
+(26, 3, 5, 'Domenick', '', 'Tempesti', 'domenick_tempesti', 'Employee', '1953-04-20', 1, '147258369', 'info@show.down', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(27, 3, 6, 'Otmar', '', 'Herbst', 'otmar_herbst', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(28, 3, 7, 'Elvis', '', 'Demeyer', 'elvis_demeyer', 'Employee', '1954-05-01', 2, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(29, 3, 8, 'Karsten', '', 'Joslin', 'karsten_joslin', 'Employee', '1959-12-05', 1, '147258369', 'info@down.show', '', '2009-07-05 23:32:07', 62, '1970-01-01 01:00:00', 0, 1),
+(30, 3, 9, 'Jeong', '', 'Reistad', 'jeong_reistad', 'Employee', '1986-01-08', 2, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(31, 3, 10, 'Arif', '', 'Merlo', 'arif_merlo', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(32, 4, 1, 'Bader', '', 'Swan', 'bader_swan', 'Employee', '1964-06-02', 1, '147258369', 'info@down.show', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(33, 4, 2, 'Berni', '', 'Genin', 'berni_genin', 'Employee', '1959-11-30', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(34, 4, 3, 'Guoxiang', '', 'Nooteboom', 'guoxiang_nooteboom', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(35, 4, 4, 'Cristinel', '', 'Bouloucos', 'cristinel_bouloucos', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(36, 4, 5, 'Kazuhide', '', 'Peha', 'kazuhide_peha', 'Employee', '1953-04-20', 1, '147258369', 'info@show.down', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(37, 4, 6, 'Lillian', '', 'Haddadi', 'lillian_haddadi', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(38, 4, 7, 'Mayuko', '', 'Warwick', 'mayuko_warwick', 'Employee', '1954-05-01', 2, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(39, 4, 8, 'Ramzi', '', 'Erde', 'ramzi_erde', 'Employee', '1959-12-05', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(40, 4, 9, 'Shahaf', '', 'Famili', 'shahaf_famili', 'Employee', '1986-01-08', 2, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(41, 4, 10, 'Bojan', '', 'Montemayor', 'bojan_montemayor', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(42, 5, 1, 'Suzette', '', 'Pettey', 'suzette_pettey', 'Employee', '1964-06-02', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(43, 5, 2, 'Prasadram', '', 'Heyers', 'prasadram_heyers', 'Employee', '1959-11-30', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(44, 5, 3, 'Yongqiao', '', 'Berztiss', 'yongqiao_berztiss', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(45, 5, 4, 'Divier', '', 'Reistad', 'divier_reistad', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(46, 5, 5, 'Domenick', '', 'Tempesti', 'domenick_tempesti', 'Employee', '1953-04-20', 1, '147258369', 'info@show.down', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(47, 5, 6, 'Otmar', '', 'Herbst', 'otmar_herbst', 'Employee', '1953-04-20', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(48, 5, 7, 'Elvis', '', 'Demeyer', 'elvis_demeyer', 'Employee', '1954-05-01', 2, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(49, 5, 8, 'Karsten', '', 'Joslin', 'karsten_joslin', 'Employee', '1959-12-05', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(50, 5, 9, 'Jeong', '', 'Reistad', 'jeong_reistad', 'Employee', '1986-01-08', 2, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1),
+(51, 5, 10, 'Arif', '', 'Merlo', 'arif_merlo', 'Employee', '1954-05-01', 1, '147258369', 'info@down.show', '', '2009-07-05 23:35:01', 62, '1970-01-01 01:00:00', 0, 1);
+
+
+-- --------------------------------------------------------
+
+--
+-- Dumping data for table `#__beer_offices`
+--
+
+INSERT INTO `#__beer_offices` (`beer_office_id`, `title`, `alias`, `description`, `address1`, `address2`, `city`, `state`, `postcode`, `country`, `phone`, `fax`, `coordinates`, `created`, `created_by`, `modified`, `modified_by`, `enabled`) VALUES
+(1, 'Unites States', 'belgium', '', '1 Infinite Loop', '', 'Cupertino', 'CA', '95014', 'US', '147258369', '13456789', '50.9873946,5.0474845', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(2, 'Belgium', 'belgium', '', 'Chocolate 1', '', 'Brussel', '', '1000', 'BE', '1592648', '2615948', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(3, 'Netherlands', 'netherlands', '', 'Nederland 1', '', 'Amsterdam', '', '1000', 'NL', '147258369', '147258369', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(4, 'Australia', 'australia', '', 'Australia 1', '', 'Sydney', 'QLD', '2000', 'AU', '147258369', '147258369', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(5, 'Cameroon', 'cameroon', '', 'Cameroon 1', '', 'Yaoundé', '', '1000', 'CM', '147258369', '147258369', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(6, 'Canada', 'canada', '', 'Canada 1', '', 'Quebec', 'QC', '1000', 'CA', '147258369', '147258369', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(7, 'New Zealand', 'new-zealand', '', 'New Zealand 1', '', 'Wellington', 'GIS', '1000', 'NZ', '147258369', '147258369', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(8, 'Virgin Islands', 'virgin-islands', '', 'Virgin 1', '', 'Island', '', '3000', 'VI', '147258369', '147258369', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(9, 'Antarctica', 'antarctica', '', 'Snow 1', '', 'Cold', '', '1000', 'AQ', '147258369', '147258369', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1),
+(10, 'United Kingdom', 'united-kingdom', '', 'King 1', '', 'London', '', '2000', 'GB', '147258369', '147258369', '', '0000-00-00 00:00:00', 62, '1970-01-01 01:00:00', 0, 1);
+
