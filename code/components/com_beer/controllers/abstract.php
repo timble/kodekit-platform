b1259e36a435d10640e06f18d94fbf872514c26c
Mathias Verraes <mathias@verraes.net>
2009-07-05 19:38:18 +0000

    only show enabled items

--- code/components/com_beer/controllers/abstract.php
+++ code/components/com_beer/controllers/abstract.php
@@ -19,7 +19,7 @@ abstract class BeerControllerAbstract extends KControllerForm
 		$suffix = KInflector::pluralize($this->getClassName('suffix'));
 		$model = KFactory::get('site::com.beer.model.'.$suffix);
 
-		$model->setState('enabled',				KRequest::get('request.enabled', 'int'));
+		$model->setState('enabled',				1);
 		$model->setState('beer_department_id', 	KRequest::get('request.beer_department_id', 'int'));
 		$model->setState('beer_office_id', 		KRequest::get('request.beer_office_id', 'int'));
 		$model->setState('search', 				KRequest::get('request.search', 'string'));
