b3d2ea809a2e4e7aaa97b75b8148c2814775b0c7
Mathias Verraes <mathias@verraes.net>
2009-07-05 17:32:36 +0000

    fixes

--- code/components/com_beer/views/office/tmpl/default.php
+++ code/components/com_beer/views/office/tmpl/default.php
@@ -9,7 +9,7 @@
 	<img src="<?= @$mediaurl;?>/com_beer/images/flags/<?= strtolower(@$office->country);?>.png" alt="<?= @$office->country;?> flag"/><?= @$office->address1 . ' ' . @$office->address2 . ' ' . @$office->city . ' ' . @$office->state . ' ' . @$office->postcode; ?>
 	<span class="phone"><?= @$office->phone; ?></spam>
 	<span class="fax"><?= @$office->fax; ?></span>
-	<span class="people"><?= @$office->people; ?><a href="<?=@route('option=com_beer&view=people&filter[office]='.$person->beer_office_id) ?>" /></a></span>
+	<span class="people"><a href="<?=@route('option=com_beer&view=people&beer_office_id='.@$office->id) ?>"><?= @$office->people; ?> <?=@text('Employee(s)')?></a></span>
 	<span class="map"><a href="#" onclick="showAddress(<?= @$office->address1 . ' ' . @$office->address2 . ', ' . @$office->city . ', ' . @$office->state . ', ' . @$office->postcode . ', ' . @$office->country; ?>); return false"><?= @text('Map'); ?></a></span>
 	</div>
 <div id="beer_desc">
@@ -18,7 +18,7 @@
 </div>
 <div id="map_canvas" style="width: 500px; height: 300px"></div>
 
-	
-	
+
+
 
 
