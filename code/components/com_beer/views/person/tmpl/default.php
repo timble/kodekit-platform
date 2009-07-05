9d76460c25dcc9fe29cfeadea7ade287d9fa146c
Shayne Bartlett <shayne@timble.net>
2009-07-05 10:23:14 +0000

    Added links to Office, Department and vcard

--- code/components/com_beer/views/person/tmpl/default.php
+++ code/components/com_beer/views/person/tmpl/default.php
@@ -1,9 +1,13 @@
 <? /** $Id$ */ ?>
 <? @style(@$mediaurl.'/com_beer/css/default.css'); ?>
+<? @script(@$mediaurl.'/plg_koowa/js/koowa.js'); ?>
+	
 <div id="beer_header">
-	<div id="gravatar"><img src="http://www.gravatar.com/avatar.php?gravatar_id=<?= md5( strtolower(@$person->email) ); ?>&size=48" alt="Gravatar" /></div>
+	<div id="gravatar"><img src="http://www.gravatar.com/avatar.php?gravatar_id=<?= md5( strtolower(@$person->email) ); ?>&size=62" alt="Gravatar" /></div>
 	<h1 class="componentheading"><?= @$person->name?></h1>
 	<h2 id="beer_position"><?= @$person->position?></h2>
+	<h2 id="beer_position"><a href="<?= @route('index.php?option=com_beer&amp;view=department&id=' . @$person->beer_department_id) ; ?>" ><?= @$person->department; ?></a></h2>
+	<h2 id="beer_position"><a href="<?= @route('index.php?option=com_beer&amp;view=office&id=' . @$person->beer_office_id) ; ?>" ><?= @$person->office; ?></a></h2>
 	<div class="clr"></div>
 </div>
 <div id="beer_info">
@@ -11,7 +15,7 @@
 	<span class="email"><?= @$person->email?></span>
 	<span class="dob"><?= @$person->birthday?></span>
 	<span class="gender_<?= @$person->gender?>"><?= @$person->gender == "1" ? @text('Male') : @text('Female'); ?></span>
-	<span class="vcard"><?= @text('VCard'); ?></span>
+	<span class="vcard"><a href="<?=@route('option=com_beer&view=person&format=vcard&id='.@$person->id) ?>" /><?= @text('VCard'); ?></a></span>
 	</div>
 <div id="beer_desc">
 	<h2><?= @text('Bio'); ?></h2>
