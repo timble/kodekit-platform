2100bdee1815ae25b37565b9acb9aee1b46965a0
Mathias Verraes <mathias@verraes.net>
2009-07-05 12:33:34 +0000

    installer

--- code/administrator/components/com_beer/install/install.php
+++ code/administrator/components/com_beer/install/install.php
@@ -4,171 +4,44 @@
 defined('_JEXEC') or die('Restricted access');
 
 
+jimport('joomla.filesystem.folder');
+jimport('joomla.filesystem.file');
+
+$src = JPATH_ROOT.DS.'administrator'.DS.'components'.DS.'com_beer'.DS.'koowa';
+
+JFile::move($src.DS.'plugin'.DS.'koowa.xml', JPATH_ROOT);
+JFile::move($src.DS.'plugin'.DS.'koowa.php', JPATH_ROOT);
+JFolder::move($src.DS.'plugin'.DS.'koowa', JPATH_ROOT);
+JFolder::move($src.DS.'media'.DS.'plg_koowa', JPATH_ROOT);
+
 $status = new JObject();
 
-/***********************************************************************************************
- * ---------------------------------------------------------------------------------------------
- * KOOWA SYSTEM PLUGIN
- * ---------------------------------------------------------------------------------------------
- * **********************************************************************************************/
 
-// Set the installation path
-$element =& $this->manifest->getElementByPath('koowa_plugin/files');
-if (is_a($element, 'JSimpleXMLElement') && count($element->children())) {
-	$files =& $element->children();
-	foreach ($files as $file) {
-		if ($file->attributes('plugin')) {
-			$pname = $file->attributes('plugin');
-			break;
-		}
-	}
-}
-$group = 'system';
-if (!empty ($pname) && !empty($group)) {
-	$this->parent->setPath('extension_root', JPATH_ROOT.DS.'plugins'.DS.$group);
-} else {
-	$this->parent->abort(JText::_('Plugin').' '.JText::_('Install').': '.JText::_('No plugin file specified'));
-	return false;
-}
-
-/**
- * ---------------------------------------------------------------------------------------------
- * Filesystem Processing Section
- * ---------------------------------------------------------------------------------------------
- */
-
-// If the plugin directory does not exist, lets create it
-$created = false;
-if (!file_exists($this->parent->getPath('extension_root'))) {
-	if (!$created = JFolder::create($this->parent->getPath('extension_root'))) {
-		$this->parent->abort(JText::_('Plugin').' '.JText::_('Install').': '.JText::_('Failed to create directory').': "'.$this->parent->getPath('extension_root').'"');
-		return false;
-	}
-}
-
-/*
- * If we created the plugin directory and will want to remove it if we
- * have to roll back the installation, lets add it to the installation
- * step stack
- */
-if ($created) {
-	$this->parent->pushStep(array ('type' => 'folder', 'path' => $this->parent->getPath('extension_root')));
-}
-
-// Copy all necessary files
-if ($this->parent->parseFiles($element, -1) === false) {
-	// Install failed, roll back changes
-	$this->parent->abort();
-	return false;
-}
 
 /**
  * ---------------------------------------------------------------------------------------------
  * Database Processing Section
  * ---------------------------------------------------------------------------------------------
  */
-$db = &JFactory::getDBO();
+$row = JTable::getInstance('plugin');
+$row->name = 'Nooku Framework (Codename Koowa) NOT FOR PRODUCTION USE';
+$row->ordering = 1;
+$row->folder = 'system';
+$row->iscore = 0;
+$row->access = 0;
+$row->client_id = 0;
+$row->element = 'koowa';
+$row->published = 1;
+$row->params = '';
 
-// Check to see if a plugin by the same name is already installed
-$query = 'SELECT `id`' .
-		' FROM `#__plugins`' .
-		' WHERE folder = '.$db->Quote($group) .
-		' AND element = '.$db->Quote($pname);
-$db->setQuery($query);
-if (!$db->Query()) {
+if (!$row->store()) {
 	// Install failed, roll back changes
 	$this->parent->abort(JText::_('Plugin').' '.JText::_('Install').': '.$db->stderr(true));
 	return false;
 }
-$id = $db->loadResult();
-
-// Was there a plugin already installed with the same name?
-if ($id) {
-
-	if (!$this->parent->getOverwrite())
-	{
-		// Install failed, roll back changes
-		$this->parent->abort(JText::_('Plugin').' '.JText::_('Install').': '.JText::_('Plugin').' "'.$pname.'" '.JText::_('already exists!'));
-		return false;
-	}
-
-} else {
-	$row =& JTable::getInstance('plugin');
-	$row->name = 'Nooku Framework (Codename Koowa) NOT FOR PRODUCTION USE';
-	$row->ordering = 1;
-	$row->folder = $group;
-	$row->iscore = 0;
-	$row->access = 0;
-	$row->client_id = 0;
-	$row->element = $pname;
-	$row->published = 1;
-	$row->params = '';
-
-	if (!$row->store()) {
-		// Install failed, roll back changes
-		$this->parent->abort(JText::_('Plugin').' '.JText::_('Install').': '.$db->stderr(true));
-		return false;
-	}
-}
 
 $status->set('koowa_plugin', true);
 
-
-/***********************************************************************************************
- * ---------------------------------------------------------------------------------------------
- * KOOWA MEDIA PLUGIN
- * ---------------------------------------------------------------------------------------------
- * **********************************************************************************************/
-
-// Set the installation path
-$element =& $this->manifest->getElementByPath('koowa_media/files');
-if (is_a($element, 'JSimpleXMLElement') && count($element->children())) {
-	$files =& $element->children();
-	foreach ($files as $file) {
-		if ($file->attributes('plugin')) {
-			$pname = $file->attributes('plugin');
-			break;
-		}
-	}
-}
-$group = 'system';
-if (!empty ($pname) && !empty($group)) {
-	$this->parent->setPath('extension_root', JPATH_ROOT.DS.'plugins'.DS.$group);
-} else {
-	$this->parent->abort(JText::_('Plugin').' '.JText::_('Install').': '.JText::_('No plugin file specified'));
-	return false;
-}
-
-/**
- * ---------------------------------------------------------------------------------------------
- * Filesystem Processing Section
- * ---------------------------------------------------------------------------------------------
- */
-
-// If the plugin directory does not exist, lets create it
-$created = false;
-if (!file_exists($this->parent->getPath('extension_root'))) {
-	if (!$created = JFolder::create($this->parent->getPath('extension_root'))) {
-		$this->parent->abort(JText::_('Plugin').' '.JText::_('Install').': '.JText::_('Failed to create directory').': "'.$this->parent->getPath('extension_root').'"');
-		return false;
-	}
-}
-
-/*
- * If we created the plugin directory and will want to remove it if we
- * have to roll back the installation, lets add it to the installation
- * step stack
- */
-if ($created) {
-	$this->parent->pushStep(array ('type' => 'folder', 'path' => $this->parent->getPath('extension_root')));
-}
-
-// Copy all necessary files
-if ($this->parent->parseFiles($element, -1) === false) {
-	// Install failed, roll back changes
-	$this->parent->abort();
-	return false;
-}
 
 
 /***********************************************************************************************
