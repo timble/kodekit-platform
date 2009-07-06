40859e9a6951e2bca60d198816f7c699c930c6d9
Mathias Verraes <mathias@verraes.net>
2009-07-06 00:49:57 +0000

    fixes

--- code/components/com_beer/views/office/tmpl/default.php
+++ code/components/com_beer/views/office/tmpl/default.php
@@ -5,11 +5,14 @@
 
 <h1 class="componentheading"><?= @$office->title; ?></h1>
 <div id="beer_info">
-	<img src="<?= @$mediaurl;?>/com_beer/images/flags/<?= strtolower(@$office->country);?>.png" alt="<?= @$office->country;?> flag"/><?= @$office->address1 . ' ' . @$office->address2 . ' ' . @$office->city . ' ' . @$office->state . ' ' . @$office->postcode; ?>
-	<span class="phone"><?= @$office->phone; ?></spam>
+	<img src="<?= @$mediaurl;?>/com_beer/images/flags/<?= strtolower(@$office->country);?>.png" alt="<?= @$office->country;?> flag"/>
+	<?= @$office->address?>
+	<span class="phone"><?= @$office->phone; ?></span>
 	<span class="fax"><?= @$office->fax; ?></span>
 	<span class="people"><a href="<?=@route('option=com_beer&view=people&beer_office_id='.@$office->id) ?>"><?= @$office->people; ?> <?=@text('Employee(s)')?></a></span>
+	<!-- @todo
 	<span class="map"><a href="#" onclick="showAddress(<?= @$office->address1 . ' ' . @$office->address2 . ', ' . @$office->city . ', ' . @$office->state . ', ' . @$office->postcode . ', ' . @$office->country; ?>); return false"><?= @text('Map'); ?></a></span>
+	 -->
 </div>
 <div id="beer_desc">
 	<h2><?= @text('Information'); ?></h2>
