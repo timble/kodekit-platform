b1fc7c26f976d8ee5a6aae74cc999c2ee1038fff
Mathias Verraes <mathias@verraes.net>
2009-07-30 08:04:16 +0000

    Fixed KFilterFloat stripping decimal point

--- code/plugins/system/koowa/filter/float.php
+++ code/plugins/system/koowa/filter/float.php
@@ -27,7 +27,7 @@ class KFilterFloat extends KFilterAbstract
 	{
 		return (false !== filter_var($value, FILTER_VALIDATE_FLOAT));
 	}
-	
+
 	/**
 	 * Sanitize a value
 	 *
@@ -36,8 +36,8 @@ class KFilterFloat extends KFilterAbstract
 	 */
 	protected function _sanitize($value)
 	{
-		return (float) filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT, 
-			FILTER_FLAG_ALLOW_FRACTION & FILTER_FLAG_ALLOW_THOUSAND & FILTER_FLAG_ALLOW_SCIENTIFIC);
+		return (float) filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT,
+			FILTER_FLAG_ALLOW_FRACTION | FILTER_FLAG_ALLOW_THOUSAND | FILTER_FLAG_ALLOW_SCIENTIFIC);
 	}
 }
 
