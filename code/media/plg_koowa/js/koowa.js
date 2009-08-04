01e96424bf5fa7da6a5385dd85811c2177a06f71
Mathias Verraes <mathias@verraes.net>
2009-08-04 18:07:58 +0000

    Pagination

--- code/media/plg_koowa/js/koowa.js
+++ code/media/plg_koowa/js/koowa.js
@@ -4,7 +4,7 @@
  * @package     Koowa_Media
  * @subpackage  Javascript
  * @copyright	Copyright (C) 2007 - 2009 Johan Janssens and Mathias Verraes. All rights reserved.
- * @license		GNU GPL <http://www.gnu.org/licenses/gpl.html>
+ * @license		GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * @link     	http://www.koowa.org
  */
 
@@ -14,26 +14,10 @@
 var Koowa = {};
 
 /**
- * Table class
- */
-Koowa.Table = 
-{
-	sorting: function(order, dir) 
-	{
-		var form = document.adminForm;
-		form.filter_order.value 	= order;
-		form.filter_direction.value	= dir;
-		Koowa.Form.submit( 'browse' );
-	}
-}
-
-
-/**
  * Form class
  */
 Koowa.Form = 
-{
-	
+{	
 	addField: function(name, value)
 	{
 		var el = document.createElement('input');
@@ -151,24 +135,66 @@ String.extend(
 		
 		return defaultValue;
 	},
-	
-	parseQueryString: function() 
-	{
-		var vars = this.split(/[&;]/);
-		var rs = {};
-		if (vars.length) vars.each(function(val) {
-			var keys = val.split('=');
-			if (keys.length && keys.length == 2) rs[keys[0]] = encodeURIComponent(keys[1]);
-		});
-		
-		return rs;
-	},
- 
+
 	parseUri: function()
 	{
 		var bits = this.match(/^(?:([^:\/?#.]+):)?(?:\/\/)?(([^:\/?#]*)(?::(\d*))?)((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[\?#]|$)))*\/?)?([^?#\/]*))?(?:\?([^#]*))?(?:#(.*))?/);
 		return (bits)
 			? bits.associate(['uri', 'scheme', 'authority', 'domain', 'port', 'path', 'directory', 'file', 'query', 'fragment'])
 			: null;
+	},
+	
+	// backported from Mootools 1.2.3
+	parseQueryString: function(){
+		//var vars = this.split(/[&;]/), res = {};
+		var vars = this.split(/[&;]/), res = new Koowa.Query;
+		if (vars.length) vars.each(function(val){
+			var index = val.indexOf('='),
+				keys = index < 0 ? [''] : val.substr(0, index).match(/[^\]\[]+/g),
+				value = decodeURIComponent(val.substr(index + 1)),
+				obj = res;
+			keys.each(function(key, i){
+				var current = obj[key];
+				if(i < keys.length - 1)
+					obj = obj[key] = current || {};
+				else if($type(current) == 'array')
+					current.push(value);
+				else
+					obj[key] = $defined(current) ? [current, value] : value;
+			});
+		});
+		return res;
+	},
+
+	// backported from Mootools 1.2.3
+	cleanQueryString: function(method){
+		return this.split('&').filter(function(val){
+			var index = val.indexOf('='),
+			key = index < 0 ? '' : val.substr(0, index),
+			value = val.substr(index + 1);
+			return method ? method.run([key, value]) : $chk(value);
+		}).join('&');
+	}
+	
+});
+
+
+Koowa.Query = new Class({
+	toString: function() {
+		var result = [];
+		for (var key in this) {
+			// Make sure it's not a function
+			if (!(this[key] instanceof Function)) {
+				// we only go one level deep for now
+				if(this[key] instanceof Object) {
+					for (var subkey in this[key]) {
+						result.push(key + '[' + subkey + ']' + '=' + this[key][subkey]);
+					}
+				} else {
+					result.push(key + '=' + this[key]);
+				}
+			}
+		}
+		return result.join('&');
 	}
 });
