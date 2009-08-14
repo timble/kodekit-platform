19d4da2e3332cece2bb7d90b225102d1e32a2810
Mathias Verraes <mathias@verraes.net>
2009-08-14 22:02:16 +0000

    Removed joomla install

--- code/modules/mod_syndicate/tmpl/default.php
+++
@@ -1,4 +0,0 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
-<a href="<?php echo $link ?>">
-	<?php echo JHTML::_('image.site', 'livemarks.png', '/images/M_images/', NULL, NULL, 'feed-image'); ?> <span><?php echo $params->get('text') ?></span></a>
