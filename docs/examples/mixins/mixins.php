fab3999c50158c0f1af0b1b01e52d82557f14f46
Johan Janssens <johan@timble.net>
2009-09-03 15:51:32 +0000

    Docblock updates, fixed copyright information and email addresses

--- docs/examples/mixins/mixins.php
+++ docs/examples/mixins/mixins.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @version     $Id$
- * @package     Koowa
+ * @category	Koowa
  * @copyright   Copyright (C) 2007 - 2009 Johan Janssens and Mathias Verraes. All rights reserved.
  * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
  * @link        http://www.koowa.org
