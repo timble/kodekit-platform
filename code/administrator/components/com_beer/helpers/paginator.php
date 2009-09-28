7b08d639c39dd9d521602653a1c56ca115df677e
Johan Janssens <johan@timble.net>
2009-09-28 22:10:01 +0000

    Set svn property

--- code/administrator/components/com_beer/helpers/paginator.php
+++ code/administrator/components/com_beer/helpers/paginator.php
@@ -2,7 +2,7 @@
 /**
  * Business Enterprise Employee Repository (B.E.E.R)
  * 
- * @version		$Id: chart.php 228 2009-09-23 13:59:34Z tom $
+ * @version		$Id$
  * @package		Beer
  * @copyright	Copyright (C) 2009 Nooku. All rights reserved.
  * @license 	GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
