0ecd348280d55dcf9742c55a31d0fdaa26997b34
David Deutsch <skoremail@googlemail.com>
2009-10-20 18:10:25 +0000

    fixing duplicated ids in hcard rendering

--- code/components/com_profiles/views/person/tmpl/hcard.php
+++ code/components/com_profiles/views/person/tmpl/hcard.php
@@ -2,8 +2,8 @@
 <? defined('KOOWA') or die('Restricted access'); ?>
 
 <div class="vcard">
-	<div id="profiles_header">
-		<div id="gravatar">
+	<div class="profiles-header">
+		<div class="profiles-gravatar">
 			<span class="photo">
 				<img src="http://www.gravatar.com/avatar.php?gravatar_id=<?= md5( strtolower(@$person->email) ); ?>&size=62" alt="Gravatar" />
 			</span>
@@ -11,20 +11,20 @@
 		<h1 class="componentheading">
 			<span class="fn"><?= @$person->name?></span>
 		</h1>
-		<h2 id="profiles_position">
+		<h2 class="profiles-position">
 			<span class="title"><?= @$person->position?></span>
 		</h2>
-		<h2 id="profiles_position">
+		<h2 class="profiles-position">
 			<span class="org">
 				<a href="<?= @route('view=department&id=' . @$person->department_slug) ; ?>" ><?= @$person->department; ?></a>
 			</span>
 		</h2>
-		<h2 id="profiles_position">
+		<h2 class="profiles-position">
 			<a href="<?= @route('view=office&id=' . @$person->office_slug) ; ?>" ><?= @$person->office; ?></a>
 		</h2>
 		<div class="clr"></div>
 	</div>
-	<div id="profiles_info">
+	<div class="profiles-info">
 		<span class="tel">
 			<span class="type"><?= @text('mobile');?>: </span>
 			<span class="value"><?= @$person->mobile?></span>
@@ -36,7 +36,7 @@
 			<a href="<?=@route('view=person&format=vcard&id='.@$person->slug) ?>" /><?= @text('VCard'); ?></a>
 		</span>
 	</div>
-	<div id="profiles_desc">
+	<div class="profiles-desc">
 		<h2><?= @text('Bio'); ?></h2>
 		<span class="note"><?= @$person->bio; ?></span>
 	</div>
