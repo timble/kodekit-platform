f84e0fd7faa619b206bd44bdcb9ebc87dde4c6b6
Johan Janssens <johan@timble.net>
2009-11-30 02:14:14 +0000

    Syncing with 100-view branch

---
+++ code/modules/mod_profiles/tmpl/html.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Profiles
+ * @copyright	Copyright (C) 2009 Nooku. All rights reserved.
+ * @license 	GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link     	http://www.nooku.org
+ */
+
+class modProfilesHtml extends modDefaultHtml
+{
+	
+}
