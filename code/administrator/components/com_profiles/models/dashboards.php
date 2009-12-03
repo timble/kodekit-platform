b0581a23e8e68c6bfd319f6bb1d1311b6aa85732
Johan Janssens <johan@timble.net>
2009-12-03 23:58:10 +0000

    Docblocks

--- code/administrator/components/com_profiles/models/dashboards.php
+++ code/administrator/components/com_profiles/models/dashboards.php
@@ -1,8 +1,12 @@
 <?php
-
 /**
- * @todo Dummy model until I figure out a better way to do it
+ * @version		$Id$
+ * @package		Profiles
+ * @copyright	Copyright (C) 2009 Nooku. All rights reserved.
+ * @license 	GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link     	http://www.nooku.org
  */
+
 class ProfilesModelDashboards extends KModelAbstract
 {
 	
