061429dc232d5a208a5e3ade7474bc84c519e164
Johan Janssens <johan@timble.net>
2010-01-22 17:25:52 +0000

    Codeblocks

--- code/administrator/modules/mod_default/html.php
+++ code/administrator/modules/mod_default/html.php
@@ -2,7 +2,7 @@
 /**
 * @version		$Id$
 * @category		Koowa
-* @package      Koowa
+* @package      Koowa_Modules
 * @copyright    Copyright (C) 2007 - 2009 Johan Janssens and Mathias Verraes. All rights reserved.
 * @license      GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
 * @link         http://www.koowa.org
