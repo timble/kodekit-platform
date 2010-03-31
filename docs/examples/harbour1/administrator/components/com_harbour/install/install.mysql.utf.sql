5d31bb3a3a84c2f13bad35ba9a2364a6b5a6ca91
Johan Janssens <johan@timble.net>
2010-03-31 00:11:31 +0000

    Removed harbour1, and 2 and renamed harbour3 to harbour.

--- docs/examples/harbour1/administrator/components/com_harbour/install/install.mysql.utf.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `#__harbour_boats` (
-  `harbour_boat_id` SERIAL,
-  `name` varchar(255) NOT NULL
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
-
-INSERT INTO `#__harbour_boats` (`name`) VALUES
-('Herald of Free Enterprise'),
-('Titanic'),
-('Flying Dutchman');
--- docs/examples/harbour2/administrator/components/com_harbour/views/boat/tmpl/default.php
+++
@@ -1,6 +0,0 @@
-<? defined('KOOWA') or die('Restricted access'); ?>
-
-<h1><?=@$boat->name?></h1>
-<p>
-	<?=@text('Boat id')?>: <?=@$boat->id?>
-</p>
