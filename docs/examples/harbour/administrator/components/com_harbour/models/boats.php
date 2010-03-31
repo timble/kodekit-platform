2301f01945969ba6e3b2f91439d03d638d72b5e9
Johan Janssens <johan@timble.net>
2010-03-31 19:57:00 +0000

    Cleanup

--- docs/examples/harbour/administrator/components/com_harbour/models/boats.php
+++ docs/examples/harbour/administrator/components/com_harbour/models/boats.php
@@ -10,7 +10,7 @@ class ComHarbourModelBoats extends KModelTable
 
 	protected function _buildQueryWhere(KDatabaseQuery $query)
     {
-       	$search     = $this->_state->search;
+       	$search = $this->_state->search;
 
        	if ($search) {
          	$query->where('tbl.name', 'LIKE', '%'.$search.'%');
