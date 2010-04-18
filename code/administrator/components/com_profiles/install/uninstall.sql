c11f4af9c171d5682b23d24fb06d92a44df742ab
Torkil Johnsen <torkil@torkiljohnsen.com>
2010-04-18 12:52:33 +0000

    Bug in uninstall. Readability.

--- code/administrator/components/com_profiles/install/uninstall.sql
+++ code/administrator/components/com_profiles/install/uninstall.sql
@@ -1,4 +1,12 @@
 -- $Id$
 
-DROP TABLE IF EXISTS `#__profiles_departments`, `#__profiles_offices`, `#__profiles_people`;
-DROP VIEW IF EXISTS `#__profiles_view_people`, `#__profiles_view_departments`, `#__profiles_view_offices`, #__profiles_users`;
+DROP TABLE IF EXISTS
+    `#__profiles_departments`,
+    `#__profiles_offices`,
+    `#__profiles_people`;
+
+DROP VIEW IF EXISTS 
+    `#__profiles_view_people`,
+    `#__profiles_view_departments`,
+    `#__profiles_view_offices`,
+    `#__profiles_users`;
--- code/administrator/components/com_terms/install/uninstall.sql
+++ code/administrator/components/com_terms/install/uninstall.sql
@@ -1,3 +1,5 @@
 -- $Id$
 
-DROP TABLE IF EXISTS `#__terms_terms`, `#__terms_relations;
+DROP TABLE IF EXISTS 
+    `#__terms_terms`,
+    `#__terms_relations;
