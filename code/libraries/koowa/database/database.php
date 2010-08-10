313e0273a6550da585a6fe3386925b0085c642dc
Johan Janssens <johan@timble.net>
2010-08-10 22:24:41 +0000

    Added new FETCH_ARRAY, FETCH_ARRAY_LIST, FETCH_FIELD, FETCH_FIELD_LIST, FETCH_OBJECT, FETCH_OBJECT_LIST fetch mode constants.

--- code/libraries/koowa/database/database.php
+++ code/libraries/koowa/database/database.php
@@ -34,9 +34,15 @@ class KDatabase
 	/**
 	 * Database fetch mode
 	 */
-	const FETCH_ROWSET  = 0;
-	const FETCH_ROW     = 1;
-	const FETCH_FIELD   = 2;
+	const FETCH_ROW         = 0;
+	const FETCH_ROWSET      = 1;
+
+	const FETCH_ARRAY       = 0;
+	const FETCH_ARRAY_LIST  = 1;
+	const FETCH_FIELD       = 2;
+	const FETCH_FIELD_LIST  = 3;
+	const FETCH_OBJECT      = 4;
+	const FETCH_OBJECT_LIST = 5;
 	
 	/**
 	 * Row states
