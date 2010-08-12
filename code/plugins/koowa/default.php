aec75c5365771ee055d94bb0d4a59f4ce2230853
Johan Janssens <johan@timble.net>
2010-08-12 20:25:58 +0000

    Added Nooku Framework svn:externals and plugin files. Linked to http://svn2.assembla.com/svn/nooku-framework/trunk/code

---
+++ code/plugins/koowa/default.php
@@ -0,0 +1,125 @@
+<?php
+/**
+* @version		$Id: default.php 1929 2010-04-26 16:08:30Z johanjanssens $
+* @category		Koowa
+* @package      Koowa_Plugins
+* @copyright    Copyright (C) 2007 - 2010 Johan Janssens and Mathias Verraes. All rights reserved.
+* @license      GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+* @link         http://www.koowa.org
+*/
+
+/**
+ * Default Koowa plugin
+ * 
+ * Koowa plugins can handle a number of events that are dynamically generated. The following 
+ * is a list of available events. This list is not meant to be exclusive.
+ * 
+ * onApplicationBefore[Action]
+ * onApplicationAfte[Action]  
+ * where [Action] is Initialise, Route, Dispatch, Render, Login, Logout, Redirect or Close
+ * 
+ * onControllerBefore[Action]
+ * onControllerAfter[Action]
+ * where [Action] is Browse, Read, Edit, Add, Delete or any custom controller action
+ * 
+ * onDatabaseBefore[Action]
+ * onDatabaseAfter[Action]
+ * where [Action] is Select, Insert, Update or Delete
+ * 
+ * You can create your own Koowa plugins very easily :
+ * 
+ * <code>
+ * <?php
+ *  class plgKoowaFoo extends plgKoowaDefault
+ * {
+ * 		public function onApplicationBeforeRoute(KCommandcontext $context)
+ * 		{
+ * 			//The caller is a reference to the object that is triggering this event
+ * 			$caller = $context->caller;
+ * 
+ * 			//The result is the actual result of the event, if this is an after event 
+ * 			//the result will contain the result of the action.
+ * 			$result = $context->result;
+ * 
+ * 			//The context object can also contain a number of custom properties
+ *          print_r($context);
+ * 		}	
+ * }	
+}
+ * </code>
+ *
+ * @author		Johan Janssens <johan@koowa.org>
+ * @category   	Koowa
+ * @package     Koowa_Plugins
+ * @subpackage  Koowa
+ */
+abstract class PlgKoowaDefault extends KEventHandler
+{	
+	/**
+	 * A JParameter object holding the parameters for the plugin
+	 *
+	 * @var	A JParameter object
+	 */
+	public $params	= null;
+
+	/**
+	 * The name of the plugin
+	 *
+	 * @var		string
+	 */
+	protected $_name = null;
+
+	/**
+	 * The plugin type
+	 *
+	 * @var		string
+	 */
+	protected $_type = null;
+	
+	/**
+	 * Constructor
+	 */
+	function __construct($dispatcher, $config = array())
+	{
+		if ( isset( $config['params'] ) ) 
+		{
+			if(is_a($config['params'], 'JParameter')) {
+				$this->params = $config['params'];
+			} else {
+				$this->params = new JParameter($config['params']);
+			}
+		}
+
+		if ( isset( $config['name'] ) ) {
+			$this->_name = $config['name'];
+		}
+
+		if ( isset( $config['type'] ) ) {
+			$this->_type = $config['type'];
+		}
+		
+		//Register the plugin with the dispatcher
+		$dispatcher->register($this);
+		
+		//Force the identifier to NULL for now
+		$config['identifier'] = null;
+
+		parent::__construct(new KConfig($config));
+	}
+	
+	/**
+	 * Loads the plugin language file
+	 *
+	 * @param	string 	$extension 	The extension for which a language file should be loaded
+	 * @param	string 	$basePath  	The basepath to use
+	 * @return	boolean	True, if the file has successfully loaded.
+	 */
+	public function loadLanguage($extension = '', $basePath = JPATH_BASE)
+	{
+		if(empty($extension)) {
+			$extension = 'plg_'.$this->_type.'_'.$this->_name;
+		}
+
+		return KFactory::get('lib.joomla.language')->load( strtolower($extension), $basePath);
+	}
+}
---
+++ code/plugins/system/koowa.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * @version     $Id: koowa.php 2050 2010-05-15 20:30:30Z johanjanssens $
+ * @category	Koowa
+ * @package     Koowa_Plugins
+ * @subpackage  System
+ * @copyright   Copyright (C) 2007 - 2010 Johan Janssens and Mathias Verraes. All rights reserved.
+ * @license     GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+ * @link        http://www.koowa.org
+ */
+
+/**
+ * Koowa System plugin
+ *
+ * @author		Mathias Verraes <mathias@koowa.org>
+ * @category	Koowa
+ * @package		Koowa
+ */
+class plgSystemKoowa extends JPlugin
+{
+	public function __construct($subject, $config = array())
+	{
+		// Check if Koowa is active
+		if(JFactory::getApplication()->getCfg('dbtype') != 'mysqli')
+		{
+    		JError::raiseWarning(0, JText::_("Koowa plugin requires MySQLi Database Driver. Please change your database configuration settings to 'mysqli'"));
+    		return;
+		}
+		
+		//Set constants
+		define('KDEBUG', JDEBUG);
+		
+		//Set exception handler
+		set_exception_handler(array($this, 'exceptionHandler'));
+		
+		// Require the library loader
+		JLoader::import('libraries.koowa.koowa', JPATH_ROOT);
+		JLoader::import('libraries.koowa.loader.loader', JPATH_ROOT);
+		
+		//Instantiate the singletons
+		KLoader::instantiate();
+		KFactory::instantiate();
+		KRequest::instantiate();
+	
+		//Add loader adapters
+		KLoader::addAdapter(new KLoaderAdapterJoomla());
+		KLoader::addAdapter(new KLoaderAdapterModule());
+		KLoader::addAdapter(new KLoaderAdapterPlugin());
+        KLoader::addAdapter(new KLoaderAdapterComponent());
+            
+		//Add factory adapters
+		KFactory::addAdapter(new KFactoryAdapterJoomla());
+		KFactory::addAdapter(new KFactoryAdapterModule());
+		KFactory::addAdapter(new KFactoryAdapterPlugin());
+		KFactory::addAdapter(new KFactoryAdapterComponent());
+		
+		// Decorate the application object
+		$app  =& JFactory::getApplication();
+		$app  = new KDecoratorJoomlaApplication($app);
+		
+		//Create the koowa database object
+		$db  = KFactory::get('lib.koowa.database.adapter.mysqli')
+			->setConnection(JFactory::getDBO()->_resource)
+			->setTablePrefix(JFactory::getDBO()->_table_prefix);
+		
+        //Set factory identifier aliasses
+        KFactory::map('lib.koowa.database'   , $db);
+        KFactory::map('lib.koowa.application', 'lib.joomla.application');
+        KFactory::map('lib.koowa.language'   , 'lib.joomla.language');
+        KFactory::map('lib.koowa.user'       , 'lib.joomla.user');
+        
+        //Send a header that tells Nooku Desktop that this is Nooku Server
+        JResponse::setHeader('x-nooku-desktop', 'version=1.0;');
+        
+        //If the format is AJAX we create a 'raw' document rendered and force it's type to the active format 
+        //if the format is 'html' or if the tmpl is empty.
+        if(KRequest::type() == 'AJAX') 
+        {
+        	if(KRequest::get('get.format', 'cmd', 'html') != 'html' || KRequest::get('get.tmpl', 'cmd') === '')
+        	{
+        		$format = JRequest::getWord('format', 'html');
+        	
+        		JRequest::setVar('format', 'raw');   //force format to raw
+        		JFactory::getDocument()->setType($format);
+        		JRequest::setVar('format', $format); //revert format to original
+        	}
+        }
+        
+		//Load the koowa plugins
+		JPluginHelper::importPlugin('koowa', null, true, KFactory::get('lib.koowa.event.dispatcher'));
+
+		parent::__construct($subject, $config = array());
+	}
+	
+	/**
+	 * Prettify the output using Tidy filter (if available) and debug has been
+	 * enabled
+	 *
+	 * @return void
+	 */
+	public function onAfterRender()
+	{
+		/*if(KDEBUG)
+		{
+			$config =  array(
+					'indent'            => true,
+                	'indent-attributes' => true,
+                	'wrap'              => 120,
+			);
+	
+			$filter = new KFilterTidy(array('config' => $config));
+			$result = $filter->sanitize(JResponse::getBody());
+		
+			JResponse::setBody($result);
+		}*/
+	}
+	
+ 	/**
+	 * Catch all exception handler
+	 *
+	 * Calls the Joomla error handler to process the exception
+	 *
+	 * @param object an Exception object
+	 * @return void
+	 */
+	public function exceptionHandler($exception)
+	{
+		$this->_exception = $exception; //store the exception for later use
+		
+		//Change the Joomla error handler to our own local handler and call it
+		JError::setErrorHandling( E_ERROR, 'callback', array($this,'errorHandler'));
+		JError::raiseError('500', $exception->getMessage());
+	}
+
+	/**
+	 * Custom JError callback
+	 *
+	 * Push the exception call stack in the JException returned through the call back
+	 * adn then rener the custom error page
+	 *
+	 * @param object A JException object
+	 * @return void
+	 */
+	public function errorHandler($error)
+	{
+		$error->backtrace = $this->_exception->getTrace();
+		$error->file      = $this->_exception->getFile();
+		$error->line      = $this->_exception->getLine();
+		
+		if(KFactory::get('lib.joomla.config')->getValue('config.debug')) {
+			$error->message   = $this->_exception;
+		} else {
+			$error->message   = '';
+		}
+		
+		JError::customErrorPage($error);
+	}
+}
+
+/**
+ * PHP5.3 compatibility
+ */
+if(false === function_exists('lcfirst'))
+{
+    /**
+     * Make a string's first character lowercase
+     *
+     * @param string $str
+     * @return string the resulting string.
+     */
+    function lcfirst( $str ) {
+        $str[0] = strtolower($str[0]);
+        return (string)$str;
+    }
+}
