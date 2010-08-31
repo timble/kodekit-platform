f8daacedb7386836ac0d069415706c9ee205512c
Johan Janssens <johan@timble.net>
2010-08-31 13:13:01 +0000

    Fixed typo

--- code/libraries/koowa/filter/numeric.php
+++ code/libraries/koowa/filter/numeric.php
@@ -40,7 +40,7 @@ class KFilterNumeric extends KFilterAbstract
 	protected function _sanitize($value)
 	{
 		return (string) filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT, 
-			FILTER_FLAG_ALLOW_FRACTION & FILTER_FLAG_ALLOW_THOUSAND & FILTER_FLAG_ALLOW_SCIENTIFIC);
+			FILTER_FLAG_ALLOW_FRACTION | FILTER_FLAG_ALLOW_THOUSAND | FILTER_FLAG_ALLOW_SCIENTIFIC);
 	}
 }
 
