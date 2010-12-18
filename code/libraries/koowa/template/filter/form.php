230a9e3df1874d03b7da598e8c27e12034c1e922
Johan Janssens <johan@timble.net>
2010-12-18 20:22:33 +0000

    KTemplateFilterForm is now a write filter instead of a read filter. If the form method is GET, the filter will parse the form action query information and for each query param it will add a hidden form field. You no longer need to add the hidden form field manually. See also : http://www.w3.org/TR/html401/interact/forms.html#h-17.13.3.4

--- code/libraries/koowa/template/filter/form.php
+++ code/libraries/koowa/template/filter/form.php
@@ -10,14 +10,19 @@
 */
 
 /**
- * Template read filter to handle form html elements
+ * Template write filter to handle form html elements
+ * 
+ * For forms that use a post method this filter adds a token to prevent CSRF. For forms
+ * that use a get method this filter adds the action url query params as hidden fields
+ * to comply with the html form standard.
  *
  * @author		Johan Janssens <johan@nooku.org>
  * @category	Koowa
  * @package     Koowa_Template
  * @subpackage	Filter
+ * @see         http://www.w3.org/TR/html401/interact/forms.html#h-17.13.3.4
  */
-class KTemplateFilterForm extends KTemplateFilterAbstract implements KTemplateFilterRead
+class KTemplateFilterForm extends KTemplateFilterAbstract implements KTemplateFilterWrite
 {
 	/**
 	 * Add unique token field
@@ -25,22 +30,29 @@ class KTemplateFilterForm extends KTemplateFilterAbstract implements KTemplateFi
 	 * @param string
 	 * @return KTemplateFilterForm
 	 */
-	public function read(&$text)
-	{
-		// match all forms where method="post"
-		$form 		= '<\s*form\s*';
-		$anything 	= '.*';
-		$quote		= '["\']';
-		$method 	= '\s*method\s*=\s*'.$quote.'post'.$quote;
-		$close		= '>';
-		$pattern = '/('.$form.$anything.$method.$anything.$close.')/i';
-
-		// add hidden token field to each match
-		$replace = '\1'
-			.PHP_EOL
-			.'<input type="hidden" name="_token" value="'.JUtility::getToken().'" />';
-		$text = preg_replace($pattern, $replace, $text);
+	public function write(&$text)
+	{	
+		// POST : Add token 
+		$text    = preg_replace('/(<form.*method="post".*>)/i', 
+			'\1'.PHP_EOL.'<input type="hidden" name="_token" value="'.JUtility::getToken().'" />', 
+			$text
+		);
 		
-		return $this;
+		// GET : Add query params
+		$matches = array();
+		if(preg_match_all('#<form.*action=".*\?(.*)".*method="get".*>#iU', $text, $matches))
+		{
+			foreach($matches[1] as $key => $query)
+			{
+				parse_str(str_replace('&amp;', '&', $query), $query);
+				
+				$input = '';
+				foreach($query as $name => $value) {
+					$input .= PHP_EOL.'<input type="hidden" name="'.$name.'" value="'.$value.'" />';
+				}
+				
+				$text = str_replace($matches[0][$key], $matches[0][$key].$input, $text);
+			}	
+		}
 	}
 }
