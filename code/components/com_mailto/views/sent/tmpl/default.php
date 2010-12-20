efdaf5ce3343763500c6b816f9e4ebe66a7529cd
Johan Janssens <johan@timble.net>
2010-12-20 23:46:47 +0000

    re #36 : Moved com_mailto assets to media folder.

--- code/components/com_mailto/views/sent/tmpl/default.php
+++ code/components/com_mailto/views/sent/tmpl/default.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die('Restricted access'); ?>
 <div style="padding: 10px;">
 	<div style="text-align:right">
 		<a href="javascript: void window.close()">
-			<?php echo JText::_('Close Window'); ?> <img src="<?php echo JURI::base() ?>components/com_mailto/assets/close-x.png" border="0" alt="" title="" /></a>
+			<?php echo JText::_('Close Window'); ?> <img src="<?php echo JURI::root(true) ?>/media/com_mailto/images/close-x.png" border="0" alt="" title="" /></a>
 	</div>
 
 	<div class="componentheading">
