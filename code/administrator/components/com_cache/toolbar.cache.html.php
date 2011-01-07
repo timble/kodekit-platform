68444ae5b13a1d0be9fdf3fe8eabf87d0bc4ef72
Johan Janssens <johan@timble.net>
2011-01-07 00:13:56 +0000

    Removed unneeded information from the toolbar title.

--- code/administrator/components/com_cache/toolbar.cache.html.php
+++ code/administrator/components/com_cache/toolbar.cache.html.php
@@ -26,13 +26,13 @@ class TOOLBAR_cache
 	*/
 	function _DEFAULT() {
 
-		JToolBarHelper::title( JText::_( 'Cache Manager - Clean Cache Admin' ), 'checkin.png' );
+		JToolBarHelper::title( JText::_( 'Cache Manager' ), 'checkin.png' );
 		JToolBarHelper::custom( 'delete', 'delete.png', 'delete_f2.png', 'Delete', true );
 	}
 
 	function _PURGEADMIN() {
 
-		JToolBarHelper::title( JText::_( 'Cache Manager - Purge Cache Admin' ), 'checkin.png' );
+		JToolBarHelper::title( JText::_( 'Cache Manager' ), 'checkin.png' );
 		JToolBarHelper::custom( 'purge', 'delete.png', 'delete_f2.png', 'Purge expired', false );
 	}
 }
