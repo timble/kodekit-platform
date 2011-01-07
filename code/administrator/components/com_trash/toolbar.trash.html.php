e74c73136de53745bcc5aa5f83e7a967747dc2b5
Johan Janssens <johan@timble.net>
2011-01-07 00:09:57 +0000

    Removed unneeded information from the toolbar title.

--- code/administrator/components/com_trash/toolbar.trash.html.php
+++ code/administrator/components/com_trash/toolbar.trash.html.php
@@ -19,28 +19,26 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 * @package		Joomla
 * @subpackage	Trash
 */
-class TOOLBAR_Trash {
-	function _DEFAULT() {
+class TOOLBAR_Trash 
+{
+	function _DEFAULT() 
+	{
 		$task	= JRequest::getCmd('task', 'viewMenu');
 		$return	= JRequest::getCmd('return', 'viewContent', 'post');
 
-		if ( $task == 'viewMenu' || $return == 'viewMenu') {
-			$text = ': <small><small>['. JText::_( 'Menu Items' ) .']</small></small>';
-		} else {
-			$text = ': <small><small>['. JText::_( 'Articles' ) .']</small></small>';
-		}
-
-		JToolBarHelper::title( JText::_( 'Trash Manager' ) . $text, 'trash.png' );
+		JToolBarHelper::title( JText::_( 'Trash Manager' ), 'trash.png' );
 		JToolBarHelper::custom('restoreconfirm','restore.png','restore_f2.png', 'Restore', true);
 		JToolBarHelper::custom('deleteconfirm','delete.png','delete_f2.png', 'Delete', true);
 	}
 
-	function _RESTORE() {
+	function _RESTORE() 
+	{
 		JToolBarHelper::title( JText::_( 'Restore Items' ), 'restoredb.png' );
 		JToolBarHelper::cancel();
 	}
 
-	function _DELETE() {
+	function _DELETE() 
+	{
 		JToolBarHelper::title( JText::_( 'Delete Items' ), 'delete_f2.png' );
 		JToolBarHelper::cancel();
 	}
