8d74fd6bd97c33825ab44cb7e1fca311b6dddf13
Stian Didriksen <stian@timble.net>
2011-02-01 13:51:59 +0000

    re #62 functional, just missing states for css purposes. The "None" editor can be deleted.

--- code/plugins/editors/tinymce.php
+++ code/plugins/editors/tinymce.php
@@ -485,11 +485,30 @@ class plgEditorTinymce extends JPlugin
 		$doc =& JFactory::getDocument();
 
 		$js= "
+			function insertAtCursor(myField, myValue) {
+				if (document.selection) {
+					// IE support
+					myField.focus();
+					sel = document.selection.createRange();
+					sel.text = myValue;
+				} else if (myField.selectionStart || myField.selectionStart == '0') {
+					// MOZILLA/NETSCAPE support
+					var startPos = myField.selectionStart;
+					var endPos = myField.selectionEnd;
+					myField.value = myField.value.substring(0, startPos)
+						+ myValue
+						+ myField.value.substring(endPos, myField.value.length);
+				} else {
+					myField.value += myValue;
+				}
+			}
+		
 			function isBrowserIE() {
 				return navigator.appName==\"Microsoft Internet Explorer\";
 			}
 
 			function jInsertEditorText( text, editor ) {
+				insertAtCursor( document.getElementById(editor), text );
 				if (isBrowserIE()) {
 					if (window.parent.tinyMCE) {
 						window.parent.tinyMCE.selectedInstance.selection.moveToBookmark(window.parent.global_ie_bookmark);
