eb1e27aa1a145632e375bd2b4093f1b4c24861d8
Johan Janssens <johan@timble.net>
2011-03-01 23:43:32 +0000

    Added new KTemplateHelperDate::humanize() function. This function allows to to generated humanized dates.

--- code/libraries/koowa/template/helper/date.php
+++ code/libraries/koowa/template/helper/date.php
@@ -21,12 +21,11 @@
 class KTemplateHelperDate extends KTemplateHelperAbstract
 {
 	/**
-	 * Returns formated date according to current local and adds time offset
+	 * Returns formatted date according to current local and adds time offset.
 	 *
-	 * @param	string	A date in ISO 8601 format or a unix time stamp
-	 * @param	string	format optional format for strftime
-	 * @returns	string	formated date
-	 * @see		strftime
+	 * @param  array   An optional array with configuration options.
+	 * @return string  Formatted date.
+	 * @see    strftime
 	 */
 	public function format($config = array())
 	{
@@ -36,11 +35,82 @@ class KTemplateHelperDate extends KTemplateHelperAbstract
 			'format'	 => '%A, %d %B %Y',
 			'gmt_offset' => 0,
  		));
- 		
+
  		if(!is_numeric($config->date)) {
  			$config->date =  strtotime($config->date);
  		}
- 		
- 		return strftime($config->format, $config->date + 3600 * $config->gmt_offset); 
+
+ 		return strftime($config->format, $config->date + 3600 * $config->gmt_offset);
 	}
+
+	/**
+	 * Returns human readable date.
+	 *
+	 * @param  array   An optional array with configuration options.
+	 * @return string  Formatted date.
+	 */
+    public function humanize($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'date'              => null,
+            'gmt_offset'        => 0,
+            'smallest_period'   => 'day'
+        ));
+
+        $periods    = array('second', 'minute', 'hour', 'day', 'week', 'month', 'year');
+        $lengths    = array(60, 60, 24, 7, 4.35, 12, 10);
+        $now        = gmmktime();
+        $time       = is_numeric($config->date) ? $config->date : strtotime($config->date);
+
+        if($config->gmt_offset != 0) {
+            $time =  $time + 3600 * $config->gmt_offset;
+        }
+
+        if($now > $time)
+        {
+            $difference = $now - $time;
+            $tense      = 'ago';
+        }
+        else
+        {
+            $difference = $time;
+            $tense      = 'from now';
+        }
+
+        for($i = 0; $difference >= $lengths[$i] && $i < 6; $i++) {
+            $difference /= $lengths[$i];
+        }
+
+        $difference         = round($difference);
+        $period_index       = array_search($config->smallest_period, $periods);
+        $omitted_periods    = $periods;
+        array_splice($omitted_periods, $period_index);
+
+        if(in_array($periods[$i], $omitted_periods))
+        {
+            $difference = 1;
+            $i          = $period_index;
+        }
+
+        if($periods[$i] == 'day')
+        {
+            switch($difference)
+            {
+                case 1:
+                    return 'Today';
+                    break;
+
+                case 2:
+                    return $tense == 'ago' ? 'Yesterday' : 'Tomorrow';
+                    break;
+            }
+        }
+
+        if($difference != 1) {
+            $periods[$i].= 's';
+        }
+
+        return sprintf(JText::_('%s '.$periods[$i].' '.$tense), $difference);
+    }
 }
