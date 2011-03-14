6d1779a1087005a08445ebc9835fe752186e5462
Johan Janssens <johan@timble.net>
2011-03-14 16:45:42 +0000

    Added specialized ComDefaultTemplateFilterForm classes to set the 'token_value' to JUtility::getToken()

---
+++ code/administrator/components/com_default/templates/filters/form.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @version     $Id$
+ * @category	Nooku
+ * @package     Nooku_Components
+ * @subpackage  Default
+ * @copyright   Copyright (C) 2007 - 2010 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Form Filter
+.*
+ * @author      Johan Janssens <johan@nooku.org>
+ * @category    Nooku
+ * @package     Nooku_Components
+ * @subpackage  Default
+ */
+class ComDefaultTemplateFilterForm extends KTemplateFilterForm
+{
+	/**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param   object  An optional KConfig object with configuration options
+     * @return void
+     */
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'token_value'   => JUtility::getToken(),
+        ));
+
+        parent::_initialize($config);
+    }
+}
