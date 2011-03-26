69b4e422f8b203a2f0ac40ac48a9d615cd0e3fbb
Johan Janssens <johan@timble.net>
2011-03-26 02:06:23 +0000

    Moved the re-creating of the document for AJAX request to the onAfterInitialise to fix a problem with language not being set correctly in the document.

--- code/plugins/system/koowa.php
+++ code/plugins/system/koowa.php
@@ -130,6 +130,11 @@ class plgSystemKoowa extends JPlugin
 	 */
 	public function onAfterInitialise()
 	{  
+	    /*
+	     * Try to log the user in
+	     * 
+	     * If the request contains authorization information we try to log the user in
+	     */
 	    if(KRequest::has('server.PHP_AUTH_USER') && KRequest::has('server.PHP_AUTH_PW')) 
 	    {
 	        $credentials = array(
@@ -143,9 +148,34 @@ class plgSystemKoowa extends JPlugin
         	    return false;      
 	        }
 	        
+	        //Reset the user object in the factory
+	        KFactory::set('lib.koowa.user', JFactory::getUser());
+	         
 	        //Force the token
 	        KRequest::set('request._token', JUtility::getToken());
 	    }
+	    
+	    /*
+	     * Special handling for AJAX requests
+	     * 
+	     * If the format is AJAX and the format is 'html' or the tmpl is empty we re-create 
+	     * a 'raw' document rendered and force it's type to the active format
+	     */
+        if(KRequest::type() == 'AJAX') 
+        {
+        	if(KRequest::get('get.format', 'cmd', 'html') != 'html' || KRequest::get('get.tmpl', 'cmd') === '')
+        	{
+        		$format = JRequest::getWord('format', 'html');
+        	
+        		JRequest::setVar('format', 'raw');   //force format to raw
+        		
+        		$document =& JFactory::getDocument();
+        		$document = null;
+        		$document = JFactory::getDocument()->setType($format);
+        		
+        		JRequest::setVar('format', $format); //revert format to original
+        	}
+        }
 	}
 	
  	/**
