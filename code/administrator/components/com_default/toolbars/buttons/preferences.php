3251a1fc92868ca8685b89f458057640a68b9d0e
Johan Janssens <johan@timble.net>
2011-03-29 11:51:38 +0000

    Added configurable width and height support.

--- code/administrator/components/com_default/toolbars/buttons/preferences.php
+++ code/administrator/components/com_default/toolbars/buttons/preferences.php
@@ -17,8 +17,28 @@
  * @package     Nooku_Components
  * @subpackage  Default
  */
-class ComWeblinksToolbarButtonPreferences extends KToolbarButtonDefault
-{
+class ComDefaultToolbarButtonPreferences extends KToolbarButtonDefault
+{    
+ 	/**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param   object  An optional KConfig object with configuration options
+     * @return  void
+     */
+    protected function _initialize(KConfig $config)
+    {
+        $name = $this->_identifier->name;
+
+        $config->append(array(
+            'width'   => '640',
+            'height'  => '480',
+        ));
+
+        parent::_initialize($config);
+    }
+	
 	/**
 	 * Generates the link
 	 *
@@ -48,7 +68,7 @@ class ComWeblinksToolbarButtonPreferences extends KToolbarButtonDefault
 
 		$html 	= array ();
 		$html[]	= '<td class="button" id="'.$this->getId().'">';
-		$html[]	= '<a '.$href.' '.$onclick.' class="toolbar modal" rel="{handler: \'iframe\', size: {x: 640, y: 480}}">';
+		$html[]	= '<a '.$href.' '.$onclick.' class="toolbar modal" rel="{handler: \'iframe\', size: {x: '.$this->_option->width.', y: '.$this->_options->height.'}}">';
 
 		$html[]	= '<span class="'.$this->getClass().'" title="'.$text.'">';
 		$html[]	= '</span>';
