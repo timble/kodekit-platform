922ce26a02339276cd7172c75d2e1ca8c4c2875a
Johan Janssens <johan@timble.net>
2011-04-06 15:08:07 +0000

    Added default toolbar to fix compatibility issues with Joomla 1.6.

---
+++ code/administrator/components/com_default/toolbars/default.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @version   	$Id$
+ * @category	Nooku
+ * @package     Nooku_Components
+ * @subpackage  Default
+ * @copyright  	Copyright (C) 2007 - 2010 Johan Janssens. All rights reserved.
+ * @license   	GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Default Toolbar
+.*
+ * @author      Johan Janssens <johan@nooku.org>
+ * @category    Nooku
+ * @package     Nooku_Components
+ * @subpackage  Default
+ */
+class ComDefaultToolbarDefault extends KToolbarDefault
+{
+    public function render()
+	{
+		$id		= 'toolbar-'.$this->getName();
+		$html = array ();
+
+		// Start toolbar div
+		if (JVERSION::isCompatible('1.6')) {
+			$html[] = '<div class="toolbar" id="'.$id.'">';
+		} else {
+			$html[] = '<div class="toolbar-list" id="'.$id.'">';
+		}
+
+		$html[] = '<table class="toolbar"><tr>';
+
+		// Render each button in the toolbar
+		foreach ($this->_buttons as $button)
+		{
+			if(!($button instanceof KToolbarButtonInterface))
+			{
+				$app		= $this->_identifier->application;
+				$package	= $this->_identifier->package;
+				$button = KFactory::tmp($app.'::com.'.$package.'.toolbar.button.'.$button);
+			}
+
+			$button->setParent($this);
+			$html[] = $button->render();
+		}
+
+		// End toolbar div
+		$html[] = '</tr></table>';
+		$html[] = '</div>';
+
+		return implode(PHP_EOL, $html);
+	}
+
+	public function renderTitle()
+	{
+		//strip the extension
+		$icon  = preg_replace('#\.[^.]*$#', '', $this->_icon);
+		$title = JText::_($this->_title);
+
+		$html  = '<div class="header pagetitle icon-48-'.$icon.'">';
+		if (JVERSION::isCompatible('1.6')) {
+			$html .= $title;
+		} else {
+			$html .= '<h2>'.$title.'</h2>';
+		}
+		
+		$html .= '</div>';
+
+		return $html;
+	}
+}
