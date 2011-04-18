4e841ceca4dd949e8c5bb554491ecf9dc00ac67d
Johan Janssens <johan@timble.net>
2011-04-18 13:11:00 +0000

    Docblocks

--- code/administrator/components/com_sites/sites.php
+++ code/administrator/components/com_sites/sites.php
@@ -3,7 +3,7 @@
  * @version     $Id$
  * @category	Nooku
  * @package     Nooku_Server
- * @subpackage  Sections
+ * @subpackage  Sites
  * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
@@ -15,7 +15,7 @@
  * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @category	Nooku
  * @package     Nooku_Server
- * @subpackage  Sections    
+ * @subpackage  Sites   
  */
 
 echo KFactory::get('admin::com.sites.dispatcher')->dispatch();
