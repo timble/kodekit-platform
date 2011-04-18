65e2af5632e9827b66349fc08d4e84a37286b8a4
Johan Janssens <johan@timble.net>
2011-04-18 13:10:39 +0000

    re #3 : Added dispatcher.

---
+++ code/administrator/components/com_sites/sites.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @version     $Id$
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Sections
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Component Loader
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Sections    
+ */
+
+echo KFactory::get('admin::com.sites.dispatcher')->dispatch();
