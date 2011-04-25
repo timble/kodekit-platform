72e58aafc51a77c96b6313bf198989d2b1723f9b
Johan Janssens <johan@timble.net>
2011-04-25 01:14:30 +0000

    Removed JAdministratorHelper class. findOption functionality moved into dispatch.

--- code/administrator/index.php
+++ code/administrator/index.php
@@ -20,7 +20,6 @@ define('DS', DIRECTORY_SEPARATOR);
 
 require_once( JPATH_BASE .DS.'includes'.DS.'defines.php' );
 require_once( JPATH_BASE .DS.'includes'.DS.'framework.php' );
-require_once( JPATH_BASE .DS.'includes'.DS.'helper.php' );
 require_once( JPATH_BASE .DS.'includes'.DS.'toolbar.php' );
 
 //Nooku Server identification information
@@ -64,8 +63,7 @@ $mainframe->triggerEvent('onAfterRoute');
  *
  * NOTE :
  */
-$option = JAdministratorHelper::findOption();
-$mainframe->dispatch($option);
+$mainframe->dispatch();
 
 // trigger the onAfterDispatch events
 JDEBUG ? $_PROFILER->mark('afterDispatch') : null;
