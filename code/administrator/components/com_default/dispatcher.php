428167ce47a13ce4e2a78bb6d20db8e11be86fd9
Johan Janssens <johan@timble.net>
2011-04-26 16:08:13 +0000

    Cleanup

--- code/administrator/components/com_default/dispatcher.php
+++ code/administrator/components/com_default/dispatcher.php
@@ -54,10 +54,8 @@ class ComDefaultDispatcher extends KDispatcherDefault
         //Redirect if no view information can be found in the request
         if(!KRequest::has('get.view')) 
         {
-            $view = count($context->data) ? $context->data : $this->_controller_default;
-            
             $url = clone(KRequest::url());
-            $url->query['view'] = $view;
+            $url->query['view'] = $this->getRequest()->view;
             
             KFactory::get('lib.joomla.application')->redirect($url);
         }
