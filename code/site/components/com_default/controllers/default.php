e907eec5e808c49825b92a14b985df03ab966087
Johan Janssens <johan@timble.net>
2011-04-26 10:50:07 +0000

    - ComDefaultControllerDefault now extends KControllerResource - Renamed _actionDisplay to _actionGet. Display is now an alias for Get.

--- code/site/components/com_default/controllers/default.php
+++ code/site/components/com_default/controllers/default.php
@@ -10,32 +10,15 @@
  */
 
 /**
- * Default View Controller
+ * Default Controller
 .*
  * @author      Johan Janssens <johan@nooku.org>
  * @category    Nooku
  * @package     Nooku_Components
  * @subpackage  Default
  */
-class ComDefaultControllerDefault extends KControllerForm
+class ComDefaultControllerDefault extends KControllerResource
 {
- 	/**
-     * Constructor
-     *
-     * @param   object  An optional KConfig object with configuration options
-     */
-    public function __construct(KConfig $config)
-    {
-        parent::__construct($config);
-        
-         //Enqueue the authorization command
-        $command = clone $this->_identifier;
-	    $command->path = 'command';
-		$command->name = 'authorize';
-	    
-        $this->getCommandChain()->enqueue( KFactory::get($command));
-    }
-    
     /**
      * Set the request information
      * 
@@ -62,13 +45,13 @@ class ComDefaultControllerDefault extends KControllerForm
      * @param   KCommandContext A command context object
      * @return  KDatabaseRow(set)   A row(set) object containing the data to display
      */
-    protected function _actionDisplay(KCommandContext $context)
+    protected function _actionGet(KCommandContext $context)
     {
         //Load the language file for HMVC requests who are not routed through the dispatcher
         if(!$this->isDispatched()) {
             KFactory::get('lib.joomla.language')->load('com_'.$this->getIdentifier()->package); 
         }
         
-        return parent::_actionDisplay($context);
+        return parent::_actionGet($context);
     }
 }
