e817538f0c22fe5d0bf4e75813ad7cfc05e418c3
Johan Janssens <johan@timble.net>
2011-04-26 10:52:21 +0000

    - Renamed ComDefaultControllerView to ComDefaultControllerPage - Renamed _actionDisplay to _actionGet. Display is now an alias of Get.

---
+++ code/administrator/components/com_default/controllers/page.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @version     $Id: default.php 2939 2011-03-18 01:13:04Z johanjanssens $
+ * @category	Nooku
+ * @package     Nooku_Components
+ * @subpackage  Default
+ * @copyright   Copyright (C) 2007 - 2010 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+
+/**
+ * Page Controller
+.*
+ * @author      Johan Janssens <johan@nooku.org>
+ * @category    Nooku
+ * @package     Nooku_Components
+ * @subpackage  Default
+ */
+class ComDefaultControllerPage extends KControllerPage
+{
+    /**
+     * Set the request information
+     * 
+     * This function translates 'limitstart' to 'offset' for compatibility with Joomla
+     *
+     * @param array An associative array of request information
+     * @return KControllerBread
+     */
+    public function setRequest(array $request = array())
+    {
+        if(isset($request['limitstart'])) {
+            $request['offset'] = $request['limitstart'];
+        }
+        
+        $this->_request = new KConfig($request);
+        return $this;
+    }
+     
+    /**
+     * Display action
+     * 
+     * If the controller was not dispatched manually load the langauges files 
+     *
+     * @param   KCommandContext A command context object
+     * @return  KDatabaseRow(set)   A row(set) object containing the data to display
+     */
+    protected function _actionGet(KCommandContext $context)
+    {
+        //Load the language file for HMVC requests who are not routed through the dispatcher
+        if(!$this->isDispatched()) {
+            KFactory::get('lib.joomla.language')->load('com_'.$this->getIdentifier()->package); 
+        }
+        
+        return parent::_actionGet($context);
+    }
+}
