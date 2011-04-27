c4aa93378756828456ea1627117a6ac5a8e575ee
Johan Janssens <johan@timble.net>
2011-04-27 01:38:58 +0000

    Consolidated __call() function of KControllerPage and KControllerResource

--- code/libraries/koowa/controller/resource.php
+++ code/libraries/koowa/controller/resource.php
@@ -100,7 +100,7 @@ abstract class KControllerResource extends KControllerPage
 	 *
 	 * @return	KViewAbstract
 	 */
-	public function getView()
+    public function getView()
 	{
 	    if(!$this->_view instanceof KViewAbstract)
 		{	
@@ -319,36 +319,5 @@ abstract class KControllerResource extends KControllerPage
 	    $row->setData($state);
 	             
         return parent::execute($action, $context); 
-	}
-
-	/**
-	 * Supports a simple form Fluent Interfaces. Allows you to set the request 
-	 * properties by using the request property name as the method name.
-	 *
-	 * For example : $controller->view('name')->limit(10)->browse();
-	 *
-	 * @param	string	Method name
-	 * @param	array	Array containing all the arguments for the original call
-	 * @return	KControllerBread
-	 *
-	 * @see http://martinfowler.com/bliki/FluentInterface.html
-	 */
-	public function __call($method, $args)
-	{
-		//Check first if we are calling a mixed in method. This prevents the model being 
-		//loaded durig object instantiation. 
-		if(!isset($this->_mixed_methods[$method])) 
-        {
-			//Check if the method is a state property
-			$state = $this->getModel()->getState();
-		
-			if(isset($state->$method)) 
-			{
-				$this->$method = $args[0];
-				return $this;
-			}
-        }
-		
-		return parent::__call($method, $args);
 	}
 }
