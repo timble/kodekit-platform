2f18072ade50020af46cf9b6ed6965a8b1dc82fa
Israel Canasa <israel@timble.net>
2011-05-03 16:56:06 +0000

    re #15 - Added loggable controller behavior.
    Also removed ComLogsControllerLog::_actionAdd() because it's now recommended to simply use the loggable behavior.

---
+++ code/administrator/components/com_logs/controllers/behaviors/loggable.php
@@ -0,0 +1,66 @@
+<?php
+
+class ComLogsControllerBehaviorLoggable extends KControllerBehaviorAbstract
+{
+    public function __construct(KConfig $config)
+    { 
+        parent::__construct($config);
+
+        $this->registerCallback('after.edit'  , array($this, 'log'));
+		$this->registerCallback('after.add', array($this, 'log'));
+		$this->registerCallback('after.delete'  , array($this, 'log'));
+		$this->registerCallback('after.read', array($this, 'log'));
+    }
+    
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'priority'   => KCommand::PRIORITY_LOW,
+        ));
+
+        parent::_initialize($config);
+    }
+    
+    public function log(KCommandContext $context)
+    {
+        $identifier = $context->caller->getIdentifier();
+				
+		$data = array();
+		$data['action']			= $context->action;
+		$data['application']	= $identifier->application;
+		$data['type']			= $identifier->type;
+		$data['package']		= $identifier->package;
+		$data['name']			= $identifier->name;
+
+		$rowset = array();
+		if ($context->result instanceof KDatabaseRowAbstract) {
+			$rowset[] =  $context->result;
+		} else {
+			$rowset = KConfig::toData($context->result);
+		}
+
+		foreach ($rowset as $row)
+		{
+			//Only log if the row status is valid.
+			$status = $row->getStatus();
+			
+			if(!empty($status))
+			{
+				if ($row->title) {
+					$data['title'] = $row->title;
+				} elseif ($row->name) {
+					$data['title'] = $row->name;
+				} else {
+					$data['title'] = '#'.$row->id;
+				}
+
+				$data['row_id'] = $row->id;
+				
+				KFactory::tmp('admin::com.logs.model.logs')
+					->getItem()
+					->setData($data)
+					->save();
+			}
+		}
+    }
+}
