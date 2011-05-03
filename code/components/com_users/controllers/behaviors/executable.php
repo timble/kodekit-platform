61e9aee27cc9a5ffc6fd42d066f917fb8f26c8b6
Johan Janssens <johan@timble.net>
2011-05-03 11:30:23 +0000

    Initial import. Implemented controller behaviors.

---
+++ code/components/com_users/controllers/behaviors/executable.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @version     $Id$
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Users
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Authorize Command
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Users
+ */
+class ComUsersControllerBehaviorExecutable extends ComDefaultControllerCommandAuthorize
+{
+    protected function _beforeAdd(KCommandContext $context)
+    {
+        $parameters = JComponentHelper::getParams('com_users');
+
+        if($parameters->get('allowUserRegistration') == '0') {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    protected function _beforeEdit(KCommandContext $context)
+    {
+        $request = $context->caller->getRequest();
+        
+        if($request->id == 0 || $request->id != KFactory::get('lib.joomla.user')->id) {
+            return false;
+        }
+        
+        $result = !KFactory::get('lib.joomla.user')->guest;       
+        return $result;
+    }
+}
