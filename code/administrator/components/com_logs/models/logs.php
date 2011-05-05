1ecbdb76aa6a1468c2433128d6cff3ba52b05050
Gergo Erdosi <gergo@timble.net>
2011-05-05 10:14:18 +0000

    Fixed bugs.

--- code/administrator/components/com_logs/models/logs.php
+++ code/administrator/components/com_logs/models/logs.php
@@ -4,13 +4,13 @@
 class ComLogsModelLogs extends KModelTable
 {
 	protected $_column;
-	
+
 	public function __construct(KConfig $config)
 	{
 		$config->table_behaviors = array('creatable');
-		
+
 		parent::__construct($config);
-		
+
 		$this->_state
 			->insert('application', 'cmd')
 			->insert('type', 'cmd')
@@ -18,48 +18,48 @@ class ComLogsModelLogs extends KModelTable
 			->insert('name', 'cmd')
 			->insert('action', 'cmd')
 			->insert('user', 'cmd');
-		
+
 		$this->_state
 			->remove('sort')->insert('sort', 'cmd', 'created_on')
 			->remove('direction')->insert('direction', 'word', 'desc');
 	}
-	
+
 	protected function _buildQueryColumns(KDatabaseQuery $query)
 	{
 		parent::_buildQueryColumns($query);
-		
+
 		$query->select('users.name AS created_by_name');
 	}
-	
+
 	protected function _buildQueryJoins(KDatabaseQuery $query)
 	{
 		$query->join('LEFT', 'users AS users', 'users.id = tbl.created_by');
 	}
-	
+
 	protected function _buildQueryWhere(KDatabaseQuery $query)
 	{
 		parent::_buildQueryWhere($query);
-		
+
 		if ($this->_state->application) {
 			$query->where('tbl.application', '=', $this->_state->application);
 		}
-		
+
 		if ($this->_state->type) {
 			$query->where('tbl.type', '=', $this->_state->type);
 		}
-		
+
 		if ($this->_state->package) {
 			$query->where('tbl.package', '=', $this->_state->package);
 		}
-		
+
 		if ($this->_state->name) {
 			$query->where('tbl.name', '=', $this->_state->name);
 		}
-		
+
 		if ($this->_state->action) {
 			$query->where('tbl.action', 'IN', $this->_state->action);
 		}
-		
+
 		if ($this->_state->user) {
 			$query->where('tbl.created_by', '=', $this->_state->user);
 		}
--- code/administrator/components/com_logs/templates/helpers/message.php
+++ code/administrator/components/com_logs/templates/helpers/message.php
@@ -1,7 +1,7 @@
 <?php
 /** $Id: message.php 1852 2010-12-13 19:40:51Z tomjanssens $ */
 
-class ComLogsTemplateHelperMessage extends ComDefaultTemplateHelperString
+class ComLogsTemplateHelperMessage extends KTemplateHelperDefault
 {
 	public function build($config = array())
 	{
@@ -9,13 +9,13 @@ class ComLogsTemplateHelperMessage extends ComDefaultTemplateHelperString
 		$config->append(array(
 			'row'      => ''
 		));
-		
+
 		//Get the row object
 		$row = $config->row;
-		
+
 		$message  = '<a class="ellipsis" href="index.php?option='.$row->type.'_'.$row->package.'&view='.$row->name.'&id='.$row->row_id.'">'.$row->title.'</a> ';
 		$message .= '<br /><small>'.date("H:i", strtotime($row->created_on)).' - '.$row->created_by_name.'</small>';
-		
+
 		return $message;
 	}
 }
