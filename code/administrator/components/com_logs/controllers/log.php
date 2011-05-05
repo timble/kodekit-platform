afe4735ae42029758d9dc40e5a44212671a960ac
Israel Canasa <israel@timble.net>
2011-05-05 08:06:22 +0000

    re #15 - Set the default layout and model depending on how com_logs is called.
    Basically, com_logs can be called as <?=KFactory::tmp('admin::com.logs.controller.log')->package('componentname')->display();?> in a template. This allows com_logs to be embedded inside components. If it was called through HMVC, it will use a special layout.
    
    The controller also makes sure that the com_logs model is used.

--- code/administrator/components/com_logs/controllers/log.php
+++ code/administrator/components/com_logs/controllers/log.php
@@ -1,7 +1,38 @@
 <?php
 /** $Id: log.php 1310 2010-09-19 14:46:25Z johanjanssens $ */
 
-class ComLogsControllerLog extends KControllerAbstract
+class ComLogsControllerLog extends ComDefaultControllerDefault
 {
-	
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'request' => KRequest::get('get', 'string'),
+        ));
+        
+        parent::_initialize($config);
+    }
+
+	public function getView()
+    {
+        if (!$this->isDispatched()) {
+            $this->_request->layout = 'package_list';
+        }
+
+        return parent::getView();
+    }
+    
+    public function getModel()
+	{
+		if(!$this->_model instanceof KModelAbstract) 
+		{   
+		    //@TODO : Pass the state to the model using the options
+		    $options = array(
+				'state' => $this->_request
+            );
+		    
+		    $this->_model = KFactory::tmp('admin::com.logs.model.logs')->set($this->_request);
+		}
+
+		return $this->_model;
+	}
 }
