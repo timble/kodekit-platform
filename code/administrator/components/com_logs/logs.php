c483ed87c0410e5691a8d8237e87043c0b0d366c
Israel Canasa <israel@timble.net>
2011-05-05 08:02:50 +0000

    re #15 - Added entry file for com_logs.

---
+++ code/administrator/components/com_logs/logs.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @version		$Id$
+ * @category	Nooku
+ * @package     Nooku_Components
+ * @subpackage  Logs
+ * @copyright	Copyright (C) 2010 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+
+/**
+ * Revision Row
+ *
+ * @author      Israel Canasa <raeldc@gmail.com>
+ * @category	Nooku
+ * @package    	Nooku_Components
+ * @subpackage 	Logs
+ */
+
+echo KFactory::get('admin::com.logs.dispatcher')->dispatch();
