fc50f32eff6cade86d8c9dda1b190c1914911885
Israel Canasa <israel@timble.net>
2011-05-05 08:08:15 +0000

    re #15 - Add the new template layout that will be used when com_logs is called through HMVC.

---
+++ code/administrator/components/com_logs/views/logs/tmpl/package_list.php
@@ -0,0 +1,90 @@
+<? /** $Id: default.php 1859 2010-12-18 20:37:31Z johanjanssens $ */ ?>
+<? defined('KOOWA') or die('Restricted access'); ?>
+
+<style src="media://com_logs/css/logs-default.css" />
+
+<div id="list" class="-koowa-box-flex">
+	<form action="<?= @route('option=com_'.$state->package.'&layout=default')?>" method="get" class="-koowa-box-flex">
+		<table class="adminlist" style="clear: both;">
+			<thead>
+				<tr>
+					<th width="5">
+						<?= @text('NUM') ?>
+					</th>
+					<th>
+						<?= @helper('grid.sort', array('title' => 'Applicaton', 'column' => 'application')) ?>
+					</th>
+					<th>
+						<?= @helper('grid.sort', array('title' => 'Name', 'column' => 'name')) ?>
+					</th>
+					<th>
+						<?= @helper('grid.sort', array('title' => 'Action', 'column' => 'action')) ?>
+					</th>
+					<th>
+						<?= @helper('grid.sort', array('title' => 'Created by', 'column' => 'created_by')) ?>
+					</th>
+					<th>
+						<?= @helper('grid.sort', array('title' => 'Created on', 'column' => 'created_on')) ?>
+					</th>
+				</tr>
+				<tr class="filters">
+					<td>
+						<?= @text('Filters') ?>	
+					</td>
+					<td>
+						<?= @helper('listbox.applications',  array('attribs' => array('onchange' => 'this.form.submit();'), 'selected' => $state->application)) ?>
+					</td>
+					<td>
+						<?= @helper('listbox.names',  array('attribs' => array('onchange' => 'this.form.submit();'), 'selected' => $state->name)) ?>
+					</td>
+					<td>
+						<?= @helper('listbox.actions',  array('attribs' => array('onchange' => 'this.form.submit();'), 'selected' => $state->action)) ?>
+					</td>
+					<td colspan="2">
+					</td>
+				</tr>
+			</thead>
+			
+			<tbody>
+				<? if (count($logs)) : ?>
+					<? foreach ($logs as $log) : ?>
+                        <tr>
+                            <td align="center">
+                                <?= @helper('grid.checkbox',array('row' => $log)); ?>
+                            </td>
+                            <td>
+                                <?= $log->application ?>
+                            </td>
+                            <td>
+                                <?= $log->name ?>
+                            </td>
+                            <td>
+                                <?= $log->action ?>
+                            </td>
+                            <td>
+                                <?= $log->created_by_name ?>
+                            </td>
+                            <td>
+                                <?= $log->created_on ?>
+                            </td>
+                        </tr>
+                        <? endforeach; ?>
+				<? else : ?>
+				<tr>
+					<td colspan="8" align="center">
+						<?= @text('No items found'); ?>
+					</td>
+				</tr>
+				<? endif; ?>
+			</tbody>
+			
+			<tfoot>
+				<tr>
+					<td colspan="20">
+						<?= @helper('paginator.pagination', array('total' => $total)) ?>
+					</td>
+				</tr>
+			</tfoot>
+		</table>
+	</form>
+</div>
