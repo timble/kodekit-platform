da4e4c9e260b1e9702ec19f3dddad02a742d0e47
Israel Canasa <israel@timble.net>
2011-05-05 16:19:00 +0000

    re #15 - Added some comments

--- code/administrator/components/com_logs/controllers/log.php
+++ code/administrator/components/com_logs/controllers/log.php
@@ -1,10 +1,11 @@
 <?php
-/** $Id$ */
+/** $Id:$ */
 
 class ComLogsControllerLog extends ComDefaultControllerDefault
 {
     protected function _initialize(KConfig $config)
     {
+        // Force controller to get the GET request whether it's dispatched or not
         $config->append(array(
             'request' => KRequest::get('get', 'string'),
         ));
