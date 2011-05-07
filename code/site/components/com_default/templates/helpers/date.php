e4856e06e1bfef523c241badb47d58747cb79515
Johan Janssens <johan@timble.net>
2011-05-07 11:37:53 +0000

    Set gmt_offset config option to 0 by default

--- code/site/components/com_default/templates/helpers/date.php
+++ code/site/components/com_default/templates/helpers/date.php
@@ -49,7 +49,7 @@ class ComDefaultTemplateHelperDate extends KTemplateHelperDate
     {
         $config = new KConfig($config);
         $config->append(array(
-            'gmt_offset' => KFactory::get('lib.joomla.config')->getValue('config.offset') * 3600
+            'gmt_offset' => 0
         ));
         
        return parent::humanize($config);
