1a5dd12352490a25210528593a96c188930dc91d
Johan Janssens <johan@timble.net>
2011-05-10 23:57:03 +0000

    re #133 : Implemented template caching through ComDefaultTemplateDefault::loadPath() and filter() functions. Templates will be cached upon read filtering, if a template is cached it will not be re-loaded from the file system, instead it will be loaded from the cache. The read filtering will only be performed once before the template is stored in the cache.

--- code/site/components/com_default/templates/default.php
+++ code/site/components/com_default/templates/default.php
@@ -19,6 +19,68 @@
  */
 class ComDefaultTemplateDefault extends KTemplateDefault
 { 
+/**
+	 * Load a template by path -- first look in the templates folder for an override
+	 * 
+	 * This function tries to get the template from the cache. If it cannot be found 
+	 * the template file will be loaded from the file system.
+	 *
+	 * @param   string 	The template path
+	 * @param	array	An associative array of data to be extracted in local template scope
+	 * @return KTemplateAbstract
+	 */
+	public function loadPath($path, $data = array())
+	{
+	    $cache = KFactory::tmp('lib.joomla.cache', array('template', 'output'));
+		
+	    //Set the lifetime to 0 to make sure cache isn't garbage collected.
+	    $cache->setLifeTime(0);
+	    
+	    $identifier = md5($path); 
+	     
+	    if ($template = $cache->get($identifier)) {
+		    $this->loadString($template, $data, $path);
+	    } else {
+	        parent::loadPath($path, $data);
+	    }
+	    
+		return $this;
+	}
+	
+	/**
+	 * Pass the data through the filter chain and perform
+	 * 
+	 * This function implements a caching mechanism when reading the template. If
+	 * the tempplate cannot be found in the cache it will be filtered and stored in
+	 * the cache. Otherwise it will be loaded from the cache and returned directly.
+	 *
+	 * @param string	The filter mode
+	 * @return string	The filtered data
+	 */
+	public function filter($mode = KTemplateFilter::MODE_READ)
+	{	
+	    if($mode == KTemplateFilter::MODE_READ)
+        {
+            $cache = KFactory::tmp('lib.joomla.cache', array('template', 'output'));
+		
+		    //Set the lifetime to 0 to make sure cache isn't garbage collected.
+	        $cache->setLifeTime(0);
+	    
+	        $identifier = md5($this->_path);
+	    
+	        if (!$template = $cache->get($identifier)) 
+	        {
+	            $template = parent::filter($mode);
+	            
+	            //Store the object in the cache
+		   	    $cache->store($template, $identifier);
+	        }
+        }
+        else $template = parent::filter($mode);
+	    
+	    return $template;
+	}
+    
     /**
      * Load a template helper
      * 
