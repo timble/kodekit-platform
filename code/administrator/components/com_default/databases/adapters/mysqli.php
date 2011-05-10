b8f87920fd7a9be65ac9609e06f315b3a3a8b96e
Johan Janssens <johan@timble.net>
2011-05-10 23:47:02 +0000

    re #131 : Implemented table schame cache through ComDefaultDatabaseAdapterMysqli::getTableColumns().

--- code/administrator/components/com_default/databases/adapters/mysqli.php
+++ code/administrator/components/com_default/databases/adapters/mysqli.php
@@ -20,6 +20,14 @@
  */
 class ComDefaultDatabaseAdapterMysqli extends KDatabaseAdapterMysqli
 { 
+    /**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param   object  An optional KConfig object with configuration options.
+     * @return  void
+     */
     protected function _initialize(KConfig $config)
     {
         $db = KFactory::get('lib.joomla.database');
@@ -34,4 +42,40 @@ class ComDefaultDatabaseAdapterMysqli extends KDatabaseAdapterMysqli
           
         parent::_initialize($config);
     }
+    
+	/**
+	 * Retrieves the table schema information about the given table
+	 * 
+	 * This function try to get the table schema from the cache. If it cannot be found 
+	 * the table schema will be retrieved from the database and stored in the cache.
+	 * 
+	 * @param 	string 	A table name or a list of table names
+	 * @return	KDatabaseSchemaTable
+	 */
+	public function getTableSchema($table)
+	{
+	    if(!isset($this->_table_schema[$table]))
+		{
+		    $database = $this->getDatabase();
+	        $cache = KFactory::tmp('lib.joomla.cache', array('database', 'output'));
+	        
+	        //Set the lifetime to 0 to make sure cache isn't garbage collected.
+	        $cache->setLifeTime(0);
+	   
+	        $identifier = md5($database.$table);
+	    
+	        if (!$schema = $cache->get($identifier)) 
+	        {
+	            $schema = parent::getTableSchema($table);
+	            
+	            //Store the object in the cache
+		   	    $cache->store(serialize($schema), $identifier);
+	        }
+	        else $schema = unserialize($schema);
+	        
+	        $this->_table_schema[$table] = $schema;
+		}
+	    
+	    return $this->_table_schema[$table];
+	}
 }
