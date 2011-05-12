f03226f0abeff9aee54c3254043e8909d3476828
Johan Janssens <johan@timble.net>
2011-05-12 10:25:00 +0000

    Fixed typo.

--- code/libraries/koowa/template/abstract.php
+++ code/libraries/koowa/template/abstract.php
@@ -127,7 +127,7 @@ abstract class KTemplateAbstract extends KObject implements KObjectIdentifiable
 	    if(!$this->_view instanceof KViewAbstract)
 		{	   
 		    //Make sure we have a view identifier
-		    if(!($this->_view instanceof KIndetifier)) {
+		    if(!($this->_view instanceof KIndentifier)) {
 		        $this->setView($this->_view);
             }
 		    
