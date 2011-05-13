18b621dc36083e6cf72df71e3b756ab660d76e9a
Stian Didriksen <stian@timble.net>
2011-05-13 15:55:14 +0000

    re #86 no toolbar means no form or radios needed.

--- code/administrator/components/com_modules/views/modules/tmpl/list.php
+++ code/administrator/components/com_modules/views/modules/tmpl/list.php
@@ -12,44 +12,41 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 
 <?= @helper('behavior.tooltip') ?>
 
-<form action="<?= @route('layout=form') ?>" method="get" name="adminForm" id="modules-module-list">
-	<table class="adminlist" summary="Add Module">
-		<thead>
-			<tr>
-				<th colspan="4">
-					<?= @text('Select module') ?>
-				</th>
+<table class="adminlist" summary="Add Module">
+	<thead>
+		<tr>
+			<th colspan="4">
+				<?= @text('Select module') ?>
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+	<? $i = 0; foreach($modules as $module) : ?>
+		<? if(!$i%2) : ?>
+			<tr valign="top">
+		<? endif; ?>
+		<? $last = $i+1 == count($modules) ?>
+
+		<td width="50%">
+			<span <?= KHelperArray::toString(array(
+				'class' 	=> 'editlinktip hasTip',
+				'title'		=> @escape(@text($module->description)),
+				'name'		=> 'module',
+				'value'		=> $module->module
+			)) ?>>
+				<a href="<?= @route('view=module&layout=form&module='.$module->module) ?>">
+					<?= @text(@escape($module->name)) ?>
+				</a>
+			</span>
+		</td>
+
+		<? if($last) : ?> 
+			<td width="50%">&nbsp;</td>
+		<? endif; ?>
+		
+		<? if($i%2 || $last) : ?> 
 			</tr>
-		</thead>
-		<tbody>
-		<? $i = 0; foreach($modules as $module) : ?>
-			<? if(!$i%2) : ?>
-				<tr valign="top">
-			<? endif; ?>
-			<? $last = $i+1 == count($modules) ?>
-
-			<td width="50%">
-				<span <?= KHelperArray::toString(array(
-					'class' 	=> 'editlinktip hasTip',
-					'title'		=> @escape(@text($module->description)),
-					'name'		=> 'module',
-					'value'		=> $module->module
-				)) ?>>
-					<input type="radio" name="module" value="<?= $module->module ?>" />
-					<a href="<?= @route('view=module&layout=form&module='.$module->module) ?>">
-						<?= @text(@escape($module->name)) ?>
-					</a>
-				</span>
-			</td>
-
-			<? if($last) : ?> 
-				<td width="50%">&nbsp;</td>
-			<? endif; ?>
-			
-			<? if($i%2 || $last) : ?> 
-				</tr>
-			<? endif; ?>
-		<? $i++; endforeach ?>
-		</tbody>
-	</table>
-</form>
+		<? endif; ?>
+	<? $i++; endforeach ?>
+	</tbody>
+</table>
