64328ebc4db9a52c1069c8b30e1b7de8d2c87e1e
Johan Janssens <johan@timble.net>
2011-05-16 22:57:57 +0000

    re #68 : Docblocks

--- code/administrator/components/com_weblinks/databases/tables/weblinks.php
+++ code/administrator/components/com_weblinks/databases/tables/weblinks.php
@@ -4,7 +4,7 @@
  * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Weblinks
- * @copyright	Copyright (C) 2009 - 2011 Timble CVBA and Contributors. (http://www.timble.net)
+ * @copyright	Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link		http://www.nooku.org
  */
