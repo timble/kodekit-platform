ee443de2d95c61185392dba4d33347d2034994e8
Johan Janssens <johan@timble.net>
2011-05-18 23:12:02 +0000

    re #106 : Removed ignored words fucntionality.

--- code/components/com_search/filters/term.php
+++ code/components/com_search/filters/term.php
@@ -29,56 +29,7 @@ class ComSearchFilterTerm extends KFilterString
     {
         $value = parent::_sanitize($value);
         
-        // Apply length limitations
-		$value = $this->_limitTerm($value);
-			
-		//Rmove ignored words
-		$value = $this->_sanitizeTerm($value);
-		
-        return $value;
-    }
-	
-    public function _sanitizeTerm($term, $searchphrase = 'exact')
-	{
-		$lang = KFactory::get('lib.joomla.language');
-
-		$search_ignore	= array();
-		$tag			= $lang->getTag();
-		$ignoreFile		= $lang->getLanguagePath().DS.$tag.DS.$tag.'.ignore.php';
-		
-		if (file_exists($ignoreFile)) {
-			include $ignoreFile;
-		}
-
-	 	//Check for words to ignore
-		$aterms = explode(' ', JString::strtolower( $term ) );
-
-		//First case is single ignored word
-		if(count( $aterms ) == 1 && in_array( JString::strtolower( $term ), $search_ignore ) ) {
-			$ignored = true;
-		}
-
-		//Filter out search terms that are too small
-		foreach($aterms as $aterm ) 
-		{
-			if(JString::strlen( $aterm ) < 3) {
-				$search_ignore[] = $aterm;
-			}
-		}
-
-		//Remove ignored words from type 'all' or 'any' (not exact) searches with multiple words
-		if(count( $aterms ) > 1 && $searchphrase != 'exact' ) 
-		{
-			$pruned = array_diff( $aterms, $search_ignore );
-			$term   = implode( ' ', $pruned );
-		}
-
-		return $term;
-	}
-	
-    public function _limitTerm($term)
-	{
-		// Limit term to 20 characters
+        // Limit term to 20 characters
 		if( JString::strlen( $term ) > 20 ) {
 			$term 	= JString::substr( $term, 0, 19 );
 		}
@@ -87,7 +38,23 @@ class ComSearchFilterTerm extends KFilterString
 		if( $term && JString::strlen( $term ) < 3 ) {
 			$term = '';
 		}
+		
+        //Filter out search terms that are too small
+		$words = explode(' ', JString::strtolower( $term ) );
 
-		return $term;
-	}
+		foreach($words as $word ) 
+		{
+			if(JString::strlen( $word ) < 3) {
+				$search_ignore[] = $word;
+			}
+		}
+
+		if(count( $words ) > 1 ) 
+		{
+			$pruned = array_diff( $words, $search_ignore );
+			$term   = implode( ' ', $pruned );
+		}
+		
+        return $value;
+    }
 }
