4b238bec43045984a0d8758f525d7afb93f9ff16
Stian Didriksen <stian@timble.net>
2011-05-19 17:08:39 +0000

    re #127 updating com_user with custom validation message

--- code/administrator/components/com_users/views/users/tmpl/default.php
+++ code/administrator/components/com_users/views/users/tmpl/default.php
@@ -12,7 +12,7 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 
 <script src="media://lib_koowa/js/koowa.js" />
 
-<form action="<?= @route() ?>" method="get" name="adminForm" class="-koowa-grid">
+<form action="<?= @route() ?>" method="get" name="adminForm" class="-koowa-grid" data-validation-message="<?= @text('Please select an user from the list') ?>">
 	<table class="adminlist">
 		<thead>
 			<tr>
