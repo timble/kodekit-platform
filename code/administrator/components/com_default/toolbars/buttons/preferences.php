9ada52054122c6fdec7a74ff0f7f7656e1830857
Johan Janssens <johan@timble.net>
2011-05-21 23:47:01 +0000

    re #99 : Toolbar buttons now have an attribs property in its options, allowing you to define what attributes are rendered on the toolbar button.  The onclick event has been removed. Instead the attribute data-action defines what action this button triggers, and data-data allows you to set the data for post actions.

--- code/administrator/components/com_default/toolbars/buttons/preferences.php
+++ code/administrator/components/com_default/toolbars/buttons/preferences.php
@@ -11,7 +11,7 @@
 
 /**
  * Preferences Toolbar Button
-.*
+ *
  * @author      Johan Janssens <johan@nooku.org>
  * @category    Nooku
  * @package     Nooku_Components
@@ -22,6 +22,8 @@ class ComDefaultToolbarButtonPreferences extends KToolbarButtonGet
  	/**
      * Initializes the options for the object
      *
+     * Must include @helper('behavior.modal') in view, to load the modal behavior
+     *
      * Called from {@link __construct()} as a first step of object instantiation.
      *
      * @param   object  An optional KConfig object with configuration options
@@ -29,53 +31,17 @@ class ComDefaultToolbarButtonPreferences extends KToolbarButtonGet
      */
     protected function _initialize(KConfig $config)
     {
-        $name = $this->_identifier->name;
-
         $config->append(array(
             'width'   => '640',
             'height'  => '480',
+        ))->append(array(
+            'attribs' => array(
+                'class' => 'toolbar modal',
+                'href'  => 'index.php?option=com_config&controller=component&component=com_'.$this->_identifier->package,
+                'rel'   => '{handler: \'iframe\', size: {x: '.$config->width.', y: '.$config->height.'}}'
+            )
         ));
 
         parent::_initialize($config);
     }
-	
-	/**
-	 * Generates the link
-	 *
-	 * @return	string	Link for button
-	 */
-	public function getLink()
-	{
-        return 'index.php?option=com_config&controller=component&component=com_'.$this->_identifier->package;
-	}
-
-	/**
-	 * Renders the button
-	 *
-	 * Must include @helper('behavior.modal') in view, to load the modal behavior
-	 *
-	 * @return	string	Html for button
-	 */
-	public function render()
-	{
-		$text	= JText::_($this->_options->text);
-
-		$link   = $this->getLink();
-		$href   = !empty($link) ? 'href="'.JRoute::_($link).'"' : '';
-
-		$onclick =  $this->getOnClick();
-		$onclick = !empty($onclick) ? 'onclick="'. $onclick.'"' : '';
-
-		$html 	= array ();
-		$html[]	= '<td class="button" id="'.$this->getId().'">';
-		$html[]	= '<a '.$href.' '.$onclick.' class="toolbar modal" rel="{handler: \'iframe\', size: {x: '.$this->_option->width.', y: '.$this->_options->height.'}}">';
-
-		$html[]	= '<span class="'.$this->getClass().'" title="'.$text.'">';
-		$html[]	= '</span>';
-		$html[]	= $text;
-		$html[]	= '</a>';
-		$html[]	= '</td>';
-
-		return implode(PHP_EOL, $html);
-	}
 }
