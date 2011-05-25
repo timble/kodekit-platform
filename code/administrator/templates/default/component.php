cf10e17836abb812cb89b0298e9c2d0572d93943
Stian Didriksen <stian@timble.net>
2011-05-25 16:53:59 +0000

    updated to HTML5 doctype

--- code/administrator/templates/default/component.php
+++ code/administrator/templates/default/component.php
@@ -1,9 +1,6 @@
-<?php
-// no direct access
-defined( '_JEXEC' ) or die( 'Restricted access' );
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo  $this->language; ?>" lang="<?php echo  $this->language; ?>" dir="<?php echo  $this->direction; ?>" >
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<!DOCTYPE HTML>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 <jdoc:include type="head" />
 
