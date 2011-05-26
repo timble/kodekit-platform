2ce7d75e7412111091b941e400536e72fee238d0
Johan Janssens <johan@timble.net>
2011-05-26 12:07:26 +0000

    If time is empty return 'Never'.

--- code/libraries/koowa/template/helper/date.php
+++ code/libraries/koowa/template/helper/date.php
@@ -62,55 +62,62 @@ class KTemplateHelperDate extends KTemplateHelperAbstract
         $lengths    = array(60, 60, 24, 7, 4.35, 12, 10);
         $now        = strtotime(gmdate("M d Y H:i:s"));
         $time       = is_numeric($config->date) ? $config->date : strtotime($config->date);
-
-        if($config->gmt_offset != 0) {
-            $now =  $now + $config->gmt_offset;
-        }
-
-        if($now > $time)
-        {
-            $difference = $now - $time;
-            $tense      = 'ago';
-        }
-        else
-        {
-            $difference = $time;
-            $tense      = 'from now';
-        }
-
-        for($i = 0; $difference >= $lengths[$i] && $i < 6; $i++) {
-            $difference /= $lengths[$i];
-        }
-
-        $difference         = round($difference);
-        $period_index       = array_search($config->smallest_period, $periods);
-        $omitted_periods    = $periods;
-        array_splice($omitted_periods, $period_index);
-
-        if(in_array($periods[$i], $omitted_periods))
-        {
-            $difference = 1;
-            $i          = $period_index;
-        }
-
-        if($periods[$i] == 'day')
-        {
-            switch($difference)
-            {
-                case 1:
-                    return 'Today';
-                    break;
-
-                case 2:
-                    return $tense == 'ago' ? 'Yesterday' : 'Tomorrow';
-                    break;
+        
+        
+        if( !$time )
+        { 
+            if($config->gmt_offset != 0) {
+                $now =  $now + $config->gmt_offset;
             }
-        }
 
-        if($difference != 1) {
-            $periods[$i].= 's';
-        }
+            if($now > $time)
+            {
+                $difference = $now - $time;
+                $tense      = 'ago';
+            }
+            else
+            {
+                $difference = $time;
+                $tense      = 'from now';
+            }
 
-        return sprintf(JText::_('%s '.$periods[$i].' '.$tense), $difference);
+            for($i = 0; $difference >= $lengths[$i] && $i < 6; $i++) {
+                $difference /= $lengths[$i];
+            }
+
+            $difference         = round($difference);
+            $period_index       = array_search($config->smallest_period, $periods);
+            $omitted_periods    = $periods;
+            array_splice($omitted_periods, $period_index);
+
+            if(in_array($periods[$i], $omitted_periods))
+            {
+                $difference = 1;
+                $i          = $period_index;
+            }
+
+            if($periods[$i] == 'day')
+            {
+                switch($difference)
+                {
+                    case 1:
+                        return 'Today';
+                        break;
+
+                    case 2:
+                        return $tense == 'ago' ? 'Yesterday' : 'Tomorrow';
+                        break;
+                }
+            }
+
+            if($difference != 1) {
+                $periods[$i].= 's';
+            }
+
+            $result = sprintf(JText::_('%s '.$periods[$i].' '.$tense), $difference);
+        } 
+        else $result = JText::_('Never'); 
+
+        return $result;       
     }
 }
