62bab3ba5acbd5f63426a712862148ec9d6a7d98
Stian Didriksen <stian@timble.net>
2011-05-27 17:07:34 +0000

    re #127 updating toolbar button to work with the new format

--- code/administrator/components/com_modules/toolbars/button/duplicate.php
+++ code/administrator/components/com_modules/toolbars/button/duplicate.php
@@ -1,19 +1,16 @@
 <?php
 /** $Id$ */
 
-class ComModulesToolbarButtonDuplicate extends KToolbarButtonPost
+class ComModulesToolbarButtonDuplicate extends ComDefaultToolbarButtonDefault
 {
-	//@TODO suggest patch, duplicate is a common action (although incorrectly named "copy" in Joomla)
-	public function getOnClick()
-	{
-		$option	= KRequest::get('get.option', 'cmd');
-		$view	= KRequest::get('get.view', 'cmd');
-		$token	= JUtility::getToken();
-		$json 	= "{method:'post', url:'index.php?option=$option&view=$view&'+id, params:{action:'duplicate', _token:'$token'}}";
-
-		$msg 	= JText::_('Please select an item from the list');
-		return 'var id = Koowa.Grid.getIdQuery();'
-			.'if(id){new Koowa.Form('.$json.').submit();} '
-			.'else { alert(\''.$msg.'\'); return false; }';
-	}
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+        	'attribs'  => array(
+                'data-action' => 'duplicate'
+            )
+        ));
+        
+        parent::_initialize($config);
+    }
 }
