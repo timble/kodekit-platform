1759b9afffc56698391f7bba2a5c9666fe1f5bb8
Johan Janssens <johan@timble.net>
2011-06-02 23:50:45 +0000

    Toolbar buttons now may have a data-novalidate="novalidate" specified to tell the controller that it does not need to fire the validate event before it executes the action. This was done to fix the purge button in com_cache, as well as allowing the removal of _actionCancel in Koowa.Controller.Form as it was duplicate code.

---
+++ code/administrator/components/com_default/toolbars/buttons/cancel.php
@@ -0,0 +1,35 @@
+<?php
+/**
+* @version      $Id$
+* @category		Koowa
+* @package		Koowa_Toolbar
+* @subpackage	Button
+* @copyright    Copyright (C) 2007 - 2010 Johan Janssens. All rights reserved.
+* @license      GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+*/
+
+/**
+ * Cancel button class for a toolbar
+ * 
+ * @author      Johan Janssens <johan@nooku.org>
+ * @category    Koowa
+ * @package     Koowa_Toolbar
+ * @subpackage  Button
+ */
+class ComDefaultToolbarButtonCancel extends ComDefaultToolbarButtonDefault
+{
+   /**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param   object  An optional KConfig object with configuration options
+     * @return  void
+     */
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array('attribs' => array('data-novalidate' => 'novalidate')));	
+
+        parent::_initialize($config);
+    }
+}
