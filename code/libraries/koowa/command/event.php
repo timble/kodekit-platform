347b8f187e689bdea6771cf9870c4d12a55197d2
Johan Janssens <johan@timble.net>
2011-06-02 14:54:59 +0000

    KEventDispatcher::dispatchEvent now has a first parameter which holds the event name. The second parameter contains the event object.

--- code/libraries/koowa/command/event.php
+++ code/libraries/koowa/command/event.php
@@ -87,7 +87,7 @@ class KCommandEvent extends KCommand
         $parts = explode('.', $name);   
         $event = 'on'.ucfirst($type.KInflector::implode($parts));
                 
-        $this->_dispatcher->dispatchEvent(new KEvent($event, clone($context)));
+        $this->_dispatcher->dispatchEvent($event, new KEvent(clone($context)));
         
         return true;
     }
