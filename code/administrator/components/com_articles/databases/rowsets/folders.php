fc32bf198849d51cdd629a67e83d2840eb829a98
Gergo Erdosi <gergo@timble.net>
2011-06-08 17:18:40 +0000

    Re #124: Added folders model and rowset.

---
+++ code/administrator/components/com_articles/databases/rowsets/folders.php
@@ -0,0 +1,5 @@
+<?php
+class ComArticlesDatabaseRowsetFolders extends KDatabaseRowsetAbstract
+{
+
+}
---
+++ code/administrator/components/com_articles/models/folders.php
@@ -0,0 +1,47 @@
+<?php
+class ComArticlesModelFolders extends KModelAbstract
+{
+    public function getList()
+    {
+        if(!isset($this->_list))
+        {
+            $folders = KFactory::tmp('admin::com.articles.database.rowset.folders');
+
+            $sections = KFactory::tmp('admin::com.sections.model.sections')
+                ->set('published', 1)
+                ->set('scope', 'content')
+                ->set('limit', 0)
+                ->set('sort', 'ordering')
+                ->getList();
+
+            foreach($sections as $section)
+            {
+                $folders->insert($folders->getRow()->setData(array(
+                    'id'	      => $section->id,
+                    'title'       => $section->title,
+                    'section_id'  => 0
+                ), false));
+            }
+
+            $categories = KFactory::tmp('admin::com.categories.model.categories')
+                ->set('published', 1)
+                ->set('section', 'com_content')
+                ->set('limit', 0)
+                ->set('sort', 'ordering')
+                ->getList();
+
+            foreach($categories as $category)
+            {
+                $folders->insert($folders->getRow()->setData(array(
+                    'id'	      => $category->id,
+                    'title'		  => $category->title,
+                    'section_id'  => $category->section_id
+                ), false));
+            }
+
+            $this->_list = $folders;
+        }
+
+        return $this->_list;
+    }
+}
