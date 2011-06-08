e5c137c0bb142dc500a464f7794ba63e61b57051
Johan Janssens <johan@timble.net>
2011-06-08 15:33:04 +0000

    re #124 : Cleanup

--- code/media/com_articles/js/article.js
+++ code/media/com_articles/js/article.js
@@ -1,3 +1,22 @@
+/**
+ * @version     $Id$
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Javascript
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Article View Javascript Behavior
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Javascript
+ */
+
 window.addEvent('domready', function() {
 	var elSections = document.id('article-form-sections');
 	var elCategories = document.id('article-form-categories');
--- code/media/com_articles/js/articles.js
+++ code/media/com_articles/js/articles.js
@@ -1,3 +1,22 @@
+/**
+ * @version     $Id$
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Javascript
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Articles View Javascript Behavior
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Javascript
+ */
+
 window.addEvent('domready', function() {
 	var elForm       = document.id('articles-form');
 	var elSections   = document.id('articles-form-section');
