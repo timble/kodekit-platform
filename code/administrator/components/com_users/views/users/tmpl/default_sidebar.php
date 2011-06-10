7d2cfa114d2325b462820fbe0a6a94a29308d106
Johan Janssens <johan@timble.net>
2011-06-10 21:04:29 +0000

    Added sidebar to the users view.

---
+++ code/administrator/components/com_users/views/users/tmpl/default_sidebar.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * @version     $Id$
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Users
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+defined('KOOWA') or die( 'Restricted access' ); ?>
+
+<div id="sidebar">
+	<h3><?= @text('Groups') ?></h3>
+</div>
