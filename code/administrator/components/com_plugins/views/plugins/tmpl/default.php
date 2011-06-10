a0cc757899ce5fbee5ea6c2bc61a2c85e504ab6d
Tom Janssens <tom@timble.net>
2011-06-10 15:53:32 +0000

    re #111: Moved published filter above the table and code cleanup

--- code/administrator/components/com_plugins/views/plugins/tmpl/default.php
+++ code/administrator/components/com_plugins/views/plugins/tmpl/default.php
@@ -12,74 +12,75 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 
 <?= @template('default_sidebar'); ?>
 
-<div class="-koowa-box-flex">
-    <form action="<?= @route() ?>" method="get" class="-koowa-grid">
-        <table class="adminlist">
-            <thead>
-            	<tr>
-            		<th width="20"></th>
-            		<th class="title">
-            		    <?= @helper('grid.sort', array('column' => 'name' , 'title' => 'Name')) ?>
-            		</th>
-            		<th nowrap="nowrap" width="5%">
-            			<?= @helper('grid.sort', array('column' => 'enabled' , 'title' => 'Enabled')) ?>
-            		</th>
-            		<th width="80" nowrap="nowrap">
-            			<?= @helper('grid.sort', array('column' => 'ordering' , 'title' => 'Order')) ?>
-            		</th>
-            		<th nowrap="nowrap" width="10%">
-            			<?= @helper('grid.sort', array('column' => 'access' , 'title' => 'Access')) ?>
-            		</th>
-            	</tr>
-            	<tr>
-            		<td align="center">
-            			<?= @helper( 'grid.checkall'); ?>
-            		</td>
-            		<td>
-            			<?= @helper( 'grid.search'); ?>
-            		</td>
-            		<td align="center">
-            			<?= @helper('listbox.enabled') ?>
-            		</td>
-            		<td colspan="2"></td>
-            	</tr>
-            </thead>
-            <tfoot>
-            	<? if ($plugins) : ?>
-            	<tr>
-            		<td colspan="5">
-            			<?= @helper('paginator.pagination', array('total' => $total)) ?>
-            		</td>
-            	</tr>
-            	<? endif ?>
-            </tfoot>
-            <tbody>
-            <? foreach ($plugins as $plugin) : ?>
-            	<tr>
-            		<td width="20" align="center">
-            			<?= @helper('grid.checkbox',array('row' => $plugin)) ?>
-            		</td>
-            		<td class="title">
-            		<? if(!$plugin->locked()) : ?>
-            			<a href="<?= @route('view=plugin&id='.$plugin->id) ?>">
-            				<?= @escape($plugin->title) ?>
-            			</a>
-            		<? else : ?>
-            			<?= @escape($plugin->title) ?>
-            		<? endif ?>
-            		</td>
-            		<td align="center" width="15px">
-            			<?= @helper('grid.enable', array('row' => $plugin)) ?>
-            		</td>
-            		<td class="order">
-            			<?= @helper('grid.order', array('row'=> $plugin))?>
-            		</td>
-            		<td align="center">
-            			<?= @helper('grid.access', array('row' => $plugin)) ?>
-            		</td>
-            	</tr>
-            <? endforeach ?>
-            </tbody>
-        </table>
-    </form>
-</div>
+<form action="<?= @route() ?>" method="get" class="-koowa-grid">
+    <?= @template('default_filter'); ?>
+    <table class="adminlist">
+        <thead>
+        	<tr>
+        		<th width="20"></th>
+        		<th class="title">
+        		    <?= @helper('grid.sort', array('column' => 'name' , 'title' => 'Name')) ?>
+        		</th>
+        		<th nowrap="nowrap" width="5%">
+        			<?= @helper('grid.sort', array('column' => 'enabled' , 'title' => 'Enabled')) ?>
+        		</th>
+        		<th width="80" nowrap="nowrap">
+        			<?= @helper('grid.sort', array('column' => 'ordering' , 'title' => 'Order')) ?>
+        		</th>
+        		<th nowrap="nowrap" width="10%">
+        			<?= @helper('grid.sort', array('column' => 'access' , 'title' => 'Access')) ?>
+        		</th>
+        	</tr>
+        	<tr>
+        		<td align="center">
+        			<?= @helper( 'grid.checkall'); ?>
+        		</td>
+        		<td>
+        			<?= @helper( 'grid.search'); ?>
+        		</td>
+        		<td colspan="3"></td>
+        	</tr>
+        </thead>
+        <tfoot>
+        	<tr>
+        		<td colspan="5">
+        			<?= @helper('paginator.pagination', array('total' => $total)) ?>
+        		</td>
+        	</tr>
+        </tfoot>
+        <tbody>
+        <? foreach ($plugins as $plugin) : ?>
+        	<tr>
+        		<td width="20" align="center">
+        			<?= @helper('grid.checkbox',array('row' => $plugin)) ?>
+        		</td>
+        		<td class="title">
+        		<? if(!$plugin->locked()) : ?>
+        			<a href="<?= @route('view=plugin&id='.$plugin->id) ?>">
+        				<?= @escape($plugin->title) ?>
+        			</a>
+        		<? else : ?>
+        			<?= @escape($plugin->title) ?>
+        		<? endif ?>
+        		</td>
+        		<td align="center" width="15px">
+        			<?= @helper('grid.enable', array('row' => $plugin)) ?>
+        		</td>
+        		<td class="order">
+        			<?= @helper('grid.order', array('row'=> $plugin))?>
+        		</td>
+        		<td align="center">
+        			<?= @helper('grid.access', array('row' => $plugin)) ?>
+        		</td>
+        	</tr>
+        <? endforeach ?>
+        <? if (!$total) : ?>
+        	<tr>
+                <td colspan="8" align="center">
+                     <?= @text('No Items Found'); ?>
+                </td>
+            </tr>
+        <? endif ?>
+        </tbody>
+    </table>
+</form>
