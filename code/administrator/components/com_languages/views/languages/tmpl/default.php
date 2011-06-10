18d046023c9d6d034206df022f8255ffb43647e8
Tom Janssens <tom@timble.net>
2011-06-10 15:56:39 +0000

    re #115: Markup cleanup

--- code/administrator/components/com_languages/views/languages/tmpl/default.php
+++ code/administrator/components/com_languages/views/languages/tmpl/default.php
@@ -15,70 +15,68 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 
 <?= @template('default_sidebar'); ?>
 
-<div class="-koowa-box-flex">
-    <form action="<?= @route('application='.$state->application)?>" method="get" class="-koowa-grid">
-        <table class="adminlist"  style="clear: both;">
-        	<thead>
-        		<tr>
-        			<th width="20">
-        			</th>
-        			<th width="25%" class="title">
-        				<?= @text('Name'); ?>
-        			</th>
-        			<th width="5%">
-        				<?= @text('Default'); ?>
-        			</th>
-        			<th width="10%">
-        				<?= @text('Version'); ?>
-        			</th>
-        			<th width="10%">
-        				<?= @text('Date'); ?>
-        			</th>
-        			<th width="20%">
-        				<?= @text('Author'); ?>
-        			</th>
-        			<th width="25%">
-        				<?= @text('Author Email'); ?>
-        			</th>
-        		</tr>
-        	</thead>
-        	<tbody>
-        		<? foreach ($languages as $language): ?>
-        		<tr>
-        			<td width="20" align="center">
-        				<input type="radio" name="language[]" class="-koowa-grid-checkbox" value="<?= $language->name; ?>"
-        					<?= $language->default ? 'checked="checked"' : ''; ?> />
-        			</td>
-        			<td width="25%">
-        				<?= $language->title; ?>
-        			</td>
-        			<td width="5%" align="center">
-        				<? if($language->default): ?>
-        					<img src="media://system/images/star.png" alt="<?= @text('Default'); ?>" />
-        				<? endif; ?>
-        			</td>
-        			<td align="center">
-        				<?= $language->version; ?>
-        			</td>
-        			<td align="center">
-        				<?= @date(array('date' => $language->creationDate, 'format' => '%d %B %Y')); ?>
-        			</td>
-        			<td align="center">
-        				<?= $language->author; ?>
-        			</td>
-        			<td align="center">
-        				<?= $language->authorEmail; ?>
-        			</td>
-        		</tr>
-        		<? endforeach; ?>
-        	</tbody>
-        	<tfoot>
-        		<tr>
-        			<td colspan="8">
-        				<?= @helper('paginator.pagination', array('total' => $total)) ?>
-        			</td>
-        		</tr>
-        	</tfoot>
-        </table>
-    </form>
-</div>
+<form action="<?= @route('application='.$state->application)?>" method="get" class="-koowa-grid">
+    <table class="adminlist"  style="clear: both;">
+    	<thead>
+    		<tr>
+    			<th width="20">
+    			</th>
+    			<th width="25%" class="title">
+    				<?= @text('Name'); ?>
+    			</th>
+    			<th width="5%">
+    				<?= @text('Default'); ?>
+    			</th>
+    			<th width="10%">
+    				<?= @text('Version'); ?>
+    			</th>
+    			<th width="10%">
+    				<?= @text('Date'); ?>
+    			</th>
+    			<th width="20%">
+    				<?= @text('Author'); ?>
+    			</th>
+    			<th width="25%">
+    				<?= @text('Author Email'); ?>
+    			</th>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		<? foreach ($languages as $language): ?>
+    		<tr>
+    			<td width="20" align="center">
+    				<input type="radio" name="language[]" class="-koowa-grid-checkbox" value="<?= $language->name; ?>"
+    					<?= $language->default ? 'checked="checked"' : ''; ?> />
+    			</td>
+    			<td width="25%">
+    				<?= $language->title; ?>
+    			</td>
+    			<td width="5%" align="center">
+    				<? if($language->default): ?>
+    					<img src="media://system/images/star.png" alt="<?= @text('Default'); ?>" />
+    				<? endif; ?>
+    			</td>
+    			<td align="center">
+    				<?= $language->version; ?>
+    			</td>
+    			<td align="center">
+    				<?= @date(array('date' => $language->creationDate, 'format' => '%d %B %Y')); ?>
+    			</td>
+    			<td align="center">
+    				<?= $language->author; ?>
+    			</td>
+    			<td align="center">
+    				<?= $language->authorEmail; ?>
+    			</td>
+    		</tr>
+    		<? endforeach; ?>
+    	</tbody>
+    	<tfoot>
+    		<tr>
+    			<td colspan="8">
+    				<?= @helper('paginator.pagination', array('total' => $total)) ?>
+    			</td>
+    		</tr>
+    	</tfoot>
+    </table>
+</form>
