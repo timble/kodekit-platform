d9616e78f71ac44c98b8937c1ecba5a9e899dbcf
Johan Janssens <johan@timble.net>
2011-06-10 21:05:17 +0000

    Moved sidebar into seperate template

---
+++ code/administrator/components/com_banners/views/banners/tmpl/default_sidebar.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @version     $Id: default.php 1708 2011-06-10 20:46:02Z johanjanssens $
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Banners
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+ 
+defined('KOOWA') or die('Restricted access'); ?>
+
+<?= @helper('behavior.modal') ?>
+
+<script src="media://lib_koowa/js/koowa.js" />
+<style src="media://lib_koowa/css/koowa.css" />
+
+<div id="sidebar">
+	<h3><?= @text('Categories') ?></h3>
+	<?= @template('admin::com.categories.view.categories.list', array('categories' => KFactory::tmp('admin::com.banners.model.categories')->getList())); ?>
+</div>
+
+<form action="<?=@route()?>" method="get" class="-koowa-grid">
+    <?= @template('default_filter'); ?>
+    <table class="adminlist">
+        <thead>
+            <tr>
+                <th width="2%"></th>
+                <th width="48%">
+                    <?= @helper('grid.sort', array('column' => 'name')); ?>
+                </th>
+                <th width="5%">
+                    <?= @helper('grid.sort', array('column' => 'showbanner', 'title' => 'published')); ?>
+                </th>
+                <th width="8%">
+                    <?= @helper('grid.sort', array('column' => 'ordering')); ?>
+                </th>
+                <th width="5%">
+                    <?= @helper('grid.sort', array('column' => 'sticky')); ?>
+                </th>
+                <th width="5%">
+                    <?= @helper('grid.sort', array('column' => 'clicks')); ?>
+                </th>
+                <th width="5%">
+                    <?= @text('Tags'); ?>
+                </th>
+            </tr>
+            <tr>
+                <td align="center">
+                    <?= @helper( 'grid.checkall'); ?>
+                </td>
+                <td>
+                    <?= @helper( 'grid.search'); ?>
+                </td>              
+                <td></td>                
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </thead>
+        
+        <tfoot>
+            <tr>
+                <td colspan="7">
+                <?= @helper('paginator.pagination', array('total' => $total)); ?>
+                </td>
+            </tr>
+        </tfoot>
+        
+        <tbody>
+            <? foreach ($banners as $banner) : ?>
+            <tr>
+                <td align="center">
+                    <?= @helper('grid.checkbox', array('row' => $banner))?>
+                </td>
+                <td align="left">
+                	<a href="<?= @route('view=banner&id='.$banner->id); ?>">
+                        <?=$banner->name?>
+                    </a>
+                </td>
+                <td align="center">
+                    <?= @helper('grid.enable', array('row' => $banner)) ?>
+                </td>
+                <td align="center">
+                    <?= @helper( 'grid.order' , array('row' => $banner, 'total' => $banner->order_total)); ?>
+                </td>
+                <td align="center">
+                    <?= $banner->sticky ? @text('Yes') : @text('No'); ?>
+                </td>
+                <td align="center">
+                    <?= $banner->clicks;?>
+                </td>
+                <td align="center">
+                    <?=$banner->tags?>
+                </td>
+            </tr>
+            <? endforeach; ?>
+        </tbody>
+    </table>
+</form>
