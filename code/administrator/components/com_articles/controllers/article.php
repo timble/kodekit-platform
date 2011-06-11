8b8e98a171bc25468383b018f48fcd94be547b4c
Johan Janssens <johan@timble.net>
2011-06-11 00:57:51 +0000

    re #124 : Implemented aliases for trash and restore. Should be moved into a controller behavior.

--- code/administrator/components/com_articles/controllers/article.php
+++ code/administrator/components/com_articles/controllers/article.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * @version     $Id$
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Articles Html View Class
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ */
+
 class ComArticlesControllerArticle extends ComDefaultControllerDefault
 {
     public function __construct(KConfig $config)
