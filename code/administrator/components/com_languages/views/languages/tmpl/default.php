e2ed833b2da3fe849ed4012a8521d201b66e3719
Johan Janssens <johan@timble.net>
2011-06-20 16:19:58 +0000

    Docblocks

--- code/administrator/components/com_languages/views/languages/tmpl/default.php
+++ code/administrator/components/com_languages/views/languages/tmpl/default.php
@@ -3,7 +3,7 @@
  * @version     $Id: form.php 1167 2011-05-11 15:37:22Z johanjanssens $
  * @category	Nooku
  * @package     Nooku_Server
- * @subpackage  Modules
+ * @subpackage  Languages
  * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
--- code/administrator/components/com_languages/views/languages/tmpl/default_sidebar.php
+++ code/administrator/components/com_languages/views/languages/tmpl/default_sidebar.php
@@ -3,7 +3,7 @@
  * @version     $Id$
  * @category	Nooku
  * @package     Nooku_Server
- * @subpackage  Modules
+ * @subpackage  Languages
  * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
