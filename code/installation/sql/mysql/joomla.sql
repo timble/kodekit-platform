5c1164fb56e7a3a52f2e4cbefe4b07a3661bbbf1
Johan Janssens <johan@timble.net>
2011-06-21 22:59:59 +0000

    Fixed typo

--- code/installation/sql/mysql/joomla.sql
+++ code/installation/sql/mysql/joomla.sql
@@ -647,6 +647,8 @@ CREATE TABLE `#__versions_revisions` (
   PRIMARY KEY  (`table`,`row`,`revision`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+# --------------------------------------------------------
+
 CREATE TABLE IF NOT EXISTS `#__files_paths` (
   `identifier` varchar(255) NOT NULL,
   `path` varchar(255) NOT NULL,
@@ -655,5 +657,4 @@ CREATE TABLE IF NOT EXISTS `#__files_paths` (
 ) DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `__files_paths` (`identifier`, `path`, `parameters`) VALUES
-('files.files', 'images', '{"upload_extensions":"bmp,csv,doc,gif,ico,jpg,jpeg,odg,odp,ods,odt,pdf,png,ppt,swf,txt,xcf,xls","upload_maxsize":"10485760","allowed_mimetypes":"image\\/jpeg,image\\/gif,image\\/png,imagee\\/bmp,application\\/x-shockwave-flash,application\\/msword,application\\/excel,application\\/pdf,application\\/powerpoint,text\\/plain,application\\/x-zip","illegal_mimetypes":"text\\/html","restrict_uploads":1,"check_mime":1,"allowed_media_usergroup":3}');
+INSERT INTO '#__files_paths` (`identifier`, `path`, `parameters`) VALUES ('files.files', 'images', '{"upload_extensions":"bmp,csv,doc,gif,ico,jpg,jpeg,odg,odp,ods,odt,pdf,png,ppt,swf,txt,xcf,xls","upload_maxsize":"10485760","allowed_mimetypes":"image\\/jpeg,image\\/gif,image\\/png,imagee\\/bmp,application\\/x-shockwave-flash,application\\/msword,application\\/excel,application\\/pdf,application\\/powerpoint,text\\/plain,application\\/x-zip","illegal_mimetypes":"text\\/html","restrict_uploads":1,"check_mime":1,"allowed_media_usergroup":3}');
