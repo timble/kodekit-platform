33171f6c5b3804545540a28db139e4e93dbdd0fa
Johan Janssens <johan@timble.net>
2011-06-21 02:15:25 +0000

    Fixed typo

--- code/administrator/components/com_installer/views/components/tmpl/default_item.php
+++ code/administrator/components/com_installer/views/components/tmpl/default_item.php
@@ -12,7 +12,9 @@ defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
 		<?php if (!$this->item->option) : ?>
 		<strong>X</strong>
 		<?php else : ?>
-		<a href="index.php?option=com_installer&amp;type=components&amp;task=<?php echo $this->item->task; ?>&amp;eid[]=<?php echo $this->item->id; ?>&amp;limitstart=<?php echo $this->pagination->limitstart; ?>&amp;<?php echo JUtility::getToken();?>=1"><img src="images/<?php echo $this->item->img; ?>" border="0" title="<?php echo $this->item->action; ?>" alt="<?php echo $this->item->alt; ?>" /></a>
+		<a href="index.php?option=com_installer&amp;type=components&amp;task=<?php echo $this->item->task; ?>&amp;eid[]=<?php echo $this->item->id; ?>&amp;limitstart=<?php echo $this->pagination->limitstart; ?>&amp;<?php echo JUtility::getToken();?>=1">
+			<img src="<?php echo JURI::root(true) ?>/media/system/images/<?php echo $this->item->img; ?>" border="0" title="<?php echo $this->item->action; ?>" alt="<?php echo $this->item->alt; ?>" />
+		</a>
 		<?php endif; ?>
 	</td>
 	<td align="center"><?php echo @$this->item->version != '' ? $this->item->version : '&nbsp;'; ?></td>
