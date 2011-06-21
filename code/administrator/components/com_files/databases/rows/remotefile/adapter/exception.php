d1bcdfb9eab4f532772016fde30db9c63eef3220
Ercan Ozkaya <ercan@timble.net>
2011-06-21 22:39:22 +0000

    re #108: Docblocks

--- code/administrator/components/com_files/databases/rows/remotefile/adapter/exception.php
+++ code/administrator/components/com_files/databases/rows/remotefile/adapter/exception.php
@@ -1,3 +1,20 @@
 <?php
+/**
+ * @version     $Id: config.php 1845 2011-06-21 22:22:23Z ercanozkaya $
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Files
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
 
+/**
+ * Specialized exception for remote file adapters
+ *
+ * @author      Ercan Ozkaya <http://nooku.assembla.com/profile/ercanozkaya>
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Files
+ */
 class ComFilesDatabaseRowRemotefileAdapterException extends KException {}
