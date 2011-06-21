dd06d643c7e8b768ee91e4867928de65c8e032bf
Ercan Ozkaya <ercan@timble.net>
2011-06-21 22:25:16 +0000

    re #108: Changed table definition and default values in installation file

--- code/administrator/components/com_files/install/install.sql
+++ code/administrator/components/com_files/install/install.sql
@@ -1,6 +1,10 @@
 CREATE TABLE IF NOT EXISTS `#__files_paths` (
   `identifier` varchar(255) NOT NULL,
   `path` varchar(255) NOT NULL,
-  `parameters` varchar(1024) NOT NULL,
-  UNIQUE KEY (`identifier`)
+  `parameters` TEXT,
+  PRIMARY KEY (`identifier`)
 ) DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `jos_files_paths` (`identifier`, `path`, `parameters`) VALUES
+('files.files', 'images', '{"upload_extensions":"bmp,csv,doc,gif,ico,jpg,jpeg,odg,odp,ods,odt,pdf,png,ppt,swf,txt,xcf,xls","upload_maxsize":"10485760","allowed_mimetypes":"image\\/jpeg,image\\/gif,image\\/png,imagee\\/bmp,application\\/x-shockwave-flash,application\\/msword,application\\/excel,application\\/pdf,application\\/powerpoint,text\\/plain,application\\/x-zip","illegal_mimetypes":"text\\/html","restrict_uploads":1,"check_mime":1,"allowed_media_usergroup":3}');
