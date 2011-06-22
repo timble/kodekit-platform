1e3ff489bbc208f40e037d33da01c45a9441ca23
Ercan Ozkaya <ercan@timble.net>
2011-06-22 14:06:14 +0000

    re #108: Fix uploader tab in images views/files/tmpl/compact.php

--- code/administrator/components/com_files/controllers/image.php
+++ code/administrator/components/com_files/controllers/image.php
@@ -10,22 +10,35 @@
  */
 
 /**
- * Image Controller Class 
+ * Image Controller Class
  *
  * @author      Ercan Ozkaya <http://nooku.assembla.com/profile/ercanozkaya>
  * @category	Nooku
  * @package     Nooku_Server
- * @subpackage  Files   
+ * @subpackage  Files
  */
 
 class ComFilesControllerImage extends ComDefaultControllerResource
 {
-	protected function _initalize(KConfig $config)
+	protected function _initialize(KConfig $config)
 	{
 		$config->append(array(
-			'persistent' => false
+			'persistent' => false,
+			'request' => array(
+				'identifier' => 'files.files'
+			)
 		));
+
 		parent::_initialize($config);
+	}
+
+	public function setRequest(array $request)
+	{
+		$config = KFactory::get('admin::com.files.database.row.config');
+		$row = KFactory::tmp('admin::com.files.model.paths')->identifier($request['identifier'])->getItem();
+		$config->setData(json_decode($row->parameters, true));
+
+		return parent::setRequest($request);
 	}
 
 	public function getView()
