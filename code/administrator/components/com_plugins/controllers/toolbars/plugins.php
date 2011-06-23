9b36c2d6ffe95a0857c9655326628ef3bcfa87af
Johan Janssens <johan@timble.net>
2011-06-23 18:59:19 +0000

    Added specialised plugin toolbar.

---
+++ code/administrator/components/com_plugins/controllers/toolbars/plugins.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @version     $Id$
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Plugins
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Plugins Toolbar Class
+ *
+ * @author      Stian Didriksen <http://nooku.assembla.com/profile/stiandidriksen>
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Plugins    
+ */
+class ComPluginsControllerToolbarPlugins extends ComDefaultControllerToolbarDefault
+{
+    public function getCommands()
+    {
+        $this->reset()
+             ->addCommand('enable')
+	    	 ->addCommand('disable');
+	    	 
+	    return parent::getCommands();
+    }
+}
