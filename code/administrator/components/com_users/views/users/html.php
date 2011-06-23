f942b64c76cab6f0fb5a076270eddd1bc5c31265
Ercan Ozkaya <ercan@timble.net>
2011-06-23 19:56:19 +0000

    re #87: Added groups filter to the users view

---
+++ code/administrator/components/com_users/views/users/html.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version     $Id: sessions.php 862 2011-04-08 01:34:13Z johanjanssens $
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Users
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Users View Class
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Users
+ */
+class ComUsersViewUsersHtml extends ComDefaultViewHtml
+{
+	public function display()
+	{
+		$this->assign('groups', KFactory::get('admin::com.users.model.groups')->getTree());
+
+		return parent::display();
+	}
+}
