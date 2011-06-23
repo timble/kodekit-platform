f24dc949f9070355a3c72b754be3a39752c687d4
Johan Janssens <johan@timble.net>
2011-06-23 18:54:50 +0000

    Added @toolbar() to explicitly include the toolbar.

--- code/administrator/components/com_languages/views/languages/tmpl/default.php
+++ code/administrator/components/com_languages/views/languages/tmpl/default.php
@@ -13,6 +13,8 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 <script src="media://lib_koowa/js/koowa.js" />
 <style src="media://lib_koowa/css/koowa.css" />
 
+<?= @toolbar(); ?>
+
 <?= @template('default_sidebar'); ?>
 
 <form action="<?= @route('application='.$state->application)?>" method="get" class="-koowa-grid">
