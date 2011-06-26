fdace7edf6034bc7e64ac238dc4b9f577500ca1e
Johan Janssens <johan@timble.net>
2011-06-26 16:27:48 +0000

    Removed @toolbar() helper. Toolbar rendering is now being done through the commandable behavior.

--- code/administrator/components/com_plugins/views/plugins/tmpl/default.php
+++ code/administrator/components/com_plugins/views/plugins/tmpl/default.php
@@ -10,8 +10,6 @@
  */
 defined('KOOWA') or die( 'Restricted access' ); ?>
 
-<?= @toolbar(); ?>
-
 <?= @template('default_sidebar'); ?>
 
 <form action="<?= @route() ?>" method="get" class="-koowa-grid">
