12271acb741d7ca2fe25bbf27c6b7f5e59c45f19
Johan Janssens <johan@timble.net>
2011-06-26 16:50:27 +0000

    Added menubar object to handle rendering of the component menubar.

---
+++ code/administrator/components/com_cache/controllers/toolbars/menubar.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version     $Id$
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Cache
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Cache Menubar Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Cache
+ */
+class ComCacheControllerToolbarMenubar extends ComDefaultControllerToolbarMenubar
+{
+    public function getCommands()
+    { 
+        $this->addCommand('Groups', array(
+        	'href' => JRoute::_('index.php?option=com_cache&view=groups'),
+        	'active' => true 
+        )));
+        
+        $this->addCommand('Keys', array(
+        	'href' => JRoute::_('index.php?option=com_cache&view=keys'),
+        ));
+         
+        return parent::getCommands();
+    }
+}
