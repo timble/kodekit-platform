269358b5aae690d825910b1d4167c9944c11aba1
Johan Janssens <johan@timble.net>
2011-06-26 16:54:49 +0000

    Added menubar object to handle rendering of the component menubar.

---
+++ code/administrator/components/com_weblinks/controllers/toolbars/menubar.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @version		$Id$
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Weblinks
+ * @copyright	Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+
+/**
+ * Weblinks Menubar Class
+ *
+ * @author    	Jeremy Wilken <http://nooku.assembla.com/profile/gnomeontherun>
+ * @category 	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Weblinks
+ */
+class ComWeblinksControllerToolbarMenubar extends ComDefaultControllerToolbarMenubar
+{
+    public function getCommands()
+    { 
+        $name = $this->getController()->getIdentifier()->name;
+        
+        $this->addCommand('Newsfeeds', array(
+        	'href'   => JRoute::_('index.php?option=com_weblinks&view=weblinks'),
+        	'active' => ($name == 'banner')
+        ));
+        
+        $this->addCommand('Categories', array(
+        	'href' => JRoute::_('index.php?option=com_weblinks&view=categories'),
+            'active' => ($name == 'category')
+        ));
+         
+        return parent::getCommands();
+    }
+}
