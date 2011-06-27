ea08c272d0e279b671513dbad4d1d3e50eb48121
Stian Didriksen <stian@timble.net>
2011-06-27 21:54:43 +0000

    re #108 reverting fix, introduced regression

--- code/administrator/components/com_files/models/paths.php
+++ code/administrator/components/com_files/models/paths.php
@@ -20,10 +20,5 @@
 
 class ComFilesModelPaths extends ComDefaultModelDefault
 {
-    public function __construct(KConfig $config)
-	{
-		parent::__construct($config);
 
-		$this->_state->insert('identifier', 'identifier', null);
-	}
 }
