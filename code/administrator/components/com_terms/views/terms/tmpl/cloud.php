78949b61321ecb4200a0f51ca37cec68eb0cea44
Johan Janssens <johan@timble.net>
2011-06-28 11:56:41 +0000

    re #14 : Changed to use more neutral term 'items'.

--- code/administrator/components/com_terms/views/terms/tmpl/cloud.php
+++ code/administrator/components/com_terms/views/terms/tmpl/cloud.php
@@ -7,7 +7,7 @@
 	<? foreach ($terms as $term) : ?>
 	<? list($component, $view) = explode("_", $term->table) ?>
 	<li>
-		<span><?= $term->count ?> <?= @text('articles are tagged with') ?></span>
+		<span><?= $term->count ?> <?= @text('items are tagged with') ?></span>
 		<a  href="<?= @route('option=com_'.$component.'&view='.$view.'&tag='.$term->slug) ?>" class="weight<?= round($term->count/$total + 1) ?>" ><?= $term->title; ?></a>
 	</li>
 	<? endforeach; ?>
