ebde7241a3f902d418740058d12b029e9aca3dd1
Israel Canasa <israel@timble.net>
2011-06-30 18:13:05 +0000

    re #15 - Used execute() method instead of registerCallback in logging events.
    Added getHandle() method so that the behavior is enqueued.
    
    Don't put actions in the _initialize. Check in the construct if defaults should be used.

--- code/administrator/components/com_logs/controllers/behaviors/loggable.php
+++ code/administrator/components/com_logs/controllers/behaviors/loggable.php
@@ -4,32 +4,39 @@
 class ComLogsControllerBehaviorLoggable extends KControllerBehaviorAbstract
 {
     protected $_title_column = '';
+    protected $_actions;
     
     public function __construct(KConfig $config)
     { 
         parent::__construct($config);
         
-        $this->registerCallback($config->actions->toArray(), array($this, 'logAction'));
-        
         $this->_title_column = $config->title_column;
+        
+        $this->_actions = $config->actions->toArray();
+        if (empty($this->_actions)) {
+            $this->_actions = array('after.edit', 'after.add', 'after.delete');
+        }
     }
     
     protected function _initialize(KConfig $config)
     {
         $config->append(array(
             'priority'   => KCommand::PRIORITY_LOW,
-            'actions' => array('after.edit', 'after.add', 'after.delete'),
+            'actions' => array(),
             'title_column' => 'title',
         ));
 
         parent::_initialize($config);
     }
     
-    public function logAction(KCommandContext $context)
+    public function execute($name, KCommandContext $context) 
     {
+        if(!in_array($name, $this->_actions))
+            return;
+        
         $identifier = $context->caller->getIdentifier();
-				
-		$data = array(
+
+        $data = array(
             'action' => $context->action,
             'application' => $identifier->application,
             'type' => $identifier->type,
@@ -37,33 +44,40 @@ class ComLogsControllerBehaviorLoggable extends KControllerBehaviorAbstract
             'name' => $identifier->name,
         );
 
-		$rowset = array();
-		if ($context->result instanceof KDatabaseRowAbstract) {
-			$rowset[] =  $context->result;
-		} else {
-			$rowset = KConfig::toData($context->result);
-		}
+        $rowset = array();
+        if ($context->result instanceof KDatabaseRowAbstract) {
+            $rowset[] =  $context->result;
+        } else {
+            $rowset = KConfig::toData($context->result);
+        }
 
-		foreach ($rowset as $row)
-		{
-			//Only log if the row status is valid.
-			$status = $row->getStatus();
-			
-			if(!empty($status))
-			{
-				if ($row->{$this->_title_column}) {
-					$data['title'] = $row->{$this->_title_column};
-				} else {
-					$data['title'] = '#'.$row->id;
-				}
+        foreach ($rowset as $row)
+        {
+            //Only log if the row status is valid.
+            $status = $row->getStatus();
 
-				$data['row_id'] = $row->id;
-				
-				KFactory::tmp('admin::com.logs.model.logs')
-					->getItem()
-					->setData($data)
-					->save();
-			}
-		}
+            if(!empty($status))
+            {
+                if ($row->{$this->_title_column}) {
+                    $data['title'] = $row->{$this->_title_column};
+                } else {
+                    $data['title'] = '#'.$row->id;
+                }
+
+                $data['row_id'] = $row->id;
+
+                KFactory::tmp('admin::com.logs.model.logs')
+                    ->getItem()
+                    ->setData($data)
+                    ->save();
+            }
+        }
+        
+        return true;
+    }
+    
+    public function getHandle()
+    {
+        return KMixinAbstract::getHandle();
     }
 }
