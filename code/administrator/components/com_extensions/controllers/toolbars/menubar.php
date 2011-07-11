c018838066be8042b8b50ab48f7829ae0fac8b24
Johan Janssens <johan@timble.net>
2011-07-11 15:46:29 +0000

    re #141 : Fixed menubar

--- code/administrator/components/com_extensions/controllers/toolbars/menubar.php
+++ code/administrator/components/com_extensions/controllers/toolbars/menubar.php
@@ -3,7 +3,7 @@
  * @version     $Id$
  * @category	Nooku
  * @package     Nooku_Server
- * @subpackage  Modules
+ * @subpackage  Extensions
  * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
@@ -15,7 +15,7 @@
  * @author      Stian Didriksen <http://nooku.assembla.com/profile/stiandidriksen>
  * @category	Nooku
  * @package     Nooku_Server
- * @subpackage  Modules
+ * @subpackage  Extensions
  */
 class ComExtensionsControllerToolbarMenubar extends ComDefaultControllerToolbarMenubar
 {
