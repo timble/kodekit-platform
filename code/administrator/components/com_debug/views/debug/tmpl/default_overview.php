18482a17d534ab15f67cf36ed409340ac1446192
Johan Janssens <johan@timble.net>
2011-07-13 20:41:14 +0000

    re #16 : Seperate default layout in different templates.

---
+++ code/administrator/components/com_debug/views/debug/tmpl/default_overview.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @version     $Id$
+ * @category	Nooku
+ * @package     Nooku_Components
+ * @subpackage  Debug
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+defined('KOOWA') or die( 'Restricted access' ); ?>
+
+<h4><?= @text('Memory Usage' ) ?></h4>
+<?= JProfiler::getInstance('Application')->getMemory(); ?>
