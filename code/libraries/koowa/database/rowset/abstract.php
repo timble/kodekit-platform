13bea72b4401e4c8f2a697a9af566bac69e52bb9
Johan Janssens <johan@timble.net>
2011-07-17 13:56:13 +0000

    Revered rev 3697

--- code/libraries/koowa/database/rowset/abstract.php
+++ code/libraries/koowa/database/rowset/abstract.php
@@ -227,6 +227,37 @@ abstract class KDatabaseRowsetAbstract extends KObjectSet implements KDatabaseRo
         
         return $this;
     }
+    
+	/**
+     * Retrieve an array of column values
+     *
+     * @param   string  The column name.
+     * @return  array   An array of all the column values
+     */
+    public function getColumn($column)
+    {
+        $result = array();
+        foreach($this as $key => $row) {
+            $result[$key] = $row->$column;        
+        }
+
+        return $result;
+    }
+
+    /**
+     * Set the value of all the columns
+     *
+     * @param   string  The column name.
+     * @param   mixed   The value for the property.
+     * @return  void
+     */
+    public function setColumn($column, $value)
+    {
+        //Set the data
+        foreach($this as $row) {
+            $row->$column = $value;
+        }
+    }
    
     /**
      * Gets the identitiy column of the rowset
--- code/libraries/koowa/database/rowset/table.php
+++ code/libraries/koowa/database/rowset/table.php
@@ -153,37 +153,6 @@ class KDatabaseRowsetTable extends KDatabaseRowsetAbstract
 	}
 	
 	/**
-     * Retrieve an array of column values
-     *
-     * @param   string  The column name.
-     * @return  array   An array of all the column values
-     */
-    public function getColumn($column)
-    {
-        $result = array();
-        foreach($this as $key => $row) {
-            $result[$key] = $row->$column;        
-        }
-
-        return $result;
-    }
-
-    /**
-     * Set the value of all the columns
-     *
-     * @param   string  The column name.
-     * @param   mixed   The value for the property.
-     * @return  void
-     */
-    public function setColumn($column, $value)
-    {
-        //Set the data
-        foreach($this as $row) {
-            $row->$column = $value;
-        }
-    }
-
-	/**
 	 * Forward the call to each row
 	 * 
 	 * This functions overloads KDatabaseRowsetAbstract::__call and implements 
