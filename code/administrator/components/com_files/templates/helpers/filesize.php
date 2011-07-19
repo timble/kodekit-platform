79ac6276c018fd9127d5d8314225236d01cad08a
Johan Janssens <johan@timble.net>
2011-07-19 11:55:07 +0000

    Added size config option.

--- code/administrator/components/com_files/templates/helpers/filesize.php
+++ code/administrator/components/com_files/templates/helpers/filesize.php
@@ -24,18 +24,22 @@ class ComFilesTemplateHelperFilesize extends KTemplateHelperAbstract
 	{
 		$config = new KConfig($config);
 		$config->append(array(
+		    'size'  => 0,
 			'sizes' => array('Bytes', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb')
 		));
+		
 		$bytes = $config->size;
 		$result = '';
 		$format = (($bytes > 1024*1024 && $bytes % 1024 !== 0) ? '%.2f' : '%d').' %s';
 
-		foreach ($config->sizes as $s) {
+		foreach ($config->sizes as $s) 
+		{
 			$size = $s;
 			if ($bytes < 1024) {
 				$result = $bytes;
 				break;
 			}
+			
 			$bytes /= 1024;
 		}
 
