420b3799c63a1e2f6b94c19230693c7dd56f340a
Johan Janssens <johan@timble.net>
2011-07-22 12:30:24 +0000

    Retrieve and render the modules through the Nooku API's.

--- code/administrator/components/com_cpanel/admin.cpanel.html.php
+++ code/administrator/components/com_cpanel/admin.cpanel.html.php
@@ -29,17 +29,16 @@ class HTML_cpanel
 	*/
 	function display()
 	{
-		global $mainframe;
-
-		$modules = &JModuleHelper::getModules('cpanel');
-        // TODO: allowAllClose should default true in J!1.6, so remove the array when it does.
-		$pane = &JPane::getInstance('sliders', array('allowAllClose' => true));
+		$modules = KFactory::tmp('admin::com.extensions.model.modules')->application('administrator')->position('cpanel')->enabled(1)->getList();
+		
+		$pane = JPane::getInstance('sliders', array('allowAllClose' => true));
 		echo $pane->startPane("content-pane");
 
-		foreach ($modules as $module) {
+		foreach ($modules as $module) 
+		{
 			$title = $module->title ;
 			echo $pane->startPanel( $title, 'cpanel-panel-'.$module->name );
-			echo JModuleHelper::renderModule($module);
+			echo KFactory::tmp('admin::mod.'.substr($module->type, 4).'.html')->module($module)->params($module->params)->display();
 			echo $pane->endPanel();
 		}
 
