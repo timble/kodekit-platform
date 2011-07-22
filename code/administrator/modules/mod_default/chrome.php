a06b0974e7515dfcf0ebf68bc5680024fd64997e
Johan Janssens <johan@timble.net>
2011-07-22 12:16:33 +0000

    re #146 : Added chrome filter to render the module chrome. The chrome is rendered based on the new ModDefaultHtml 'styles' view data property.

---
+++ code/administrator/modules/mod_default/chrome.php
@@ -0,0 +1,95 @@
+<?php
+/**
+* @version      $Id$
+* @category		Koowa
+* @package      Koowa_Template
+* @subpackage	Filter
+* @copyright    Copyright (C) 2007 - 2010 Johan Janssens. All rights reserved.
+* @license      GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+* @link 		http://www.nooku.org
+*/
+
+/**
+ * Module Chrome Filter 
+ *
+ * @author		Johan Janssens <johan@nooku.org>
+ * @category	Koowa
+ * @package     Koowa_Template
+ * @subpackage	Filter
+ */
+class ModDefaultChrome extends KTemplateFilterAbstract implements KTemplateFilterWrite
+{
+  	/**
+	 * Is the chrome loaded
+	 *
+	 * @var boolean
+	 */
+	protected static $_loaded = false;
+  	
+  	/**
+     * Constructor.
+     *
+     * @param   object  An optional KConfig object with configuration options
+     */
+    public function __construct( KConfig $config = null) 
+    { 
+        parent::__construct($config);
+        
+        //Load the theme chrome functions
+        if(!self::$_loaded)
+        {
+            KLoader::load(JPATH_THEMES.'/system/html/modules.php');
+		    KLoader::load(JPATH_THEMES.'/'.$config->template.'/html/modules.php');
+        }
+    }
+	
+	/**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param   object  An optional KConfig object with configuration options
+     * @return void
+     */
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'priority'   => KCommand::PRIORITY_LOW,
+            'template'   => KFactory::get('lib.joomla.application')->getTemplate()
+        ));
+
+        parent::_initialize($config);
+    }
+    
+	/**
+	 * Render the module chrome
+	 *
+	 * @param string Block of text to parse
+	 * @return ModDefaultFilterChrome
+	 */
+	public function write(&$text)
+	{
+		$data = (object) $this->getTemplate()->getData();
+		
+	    foreach($data->styles as $style)
+		{
+            $method = 'modChrome_'.$style;
+                
+			// Apply chrome and render module
+		    if (function_exists($method))
+			{
+		        $data->module->style   = implode(' ', $data->styles);
+		        $data->module->content = $text;
+		       
+				ob_start();
+				    $method($data->module, $data->module->params, $data->attribs);
+				    $data->module->content = ob_get_contents();
+				ob_end_clean();
+			}
+              
+            $text = $data->module->content;
+	    }
+	    
+	    return $this;
+    }
+}
