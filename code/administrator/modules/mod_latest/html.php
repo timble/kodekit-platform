30423bffa6e6551068eea4f2b6894d0e10a6a397
Johan Janssens <johan@timble.net>
2011-07-22 12:22:49 +0000

    re #155 : Refactored admin::mod.latest

--- code/administrator/modules/mod_latest/html.php
+++ code/administrator/modules/mod_latest/html.php
@@ -1,24 +1,28 @@
 <?php
 /**
- * @version     $Id: mod_banners.php 2176 2011-07-12 14:08:26Z johanjanssens $
+ * @version     $Id$
  * @category    Nooku
  * @package     Nooku_Server
- * @subpackage  Articles
+ * @subpackage  Users
  * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
 
 /**
- * Module Users
+ * Articles Module Html View Class
  *
  * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @category    Nooku
  * @package     Nooku_Server
  * @subpackage  Articles
  */
-
-echo KFactory::tmp('admin::mod.latest.html')
-    	->assign('module' , $module)
-    	->assign('attribs', $attribs)
-    	->display();
+ 
+class ModLatestHtml extends ModDefaultHtml
+{
+    public function display()
+    { 
+        $this->articles = KFactory::tmp('admin::com.articles.model.articles')->sort('created')->direction('desc')->limit(10)->getList(); 
+        return parent::display();
+    }
+} 
