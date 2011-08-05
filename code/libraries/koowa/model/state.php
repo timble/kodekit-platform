c3cb41d042e5d50927520d078bcbf395f914b088
Johan Janssens <johan@timble.net>
2011-08-05 01:02:10 +0000

    Fixed : isUnique() returns wrong result when using related states.

--- code/libraries/koowa/model/state.php
+++ code/libraries/koowa/model/state.php
@@ -182,14 +182,14 @@ class KModelState extends KModelAbstract
                 if($unique) 
                  {
                     //Unique values cannot be null or an empty string
-                    if($state->unique && (!empty($state->value) || is_numeric($state->value))) 
+                    if($state->unique && (!empty($state->value) || is_numeric($state->value) || !(is_array($state->value) && count($state->value) > 1))) 
                     {
                         $result = true;
                         
                         //Check related states to see if they are set
                         foreach($state->required as $required)
                         {
-                            if(empty($this->_state[$required]->value) && !is_numeric($state->value)) 
+                            if(empty($this->_state[$required]->value) && !is_numeric($this->_state[$required]->value)) 
                             {
                                 $result = false;
                                 break;
@@ -221,27 +221,8 @@ class KModelState extends KModelAbstract
      */
     public function isUnique()
     {
-        $unique = false;
-        
-        //Get the unique states
         $states = $this->getData(true);
-        
-        if(!empty($states)) 
-        {
-            $unique = true;
-            
-            //If a state contains multiple values the state is not unique
-            foreach($states as $state) 
-            {
-                if(is_array($state) && count($state) > 1) 
-                {
-                    $unique = false;
-                    break;
-                }
-            }
-        }
-        
-        return $unique;
+        return !empty($states);
     }
     
     /**
