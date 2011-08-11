f87a05d2d51b52eed26ad51a1bf882422d63818c
Johan Janssens <johan@timble.net>
2011-08-11 01:02:52 +0000

    Re-moved KModelPaginator. Moved functionality into KTemplateHelperPaginatior

--- code/administrator/components/com_default/templates/helpers/paginator.php
+++ code/administrator/components/com_default/templates/helpers/paginator.php
@@ -38,22 +38,13 @@ class ComDefaultTemplateHelperPaginator extends KTemplateHelperPaginator
             'limit'   => 0,
         ));
         
-        // Paginator object
-        $paginator = KFactory::tmp('lib.koowa.model.paginator')->setData(
-                array('total'  => $config->total,
-                      'offset' => $config->offset,
-                      'limit'  => $config->limit,
-                      'display' => $config->display)
-        );
-                
-        // Get the paginator data
-        $list = $paginator->getList();
+        $this->_initialize($config);
         
         $html  = '<div class="container">';
         $html  = '<div class="pagination">';
-        $html .= '<div class="limit">'.JText::_('Display NUM').' '.$this->limit($config->toArray()).'</div>';
-        $html .=  $this->_pages($list);
-        $html .= '<div class="limit"> '.JText::_('Page').' '.$paginator->current.' '.JText::_('of').' '.$paginator->count.'</div>';
+        $html .= '<div class="limit">'.JText::_('Display NUM').' '.$this->limit($config).'</div>';
+        $html .=  $this->_pages($this->_items($config));
+        $html .= '<div class="limit"> '.JText::_('Page').' '.$config->current.' '.JText::_('of').' '.$config->count.'</div>';
         $html .= '</div>';
         $html .= '</div>';
         
