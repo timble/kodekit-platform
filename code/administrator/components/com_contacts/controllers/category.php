26dbcd5af27f469a056000ad7d7ba9805c8b9bfd
Johan Janssens <johan@timble.net>
2011-08-19 22:06:39 +0000

    Extend from new ComCategoriesControllerCategory controller.

--- code/administrator/components/com_contacts/controllers/category.php
+++ code/administrator/components/com_contacts/controllers/category.php
@@ -17,41 +17,8 @@
  * @package     Nooku_Server
  * @subpackage  Contacts    
  */
-class ComContactsControllerCategory extends ComDefaultControllerDefault
-{ 
-    protected function _actionGet(KCommandContext $context)
-    {
-        $view = $this->getView();
-        
-	    //Set the layout
-        if($view instanceof KViewTemplate) 
-	    {
-	        $layout = clone $view->getIdentifier();
-	        $layout->package  = 'categories';
-	        $layout->name     = $view->getLayout();
-	        
-	        //Force re-creation of the filepath to load the category templates
-	        $layout->filepath = ''; 
- 
-	        $view->setLayout($layout);
-	    }
-	    
-	    //Set the toolbar
-	    if($this->isCommandable()) {
-	        $this->setToolbar('admin::com.categories.controller.toolbar.'.$view->getName());
-	    }
-	    
-        return parent::_actionGet($context);
-    }
-    
-    public function setModel($model)
-    {
-        $model = parent::setModel($model);
-        $model->package = 'categories';
-        
-        return $model; 
-    }
-     
+class ComContactsControllerCategory extends ComCategoriesControllerCategory
+{    
 	public function getRequest()
 	{
 		$this->_request['section'] = 'com_contact_details';
