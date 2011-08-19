a43ab9055a260a08ff3eac9959940975e321d8d6
Johan Janssens <johan@timble.net>
2011-08-19 22:06:12 +0000

    Extend from new ComCategoriesControllerCategory controller.

--- code/administrator/components/com_articles/controllers/category.php
+++ code/administrator/components/com_articles/controllers/category.php
@@ -17,42 +17,9 @@
  * @package     Nooku_Server
  * @subpackage  Articles
  */
-class ComArticlesControllerCategory extends ComDefaultControllerDefault
+class ComArticlesControllerCategory extends ComCategoriesControllerCategory
 { 
-    protected function _actionGet(KCommandContext $context)
-    {
-        $view = $this->getView();
-        
-	    //Set the layout
-        if($view instanceof KViewTemplate) 
-	    {
-	        $layout = clone $view->getIdentifier();
-	        $layout->package  = 'categories';
-	        $layout->name     = $view->getLayout();
-	        
-	        //Force re-creation of the filepath to load the category templates
-	        $layout->filepath = ''; 
- 
-	        $view->setLayout($layout);
-	    }
-	    
-	    //Set the toolbar
-	    if($this->isCommandable()) {
-	        $this->setToolbar('admin::com.categories.controller.toolbar.'.$view->getName());
-	    }
-	    
-        return parent::_actionGet($context);
-    }
-    
-    public function setModel($model)
-    {
-        $model = parent::setModel($model);
-        $model->package = 'categories';
-        
-        return $model; 
-    }
-     
-	public function getRequest()
+    public function getRequest()
 	{
 		$this->_request['section'] = 'com_content';
 	    return $this->_request;
