cc12ec22f997818af1bff704bf4f06699e8ba07f
Johan Janssens <johan@timble.net>
2011-08-23 16:15:49 +0000

    Fixed : Filters are added multiple times to KTemplate command chain

--- code/libraries/koowa/view/template.php
+++ code/libraries/koowa/view/template.php
@@ -293,7 +293,7 @@ abstract class KViewTemplate extends KViewAbstract
             	'view' => $this
             );
             
-            $this->_template = KFactory::get($this->_template, $options);
+            $this->_template = KFactory::tmp($this->_template, $options);
         }
         
         return $this->_template;
