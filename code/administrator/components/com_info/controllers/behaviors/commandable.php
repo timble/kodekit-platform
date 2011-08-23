4ebc3517e60091986a90dc0ed48fb61399b1b9c5
Johan Janssens <johan@timble.net>
2011-08-23 20:39:53 +0000

    re #125 : Added specialised commandable behavior to turn off rendering of the toolbar and title.

---
+++ code/administrator/components/com_info/controllers/behaviors/commandable.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @version     $Id$
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Info
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Commandable Controller Behavior Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Info
+ */
+
+class ComInfoControllerBehaviorCommandable  extends ComDefaultControllerBehaviorCommandable
+{  
+	/**
+     * Initializes the default configuration for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param 	object 	An optional KConfig object with configuration options.
+     * @return void
+     */
+    protected function _initialize(KConfig $config)
+    {
+        parent::_initialize($config);
+        
+        //Only render the menubar
+        $config->render = array('menubar');
+    }
+}
