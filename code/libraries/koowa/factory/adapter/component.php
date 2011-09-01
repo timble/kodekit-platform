15d0f07aaf3b4b3d77004f928ce2e175d123d435
Johan Janssens <johan@timble.net>
2011-09-01 02:23:32 +0000

    re #135 : Adapters are now directly loaded, no need to check if the identifier matches the adapter type.

--- code/libraries/koowa/factory/adapter/component.php
+++ code/libraries/koowa/factory/adapter/component.php
@@ -18,6 +18,13 @@
  */
 class KFactoryAdapterComponent extends KFactoryAdapterAbstract
 {
+	/** 
+	 * The adapter type
+	 * 
+	 * @var string
+	 */
+	protected $_type = 'com';
+	
 	/**
 	 * Create an instance of a class based on a class identifier
 	 * 
@@ -31,67 +38,62 @@ class KFactoryAdapterComponent extends KFactoryAdapterAbstract
 	 *                     -> Framework Specific 
 	 *                     -> Framework Default
 	 *
-	 * @param mixed  		 Identifier or Identifier object - application::com.component.[.path].name
+	 * @param mixed  		 Identifier or Identifier object - com:[//application/]component.view.[.path].name
 	 * @param 	object 		 An optional KConfig object with configuration options
 	 * @return object|false  Return object on success, returns FALSE on failure
 	 */
 	public function instantiate($identifier, KConfig $config)
 	{
-		$classname = false;
-		
-		if($identifier->type == 'com') 
-		{			
-			$path      = KInflector::camelize(implode('_', $identifier->path));
-        	$classname = 'Com'.ucfirst($identifier->package).$path.ucfirst($identifier->name);
+	    $path      = KInflector::camelize(implode('_', $identifier->path));
+        $classname = 'Com'.ucfirst($identifier->package).$path.ucfirst($identifier->name);
         		        	
-      		//Don't allow the auto-loader to load component classes if they don't exists yet
-			if (!class_exists( $classname, false ))
+      	//Don't allow the auto-loader to load component classes if they don't exists yet
+		if (!class_exists( $classname, false ))
+		{
+			//Find the file
+			if($path = KLoader::load($identifier))
 			{
-				//Find the file
-				if($path = KLoader::load($identifier))
-				{
-					//Don't allow the auto-loader to load component classes if they don't exists yet
-					if (!class_exists( $classname, false )) {
-						throw new KFactoryAdapterException("Class [$classname] not found in file [".$path."]" );
-					}
+				//Don't allow the auto-loader to load component classes if they don't exists yet
+				if (!class_exists( $classname, false )) {
+					throw new KFactoryAdapterException("Class [$classname] not found in file [".$path."]" );
 				}
-				else 
-				{
-					$classpath = $identifier->path;
-					$classtype = !empty($classpath) ? array_shift($classpath) : '';
+			}
+			else 
+			{
+				$classpath = $identifier->path;
+				$classtype = !empty($classpath) ? array_shift($classpath) : '';
 					
-					//Create the fallback path and make an exception for views
-					$path = ($classtype != 'view') ? KInflector::camelize(implode('_', $classpath)) : '';
+				//Create the fallback path and make an exception for views
+				$path = ($classtype != 'view') ? KInflector::camelize(implode('_', $classpath)) : '';
 							
-					/*
-					 * Find the classname to fallback too and auto-load the class
-					 * 
-					 * Fallback sequence : -> Named Component Specific 
-					 *                     -> Named Component Default  
-					 *                     -> Default Component Specific 
-					 *                     -> Default Component Defaukt
-					 *                     -> Framework Specific 
-					 *                     -> Framework Default
-					 */
-					if(class_exists('Com'.ucfirst($identifier->package).ucfirst($classtype).$path.ucfirst($identifier->name))) {
-						$classname = 'Com'.ucfirst($identifier->package).ucfirst($classtype).$path.ucfirst($identifier->name);
-					} elseif(class_exists('Com'.ucfirst($identifier->package).ucfirst($classtype).$path.'Default')) {
-						$classname = 'Com'.ucfirst($identifier->package).ucfirst($classtype).$path.'Default';
-					} elseif(class_exists('ComDefault'.ucfirst($classtype).$path.ucfirst($identifier->name))) {
-						$classname = 'ComDefault'.ucfirst($classtype).$path.ucfirst($identifier->name);
-					} elseif(class_exists('ComDefault'.ucfirst($classtype).$path.'Default')) {
-						$classname = 'ComDefault'.ucfirst($classtype).$path.'Default';
-					} elseif(class_exists( 'K'.ucfirst($classtype).$path.ucfirst($identifier->name))) {
-						$classname = 'K'.ucfirst($classtype).$path.ucfirst($identifier->name);
-					} elseif(class_exists('K'.ucfirst($classtype).$path.'Default')) {
-						$classname = 'K'.ucfirst($classtype).$path.'Default';
-					} else {
-						$classname = false;
-					}
+				/*
+				 * Find the classname to fallback too and auto-load the class
+				 * 
+				 * Fallback sequence : -> Named Component Specific 
+				 *                     -> Named Component Default  
+				 *                     -> Default Component Specific 
+				 *                     -> Default Component Defaukt
+				 *                     -> Framework Specific 
+				 *                     -> Framework Default
+				 */
+				if(class_exists('Com'.ucfirst($identifier->package).ucfirst($classtype).$path.ucfirst($identifier->name))) {
+					$classname = 'Com'.ucfirst($identifier->package).ucfirst($classtype).$path.ucfirst($identifier->name);
+				} elseif(class_exists('Com'.ucfirst($identifier->package).ucfirst($classtype).$path.'Default')) {
+					$classname = 'Com'.ucfirst($identifier->package).ucfirst($classtype).$path.'Default';
+				} elseif(class_exists('ComDefault'.ucfirst($classtype).$path.ucfirst($identifier->name))) {
+					$classname = 'ComDefault'.ucfirst($classtype).$path.ucfirst($identifier->name);
+				} elseif(class_exists('ComDefault'.ucfirst($classtype).$path.'Default')) {
+					$classname = 'ComDefault'.ucfirst($classtype).$path.'Default';
+				} elseif(class_exists( 'K'.ucfirst($classtype).$path.ucfirst($identifier->name))) {
+					$classname = 'K'.ucfirst($classtype).$path.ucfirst($identifier->name);
+				} elseif(class_exists('K'.ucfirst($classtype).$path.'Default')) {
+					$classname = 'K'.ucfirst($classtype).$path.'Default';
+				} else {
+					$classname = false;
 				}
 			}
 		}
-
+		
 		return $classname;
 	}
 }
