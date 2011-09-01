fb76a86fa64fc796cc496b1ccc0060fd790d70b0
Johan Janssens <johan@timble.net>
2011-09-01 02:35:00 +0000

    re #135 : Changed identifier to URL format (type:[//application/]package.path.name)

--- code/libraries/koowa/template/stack.php
+++ code/libraries/koowa/template/stack.php
@@ -14,7 +14,7 @@
   * Implements a simple stack collection (LIFO) 
   * 
   * The stack is implemented as a signleton. After instantiation the object can
-  * be accessed using lib.koowa.template.stack identifier.
+  * be accessed using koowa.template.stack identifier.
   * 
   * @author     Johan Janssens <johan@nooku.org>
   * @category   Koowa
@@ -63,7 +63,9 @@ class KTemplateStack extends KObject implements KObjectIdentifiable
                 $config = new KConfig($config);
             }
             
-            $instance = new self($config);
+            //Create the singleton
+            $classname = $config->identifier->classname;
+            $instance = new $classname($config);
         }
         
         return $instance;
