8106196018b35b8a1fe88720abe3677b2db1a8f9
Johan Janssens <johan@timble.net>
2011-09-01 02:24:01 +0000

    re #135 : Adapters are now directly loaded, no need to check if the identifier matches the adapter type.

--- code/libraries/koowa/factory/adapter/joomla.php
+++ code/libraries/koowa/factory/adapter/joomla.php
@@ -18,6 +18,13 @@
  */
 class KFactoryAdapterJoomla extends KFactoryAdapterAbstract
 {
+	/** 
+	 * The adapter type
+	 * 
+	 * @var string
+	 */
+	protected $_type = 'joomla';
+	
 	/**
 	 * The alias type map
 	 *
@@ -32,26 +39,20 @@ class KFactoryAdapterJoomla extends KFactoryAdapterAbstract
 	/**
 	 * Create an instance of a class based on a class identifier
 	 *
-	 * @param 	mixed  		 Identifier or Identifier object - lib.joomla.[.path].name
+	 * @param 	mixed  		 Identifier or Identifier object - joomla.[.path].name
 	 * @param 	object 		 An optional KConfig object with configuration options
 	 * @return object|false  Return object on success, returns FALSE on failure
 	 */
 	public function instantiate($identifier, KConfig $config)
 	{
-		$instance = false;
-
-		if($identifier->type == 'lib' && $identifier->package == 'joomla')
-		{
-			$name = ucfirst($identifier->name);
+        $name = ucfirst($identifier->package);
 			
-			//Check to see of the type is an alias
-			if(array_key_exists($name, $this->_alias_map)) {
-				$name = $this->_alias_map[$name];
-			}
-
-			$instance = call_user_func_array(array('JFactory', 'get'.$name), $config->toArray());
+		//Check to see of the type is an alias
+		if(array_key_exists($name, $this->_alias_map)) {
+			$name = $this->_alias_map[$name];
 		}
 
+		$instance = call_user_func_array(array('JFactory', 'get'.$name), $config->toArray());
 		return $instance;
 	}
 }
