99642a38d616ec8a8ccf8d06e59baf393f88120e
Johan Janssens <johan@timble.net>
2011-09-01 02:42:52 +0000

    re #135 : Changed identifier to URL format (type:[//application/]package.path.name)

--- code/site/components/com_default/templates/helpers/behavior.php
+++ code/site/components/com_default/templates/helpers/behavior.php
@@ -51,7 +51,7 @@ class ComDefaultTemplateHelperBehavior extends KTemplateHelperBehavior
     public function keepalive($config = array())
     {
         //Get the config session lifetime
-        $lifetime = KFactory::get('lib.joomla.session')->getExpire() * 1000;
+        $lifetime = KFactory::get('joomla:session')->getExpire() * 1000;
 
         //Refresh time is 1 minute less than the liftime
         $refresh =  ($lifetime <= 60000) ? 30000 : $lifetime - 60000;
