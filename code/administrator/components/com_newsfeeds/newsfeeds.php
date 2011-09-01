322eff5b5497a67016d84a706f197168f10f339e
Johan Janssens <johan@timble.net>
2011-09-01 03:03:57 +0000

    Synced with changed in framework. Changed identifier to URL format (type:[//application/]package.path.name) and replaced KFactory::tmp() with KFactory::get()

--- code/administrator/components/com_newsfeeds/newsfeeds.php
+++ code/administrator/components/com_newsfeeds/newsfeeds.php
@@ -18,8 +18,8 @@
  * @subpackage  Newsfeeds
  */
 
-if (!KFactory::get('lib.joomla.user')->authorize( 'com_newsfeeds', 'manage' )) {
-	KFactory::get('lib.joomla.application')->redirect( 'index.php', JText::_('ALERTNOTAUTH') );
+if (!KFactory::get('joomla:user')->authorize( 'com_newsfeeds', 'manage' )) {
+	KFactory::get('joomla:application')->redirect( 'index.php', JText::_('ALERTNOTAUTH') );
 }
 
-echo KFactory::get('admin::com.newsfeeds.dispatcher')->dispatch();
+echo KFactory::get('com://admin/newsfeeds.dispatcher')->dispatch();
