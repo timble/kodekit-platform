4b4844a4f7adedc4d6cb8258efccde7d468fdf01
Johan Janssens <johan@timble.net>
2011-09-06 02:08:21 +0000

    re #160 : Restructure the framework folder structure to follow that of Nooku Server.

--- code/components/com_default/dispatcher.php
+++ code/components/com_default/dispatcher.php
@@ -17,7 +17,7 @@
  * @package     Nooku_Components
  * @subpackage  Default
  */
-class ComDefaultDispatcher extends KDispatcherDefault implements KObjectInstantiatable
+class ComDefaultDispatcher extends KDispatcherDefault
 { 
  	/**
      * Initializes the options for the object
@@ -42,7 +42,7 @@ class ComDefaultDispatcher extends KDispatcherDefault implements KObjectInstanti
      *
      * @return KDispatcherDefault
      */
-    public static function getInstance($config = array())
+    public static function instantiate($config = array())
     {
         static $instance;
         
