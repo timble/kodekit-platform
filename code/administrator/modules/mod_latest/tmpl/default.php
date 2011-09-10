892014bab6e433d88cc267d1bc5a5defcd5b4b20
Johan Janssens <johan@timble.net>
2011-09-10 21:05:13 +0000

    Removed 'joomla' KLoader and KFactory adapter.

--- code/administrator/modules/mod_latest/tmpl/default.php
+++ code/administrator/modules/mod_latest/tmpl/default.php
@@ -37,7 +37,7 @@ defined('KOOWA') or die('Restricted access'); ?>
 			<?= @helper('com://admin/articles.template.helper.date.humanize', array('date' => $article->created_on));?>
 		</td>
 		<td>
-			<? if (KFactory::get('joomla:user')->authorize( 'com_users', 'manage' )) : ?>
+			<? if (JFactory::getUser()->authorize( 'com_users', 'manage' )) : ?>
 			   	<a href="<?= @route('index.php?option=com_users&view=user&id='. $article->created_by); ?>" title="<?= @text( 'Edit User' ) ?>">
 			   		<?= @escape($article->created_by_name) ?>
 			   	</a>
