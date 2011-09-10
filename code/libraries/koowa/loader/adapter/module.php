cd7987b0654b7627b2a1696cfaa4d977b540697b
Johan Janssens <johan@timble.net>
2011-09-10 22:13:49 +0000

    re #135 : Renamed _pathFromClassname to findPath and moved _pathFromIdentifier functionality to KIdentifier adapters.

--- code/libraries/koowa/loader/adapter/module.php
+++ code/libraries/koowa/loader/adapter/module.php
@@ -39,7 +39,7 @@ class KLoaderAdapterModule extends KLoaderAdapterAbstract
 	 * @param  string		  	The class name 
 	 * @return string|false		Returns the path on success FALSE on failure
 	 */
-	protected function _pathFromClassname($classname)
+	public function findPath($classname, $basepath = null)
 	{	
 		$path = false; 
 		
@@ -48,14 +48,23 @@ class KLoaderAdapterModule extends KLoaderAdapterAbstract
 			
 		if (array_shift($parts) == 'mod') 
 		{	
-			$module = 'mod_'.strtolower(array_shift($parts));
+		    //Switch the basepath
+		    if(isset($basepath)) {
+		        $this->_basepath = $basepath;
+		    }
+		    
+		    $module = 'mod_'.strtolower(array_shift($parts));
 			$file 	   = array_pop($parts);
 				
 			if(count($parts)) 
 			{
-				foreach($parts as $key => $value) {
-					$parts[$key] = KInflector::pluralize($value);
-				}
+				if($parts[0] != 'view') 
+			    {
+			        foreach($parts as $key => $value) {
+					    $parts[$key] = KInflector::pluralize($value);
+				    }
+			    } 
+			    else $parts[0] = KInflector::pluralize($parts[0]);
 					
 				$path = implode('/', $parts);
 				$path = $path.'/'.$file;
@@ -67,42 +76,5 @@ class KLoaderAdapterModule extends KLoaderAdapterAbstract
 		
 		return $path;
 		
-	}
-
-	/**
-	 * Get the path based on an identifier
-	 *
-	 * @param  object  			An Identifier object - application::mod.module.[.path].name
-	 * @return string|false		Returns the path on success FALSE on failure
-	 */
-	protected function _pathFromIdentifier($identifier)
-	{
-		$path = false;
-		
-		if($identifier->type == 'mod')
-		{		
-			$parts = $identifier->path;
-			$name  = $identifier->package;
-			
-		    //Store the basepath for re-use
-		    if($identifier->basepath) {
-	            $this->_basepath = $identifier->basepath;
-		    }
-				
-			if(!empty($identifier->name))
-			{
-				if(count($parts)) 
-				{
-					$path    = KInflector::pluralize(array_shift($parts)).
-					$path   .= count($parts) ? '/'.implode('/', $parts) : '';
-					$path   .= DS.strtolower($identifier->name);	
-				} 
-				else $path  = strtolower($identifier->name);	
-			}
-				
-			$path = $this->_basepath.'/modules/mod_'.$name.'/'.$path.'.php';			
-		}	
-		
-		return $path;
 	}
 }
