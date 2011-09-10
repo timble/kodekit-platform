3d2df218e6d69f9ad0398fd274f427de628bdc34
Johan Janssens <johan@timble.net>
2011-09-10 21:07:55 +0000

    Removed 'joomla' KLoader and KFactory adapter.

--- code/components/com_users/controllers/logout.php
+++ code/components/com_users/controllers/logout.php
@@ -21,7 +21,7 @@ class ComUsersControllerLogout extends ComDefaultControllerResource
 {
     protected function _actionLogout(KCommandContext $context)
     {
-		$result = KFactory::get('joomla:application')->logout();
+		$result = JFactory::getApplication()->logout();
 
 		if(!JError::isError($result)) {
 		    $this->_redirect = 'index.php?Itemid='.JSite::getMenu()->getDefault()->id;
