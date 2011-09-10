5a308e0efc2aeea1a21f4e9f53c8120e126bd76c
Johan Janssens <johan@timble.net>
2011-09-10 21:01:41 +0000

    Removed 'joomla' KLoader and KFactory adapter.

--- code/components/com_users/views/login/html.php
+++ code/components/com_users/views/login/html.php
@@ -23,8 +23,8 @@ class ComUsersViewLoginHtml extends ComDefaultViewHtml
     {
         $title = JText::_('Login');
 
-        KFactory::get('joomla:application')->getPathway()->addItem($title);
-        KFactory::get('joomla:document')->setTitle($title);
+        JFactory::getApplication()->getPathway()->addItem($title);
+        JFactory::getDocument()->setTitle($title);
         
         $this->parameters = $this->getParameters();
 
