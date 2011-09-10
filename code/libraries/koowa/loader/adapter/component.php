f8e148c04ef0a1c7373630b9daaf941b4d4a0629
Johan Janssens <johan@timble.net>
2011-09-10 22:13:18 +0000

    re #135 : Renamed _pathFromClassname to findPath and moved _pathFromIdentifier functionality to KIdentifier adapters.

--- code/libraries/koowa/loader/adapter/component.php
+++ code/libraries/koowa/loader/adapter/component.php
@@ -39,24 +39,33 @@ class KLoaderAdapterComponent extends KLoaderAdapterAbstract
 	 * @param  string		  	The class name 
 	 * @return string|false		Returns the path on success FALSE on failure
 	 */
-	protected function _pathFromClassname($classname)
+	public function findPath($classname, $basepath = null)
 	{
 		$path = false; 
-		
+			
 		$word  = strtolower(preg_replace('/(?<=\\w)([A-Z])/', '_\\1', $classname));
 		$parts = explode('_', $word);
 			
 		if (array_shift($parts) == 'com') 
 		{
-			$component = 'com_'.strtolower(array_shift($parts));
+		    //Switch the basepath
+		    if(isset($basepath)) {
+		        $this->_basepath = $basepath;
+		    }
+		    
+		    $component = 'com_'.strtolower(array_shift($parts));
 			$file 	   = array_pop($parts);
 				
 			if(count($parts)) 
 			{
-				foreach($parts as $key => $value) {
-					$parts[$key] = KInflector::pluralize($value);
-				}
-					
+			    if($parts[0] != 'view') 
+			    {
+			        foreach($parts as $key => $value) {
+					    $parts[$key] = KInflector::pluralize($value);
+				    }
+			    } 
+			    else $parts[0] = KInflector::pluralize($parts[0]);
+			    
 				$path = implode('/', $parts);
 				$path = $path.'/'.$file;
 			} 
@@ -64,45 +73,7 @@ class KLoaderAdapterComponent extends KLoaderAdapterAbstract
 			
 			$path = $this->_basepath.'/components/'.$component.'/'.$path.'.php';
 		}
-		
-		return $path;
-	}
-
-	/**
-	 * Get the path based on an identifier
-	 *
-	 * @param  object  			An Identifier object - com:[//application/]component.view.[.path].name
-	 * @return string|false		Returns the path on success FALSE on failure
-	 */
-	protected function _pathFromIdentifier($identifier)
-	{
-		$path = false;
-		
-		if($identifier->type == 'com')
-		{
-			$parts = $identifier->path;
-				
-			$component = 'com_'.strtolower($identifier->package);
-			
-			//Store the basepath for re-use
-		    if($identifier->basepath) {
-	            $this->_basepath = $identifier->basepath;
-		    }
-
-			if(!empty($identifier->name))
-			{
-				if(count($parts)) 
-				{
-					$path    = KInflector::pluralize(array_shift($parts));
-					$path   .= count($parts) ? '/'.implode('/', $parts) : '';
-					$path   .= '/'.strtolower($identifier->name);	
-				} 
-				else $path  = strtolower($identifier->name);	
-			}
-				
-			$path = $this->_basepath.'/components/'.$component.'/'.$path.'.php';
-		}	
-		
+	
 		return $path;
 	}
 }
