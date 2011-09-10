668df11bad95005d9c3c70fc2c132598fc838d75
Johan Janssens <johan@timble.net>
2011-09-10 22:23:10 +0000

    re #135 : Removed path() function replaced by findPath() no longer being implemented by abstract class.

--- code/libraries/koowa/loader/adapter/abstract.php
+++ code/libraries/koowa/loader/adapter/abstract.php
@@ -38,7 +38,7 @@ abstract class KLoaderAdapterAbstract implements KLoaderAdapterInterface
 	 * 
 	 * @var string
 	 */
-	protected $_prefiex = '';
+	protected $_prefix = '';
 	
 	/**
      * Constructor.
@@ -79,39 +79,4 @@ abstract class KLoaderAdapterAbstract implements KLoaderAdapterInterface
 	{
 		return $this->_prefix;
 	}
-    
-    /**
-     * Get the path based on a class name or an identifier
-     *
-     * @param  string|object    The class name or an identifier -[application::]type.package.[.path].name
-     * @return string|false     Returns the path on success FALSE on failure
-     */
-    public function path($identifier)
-    {
-        $path = false;
-        
-        if($identifier instanceof KIdentifierInterface) {
-            $path = $this->_pathFromIdentifier($identifier);
-        } else {
-            $path = $this->_pathFromClassname($identifier);
-        }
-        
-        return $path;
-    }
-    
-    /**
-     * Get the path based on an identifier
-     *
-     * @param  object           An Identifier object - [application::]type.package.[.path].name
-     * @return string|false     Returns the path on success FALSE on failure
-     */
-    abstract protected function _pathFromIdentifier($identifier);
-    
-    /**
-     * Get the path based on a class name
-     *
-     * @param  string           The class name 
-     * @return string|false     Returns the path on success FALSE on failure
-     */
-    abstract protected function _pathFromClassname($classname); 
 }
