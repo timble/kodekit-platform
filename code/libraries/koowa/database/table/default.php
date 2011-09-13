137567e02038ae6afdac50ad83597c710be0ee6a
Johan Janssens <johan@timble.net>
2011-09-13 10:28:01 +0000

    re #161 : Moved instances array into class scope as private static.

--- code/libraries/koowa/database/table/default.php
+++ code/libraries/koowa/database/table/default.php
@@ -20,28 +20,28 @@
 class KDatabaseTableDefault extends KDatabaseTableAbstract implements KObjectInstantiatable
 {
 	/**
+     * Associative array of table instances
+     * 
+     * @var array
+     */
+    private static $_instances = array();
+	
+	/**
      * Force creation of a singleton
      *
      * @return KDatabaseTableDefault
      */
     public static function getInstance($config = array())
     {
-        static $instances;
-        
-        // For fallbacks we need to store instancess depending on classname
-        if ($instances === NULL) {
-            $instances = array();
-        }
-
         // Check if an instance with this identifier already exists or not
-        $instance = (string)$config->identifier;
-        if (!isset($instances[$instance]))
+        $instance = (string) $config->identifier;
+        if (!isset(self::$_instances[$instance]))
         {
             //Create the singleton
             $classname = $config->identifier->classname;
-            $instances[$instance] = new $classname($config);
+            self::$_instances[$instance] = new $classname($config);
         }
         
-        return $instances[$instance];
+        return self::$_instances[$instance];
     }
 }
