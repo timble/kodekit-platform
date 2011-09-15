2de5e436872b84b906bd08915b53e46e6c30a0f4
Ercan Ozkaya <ercan@timble.net>
2011-09-15 14:26:22 +0000

    re #148: Add new database schema changes

--- code/administrator/components/com_files/install/install.sql
+++ code/administrator/components/com_files/install/install.sql
@@ -1,13 +1,15 @@
-CREATE TABLE IF NOT EXISTS `jos_files_containers` (
-  `files_container_id` varchar(255) NOT NULL,
+CREATE TABLE `jos_files_containers` (
+  `files_container_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `slug` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `path` varchar(255) NOT NULL,
-  `parameters` TEXT,
-  PRIMARY KEY (`files_container_id`)
-) DEFAULT CHARSET=utf8;
+  `parameters` text NOT NULL,
+  PRIMARY KEY (`files_container_id`),
+  UNIQUE KEY `slug` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-INSERT INTO `jos_files_containers` (`files_container_id`, `path`, `parameters`) VALUES
-('com_files.files', 'images', '{"upload_extensions":"bmp,csv,doc,gif,ico,jpg,jpeg,odg,odp,ods,odt,pdf,png,ppt,swf,txt,xcf,xls","upload_maxsize":"10485760","allowed_mimetypes":"image\\/jpeg,image\\/gif,image\\/png,imagee\\/bmp,application\\/x-shockwave-flash,application\\/msword,application\\/excel,application\\/pdf,application\\/powerpoint,text\\/plain,application\\/x-zip","illegal_mimetypes":"text\\/html","restrict_uploads":1,"check_mime":1,"allowed_media_usergroup":3}');
+INSERT INTO `jos_files_containers` (`slug`, `title`, `path`, `parameters`) VALUES
+('com_files.files', 'Images', 'images', '{"upload_extensions":"bmp,csv,doc,gif,ico,jpg,jpeg,odg,odp,ods,odt,pdf,png,ppt,swf,txt,xcf,xls","upload_maxsize":"10485760","allowed_mimetypes":"image\\/jpeg,image\\/gif,image\\/png,image\\/bmp,application\\/x-shockwave-flash,application\\/msword,application\\/excel,application\\/pdf,application\\/powerpoint,text\\/plain,application\\/x-zip","illegal_mimetypes":"text\\/html","restrict_uploads":1,"check_mime":1,"allowed_media_usergroup":3}');
 
 CREATE TABLE IF NOT EXISTS `jos_files_thumbnails` (
   `files_thumbnail_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
