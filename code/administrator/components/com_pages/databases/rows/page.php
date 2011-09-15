fa8729b35db7a693ec7a9277d9a16bc53f742102
Johan Janssens <johan@timble.net>
2011-09-15 14:38:57 +0000

    Synced with changes in framework.

--- code/administrator/components/com_pages/databases/rows/page.php
+++ code/administrator/components/com_pages/databases/rows/page.php
@@ -20,7 +20,7 @@
 
 class ComPagesDatabaseRowPage extends ComPagesDatabaseRowNode implements KObjectInstantiatable
 {
-	public static function getInstance(KConfig $config)
+	public static function getInstance(KConfig $config, KFactoryInterface $factory = null)
 	{
 		if($config->data['type']) {
 			$type = $config->data['type'] == 'menulink' ? 'Link' : ucfirst($config->data['type']);
