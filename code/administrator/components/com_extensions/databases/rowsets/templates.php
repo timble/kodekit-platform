5b0cf26b3365c6e207b2ea053bb937ecbeb0701f
Johan Janssens <johan@timble.net>
2011-09-18 10:51:40 +0000

    Reverted rev 2768

---
+++ code/administrator/components/com_extensions/databases/rowsets/templates.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @version     $Id$
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Extensions
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Template Database Rowset Class
+ *
+ * @author      Stian Didriksen <http://nooku.assembla.com/profile/stiandidriksen>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Extensions
+ */
+class ComExtensionsDatabaseRowsetTemplates extends KDatabaseRowsetAbstract
+{       
+    
+}
