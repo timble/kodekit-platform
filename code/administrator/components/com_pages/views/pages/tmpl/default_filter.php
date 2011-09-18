b2b8d1855212a2122989bc1bf5532ba65a3a458c
Johan Janssens <johan@timble.net>
2011-09-18 00:45:23 +0000

    Changed enabled state filter to boolean. Filter has been adapter to return NULL for empty strings.

--- code/administrator/components/com_pages/views/pages/tmpl/default_filter.php
+++ code/administrator/components/com_pages/views/pages/tmpl/default_filter.php
@@ -13,17 +13,17 @@ defined('KOOWA') or die('Restricted access') ?>
 
 <div id="filter" class="group">
 	<ul>
-		<li class="<?= !is_numeric($state->enabled) && !$state->deleted ? 'active' : ''; ?> separator-right">
+		<li class="<?= !is_bool($state->enabled) && !$state->deleted ? 'active' : ''; ?> separator-right">
 			<a href="<?= @route('enabled=&deleted=' ) ?>">
 			    <?= 'All' ?>
 			</a>
 		</li>
-		<li class="<?= $state->enabled == '1' ? 'active' : ''; ?>">
+		<li class="<?= $state->enabled === true ? 'active' : ''; ?>">
 			<a href="<?= @route('enabled=1' ) ?>">
 			    <?= @text('Published') ?>
 			</a> 
 		</li>
-		<li class="<?= $state->enabled == '0' ? 'active' : ''; ?>">
+		<li class="<?= $state->enabled === false ? 'active' : ''; ?>">
 			<a href="<?= @route('enabled=0' ) ?>">
 			    <?= @text('Unpublished') ?>
 			</a> 
