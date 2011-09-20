3d47a1722076fc0f1c28921343d9da9e61f292e9
Johan Janssens <johan@timble.net>
2011-09-20 13:03:20 +0000

    re #135 : Removed defaults from the getInstance function parameters.

--- code/libraries/koowa/filter/abstract.php
+++ code/libraries/koowa/filter/abstract.php
@@ -79,9 +79,11 @@ abstract class KFilterAbstract implements KFilterInterface
     /**
      * Force creation of a singleton
      *
+     * @param 	array 	An optional array with configuration options
+     * @param 	object	A KFactoryInterface object
      * @return KFilterInterface
      */
-    public static function getInstance($config = array(), KFactoryInterface $factory = null)
+    public static function getInstance($config, KFactoryInterface $factory)
     { 
        // Check if an instance with this identifier already exists or not
         if (!$factory->exists($config->identifier))
