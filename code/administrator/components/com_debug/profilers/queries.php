6a92b7bf45c10412ecf82114977172ef6c7bb9f2
Johan Janssens <johan@timble.net>
2011-09-20 12:59:39 +0000

    Removed defaults from the getInstance function parameters.

--- code/administrator/components/com_debug/profilers/queries.php
+++ code/administrator/components/com_debug/profilers/queries.php
@@ -37,9 +37,11 @@ class ComDebugProfilerQueries extends KEventListener implements KObjectInstantia
 	/**
      * Force creation of a singleton
      *
+     * @param 	array 	An optional array with configuration options
+     * @param 	object	A KFactoryInterface object
      * @return ComDebugProfilerEvents
      */
-    public static function getInstance($config = array(), KFactoryInterface $factory = null)
+    public static function getInstance($config, KFactoryInterface $factory)
     {
         if (!$factory->exists($config->identifier)) 
         {
