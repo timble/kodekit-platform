65acb4b9561f8938245416e385c859a673ec8eb2
Israel Canasa <israel@timble.net>
2011-09-21 13:42:24 +0000

    Added docblocks

--- code/administrator/components/com_logs/controllers/behaviors/loggable.php
+++ code/administrator/components/com_logs/controllers/behaviors/loggable.php
@@ -1,5 +1,22 @@
 <?php
-/** $Id$ */
+/**
+ * @version		$Id$
+ * @category	Nooku
+ * @package     Nooku_Components
+ * @subpackage  Logs
+ * @copyright	Copyright (C) 2010 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+
+/**
+ * Revision Row
+ *
+ * @author      Israel Canasa <israel@timble.net>
+ * @category	Nooku
+ * @package    	Nooku_Components
+ * @subpackage 	Logs
+ */
 
 class ComLogsControllerBehaviorLoggable extends KControllerBehaviorAbstract
 {
