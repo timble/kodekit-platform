93707af1796bb5d3bba56e50517f36182c257da6
Ercan Ozkaya <ercan@timble.net>
2011-09-26 19:21:39 +0000

    re #148: Clear input box after folder creation

--- code/libraries/joomla/environment/uri.php
+++ code/libraries/joomla/environment/uri.php
@@ -166,7 +166,7 @@ class JURI extends JObject
 				 else
 				 {
 					// IIS uses the SCRIPT_NAME variable instead of a REQUEST_URI variable... thanks, MS
-					$theURI = 'http' . $https . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+					$theURI = 'http' . $https . (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '') . $_SERVER['SCRIPT_NAME'];
 		
 					// If the query string exists append it to the URI string
 					if (isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING'])) {
