7852c3e3cd59121ecb7ca335de00e817906f2491
Ercan Ozkaya <ercan@timble.net>
2011-09-28 15:04:05 +0000

    re #148: Update database tables

--- code/installation/sql/mysql/joomla.sql
+++ code/installation/sql/mysql/joomla.sql
@@ -626,15 +626,18 @@ CREATE TABLE `#__versions_revisions` (
 
 # --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `#__files_containers` (
-  `files_container_id` varchar(255) NOT NULL,
+CREATE TABLE `#__files_containers` (
+  `files_container_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `slug` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `path` varchar(255) NOT NULL,
-  `parameters` TEXT,
-  PRIMARY KEY (`files_container_id`)
-) DEFAULT CHARSET=utf8;
+  `parameters` text NOT NULL,
+  PRIMARY KEY (`files_container_id`),
+  UNIQUE KEY `slug` (`slug`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `#__files_containers` (`files_container_id`, `path`, `parameters`) VALUES
-('com_files.files', 'images', '{"upload_extensions":"bmp,csv,doc,gif,ico,jpg,jpeg,odg,odp,ods,odt,pdf,png,ppt,swf,txt,xcf,xls","upload_maxsize":"10485760","allowed_mimetypes":"image\\/jpeg,image\\/gif,image\\/png,imagee\\/bmp,application\\/x-shockwave-flash,application\\/msword,application\\/excel,application\\/pdf,application\\/powerpoint,text\\/plain,application\\/x-zip","illegal_mimetypes":"text\\/html","restrict_uploads":1,"check_mime":1,"allowed_media_usergroup":3}');
+INSERT INTO `#__files_containers` (`slug`, `title`, `path`, `parameters`) VALUES
+('files-files', 'Images', 'images', '{"upload_extensions":"bmp,csv,doc,gif,ico,jpg,jpeg,odg,odp,ods,odt,pdf,png,ppt,swf,txt,xcf,xls","upload_maxsize":"10485760","allowed_mimetypes":"image\\/jpeg,image\\/gif,image\\/png,image\\/bmp,application\\/x-shockwave-flash,application\\/msword,application\\/excel,application\\/pdf,application\\/powerpoint,text\\/plain,application\\/x-zip","illegal_mimetypes":"text\\/html","restrict_uploads":1,"check_mime":1,"allowed_media_usergroup":3}');
 
 CREATE TABLE IF NOT EXISTS `#__files_thumbnails` (
   `files_thumbnail_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
