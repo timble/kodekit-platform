d704581a2117e427570feb17e88c73fc991ec4e9
Johan Janssens <johan@timble.net>
2011-10-02 22:07:32 +0000

    Synced with changes in framework. Use KObject->getService() instead of KFactory::get() and KObject->getIdentifier() instead of KIdentifier::identify() or $this->_identifier.

--- code/administrator/components/com_users/views/users/html.php
+++ code/administrator/components/com_users/views/users/html.php
@@ -21,7 +21,7 @@ class ComUsersViewUsersHtml extends ComDefaultViewHtml
 {
 	public function display()
 	{
-		$this->assign('groups', KFactory::get('com://admin/groups.model.groups')->getList());
+		$this->assign('groups', $this->getService('com://admin/groups.model.groups')->getList());
 
 		return parent::display();
 	}
