c278637f6bdb75b47e3f3d8dd33584cfc029bf79
Johan Janssens <johan@timble.net>
2011-10-02 22:03:31 +0000

    Synced with changes in framework. Use KObject->getService() instead of KFactory::get() and KObject->getIdentifier() instead of KIdentifier::identify() or $this->_identifier.

--- code/administrator/components/com_info/models/directories.php
+++ code/administrator/components/com_info/models/directories.php
@@ -72,7 +72,7 @@ class ComInfoModelDirectories extends KModelAbstract
                 );
             }
 
-            $this->_list = KFactory::get('com://admin/info.database.rowset.configuration')
+            $this->_list = $this->getService('com://admin/info.database.rowset.configuration')
                 ->addData($rows, false);
         }
 
