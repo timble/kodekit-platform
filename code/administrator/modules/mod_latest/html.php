80de4035b6cbdb2cb06e2843e9082e4b14f58fc3
Johan Janssens <johan@timble.net>
2011-10-02 22:08:16 +0000

    Synced with changes in framework. Use KObject->getService() instead of KFactory::get() and KObject->getIdentifier() instead of KIdentifier::identify() or $this->_identifier.

--- code/administrator/modules/mod_latest/html.php
+++ code/administrator/modules/mod_latest/html.php
@@ -22,7 +22,7 @@ class ModLatestHtml extends ModDefaultHtml
 {
     public function display()
     { 
-        $this->articles = KFactory::get('com://admin/articles.model.articles')->sort('created')->direction('desc')->limit(10)->getList(); 
+        $this->articles = $this->getService('com://admin/articles.model.articles')->sort('created')->direction('desc')->limit(10)->getList(); 
         return parent::display();
     }
 } 
