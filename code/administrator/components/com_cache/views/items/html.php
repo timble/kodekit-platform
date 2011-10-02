b02104c108019a63088f7236c6fa4653860867ec
Johan Janssens <johan@timble.net>
2011-10-02 21:46:38 +0000

    Synced with changes in framework. Use KObject->getService() instead of KFactory::get() and KObject->getIdentifier() instead of KIdentifier::identify() or $this->_identifier.

--- code/administrator/components/com_cache/views/items/html.php
+++ code/administrator/components/com_cache/views/items/html.php
@@ -23,7 +23,7 @@ class ComCacheViewItemsHtml extends ComDefaultViewHtml
 	{
         $group = $this->getModel()->getState()->group;
         
-	    $this->groups = KFactory::get('com://admin/cache.model.groups')->getList();
+	    $this->groups = $this->getService('com://admin/cache.model.groups')->getList();
 	    $this->size   = !empty($group) ? $this->groups->find($group)->size : $this->groups->size;
         $this->count  = !empty($group)? $this->groups->find($group)->count : $this->groups->count;
         
