9206d713ae2a41c7d3259d8f5ec096c5c2581379
Johan Janssens <johan@timble.net>
2011-10-02 21:41:35 +0000

    Synced with changes in framework. Use @service() instead of KFactory::get().

--- code/administrator/components/com_categories/views/categories/tmpl/default_sidebar.php
+++ code/administrator/components/com_categories/views/categories/tmpl/default_sidebar.php
@@ -12,5 +12,5 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 
 <div id="sidebar">
     <h3><?= @text('Sections') ?></h3>
-    <?= @template('com://admin/articles.view.sections.list', array('state' => $state, 'sections' => KFactory::get('com://admin/articles.model.sections')->getList())); ?>
+    <?= @template('com://admin/articles.view.sections.list', array('state' => $state, 'sections' => @service('com://admin/articles.model.sections')->getList())); ?>
 </div>
