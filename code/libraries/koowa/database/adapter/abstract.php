1568ef97d0336b999aacbf596ae4660b63309999
Johan Janssens <johan@timble.net>
2011-10-02 20:23:58 +0000

    re #135 : KDatabaseAdapterAbstract no longer implements KObjectIdentifiable.

--- code/libraries/koowa/database/adapter/abstract.php
+++ code/libraries/koowa/database/adapter/abstract.php
@@ -18,7 +18,7 @@
  * @subpackage  Adapter
  * @uses 		KPatternCommandChain
  */
-abstract class KDatabaseAdapterAbstract extends KObject implements KDatabaseAdapterInterface, KObjectIdentifiable
+abstract class KDatabaseAdapterAbstract extends KObject implements KDatabaseAdapterInterface
 {
 	/**
 	 * Active state of the connection
@@ -142,17 +142,6 @@ abstract class KDatabaseAdapterAbstract extends KObject implements KDatabaseAdap
         parent::_initialize($config);
     }
     
-	/**
-	 * Get the object identifier
-	 * 
-	 * @return	KIdentifier	
-	 * @see 	KObjectIdentifiable
-	 */
-	public function getIdentifier()
-	{
-		return $this->_identifier;
-	}
-
 	/**
 	 * Get a database query object
 	 *
