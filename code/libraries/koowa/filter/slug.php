348492957a08e49fe76c917b3f43cbd740992116
Johan Janssens <johan@timble.net>
2011-10-02 21:17:26 +0000

    re #135 : Use KObject->getService() instead of KFactory::get().

--- code/libraries/koowa/filter/slug.php
+++ code/libraries/koowa/filter/slug.php
@@ -75,7 +75,7 @@ class KFilterSlug extends KFilterAbstract
 	 */
 	protected function _validate($value)
 	{
-		return KFactory::get('koowa:filter.cmd')->validate($value);
+		return $this->getService('koowa:filter.cmd')->validate($value);
 	}
 	
 	/**
@@ -93,7 +93,7 @@ class KFilterSlug extends KFilterAbstract
 		$value = str_replace($this->_separator, ' ', $value);
 		
 		//convert to ascii characters
-		$value = KFactory::get('koowa:filter.ascii')->sanitize($value);
+		$value = $this->getService('koowa:filter.ascii')->sanitize($value);
 		
 		//lowercase and trim
 		$value = trim(strtolower($value));
