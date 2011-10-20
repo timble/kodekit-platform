b179c68d1f89c721d295b76aa6d52442c5ccc202
Johan Janssens <johan@timble.net>
2011-10-20 16:24:31 +0000

    re #167 : Added logs_logs table.

--- code/installation/sql/mysql/joomla.sql
+++ code/installation/sql/mysql/joomla.sql
@@ -647,3 +647,18 @@ CREATE TABLE IF NOT EXISTS `#__files_thumbnails` (
   `thumbnail` text NOT NULL,
   PRIMARY KEY (`files_thumbnail_id`)
 ) DEFAULT CHARSET=utf8;
+
+# --------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `#__logs_logs` (
+	`logs_log_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+	`application` VARCHAR(10) NOT NULL DEFAULT '',
+	`type` VARCHAR(3) NOT NULL DEFAULT '',
+	`package` VARCHAR(50) NOT NULL DEFAULT '',
+	`name` VARCHAR(50) NOT NULL DEFAULT '',
+	`action` VARCHAR(50) NOT NULL DEFAULT '',
+	`row` BIGINT NOT NULL DEFAULT '0',
+	`title` VARCHAR(255) NOT NULL DEFAULT '',
+	`created_on` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`created_by` INT(11) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
