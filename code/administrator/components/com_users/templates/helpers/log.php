3751a88ca74fc2f96abf6055360e96033fa7c9f0
Johan Janssens <johan@timble.net>
2011-10-21 01:13:13 +0000

    Added specialised log template helper to deal with login and logout actions.

---
+++ code/administrator/components/com_users/templates/helpers/log.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @version		$Id$
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Users
+ * @copyright	Copyright (C) 2010 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+
+/**
+ * Log Template Helper Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @category	Nooku
+ * @package    	Nooku_Server
+ * @subpackage 	Users
+ */
+
+class ComUsersTemplateHelperLog extends ComLogsTemplateHelperLog
+{
+    public function message($config = array())
+	{
+	    $config = new KConfig($config);
+		$config->append(array(
+			'row'      => ''
+		));
+		
+		$row = $config->row;
+		
+		if($row->action == 'login' || $row->action == 'logout') 
+		{    
+		    $item = $this->getTemplate()->getView()->createRoute('option='.$row->type.'_'.$row->package.'&view='.$row->name.'&id='.$row->row_id);
+		    
+		    $message   = '<a href="'.$item.'">'.$row->title.'</a>'; 
+		    $message  .= ' <span class="action">'.$row->status.'</span>';
+		}
+		else $message = parent::message($config);
+		
+		return $message;
+	}
+}
