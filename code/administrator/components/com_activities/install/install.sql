4727aec3af55f32be0ba18761adbd30d0331127f
Israel Canasa <israel@timble.net>
2011-10-22 15:49:54 +0000

    re #26 - WIP: Renamed logs_logs table to activities_activities

--- code/administrator/components/com_activities/install/install.sql
+++ code/administrator/components/com_activities/install/install.sql
@@ -1,7 +1,7 @@
 -- $Id: install.sql 1244 2010-09-08 22:51:47Z johanjanssens $
 
-CREATE TABLE IF NOT EXISTS `#__logs_logs` (
-	`logs_log_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `#__activities_activities` (
+	`activities_activity_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 	`application` VARCHAR(10) NOT NULL DEFAULT '',
 	`type` VARCHAR(3) NOT NULL DEFAULT '',
 	`package` VARCHAR(50) NOT NULL DEFAULT '',
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS `#__logs_logs` (
 	`status` varchar(100) NOT NULL,
 	`created_on` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
 	`created_by` INT(11) NOT NULL DEFAULT '0',
-	PRIMARY KEY(`logs_log_id`)
+	PRIMARY KEY(`activities_activity_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
