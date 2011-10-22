11e9e91ff9902ee539ef5e2535826ab7080f2a89
Israel Canasa <israel@timble.net>
2011-10-22 16:52:13 +0000

    re #167 - Added creation of activities table in the install sql file.

--- code/installation/sql/mysql/diff_alpha4_alpha5.sql
+++ code/installation/sql/mysql/diff_alpha4_alpha5.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS `#__logs_logs` (
-	`logs_log_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `#__activities_activities` (
+	`activities_activity_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 	`application` VARCHAR(10) NOT NULL DEFAULT '',
 	`type` VARCHAR(3) NOT NULL DEFAULT '',
 	`package` VARCHAR(50) NOT NULL DEFAULT '',
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS `#__logs_logs` (
 	`action` VARCHAR(50) NOT NULL DEFAULT '',
 	`row` BIGINT NOT NULL DEFAULT '0',
 	`title` VARCHAR(255) NOT NULL DEFAULT '',
+	`status` varchar(100) NOT NULL,
 	`created_on` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-	`created_by` INT(11) NOT NULL DEFAULT '0'
+	`created_by` INT(11) NOT NULL DEFAULT '0',
+	PRIMARY KEY(`activities_activity_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
