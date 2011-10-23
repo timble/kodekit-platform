24c2afea9ad0f41cbf8098a9c3e9a59984cffd28
Johan Janssens <johan@timble.net>
2011-10-23 21:39:55 +0000

    Toolbars are now event listeners. Rendering of the list toolbar is done through the onAfterControllerBrowse event, rendering of the item toolbar is done through the onAfterControllerRead event.

---
+++ code/administrator/components/com_info/controllers/toolbars/default.php
@@ -0,0 +1,28 @@
+<?php 
+/**
+ * @version     $Id$
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Article Toolbar Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ */
+class ComInfoControllerToolbarDefault extends ComDefaultControllerToolbarDefault
+{
+    protected function _initialize(KConfig $config)
+    { 
+        parent::_initialize($config);
+        
+        $config->render = array();
+    }
+}
---
+++ code/administrator/components/com_info/controllers/toolbars/menubar.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @version     $Id: menubar.php 3024 2011-10-09 01:44:30Z johanjanssens $
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Installer
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Info Menubar Class
+ *
+ * @author      Stian Didriksen <http://nooku.assembla.com/profile/stiandidriksen>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Installer
+ */
+class ComInfoControllerToolbarMenubar extends ComDefaultControllerToolbarMenubar
+{
+  
+}
