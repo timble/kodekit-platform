9f1c03de0b7f25bbc965ca7e5fa7b1c80baa7c2d
Johan Janssens <johan@timble.net>
2011-10-23 20:29:11 +0000

    Filters are now also unsettable.

--- code/administrator/components/com_activities/views/activities/tmpl/default_filter.php
+++ code/administrator/components/com_activities/views/activities/tmpl/default_filter.php
@@ -14,38 +14,38 @@ defined('KOOWA') or die('Restricted access') ?>
 <div id="filter" class="group">
 	<ul>
 		<li class="<?= ($state->action || $state->application) ? '' : 'active'; ?>">
-			<a href="<?= @route('application=&action=&created_on=' ) ?>">
+			<a href="<?= @route('application=&action=&created_on=&direction=' ) ?>">
 			    <?= @text('All') ?>
 			</a>
 		</li>
 		
 		<li class="<?= ($state->application == 'site') ? 'active' : ''; ?> separator-left">
-			<a href="<?= @route('application=site&action='.$state->action ) ?>">
+			<a href="<?= @route($state->application == 'site' ? 'application=' : 'application=site') ?>">
 			    <?= @text('Site') ?>
 			</a>
 		</li>
 
 		<li class="<?= ($state->application == 'admin') ? 'active' : ''; ?> <?= count($actions) ? 'separator-right': ''?>">
-			<a href="<?= @route('application=admin&action='.$state->action ) ?>">
+			<a href="<?= @route($state->application == 'admin' ? 'application=' : 'application=admin' ) ?>">
 			    <?= @text('Administrator') ?>
 			</a>
 		</li>
 		
 		<? foreach ($actions as $action):?>
 		<li class="<?= ($state->action == $action->action) ? 'active' : ''; ?>">
-			<a href="<?= @route('action='.$action->action) ?>">
+			<a href="<?= @route($state->action == $action->action ? 'action=' : 'action='.$action->action) ?>">
 			    <?= ucfirst($action->action) ?>
 			</a>
 		</li>
 		<? endforeach ?>
 
 		<li class="<?= ($state->direction == 'desc') ? 'active' : ''; ?> separator-left">
-			<a href="<?= @route('direction=desc' ) ?>">
+			<a href="<?= @route($state->direction == 'desc' ? 'direction=' : 'direction=desc' ) ?>">
 			    <?= @text('Latest First') ?>
 			</a>
 		</li>
 		<li class="<?= ($state->direction == 'asc') ? 'active' : ''; ?>">
-			<a href="<?= @route('direction=asc' ) ?>">
+			<a href="<?= @route($state->direction == 'asc' ? 'direction=' : 'direction=asc' ) ?>">
 			    <?= @text('Oldest First') ?>
 			</a>
 		</li>
