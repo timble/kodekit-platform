cecf9baf5ea8416dc4cf9b9390cff1eb25df3af8
Johan Janssens <johan@timble.net>
2011-10-23 20:47:36 +0000

    Published state now filtered as boolean.

--- code/administrator/components/com_categories/views/categories/tmpl/default_filter.php
+++ code/administrator/components/com_categories/views/categories/tmpl/default_filter.php
@@ -17,13 +17,13 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 			    <?= @text('All') ?>
 			</a>
 		</li>
-		<li class="<?= $state->published === 1 ? 'active' : ''; ?>">
-			<a href="<?= @route($state->published === 1 ? 'published=' : 'published=1') ?>">
+		<li class="<?= $state->published === true ? 'active' : ''; ?>">
+			<a href="<?= @route($state->published === true ? 'published=' : 'published=1') ?>">
 			    <?= @text('Published') ?>
 			</a> 
 		</li>
-		<li class="<?= $state->published === 0 ? 'active' : ''; ?>">
-			<a href="<?= @route($state->published === 0 ? 'published=' : 'published=0' ) ?>">
+		<li class="<?= $state->published === false ? 'active' : ''; ?>">
+			<a href="<?= @route($state->published === false ? 'published=' : 'published=0' ) ?>">
 			    <?= @text('Unpublished') ?>
 			</a> 
 		</li>
