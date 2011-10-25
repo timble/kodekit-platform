07a916245d4826e8c6a9e2d9260c6052c7eec5b8
Johan Janssens <johan@timble.net>
2011-10-25 21:27:24 +0000

    re #187 : KControllerBehaviorException now extends from KBehaviorException

--- code/libraries/koowa/controller/behavior/exception.php
+++ code/libraries/koowa/controller/behavior/exception.php
@@ -17,4 +17,4 @@
  * @package     Koowa_Controller
  * @subpackage  Behavior
  */
-class KControllerBehaviorException extends KControllerException {}
+class KControllerBehaviorException extends KBehaviorException {}
