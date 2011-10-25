2e0bac4e6c900fdfa3120bfff34314bb516bb753
Johan Janssens <johan@timble.net>
2011-10-25 21:24:31 +0000

    re #187 : Initial import of KBehavior package

---
+++ code/libraries/koowa/behavior/interface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version 	$Id$
+ * @category	Koowa
+ * @package		Koowa_Behavior
+ * @copyright	Copyright (C) 2007 - 2010 Johan Janssens. All rights reserved.
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ */
+
+/**
+ * Behavior Interface
+ *
+ * @author		Johan Janssens <johan@nooku.org>
+ * @category	Koowa
+ * @package     Koowa_Controller
+ */
+interface KBehaviorInterface extends KCommandInterface, KObjectServiceable
+{
+	
+}
