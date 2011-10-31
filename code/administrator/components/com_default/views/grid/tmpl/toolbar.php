bf9eb34fee851b050e50dd4d21f0bb94354064d4
Johan Janssens <johan@timble.net>
2011-10-31 05:33:25 +0000

    re #188 : Added grid/tmpl/toolbar.php template

---
+++ code/administrator/components/com_default/views/grid/tmpl/toolbar.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @version     $Id: default.php 3024 2011-10-09 01:44:30Z johanjanssens $
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+defined('KOOWA') or die('Restricted access') ?>
+
+<module position="title" content="replace">
+	<?= @helper('toolbar.title', array('toolbar' => $toolbar))?>
+</module>
+
+<module position="toolbar" content="replace">
+	<?= @helper('toolbar.render', array('toolbar' => $toolbar))?>
+</module>
+
+<module position="submenu" content="replace">
+	<?= @helper('menubar.render', array('menubar' => $menubar))?>
+</module>
