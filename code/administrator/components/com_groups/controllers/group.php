55755beac5fbb99081f9cff135bfee7fdb396341
Johan Janssens <johan@timble.net>
2011-11-01 09:45:41 +0000

    Moved code from com_users.

--- code/administrator/components/com_groups/controllers/group.php
+++ code/administrator/components/com_groups/controllers/group.php
@@ -24,7 +24,41 @@ class ComGroupsControllerGroup extends ComDefaultControllerDefault
         $config->append(array(
         	'behaviors' => array('com://admin/activities.controller.behavior.loggable'),
         ));
-    
+        
         parent::_initialize($config);
+        
+        //Force the toolbars
+        $config->toolbars = array('menubar', 'com://admin/groups.controller.toolbar.group');
+    }
+    
+    protected function _actionGet(KCommandContext $context)
+    {
+        $view    = $this->getView();
+        $package = KInflector::pluralize($this->getIdentifier()->name);
+        
+        if($view instanceof KViewTemplate) 
+	    {     
+	        //Set the layout identifier
+	        $layout = clone $view->getIdentifier();
+	        $layout->package  = $package;
+	        $layout->name     = $view->getLayout();
+	        $layout->filepath = ''; 
+ 
+	        $view->setLayout($layout);
+	        
+	        //Set the template identifier
+	        $template = $view->getTemplate()->getIdentifier();
+	        $template->package = $package;
+	    }
+	     
+        return parent::_actionGet($context);
+    }
+    
+    public function setModel($model)
+    {
+        $model = parent::setModel($model);
+        $model->package = KInflector::pluralize($this->getIdentifier()->name);
+        
+        return $model; 
     }
 }
