4e649bebe2df9c3f588dab682cf77f068233cddc
Johan Janssens <johan@timble.net>
2011-12-11 17:44:04 +0000

    re #176 : Removed login and logout redirection parameters.

--- code/modules/mod_login/helper.php
+++ code/modules/mod_login/helper.php
@@ -16,34 +16,6 @@ defined('_JEXEC') or die('Restricted access');
 
 class modLoginHelper
 {
-	function getReturnURL($params, $type)
-	{
-		if($itemid =  $params->get($type))
-		{  
-			$menu =& JSite::getMenu();  
-			$item = $menu->getItem($itemid); //var_dump($menu);die;
-			if ($item)
-			{
-				$url = JRoute::_($item->link.'&Itemid='.$itemid, false);
-			}
-			else
-			{
-			// stay on the same page
-			$uri = JFactory::getURI();
-			$url = $uri->toString(array('path', 'query', 'fragment'));
-			}
-				
-		}
-		else
-		{
-			// stay on the same page
-			$uri = JFactory::getURI();
-			$url = $uri->toString(array('path', 'query', 'fragment'));
-		}
-
-		return base64_encode($url);
-	}
-
 	function getType()
 	{
 		$user = & JFactory::getUser();
