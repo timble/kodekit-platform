9a5e18bd318a32065a9d41ef8349054c7ad56044
Ercan Ozkaya <ercan@timble.net>
2011-12-15 14:06:11 +0000

    Copyright notices

--- code/administrator/components/com_activities/dispatcher.php
+++ code/administrator/components/com_activities/dispatcher.php
@@ -1,5 +1,22 @@
 <?php
+/**
+ * @version		$Id$
+ * @category	Nooku
+ * @package     Nooku_Components
+ * @subpackage  Activities
+ * @copyright	Copyright (C) 2010 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
 
+/**
+ * Component Dispatcher
+ *
+ * @author      Israel Canasa <http://nooku.assembla.com/profile/israelcanasa>
+ * @category	Nooku
+ * @package    	Nooku_Components
+ * @subpackage 	Activities
+ */
 class ComActivitiesDispatcher extends ComDefaultDispatcher
 {
 	protected function _initialize(KConfig $config)
