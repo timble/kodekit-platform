148a0f6c3f375eb1fd19a1dd238f633082b30ac4
Johan Janssens <johan@timble.net>
2011-12-24 17:47:30 +0000

    re #176 : Refactored the site login module to use mod_default.

---
+++ code/modules/mod_login/tmpl/logout.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version     $Id$
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Banners
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+defined('KOOWA') or die('Restricted access'); ?>
+
+<form action="<?= @route('option=com_users&view=user&id='.$user->id) ?>" method="post" name="login" id="form-login">
+	<input type="hidden" name="action" value="logout" />
+    <? if ($show_greeting) : ?>
+	<div>
+	    <? if ($name) : ?>
+		    <?= JText::sprintf( 'HINAME', $user->name ); ?>
+	    <? else : ?>
+		    <?= JText::sprintf( 'HINAME', $user->username ); ?>
+	    <? endif; ?>
+	</div>
+    <? endif; ?>
+	<div align="center">
+		<input type="submit" name="Submit" class="button" value="<?= @text( 'BUTTON_LOGOUT'); ?>" />
+	</div>
+</form>
