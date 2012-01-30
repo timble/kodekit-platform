c9178d8e6e68ffaaf193eb79e1008bf881b8b376
Johan Janssens <johan@timble.net>
2012-01-30 22:25:30 +0000

    Only load the template modules.php file if it exists.

--- code/site/modules/mod_default/templates/filters/chrome.php
+++ code/site/modules/mod_default/templates/filters/chrome.php
@@ -29,7 +29,10 @@ class ModDefaultTemplateFilterChrome extends KTemplateFilterAbstract implements 
         parent::__construct($config);
         
         include_once JPATH_THEMES.'/system/html/modules.php';
-		include_once JPATH_THEMES.'/'.$config->template.'/html/modules.php';
+        
+        if(file_exists(JPATH_THEMES.'/'.$config->template.'/html/modules.php')) {
+		    include_once JPATH_THEMES.'/'.$config->template.'/html/modules.php';
+        }
     }
 	
 	/**
