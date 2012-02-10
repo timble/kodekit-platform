7fe6303d8790f138e25ba5368801a54b02eac92e
Johan Janssens <johan@timble.net>
2012-02-10 02:14:52 +0000

    Updated copyright date

--- code/administrator/components/com_articles/databases/rows/featured.php
+++ code/administrator/components/com_articles/databases/rows/featured.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * @version     $Id: article.php 3024 2011-10-09 01:44:30Z johanjanssens $
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Featured Article Database Row Class
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ */
+
 class ComArticlesDatabaseRowFeatured extends KDatabaseRowDefault
 {
     protected function _initialize(KConfig $config)
