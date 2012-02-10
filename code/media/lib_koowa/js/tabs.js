eed7dfc4ac971228cec5f37777a3f626e0d65e34
Johan Janssens <johan@timble.net>
2012-02-10 01:50:39 +0000

    Updated copyright date

--- code/media/lib_koowa/js/tabs.js
+++ code/media/lib_koowa/js/tabs.js
@@ -3,7 +3,7 @@
  * @category    Nooku
  * @package     Nooku_Media
  * @subpackage  Javascript
- * @copyright   Copyright (C) 2007 - 2010 Johan Janssens. All rights reserved.
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
