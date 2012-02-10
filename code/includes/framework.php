d7bff467e5a112de8aec49f783589c826469b5af
Johan Janssens <johan@timble.net>
2012-02-10 11:10:41 +0000

    Merged from trunk

--- code/includes/framework.php
+++ code/includes/framework.php
@@ -3,7 +3,7 @@
  * @version     $Id: sections.php 592 2011-03-16 00:30:35Z johanjanssens $
  * @category    Nooku
  * @package     Nooku_Server
- * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
 */
