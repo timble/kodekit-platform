2fa88ae6b37b604e798605d5a9e0946177357e6c
Stian Didriksen <stian@timble.net>
2012-02-24 16:23:16 +0000

    re #65 removing debug call

--- code/administrator/components/com_editors/views/editor/tmpl/default.php
+++ code/administrator/components/com_editors/views/editor/tmpl/default.php
@@ -1,4 +1,4 @@
 <style src="media://com_editors/codemirror/css/docs.css" />
-<?= var_export( $editors) ?>
+
 <?= @template('default_script') ?>
 <textarea id="<?= $name ?>" name="<?= $name ?>" cols="75" rows"25" class="editable"><?= $data ?></textarea>
