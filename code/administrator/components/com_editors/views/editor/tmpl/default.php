773aa929c2b054e444568ba0cdebeda0352d6229
Stian Didriksen <stian@timble.net>
2012-02-27 19:22:56 +0000

    re #65 wrapper needed

--- code/administrator/components/com_editors/views/editor/tmpl/default.php
+++ code/administrator/components/com_editors/views/editor/tmpl/default.php
@@ -1,4 +1,6 @@
 <style src="media://com_editors/codemirror/css/docs.css" />
 
 <?= @template('default_script') ?>
-<textarea id="<?= $name ?>" name="<?= $name ?>" cols="75" rows"25" class="editable"><?= $data ?></textarea>
+<div data-editor="<?= $name ?>">
+<textarea id="<?= $name ?>" name="<?= $name ?>" cols="75" rows"25" class="editable validate-editor"><?= $data ?></textarea>
+</div>
