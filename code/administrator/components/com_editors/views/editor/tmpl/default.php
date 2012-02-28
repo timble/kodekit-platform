e4d1e8176ce69a858296c43d03875fecdee3b7d7
Stian Didriksen <stian@timble.net>
2012-02-28 15:39:10 +0000

    re #65 wrap no longer needed

--- code/administrator/components/com_editors/views/editor/tmpl/default.php
+++ code/administrator/components/com_editors/views/editor/tmpl/default.php
@@ -1,6 +1,5 @@
 <style src="media://com_editors/codemirror/css/docs.css" />
 
 <?= @template('default_script') ?>
-<div data-editor="<?= $name ?>">
+
 <textarea id="<?= $name ?>" name="<?= $name ?>" cols="75" rows"25" class="editable-<?= $name ?> validate-editor"><?= $data ?></textarea>
-</div>
