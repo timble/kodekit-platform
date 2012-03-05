351664d0f1f40437126f18a9348f305898e55eb1
Tom Janssens <tom@timble.net>
2012-03-05 12:26:17 +0000

    re #199 Synced login markup with stylesheets

--- code/administrator/components/com_users/views/login/tmpl/form.php
+++ code/administrator/components/com_users/views/login/tmpl/form.php
@@ -12,39 +12,35 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 
 <h1><?= @text('Administration Login') ?></h1>
 
+<!--
 <script src="media://lib_koowa/js/koowa.js" />
 <style src="media://lib_koowa/css/koowa.css" />
+-->
 
 <?= @helper('behavior.keepalive'); ?>
 
 <form action="<?= @route('view=user') ?>" method="post" name="login" id="form-login">
     <input type="hidden" name="action" value="login" />
-
-    <p id="form-login-username">
-        <label for="username">
-            <?= @text('Username') ?><br />
-            <input name="username" id="username" type="text" class="inputbox" size="20" autofocus="autofocus" placeholder="<?= @text('Username') ?>" />
-        </label>
-    </p>
-
-    <p id="form-login-password">
-        <label for="password"><?= @text('Password') ?></label><br />
-        <input name="password" type="password" id="password" class="inputbox" size="15" placeholder="<?= @text('Password') ?>" />
-    </p>
-    <p id="form-login-site">
-     	<label for="modlgn_site"><?php echo JText::_('Site'); ?></label><br />
-        <?= @service('com://admin/sites.template.helper.listbox')->sites(array('attribs' => array('class' => 'inputbox'))); ?>
-  	</p>
+    <div class="control-group">
+        <label class="control-label" for=""><?= @text( 'Username' ); ?></label>
+        <div class="controls">
+        <input name="username" id="username" type="text" class="inputbox" size="20" autofocus="autofocus" placeholder="<?= @text('Username') ?>" />
+        </div>
+    </div>
+   <div class="control-group">
+       <label class="control-label" for=""><?= @text( 'Password' ); ?></label>
+       <div class="controls">
+            <input name="password" type="password" id="password" class="inputbox" size="15" placeholder="<?= @text('Password') ?>" />
+       </div>
+   </div>
+   <div class="control-group">
+       <label class="control-label" for="site"><?php echo JText::_('Site'); ?></label>
+       <div class="controls">
+            <?= @service('com://admin/sites.template.helper.listbox')->sites(array('attribs' => array('class' => 'inputbox'))); ?>
+       </div>
+   </div>
     <? if($error = JError::getError(true)) : ?>
         <p id="login-error-message"><?= $error->get('message') ?></p>
     <? endif ?>
-    <div class="button_holder">
-        <div class="button1">
-            <a onclick="login.submit();">
-                <?= @text('Login') ?>
-            </a>
-        </div>
-    </div>
-    <div class="clr"></div>
-    <input type="submit" style="border: 0; padding: 0; margin: 0; width: 0px; height: 0px;" value="<?= @text('Login') ?>" />
+    <input type="submit" class="btn" value="<?= @text('Login') ?>" />
 </form>
