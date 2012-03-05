e6afb9189e09ac13f9d1487271f634272afc2440
Johan Janssens <johan@timble.net>
2012-03-05 14:57:36 +0000

    re #230 : PlgKoowaDefault now extends from KEventSubscriberDefault. Added 'event_publishers' config option to lazy wire the plugin to different event publishers. To use : pass in a list of identifiers of an object that implements KMixinEvent.

--- code/plugins/koowa/default.php
+++ code/plugins/koowa/default.php
@@ -12,8 +12,27 @@
 /**
  * Default Koowa plugin
  * 
- * Koowa plugins can handle a number of events that are dynamically generated. The following 
- * is a list of available events. This list is not meant to be exclusive.
+ * Koowa plugins can handle a number of events that are dynamically generated. A plugin 
+ * need to be wired to a specific event publisher in order for it to receive events from
+ * that publisher. 
+ * 
+ * <code>
+ * <?php
+ * class PlgKoowaFoo extends PlgKoowaDefault
+ * {
+ *   protected function _initialize(KConfig $config)
+ *   {
+ *       $config->append(array(
+ *           'event_publishers' => array('com://admin/foo.controller.bar')
+ *       ));
+ *        
+ *       parent::_initialize($config);
+ *   }	
+ * }
+ * </code>
+ * 
+ * 
+ * The following is a list of available events. This list is not meant to be exclusive.
  * 
  * onBeforeController[Action]
  * onAfterController[Action]
@@ -31,8 +50,8 @@
  * {
  * 		public function onBeforeControllerBrowse(KEvent $event)
  * 		{
- * 			//The caller is a reference to the object that is triggering this event
- * 			$caller = $event->caller;
+ * 			//The publisher is a reference to the object that is triggering this event
+ * 			$publisher = $event->getPublisher();
  * 
  * 			//The result is the actual result of the event, if this is an after event 
  * 			//the result will contain the result of the action.
@@ -49,7 +68,7 @@
  * @package     Koowa_Plugins
  * @subpackage  Koowa
  */
-abstract class PlgKoowaDefault extends KEventListener
+abstract class PlgKoowaDefault extends KEventSubscriberDefault
 {
 	/**
 	 * A JRegistry object holding the parameters for the plugin
@@ -59,52 +78,49 @@ abstract class PlgKoowaDefault extends KEventListener
 	protected $_params	= null;
 
 	/**
-	 * The name of the plugin
-	 *
-	 * @var		string
-	 */
-	protected $_name = null;
-
-	/**
-	 * The plugin type
-	 *
-	 * @var		string
-	 */
-	protected $_type = null;
-	
-	/**
 	 * Constructor
 	 */
-	function __construct($dispatcher, $config = array())
+	public function __construct($dispatcher, $config = array())
 	{
-		if (isset($config['params']))
-		{
-		    if ($config['params'] instanceof JRegistry) {
-				$this->_params = $config['params'];
-			} else {
-				$this->_params = new JRegistry;
-				$this->_params->loadINI($config['params']);
-			}
-		}
-
-		if ( isset( $config['name'] ) ) {
-			$this->_name = $config['name'];
-		}
-
-		if ( isset( $config['type'] ) ) {
-			$this->_type = $config['type'];
-		}
-		
-		//Inject the identifier
-		$config['service_identifier'] = KService::getIdentifier('plg:koowa.'.$this->_name);
+	    $config = new KConfig($config);
+	    
+	    //Inject the identifier
+		$config->service_identifier = KService::getIdentifier('plg:koowa.'.$config['name']);
 		
 		//Inject the service container
-		$config['service_container'] = KService::getInstance();
+		$config->service_container = KService::getInstance();
 		
-		//Inject the dispatcher
-		$config['dispatcher'] = $dispatcher;
-
-		parent::__construct(new KConfig($config));
+		parent::__construct($config);
+	
+		//Set the plugin params
+	    if(is_string($config->params)) {
+            $config->params = $this->_parseParams($config->params);
+        }
+        
+        $this->_params = $config->params;
+        
+        //Setup lazy wiring for publishers we are subscribing too
+        foreach($config->event_publishers as $publisher) {
+            KService::setConfig($publisher, array('event_subscribers' => array($this)));
+        }
+	}
+	
+	/**
+	 * Initializes the options for the object
+	 *
+	 * Called from {@link __construct()} as a first step of object instantiation.
+	 *
+	 * @param   object  An optional KConfig object with configuration options.
+	 * @return  void
+	 */
+	protected function _initialize(KConfig $config)
+	{
+	    $config->append(array(
+        	'params'           => array(),
+        	'event_publishers' => array()
+        ));
+	    
+	    parent::_initialize($config);
 	}
 	
 	/**
@@ -117,9 +133,38 @@ abstract class PlgKoowaDefault extends KEventListener
 	public function loadLanguage($extension = '', $basePath = JPATH_BASE)
 	{
 		if(empty($extension)) {
-			$extension = 'plg_'.$this->_type.'_'.$this->_name;
+		    $extension = 'plg_'.$this->getIdentifier()->package.'_'.$this->getIdentifier()->name;
 		}
 
 		return JFactory::getLanguage()->load( strtolower($extension), $basePath);
+	}
+	
+	/**
+	 * Method to extract key/value pairs out of a string
+	 *
+	 * @param   string  String containing the parameters
+	 * @return  array   Key/Value pairs for the attributes
+	 */
+	protected function _parseParams( $string )
+	{
+	    $params = array();
+	
+	    if(!version_compare(JVERSION,'1.6.0','ge'))
+	    {
+	        $string = trim($string);
+	
+	        if(!empty($string))
+	        {
+	            foreach(explode("\n", $string) as $line)
+	            {
+	                $param = explode("=", $line, 2);
+	                $params[$param[0]] = $param[1];
+	            }
+	        }
+	    }
+	    else $params = json_decode($string);
+	     
+	    $params = new KConfig($params);
+	    return $params;
 	}
 }
