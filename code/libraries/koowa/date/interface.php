b6eeb998b29917b860166aff58c365bd40720535
Johan Janssens <johan@timble.net>
2012-03-09 23:12:38 +0000

    re #233 : Refactored KDate to use DateTime API.

---
+++ code/libraries/koowa/date/interface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @category    Koowa
+ * @package     Koowa_Date
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Date Interface
+ *
+ * @author  Gergo Erdosi <gergo@timble.net>
+ */
+interface KDateInterface implements KObjectHandlable
+{
+    
+}
