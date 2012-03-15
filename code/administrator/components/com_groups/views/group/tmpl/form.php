d2fa9471d86737e14b13595b1fcb568ad32a32a1
Tom Janssens <tom@timble.net>
2012-03-15 10:55:46 +0000

    Synced markup with recent Nooku Server changes

--- code/administrator/components/com_groups/views/group/tmpl/form.php
+++ code/administrator/components/com_groups/views/group/tmpl/form.php
@@ -18,14 +18,15 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 <?= @template('com://admin/default.view.form.toolbar'); ?>
 
 <form action="" method="post" class="-koowa-form" id="group-form">
-    <div class="grid_8">
-        <div class="panel title group">
-            <input class="inputbox required" type="text" name="name" id="title" size="40" maxlength="255" value="<?= $group->name ?>" placeholder="<?= @text('Name') ?>" />
+    <div class="editor-container">
+        <div class="title">
+            <input class="required" type="text" name="name" maxlength="255" value="<?= $group->name ?>" placeholder="<?= @text('Group name') ?>" />
         </div>
-        
-        <div class="panel">
-            <h3><?= @text( 'Group' ); ?></h3>
-        	<?= @helper('select.groups', array('name' => 'target_id', 'selected' => $group->parent_id, 'exclude' => $group)) ?>
+        <div class="editor">
+	        <div class="panel">
+	            <h3><?= @text( 'Group' ); ?></h3>
+	        	<?= @helper('select.groups', array('name' => 'target_id', 'selected' => $group->parent_id, 'exclude' => $group)) ?>
+	       	</div>
        	</div>
     </div>
 </form>
