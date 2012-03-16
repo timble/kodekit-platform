f706bcc4f27720c43495dd5bc8d41063432cd977
Arunas Mazeika <arunas@timble.net>
2012-03-16 08:55:03 +0000

    Cleanup.
    Re-direction to referrer (via redirect callback method) is now performed after add event.
    
    re #77

--- code/site/components/com_comments/aliases.php
+++ code/site/components/com_comments/aliases.php
@@ -1,3 +1,2 @@
 <?php
-KService::setAlias('com://site/comments.command.authorize', 'com://admin/comments.command.authorize');
 KService::setAlias('com://site/comments.model.comments', 'com://admin/comments.model.comments');
--- code/site/components/com_comments/controllers/comment.php
+++ code/site/components/com_comments/controllers/comment.php
@@ -7,21 +7,10 @@ class ComCommentsControllerComment extends ComDefaultControllerDefault
     {
         parent::__construct($config);
         
-        $this->registerCallback('after.save', array($this, 'afterSave'));
+        $this->registerCallback('after.add', array($this, 'redirect'));
     }
     
-    protected function _initialize(KConfig $config)
-    {
-        $config->append(array(
-            'model' => 'com://site/comments.model.comments'
-        ));
-        
-        $config->request->created_by = JFactory::getUser()->id;
-        
-        parent::_initialize($config);
-    }
-    
-    public function afterSave(KCommandContext $context)
+    public function redirect(KCommandContext $context)
     {
         $url = ($referrer = KRequest::get('post.referrer', 'base64')) ? base64_decode($referrer) : KRequest::referrer();
         $this->setRedirect($url);
