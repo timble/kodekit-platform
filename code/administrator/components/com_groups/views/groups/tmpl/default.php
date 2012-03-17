597975f725ee1326363c451432ed591d1fe9e8b3
Tom Janssens <tom@timble.net>
2012-03-17 14:58:37 +0000

    Synced markup with recent changes in Nooku Server UI

--- code/administrator/components/com_groups/views/groups/tmpl/default.php
+++ code/administrator/components/com_groups/views/groups/tmpl/default.php
@@ -3,34 +3,31 @@
  * @version     $Id$
  * @category	Nooku
  * @package     Nooku_Components
- * @subpackage  Debug
+ * @subpackage  Groups
  * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
 defined('KOOWA') or die( 'Restricted access' ); ?>
 
+<!--
 <script src="media://lib_koowa/js/koowa.js" />
 <style src="media://lib_koowa/css/koowa.css" />
+-->
 
 <?= @template('com://admin/default.view.grid.toolbar'); ?>
 
 <form action="" method="get" class="-koowa-grid">
-    <table class="adminlist">
+    <?= @template('default_scopebar') ?>
+    <table>
         <thead>
             <tr>
-                <th width="10"></th>
+                <th width="10">
+                    <?= @helper('grid.checkall'); ?>
+                </th>
                 <th>
                     <?= @helper('grid.sort', array('column' => 'Name')) ?>
                 </th>
-            </tr>
-            <tr>
-                <td align="center">
-                     <?= @helper( 'grid.checkall') ?>
-                </td>
-                <td colspan="5">
-                    <?= @helper( 'grid.search') ?>
-                </td>
             </tr>
         </thead>
 
---
+++ code/administrator/components/com_groups/views/groups/tmpl/default_scopebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @version     $Id: $
+ * @category	Nooku
+ * @package     Nooku_Components
+ * @subpackage  Groups
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+defined('KOOWA') or die( 'Restricted access' ); ?>
+
+<div class="scopebar">
+	<div class="form-search">
+		<?= @helper('grid.search') ?>
+	</div>
+</div>
