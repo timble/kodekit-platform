d17125f9ed96806c76e1bf6f286ba8853e11ad7d
Johan Janssens <johan@timble.net>
2012-04-02 18:27:01 +0000

    re #101 : Added specialised newsfeeds categories model.

---
+++ code/administrator/components/com_newsfeeds/models/categories.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @version		$Id$
+ * @category	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Newsfeeds
+ * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+
+/**
+ * Category Model Class
+ *
+ * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @category 	Nooku
+ * @package     Nooku_Server
+ * @subpackage  Newsfeeds
+ */
+class ComNewsfeedsModelCategories extends ComDefaultModelDefault
+{
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'table' => 'com://admin/categories.database.table.categories'
+        ));
+
+        parent::_initialize($config);
+    }
+    
+    protected function _buildQueryColumns(KDatabaseQuery $query)
+    {
+		parent::_buildQueryColumns($query);
+
+		$query->select('COUNT(newsfeeds.id) AS numlinks');
+    }
+
+	protected function _buildQueryGroup(KDatabaseQuery $query)
+    {
+		parent::_buildQueryGroup($query);
+		
+		$query->group('tbl.id');
+    }
+
+    protected function _buildQueryJoins(KDatabaseQuery $query)
+    {
+		parent::_buildQueryJoins($query);
+		
+		$query->join('LEFT', 'newsfeeds AS newsfeeds', 'newsfeeds.catid = tbl.id');
+    }
+
+	protected function _buildQueryOrder(KDatabaseQuery $query)
+    {
+		parent::_buildQueryOrder($query);
+		
+		$query->order('tbl.ordering', 'DESC');
+    }
+
+	protected function _buildQueryWhere(KDatabaseQuery $query)
+    {
+		parent::_buildQueryWhere($query);
+		
+		$query->where('tbl.section', '=', 'com_newsfeeds')
+			  ->where('tbl.published', '=', '1')
+			  ->where('newsfeeds.published', '=', '1')
+			  ->where('tbl.access', '<=', JFactory::getUser()->get('aid', '0'));
+    }
+}
