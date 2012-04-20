b3c321b8efe106178175aac6a0569a15c6bbe14e
Gergo Erdosi <gergo@timble.net>
2012-04-20 21:28:11 +0000

    Fix "Undefined variable: list" notice.

--- code/administrator/components/com_activities/views/activities/tmpl/simple.php
+++ code/administrator/components/com_activities/views/activities/tmpl/simple.php
@@ -11,36 +11,38 @@
 
 defined('KOOWA') or die('Restricted access') ?>
 
-<?
-foreach ($activities as $activity) {
-	$list[substr($activity->created_on, 0, 10)][] = $activity;
-}
-?>
-
-<div id="activities-list">
-	<? foreach($list as $date => $activities) : ?>
-		<h4><?= @helper('date.humanize', array('date' => $date)) ?></h4>
-		<div class="activities">
-			<? foreach($activities as $activity) : ?>
-			<div class="activity">
-				<i class="icon-<?= $activity->action ?>"></i>
-				<div>
-					<div class="ellipsis">
-						<a href="<?= @route('view=article&id='.$activity->row) ?>">
-						   <?= @escape($activity->title) ?>
-						</a>
-					</div>
-					 <div class="ellipsis">
-					 	<small class="datetime">
-					 		<?= date("H:i", strtotime($activity->created_on)) ?> - <?= $activity->created_by_name ?>
-					 	</small>
-					 </div>
-				</div>
-			</div>
-			<? endforeach ?>
-		</div>
-	<? endforeach ?>
-	<a class="btn" href="<?= @route('option=com_activities&view=activities') ?>">
-	   <?= @text('More activities') ?>
-	</a>
-</div>
+<? if(count($activities)) : ?>
+    <?
+    foreach ($activities as $activity) {
+    	$list[substr($activity->created_on, 0, 10)][] = $activity;
+    }
+    ?>
+    
+    <div id="activities-list">
+    	<? foreach($list as $date => $activities) : ?>
+    		<h4><?= @helper('date.humanize', array('date' => $date)) ?></h4>
+    		<div class="activities">
+    			<? foreach($activities as $activity) : ?>
+    			<div class="activity">
+    				<i class="icon-<?= $activity->action ?>"></i>
+    				<div>
+    					<div class="ellipsis">
+    						<a href="<?= @route('view=article&id='.$activity->row) ?>">
+    						   <?= @escape($activity->title) ?>
+    						</a>
+    					</div>
+    					 <div class="ellipsis">
+    					 	<small class="datetime">
+    					 		<?= date("H:i", strtotime($activity->created_on)) ?> - <?= $activity->created_by_name ?>
+    					 	</small>
+    					 </div>
+    				</div>
+    			</div>
+    			<? endforeach ?>
+    		</div>
+    	<? endforeach ?>
+    	<a class="btn" href="<?= @route('option=com_activities&view=activities') ?>">
+    	   <?= @text('More activities') ?>
+    	</a>
+    </div>
+<? endif ?>
