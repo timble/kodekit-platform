b6ddddbfa2cb88a7915c4de85d555f67438a74a2
Tom Janssens <tom@timble.net>
2012-05-01 11:29:30 +0000

    re #196 Refactored core output for mod_search

--- code/modules/mod_search/tmpl/default.php
+++ code/modules/mod_search/tmpl/default.php
@@ -13,38 +13,41 @@ defined('KOOWA') or die('Restricted access'); ?>
 
 <form action="<?= JRoute::_('index.php?option=com_search'); ?>" method="get">
 
-<div class="search<?= $moduleclass_sfx; ?>">
-<? $output = '<input name="term" id="mod_search_term" maxlength="' . $maxlength . '" alt="' . $button_text . '" class="inputbox' . $moduleclass_sfx . '" type="text" size="' . $width . '" value="' . $text . '"  onblur="if(this.value==\'\') this.value=\'' . $text . '\';" onfocus="if(this.value==\'' . $text . '\') this.value=\'\';" />';
+<div class="control-group">
+	<div class="controls">
+			<? $output = '<input name="term" id="mod_search_term" maxlength="' . $maxlength . '" alt="' . $button_text . '" class="inputbox' . $moduleclass_sfx . '" type="text" size="' . $width . '" value="' . $text . '"  onblur="if(this.value==\'\') this.value=\'' . $text . '\';" onfocus="if(this.value==\'' . $text . '\') this.value=\'\';" />';
+						
+			    if($button) :
+					$button = '<input type="submit" value="' . $button_text . '" class="btn' . $moduleclass_sfx . '" onclick="this.form.searchword.focus();"/>';
+				endif;
+						
+				switch($button_pos) :
+						
+				    case 'top':
+						$button = $button . '<br />';
+						$output = $button . $output;
+						break;
+							
+					case 'bottom':
+						$button = '<br />' . $button;
+						$output = $output . $button;
+						break;
+							
+					case 'right':
+						$output = $output . $button;
+						break;
+							
+					case 'left':
+					default:
+						$output = $button . $output;
+						break;
 			
-    if($button) :
-		$button = '<input type="submit" value="' . $button_text . '" class="button' . $moduleclass_sfx . '" onclick="this.form.searchword.focus();"/>';
-	endif;
-			
-	switch($button_pos) :
-			
-	    case 'top':
-			$button = $button . '<br />';
-			$output = $button . $output;
-			break;
-				
-		case 'bottom':
-			$button = '<br />' . $button;
-			$output = $output . $button;
-			break;
-				
-		case 'right':
-			$output = $output . $button;
-			break;
-				
-		case 'left':
-		default:
-			$output = $button . $output;
-			break;
-
-	endswitch;
-			
-	echo $output;
-?>
+				endswitch;
+						
+				echo $output;
+			?>
+	</div>
 </div>
+
 <input type="hidden" name="Itemid" value="<?php echo $itemid; ?>" />
 </form>
