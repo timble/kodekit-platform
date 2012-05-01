f7a6db363293ce0ec212e7f5d00c0f7c14b20ced
Tom Janssens <tom@timble.net>
2012-05-01 11:28:55 +0000

    re #196 Refactored core output for mod_breadcrumbs

--- code/modules/mod_breadcrumbs/tmpl/default.php
+++ code/modules/mod_breadcrumbs/tmpl/default.php
@@ -1,18 +1,19 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access'); ?>
-<span class="breadcrumbs pathway" itemprop="breadcrumb">
-<?php for ($i = 0; $i < $count; $i ++) :
 
-    if ($params->get('showLast', 1) || $i < $count -1) {
-		if(!empty($list[$i]->link)) {
-			echo '<a href="'.$list[$i]->link.'" class="pathway">'.$list[$i]->name.'</a>';
-		} else {
-			echo $list[$i]->name;
-		}
-	}
-	
-	if ($i < $count -1) {
-		echo ' '.$separator.' ';
-	}
-endfor; ?>
-</span>
+<ul class="breadcrumb">
+<?php for ($i = 0; $i < $count; $i ++) : ?>
+
+	<? // If not the last item in the breadcrumbs add the separator ?>
+	<? if ($i < $count - 2) : ?>
+		<? if(!empty($list[$i]->link)) : ?>
+			<li><?= '<a href="'.$list[$i]->link.'" class="pathway">'.$list[$i]->name.'</a>'; ?></li>
+		<? else : ?>
+			<li><?= $list[$i]->name; ?></li>
+		<? endif; ?>
+		<span class="divider">/</span>
+	<? elseif ($params->get('showLast', 1)) : // when $i == $count -1 and 'showLast' is true ?>
+	    <li><?=  $list[$i]->name; ?></li>
+	<? endif; ?>
+<? endfor; ?>
+</ul>
