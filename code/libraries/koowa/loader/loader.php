6663d1da4ba6aba29b2fa9fc23c76b6790e980cf
Johan Janssens <johan@timble.net>
2012-05-03 19:57:22 +0000

    Fixed typo

--- code/libraries/koowa/loader/loader.php
+++ code/libraries/koowa/loader/loader.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 	$Id$†
+ * @version 	$Id$
  * @package		Koowa_Loader
  * @copyright	Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
