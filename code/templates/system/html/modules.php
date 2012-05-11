ce5d5d062c2333303c81d53e500d92459dad6b7a
Tom Janssens <tom@timble.net>
2012-05-11 19:58:32 +0000

    re #196 Removed unused styles from system module chrome and added module type as a class attribute to the outer element

--- code/templates/system/html/modules.php
+++ code/templates/system/html/modules.php
@@ -23,75 +23,18 @@ function modChrome_none($module, &$params, &$attribs)
 }
 
 /*
- * Module chrome that wraps the module in a table
- */
-function modChrome_table($module, &$params, &$attribs)
-{ ?>
-	<table cellpadding="0" cellspacing="0" class="moduletable<?php echo $params->get('moduleclass_sfx'); ?>">
-	<?php if ($module->showtitle != 0) : ?>
-		<tr>
-			<th valign="top">
-				<?php echo $module->title; ?>
-			</th>
-		</tr>
-	<?php endif; ?>
-		<tr>
-			<td>
-				<?php echo $module->content; ?>
-			</td>
-		</tr>
-		</table>
-	<?php
-}
-
-/*
- * Module chrome that wraps the tabled module output in a <td> tag of another table
- */
-function modChrome_horz($module, &$params, &$attribs)
-{ ?>
-	<table cellspacing="1" cellpadding="0" border="0" width="100%">
-		<tr>
-			<td valign="top">
-				<?php modChrome_table($module, $params, $attribs); ?>
-			</td>
-		</tr>
-	</table>
-	<?php
-}
-
-/*
  * xhtml (divs and font headder tags)
  */
 function modChrome_xhtml($module, &$params, &$attribs)
 {
 	if (!empty ($module->content)) : ?>
-		<div class="moduletable<?php echo $params->get('moduleclass_sfx'); ?>">
+		<div class="module <?php echo $module->module; ?> <?php echo $params->get('moduleclass_sfx'); ?>">
 		<?php if ($module->showtitle != 0) : ?>
 			<h3><?php echo $module->title; ?></h3>
 		<?php endif; ?>
 			<?php echo $module->content; ?>
 		</div>
 	<?php endif;
-}
-
-/*
- * Module chrome that allows for rounded corners by wrapping in nested div tags
- */
-function modChrome_rounded($module, &$params, &$attribs)
-{ ?>
-		<div class="module<?php echo $params->get('moduleclass_sfx'); ?>">
-			<div>
-				<div>
-					<div>
-						<?php if ($module->showtitle != 0) : ?>
-							<h3><?php echo $module->title; ?></h3>
-						<?php endif; ?>
-					<?php echo $module->content; ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	<?php
 }
 
 /*
