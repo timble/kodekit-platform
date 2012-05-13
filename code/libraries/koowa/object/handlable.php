2027dd2fea9c7e90dec677b60562e09d0065543e
Johan Janssens <johan@timble.net>
2012-05-13 16:08:49 +0000

    Docblocks

--- code/libraries/koowa/object/handlable.php
+++ code/libraries/koowa/object/handlable.php
@@ -22,9 +22,6 @@ interface KObjectHandlable
 	 * This function returns an unique identifier for the object. This id can be used as
 	 * a hash key for storing objects or for identifying an object
 	 *
-	 * Override this function to implement implement dynamic commands. If you don't want
-	 * the command to be enqueued in a chain return NULL instead of a valid handle.
-	 *
 	 * @return string A string that is unique, or NULL
 	 */
 	public function getHandle();
