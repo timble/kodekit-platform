7750fa5257a54e399469b2041759c6813895edb4
Johan Janssens <johan@timble.net>
2012-06-05 20:49:38 +0000

    re #121 : Applied patch query-17

--- code/libraries/koowa/database/adapter/mysqli.php
+++ code/libraries/koowa/database/adapter/mysqli.php
@@ -203,8 +203,12 @@ class KDatabaseAdapterMysqli extends KDatabaseAdapterAbstract
 	 */
 	public function getDatabase()
 	{
-	    if(!isset($this->_database)) {
-	        $this->_database = $this->select("SELECT DATABASE()", KDatabase::FETCH_FIELD);
+	    if(!isset($this->_database))
+	    {
+	        $query = $this->getService('koowa:database.query.select')
+	            ->columns('DATABASE()');
+	        
+	        $this->_database = $this->select($query, KDatabase::FETCH_FIELD);
 	    } 
 	    
 	    return $this->_database;
