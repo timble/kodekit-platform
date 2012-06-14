c89bef2ae0e941cbdc79d2d1e5c2d61ee83cf7a9
Arunas Mazeika <arunas@timble.net>
2012-06-14 11:11:23 +0000

    Updated latest news module.
    re #180.

---
+++ code/modules/mod_latestnews/html.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * @version        $Id$
+ * @category       Nooku
+ * @package        Nooku_Server
+ * @subpackage     Articles
+ * @copyright      Copyright (C) 2009 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
+ */
+
+/**
+ * Latest news module html view class.
+ *
+ * @author     Arunas Mazeika <http://nooku.assembla.com/profile/arunasmazeika>
+ * @category   Nooku
+ * @package    Nooku_Server
+ * @subpackage Articles
+ */
+class ModLatestnewsHtml extends ModDefaultHtml
+{
+    /**
+     * Renders the views output
+     *
+     * @return string
+     */
+    public function display() {
+
+        $this->assign('params', $this->params);
+
+        // Preparing the sort and direction model states.
+        switch ($this->params->get('sort_by', 'added_on')) {
+            default:
+            case 'newest':
+                $sort      = 'created';
+                $direction = 'DESC';
+                break;
+            case 'oldest':
+                $sort      = 'created';
+                $direction = 'ASC';
+                break;
+            case 'ordering':
+                $sort      = 'ordering';
+                $direction = 'ASC';
+                break;
+        }
+
+        // Prepare category state.
+        $category = str_replace(' ', '', $this->params->get('category', ''));
+        if ($category) {
+            $category = explode(',', $category);
+        }
+
+        // Prepare section state.
+        $section = str_replace(' ', '', $this->params->get('section', ''));
+        if ($section) {
+            $section = explode(',', $section);
+        }
+
+        // Get access id.
+        $user = JFactory::getUser();
+        $aid  = $user->get('aid', 0);
+
+        $articles = $this->getService('com://admin/articles.model.articles')
+            ->set(array(
+            'aid'       => $aid,
+            'state'     => 1,
+            'limit'     => $this->params->get('count', 5),
+            'sort'      => $sort,
+            'direction' => $direction,
+            'section'   => $section,
+            'category'  => $category,
+            'featured'  => $this->params->get('show_featured', false)))
+            ->getList();
+
+        $this->assign('articles', $articles);
+
+        return parent::display();
+    }
+}
--- code/modules/mod_latestnews/tmpl/default.php
+++ code/modules/mod_latestnews/tmpl/default.php
@@ -1,10 +1,24 @@
-<?php // no direct access
-defined('_JEXEC') or die('Restricted access'); ?>
+<?php
+/**
+ * @version        $Id$
+ * @category       Nooku
+ * @package        Nooku_Server
+ * @subpackage     Articles
+ * @copyright      Copyright (C) 2009 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @author         Arunas Mazeika <http://nooku.assembla.com/profile/arunasmazeika>
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
+ */
+defined('KOOWA') or die('Restricted access'); ?>
+
 <ul class="latestnews<?php echo $params->get('moduleclass_sfx'); ?>">
-<?php foreach ($list as $item) :  ?>
-	<li class="latestnews<?php echo $params->get('moduleclass_sfx'); ?>">
-		<a href="<?php echo $item->link; ?>" class="latestnews<?php echo $params->get('moduleclass_sfx'); ?>">
-			<?php echo $item->text; ?></a>
-	</li>
-<?php endforeach; ?>
+    <?php foreach ($articles as $article): ?>
+    <li class="latestnews<?php echo $params->get('moduleclass_sfx'); ?>">
+        <?php echo @helper('com://site/articles.template.helper.article.link',
+        array(
+            'row'     => $article,
+            'text'    => @escape($article->title),
+            'attribs' => array('class' => 'latestnews' . $params->get('moduleclass_sfx'))));?>
+    </li>
+    <?php endforeach; ?>
 </ul>
