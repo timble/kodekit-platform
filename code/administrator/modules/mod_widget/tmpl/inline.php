3282d7749bc90a080dc8c86213848138e09e323f
Gergo Erdosi <gergo@timble.net>
2012-06-18 17:23:07 +0000

    Add front-end mod_widget, fix inline layout.

--- code/administrator/modules/mod_widget/tmpl/inline.php
+++ code/administrator/modules/mod_widget/tmpl/inline.php
@@ -14,9 +14,4 @@ $parts   = $url->getQuery(true);
 $package = substr($parts['option'], 4);
 $view    = $parts['view'];
 
-unset($parts['option']);
-unset($parts['view']);
-
-$action =  KInflector::isSingular($view) ? 'read' : 'browse';
-
-echo @service('admin::com.'.$package.'.controller.'.KInflector::singularize($view))->setRequest($parts)->$action();
+echo @service('com://site/'.$package.'.controller.'.KInflector::singularize($view), array('request' => $parts))->display();
