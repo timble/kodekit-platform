2f3d7df51631f2f058a61afbcf7697183f8e6681
Tom Janssens <tom@timble.net>
2012-06-29 10:21:12 +0000

    Synced HTML markup with recent changes in Nooku Server

--- code/administrator/components/com_activities/views/activities/tmpl/default_sidebar.php
+++ code/administrator/components/com_activities/views/activities/tmpl/default_sidebar.php
@@ -27,59 +27,59 @@ window.addEvent('domready', function(){
 });
 </script>
 
-<div class="sidebar">
-	<h3><?=@text( 'Components' )?></h3>
-	<ul>
-		<li class="<?= empty($state->package) ? 'active' : ''; ?>">
-			<a href="<?= @route('package=') ?>">
-		    <?= @text('All components')?>
-			</a>
+
+<h3><?=@text( 'Components' )?></h3>
+<ul>
+	<li class="<?= empty($state->package) ? 'active' : ''; ?>">
+		<a href="<?= @route('package=') ?>">
+		<?= @text('All components')?>
+		</a>
+	</li>
+	<?php foreach ($packages as $package): ?>
+		<?php if ($package->id == $state->package): ?>
+			<li class="active">
+		<?php else: ?> <li> <?php endif ?>
+			<a href="<?=@route('package='.$package->id)?>"><?=ucfirst($package->package)?></a>
 		</li>
-	    <?php foreach ($packages as $package): ?>
-		    <?php if ($package->id == $state->package): ?>
-				<li class="active">
-		    <?php else: ?> <li> <?php endif ?>
-				<a href="<?=@route('package='.$package->id)?>"><?=ucfirst($package->package)?></a>
-			</li>
-	    <?php endforeach ?>
-	</ul>
+	<?php endforeach ?>
+</ul>
 
-	<div class="activities-filter">
-		<h3><?=@text( 'Filters' )?></h3>
+<div class="activities-filter">
+	<h3><?=@text( 'Filters' )?></h3>
 
-		<form action="" method="get" id="activities-filter">
-			<fieldset>
-				<h4><?=@text( 'Start Date' )?></h4>
-				<div class="activities-calendar">
-					<?= @helper('behavior.calendar',
-							array(
-								'date' => $state->start_date,
-								'name' => 'start_date',
-								'format' => '%Y-%m-%d'
-							)); ?>
-				</div>
+	<form action="" method="get" id="activities-filter">
+		<fieldset>
+			<h4><?=@text( 'Start Date' )?></h4>
+			<div class="activities-calendar">
+				<?= @helper('behavior.calendar',
+						array(
+							'date' => $state->start_date,
+							'name' => 'start_date',
+							'format' => '%Y-%m-%d'
+						)); ?>
+			</div>
 
-				<h4><?=@text( 'Days Back' )?></h4>
-				<div class="activities-days-back">
-					<input type="text" size="3" name="days_back" value="<?=($state->days_back) ? $state->days_back : '' ?>" />
-				</div>
+			<h4><?=@text( 'Days Back' )?></h4>
+			<div class="activities-days-back">
+				<input type="text" size="3" name="days_back" value="<?=($state->days_back) ? $state->days_back : '' ?>" />
+			</div>
 
-				<h4><?=@text( 'User' )?></h4>
-				<div>
-					<?= @helper('com://admin/users.template.helper.listbox.users',
-							array(
-								'autocomplete' => true,
-								'name'		   => 'user',
-								'validate'     => false,
-								'attribs'      => array('size' => 30),
-							)) ?>
-				</div>
+			<h4><?=@text( 'User' )?></h4>
+			<div>
+				<?= @helper('com://admin/users.template.helper.listbox.users',
+						array(
+							'autocomplete' => true,
+							'name'		   => 'user',
+							'validate'     => false,
+							'attribs'      => array('size' => 30),
+						)) ?>
+			</div>
 
-				<div class="activities-buttons">
-					<input type="submit" name="submitfilter" class="btn" value="<?=@text('Filter')?>" />
-					<input type="reset" name="cancelfilter" class="btn" value="<?=@text('Reset')?>" />
-				</div>
-			</fieldset>
-		</form>
-	</div>
+			<div class="activities-buttons">
+				<input type="submit" name="submitfilter" class="btn" value="<?=@text('Filter')?>" />
+				<input type="reset" name="cancelfilter" class="btn" value="<?=@text('Reset')?>" />
+			</div>
+		</fieldset>
+	</form>
 </div>
+
