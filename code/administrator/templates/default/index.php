e6ca9d069daa48a587e3a7644104312ae1752bef
Tom Janssens <tom@timble.net>
2012-06-29 15:21:50 +0000

    re #199 Adding legacy for non-flexbox (Flexible Box Model) supported browsers

--- code/administrator/templates/default/index.php
+++ code/administrator/templates/default/index.php
@@ -8,9 +8,13 @@
 <jdoc:include type="head" />
 
 <link rel="stylesheet" href="templates/system/css/system.css" type="text/css" />
-<link href="templates/<?php echo  $this->template ?>/css/default.css" rel="stylesheet" type="text/css" />
 
+<?php if($this->params->get('flexBox', '1')) : ?>
 <script type="text/javascript" src="templates/<?php echo  $this->template ?>/js/chromatable.js"></script>
+<link href="templates/<?php echo  $this->template ?>/css/default.css" rel="stylesheet" type="text/css" />
+<?php else : ?>
+<link href="templates/<?php echo  $this->template ?>/css/legacy.css" rel="stylesheet" type="text/css" />
+<?php endif; ?>
 
 <?php if(JModuleHelper::isEnabled('menu')) : ?>
 	<script type="text/javascript" src="templates/<?php echo  $this->template ?>/js/index.js"></script>
@@ -19,11 +23,11 @@
 </head>
 <body id="minwidth-body" class="<?php echo JRequest::getVar('option', 'cmd'); ?>">
 	<div id="container">
-		<div id="header-box">
+		<div id="header-box" class="row">
 			<jdoc:include type="modules" name="menu" />
 			<jdoc:include type="modules" name="status"  />
 		</div>
-		<div id="tabs-box">
+		<div id="tabs-box" class="row">
 			<jdoc:include type="modules" name="submenu" />
 		</div>
 		<?php if($this->countModules('toolbar OR title')) : ?>
@@ -36,7 +40,7 @@
 			<div id="window-sidebar">
 				<jdoc:include type="modules" name="sidebar" />
 			</div>
-			<div id="content-box" class="container_12 <?php echo (JRequest::getInt('hidemainmenu')) ? 'form' : 'default' ?>">
+			<div id="window-content" class="<?php echo (JRequest::getInt('hidemainmenu')) ? 'form' : 'default' ?> row-fluid">
 				<jdoc:include type="component" />
 			</div>
 			<div id="window-inspector">
