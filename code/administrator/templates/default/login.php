33bec770c30372c752d6f9b87a04d86bcd9c0ba5
Johan Janssens <johan@timble.net>
2012-07-08 20:43:54 +0000

    re #199 : Make URL's absolute.

--- code/administrator/templates/default/login.php
+++ code/administrator/templates/default/login.php
@@ -6,8 +6,8 @@
 
 <meta http-equiv="X-UA-Compatible" content="chrome=1">
 
-<link rel="stylesheet" href="templates/system/css/system.css" type="text/css" />
-<link href="templates/<?php echo  $this->template ?>/css/default.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+<link href="<?php echo $this->baseurl ?>/templates/<?php echo  $this->template ?>/css/default.css" rel="stylesheet" type="text/css" />
 </head>
 
 <?php echo JHTML::_('behavior.keepalive'); ?>
@@ -15,7 +15,7 @@
 <body class="<?php echo JRequest::getVar('option', 'cmd'); ?> login">
 	<div id="container">
 		<div id="login-box" class="login">
-			<img src="templates/<?php echo $this->template ?>/images/nooku-server_logo.png" alt="Nooku Server logo">
+			<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/images/nooku-server_logo.png" alt="Nooku Server logo">
 			<jdoc:include type="message" />
 			<div id="section-box">
 				<jdoc:include type="component" />
