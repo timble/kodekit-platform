7ad51e76f747995c36efa0fece62c552a774152a
Arunas Mazeika <arunas@timble.net>
2012-07-11 12:58:46 +0000

    Fixed typo.
    re #199.

--- code/administrator/components/com_articles/views/sections/tmpl/default.php
+++ code/administrator/components/com_articles/views/sections/tmpl/default.php
@@ -16,7 +16,7 @@ defined('KOOWA') or die( 'Restricted access' ); ?>
 <?= @template('com://admin/default.view.grid.toolbar'); ?>
 
 <form action="" method="get" class="-koowa-grid">	
-	<?= @template('default_filter'); ?>
+	<?= @template('default_scopebar'); ?>
 	<table>
 		<thead>
 			<tr>
