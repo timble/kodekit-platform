25729bb34d37ddaa80eb75864d76f99ec4cb9e88
Johan Janssens <johan@timble.net>
2012-07-13 01:05:09 +0000

    re #228 : Plugin now extends from PlgKoowaDefault and replaced $mainframe to JFactory::getApplication().

--- code/plugins/system/debug.php
+++ code/plugins/system/debug.php
@@ -1,30 +1,26 @@
 <?php
 /**
- * @version     $Id: koowa.php 2775 2011-01-01 17:02:39Z johanjanssens $
- * @category	Nooku
+ * @version     $Id: default.php 2776 2011-01-01 17:08:00Z johanjanssens $
  * @package     Nooku_Plugins
- * @subpackage  System
- * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @subpackage  Koowa
+ * @copyright  	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
 
 /**
- * Koowa Debug plugin
+ * System Debug plugin
 .*
  * @author		Johan Janssens <johan@nooku.org>
- * @category	Nooku
  * @package     Nooku_Plugins
  * @subpackage  System
  */
-defined( '_JEXEC' ) or die( 'Restricted access' );
-
-class plgSystemDebug extends JPlugin
+class plgSystemDebug extends PlgKoowaDefault
 {
-    public function __construct($subject, $config = array())
+    public function __construct($config = array())
 	{
 	    //Intercept the events for profiling
-	    if(KDEBUG) 
+	    if(JFactory::getApplication()->getCfg('debug'))
 	    {
 	        //Replace the event dispatcher
 	        KService::setAlias('koowa:event.dispatcher', 'com://admin/debug.event.profiler');
@@ -33,6 +29,6 @@ class plgSystemDebug extends JPlugin
 	        KService::get('koowa:database.adapter.mysqli')->addEventSubscriber('com://admin/debug.event.subscriber.database');
 		}
 		
-		parent::__construct($subject, $config);
+		parent::__construct($config);
 	}
 }
