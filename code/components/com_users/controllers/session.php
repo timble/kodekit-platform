bf69b47cf65d306dfbd4279998813ca3cc504b36
Johan Janssens <johan@timble.net>
2012-07-13 01:26:48 +0000

    re #228 : Completely refactored session controller to allow for session forking and improved controller flow and flexibility.

--- code/components/com_users/controllers/session.php
+++ code/components/com_users/controllers/session.php
@@ -10,7 +10,7 @@
  */
 
 /**
- * Login Controller Class
+ * Session Controller Class
  *
  * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @category    Nooku
@@ -23,7 +23,11 @@ class ComUsersControllerSession extends ComDefaultControllerDefault
     {
         parent::__construct($config);
 
-        $this->registerCallback('before.add', array($this, 'validate'));
+        //Only authenticate POST requests
+        $this->registerCallback('before.post' , array($this, 'authenticate'));
+
+        //Authorize the user before adding
+        $this->registerCallback('before.add' , array($this, 'authorize'));
 
         //Set the default redirect.
         $this->setRedirect(KRequest::referrer());
@@ -48,94 +52,33 @@ class ComUsersControllerSession extends ComDefaultControllerDefault
         return parent::_actionGet($context);
     }
 
-    protected function _actionValidate(KCommandContext $context)
-    {
-        $user = $this->getService('com://admin/users.database.row.user');
-        $user->username = $context->data->username;
-        $user->load();
-
-        if($user->id)
-        {
-            list($password, $salt) = explode(':', $user->password);
-
-            $crypted = $this->getService('com://admin/users.helper.password')
-                ->getCrypted($context->data->password, $salt);
-
-            if($crypted == $password)
-            {
-                $context->data->append(array(
-                    'email'     => $user->email,
-                    'username'  => $user->username,
-                    'name'      => $user->name
-                ));
-            }
-            else
-            {
-                JError::raiseWarning('SOME_ERROR_CODE', JText::_('Wrong password!'));
-                return false;
-            }
-        }
-        else
-        {
-            JError::raiseWarning('SOME_ERROR_CODE', JText::_('Wrong username!'));
-            return false;
-        }
-
-        return true;
-    }
-
     protected function _actionAdd(KCommandContext $context)
     {
-        $options = array();
-        $options['group'] = 'USERS';
-
-        //Fetch the user object
-        $user = JFactory::getUser($context->data->username);
-
-        //If the user is blocked, redirect with an error
-        if ($user->get('block') == 1) {
-            JError::raiseWarning('SOME_ERROR_CODE', JText::_('E_NOLOGIN_BLOCKED'));
-            return false;
-        }
-
-        //Check if the users group has access
-        $acl   = JFactory::getACL();
-        $group = $acl->getAroGroup($user->get('id'));
-
-        if(!$acl->is_group_child_of( $group->name, $options['group'])) {
-            JError::raiseWarning('SOME_ERROR_CODE', JText::_('E_NOLOGIN_ACCESS'));
-            return false;
-        }
-
-        //Mark the user as logged in
-        $user->set('guest', 0);
-        $user->set('aid'  , 1);
-
-        // Fudge Authors, Editors, Publishers and Super Administrators into the special access group
-        if ($acl->is_group_child_of($group->name, 'Registered')      ||
-            $acl->is_group_child_of($group->name, 'Public Backend'))    {
-            $user->set('aid', 2);
-        }
-
-        //Set the usertype based on the ACL group name
-        $user->set('usertype', $group->name);
-
-        //Start the session
+        //Start the session (if not started already)
         $session = $this->getService('session')->start();
 
-        $session->user = $user;
-        $session->site = JFactory::getApplication()->getSite();
+        //Insert the session into the database
+        if($session->isActive())
+        {
+            //Prepare the data
+            $context->data->id    = $session->getId();
+            $context->data->name  = $context->user->name;
+            $context->data->guest = $context->user->guest;
+            $context->data->email = $context->user->email;
+            $context->data->data  = '';
+            $context->data->time  = time();
+            $context->data->client_id = 0;
+        }
 
-        //Prepare the data
-        $context->data->id      = $session->getId();
-        $context->data->name    = $user->get('name');
-        $context->data->guest   = $user->get('guest');
-        $context->data->user_id = intval($user->get('id'));
-        $context->data->data    = '';
-        $context->data->time    = time();
-        $context->data->client_id = 0;
-
+        //Add the session to the session store
         $data = parent::_actionAdd($context);
+
+        if(!$context->hasError())
+        {
+            //Set the session data
+            $session->user = $context->user;
+            $session->site = JFactory::getApplication()->getSite();
+        }
 
         return $data;
     }
@@ -143,7 +86,7 @@ class ComUsersControllerSession extends ComDefaultControllerDefault
     protected function _actionDelete(KCommandContext $context)
     {
         //Force logout from site only
-        $this->client= array(0);
+        $this->client = array(0);
 
         //Remove the session from the session store
         $data = parent::_actionDelete($context);
@@ -151,11 +94,104 @@ class ComUsersControllerSession extends ComDefaultControllerDefault
         if(!$context->hasError())
         {
             // Destroy the php session for this user if we are logging out ourselves
-            if(JFactory::getUser()->get('id') == $data->userid) {
+            if(JFactory::getUser()->email == $data->email) {
                 $this->getService('session')->destroy();
             }
         }
 
         return $data;
+    }
+
+    protected function _actionFork(KCommandContext $context)
+    {
+        //Fork the session to prevent session fixation issues
+        $session = $this->getService('session')->fork();
+
+        //Re-Load the user
+        $user = $this->getService('com://admin/users.database.row.user')
+                     ->set('email', $session->user->email)
+                     ->load();
+
+        //Store the user in the context
+        $context->user = $user;
+
+        //Re-authorize the user and add a session entity
+        $result = $this->execute('add', $context);
+
+        return $result;
+    }
+
+    protected function _actionAuthenticate(KCommandContext $context)
+    {
+        //Load the user
+        $user = $this->getService('com://admin/users.database.row.user')
+                     ->set('email', $context->data->email)
+                     ->load();
+
+        //Store the user in the context
+        $context->user = $user;
+
+        if($user->id)
+        {
+            list($password, $salt) = explode(':', $user->password);
+
+            $crypted = $this->getService('com://admin/users.helper.password')
+                             ->getCrypted($context->data->password, $salt);
+
+            if($crypted !== $password)
+            {
+                JError::raiseWarning('SOME_ERROR_CODE', JText::_('Wrong password!'));
+                return false;
+            }
+        }
+        else
+        {
+            JError::raiseWarning('SOME_ERROR_CODE', JText::_('Wrong email!'));
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function _actionAuthorize(KCommandContext $context)
+    {
+       //Make sure we have a valid user object
+       if($context->user instanceof ComUsersDatabaseRowUser)
+       {
+            $options = array();
+            $options['group'] = 'USERS';
+
+            //If the user is enabled, redirect with an error
+            if (!$context->user->enabled) {
+                JError::raiseWarning('SOME_ERROR_CODE', JText::_('E_NOLOGIN_BLOCKED'));
+                return false;
+            }
+
+            //Check if the users group has access
+            $acl   = JFactory::getACL();
+            $group = $acl->getAroGroup($context->user->id);
+
+            if(!$acl->is_group_child_of( $group->name, $options['group'])) {
+                JError::raiseWarning('SOME_ERROR_CODE', JText::_('E_NOLOGIN_ACCESS'));
+                return false;
+            }
+
+            //Mark the user as logged in
+            $context->user->guest = 0;
+            $context->user->aid   = 1;
+
+            // Fudge Authors, Editors, Publishers and Super Administrators into the special access group
+            if ($acl->is_group_child_of($group->name, 'Registered')      ||
+                $acl->is_group_child_of($group->name, 'Public Backend')) {
+                $context->user->aid = 2;
+            }
+
+            //Set the group based on the ACL group name
+            $context->user->group_name = $group->name;
+
+            return true;
+       }
+
+       return false;
     }
 }
