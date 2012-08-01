eb24e176067dff4b539529f6bbc6b0543ae13890
Johan Janssens <johan@timble.net>
2012-08-01 22:33:32 +0000

    Docblocks

--- code/components/com_search/router.php
+++ code/components/com_search/router.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @version		$Id$
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Search
  * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
@@ -13,7 +12,6 @@
  * Search Router
  *
  * @author    	Arunas Mazeika <http://nooku.assembla.com/profile/amazeika>
- * @category 	Nooku
  * @package     Nooku_Server
  * @subpackage  Search
  */
