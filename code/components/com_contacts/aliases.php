93dd30e60a31069264d98b5c345d109533cf0d3d
Johan Janssens <johan@timble.net>
2012-08-01 22:57:17 +0000

    re #229 : Map categories model to categories component

--- code/components/com_contacts/aliases.php
+++ code/components/com_contacts/aliases.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @version     $Id: aliases.php 3541 2012-04-02 18:24:42Z johanjanssens $
- * @category    Nooku
  * @package     Nooku_Server
  * @subpackage  Newsfeeds
  * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
@@ -13,10 +12,9 @@
  * Service Aliases
  *
  * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
- * @category    Nooku
  * @package     Nooku_Server
  * @subpackage  Newsfeeds
  */
 
-KService::setAlias('com://site/contacts.model.categories', 'com://admin/contacts.model.categories');
-KService::setAlias('com://site/contacts.model.contacts' , 'com://admin/contacts.model.contacts');
+KService::setAlias('com://site/contacts.model.categories', 'com://admin/categories.model.categories');
+KService::setAlias('com://site/contacts.model.contacts'  , 'com://admin/contacts.model.contacts');
