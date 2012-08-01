d188b645dd53d0db124c3a0a46242f6a6e98a4d0
Johan Janssens <johan@timble.net>
2012-08-01 23:33:03 +0000

    re #229 : Load aliases file to set categories model alias.

--- code/administrator/components/com_contacts/contacts.php
+++ code/administrator/components/com_contacts/contacts.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @version     $Id: templates.php 1161 2011-05-11 14:52:09Z johanjanssens $
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Contacts
  * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
@@ -13,7 +12,6 @@
  * Component Loader
  *
  * @author      Isreal Canasa <http://nooku.assembla.com/profile/israelcanasa>
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Contacts   
  */
@@ -21,5 +19,7 @@
 if (!JFactory::getUser()->authorize( 'com_contacts', 'manage' )) {
 	JFactory::getApplication()->redirect( 'index.php', JText::_('ALERTNOTAUTH') );
 }
+
+KLoader::loadIdentifier('com://admin/contacts.aliases');
 
 echo KService::get('com://admin/contacts.dispatcher')->dispatch();
