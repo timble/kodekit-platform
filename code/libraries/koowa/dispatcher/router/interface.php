82b990045bbe25b7089b15928460da5aaee3c02e
Johan Janssens <johan@timble.net>
2012-08-01 22:09:29 +0000

    re #259 : KDispatcherRouter::parse() and buid() functions now expect a KHttpUrl object. Removed getVar() and setVar() methods and build rule functionality.

--- code/libraries/koowa/dispatcher/router/interface.php
+++ code/libraries/koowa/dispatcher/router/interface.php
@@ -19,67 +19,19 @@
  */
 interface KDispatcherRouterInterface
 {
-	/**
-	 * Function to convert a route to an internal URI
+    /**
+     * Function to convert a route to an internal URI
      *
-     * @param   JURI  $uri  The uri.
-     * @return  array
-	 */
-	public function parse($uri);
+     * @param   KHttpUrl  $url  The url.
+     * @return  boolean
+     */
+	public function parse(KHttpUrl $uri);
 
-	/**
-	 * Function to convert an internal URI to a route
-	 *
-	 * @param	string	$string	The internal URL
-	 * @return	string	The absolute search engine friendly URL
-	 */
-	public function build($url);
-
-	/**
-	 * Set a router variable, creating it if it doesn't exist
-	 *
-	 * @param	string  $key    The name of the variable
-	 * @param	mixed   $value  The value of the variable
-	 * @param	boolean $create If True, the variable will be created if it doesn't exist yet
-     * @return \KDispatcherRouterInterface
- 	 */
-	public function setVar($key, $value, $create = true);
-
-	/**
-	 * Set the router variable array
-	 *
-	 * @param	array   $vars   An associative array with variables
-	 * @param	boolean $create If True, the array will be merged instead of overwritten
-     * @return \KDispatcherRouterInterface
- 	 */
-	public function setVars($vars = array(), $merge = true);
-
-	/**
-	 * Get a router variable
-	 *
-	 * @param	string $key   The name of the variable
-	 * @return  mixed  Value of the variable
- 	 */
-	public function getVar($key);
-
-	/**
-	 * Get the router variable array
-	 *
-	 * @return  array An associative array of router variables
- 	 */
-	public function getVars();
-
-	/**
-	 * Attach a build rule
-	 *
-	 * @param   callback $callback The function to be called.
- 	 */
-	public function attachBuildRule($callback);
-
-	/**
-	 * Attach a parse rule
-	 *
-	 * @param   callback $callback The function to be called.
- 	 */
-	public function attachParseRule($callback);
+    /**
+     * Function to convert an internal URI to a route
+     *
+     * @param	KhttpUrl   $url	The internal URL
+     * @return	boolean
+     */
+	public function build(KHttpUrl $url);
 }
