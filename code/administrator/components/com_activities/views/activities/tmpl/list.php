178788af572185b4378a2b9784c00212831a71ac
Johan Janssens <johan@timble.net>
2012-08-01 22:23:06 +0000

    Use shortcut helper identifier.

--- code/administrator/components/com_activities/views/activities/tmpl/list.php
+++ code/administrator/components/com_activities/views/activities/tmpl/list.php
@@ -22,14 +22,14 @@ defined('KOOWA') or die('Restricted access') ?>
 	    <? foreach ($activities as $activity) : ?>
 	       <? $list[substr($activity->created_on, 0, 10)][] = $activity; ?>
 	    <? endforeach; ?>
-	
+
 	    <? foreach($list as $date => $activities) : ?>
 			<h4><?= @helper('date.humanize', array('date' => $date)) ?></h4>
 			<div class="activities-day">
 			    <? foreach($activities as $activity) : ?>
 				<div class="activity">
 					<i class="icon-<?= $activity->action ?>"></i>
-				    <?= @helper('com://admin/activities.template.helper.activity.message', array('row' => $activity)) ?>
+				    <?= @helper('activity.message', array('row' => $activity)) ?>
 					<span class="info">
 						<small><?= $activity->package.' - '.$activity->name ?> | <?= date("H:i", strtotime($activity->created_on)) ?></small>
 					</span>
