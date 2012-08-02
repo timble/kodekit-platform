73d2b8ce0c8a507ca9fa2e6c1fd6aaa23c6fab4f
Johan Janssens <johan@timble.net>
2012-08-02 01:39:20 +0000

    Docblocks

--- code/administrator/components/com_articles/views/article/tmpl/form_categories.php
+++ code/administrator/components/com_articles/views/article/tmpl/form_categories.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @version     $Id$
- * @category    Nooku
  * @package     Nooku_Server
  * @subpackage  Articles
  * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
