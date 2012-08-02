d0d791827a93e2be2263b0d5d2d26680ec8b2c82
Johan Janssens <johan@timble.net>
2012-08-02 01:33:03 +0000

    Docblocks

--- code/administrator/components/com_articles/databases/tables/featured.php
+++ code/administrator/components/com_articles/databases/tables/featured.php
@@ -1,21 +1,19 @@
 <?php
 /**
  * @version     $Id$
- * @category    Nooku
  * @package     Nooku_Server
- * @subpackage  Frontpage
+ * @subpackage  Articles
  * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
 
 /**
- * Frontpage Database Table Class
+ * Featured Database Table Class
  *
- * @author      Richie Mortimer <http://nooku.assembla.com/profile/ravenlife>
- * @category    Nooku
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @package     Nooku_Server
- * @subpackage  Frontpage
+ * @subpackage  Articles
  */
 class ComArticlesDatabaseTableFeatured extends KDatabaseTableDefault
 {
