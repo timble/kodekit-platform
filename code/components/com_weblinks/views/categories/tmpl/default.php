4344e0e970f6ad17c51c8c9d70564ec3e287a413
Johan Janssens <johan@timble.net>
2012-08-02 04:35:01 +0000

    re #229 : Added empty file. Otherwise com_menus does not recognise the layout correctly

--- code/components/com_weblinks/views/categories/tmpl/default.php
+++ code/components/com_weblinks/views/categories/tmpl/default.php
@@ -1,7 +1,6 @@
 <?
 /**
  * @version		$Id$
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Weblinks
  * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
@@ -10,18 +9,3 @@
  */
 
 defined('KOOWA') or die('Restricted access'); ?>
-
-<? if ( $params->def( 'show_page_title', 1 ) ) : ?>
-<h1><?= @escape($params->get('page_title')); ?></h1>
-<? endif; ?>
-
-<? foreach($categories as $category) : ?>
-	<h2>
-		<a href="<?= @route('view=weblinks&category='. $category->id.':'.$category->slug) ?>" class="category<?= @escape($params->get( 'pageclass_sfx' )); ?>">
-			<?= @escape($category->title);?>
-		</a>		
-	</h2>
-	<p>
-	    <?= @escape($category->description);?>
-	</p>
-<? endforeach; ?>
