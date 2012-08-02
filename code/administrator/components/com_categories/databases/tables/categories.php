08c50d0d58b5086e6f3a21b9d1f11271ddbff03d
Johan Janssens <johan@timble.net>
2012-08-02 03:53:53 +0000

    re #229 : Removed section logic

--- code/administrator/components/com_categories/databases/tables/categories.php
+++ code/administrator/components/com_categories/databases/tables/categories.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @version     $Id$
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Categories
  * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
@@ -13,7 +12,6 @@
  * Categories Database Table Class
  *
  * @author      John Bell <http://nooku.assembla.com/profile/johnbell>
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Categories
  */
@@ -21,19 +19,16 @@ class ComCategoriesDatabaseTableCategories extends KDatabaseTableDefault
 {
     public function  _initialize(KConfig $config)
     {
-        $config->identity_column = 'id';
-       
         $config->append(array(
             'name'       => 'categories',
             'behaviors'  => array(
-            	'lockable', 'sluggable', 'cascadable',
-            	'com://admin/categories.database.behavior.orderable'  => array('parent_column' => 'section_id'),
+            	'lockable', 'sluggable', 'cascadable', 'nestable',
+            	'orderable'  => array('parent_column' => 'parent_id'),
             ),
             'column_map' => array(
                 'enabled'      => 'published',
                 'locked_on'    => 'checked_out_time',
                 'locked_by'    => 'checked_out',
-                'section_id'   => 'section',
                 'slug'         => 'alias'
                 ),
             ));
