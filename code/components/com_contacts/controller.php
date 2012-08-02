79582a0a5cd969d75c50ad7a85b9bf26c327ef6a
Johan Janssens <johan@timble.net>
2012-08-02 04:28:19 +0000

    re #229 : Cleanup

--- code/components/com_contacts/controller.php
+++ code/components/com_contacts/controller.php
@@ -146,7 +146,7 @@ class ContactController extends JController
 		}
 
 		$msg = JText::_( 'Thank you for your e-mail');
-		$link = JRoute::_('index.php?option=com_contact&view=contact&id='.$contact->slug.'&catid='.$contact->catslug, false);
+		$link = JRoute::_('index.php?option=com_contact&view=contact&id='.$contact->slug.'&category='.$contact->catslug, false);
 		$this->setRedirect($link, $msg);
 	}
 
