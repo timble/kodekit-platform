e2486d01b0c85c9cef93f7271d7e70ef445d85ea
Johan Janssens <johan@timble.net>
2012-08-02 04:27:56 +0000

    Docblocks

--- code/components/com_contacts/controllers/message.php
+++ code/components/com_contacts/controllers/message.php
@@ -1,7 +1,6 @@
  <?php
 /**
  * @version		$Id: weblink.php 3314 2012-02-10 02:14:52Z johanjanssens $
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Contacts
  * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
@@ -13,7 +12,6 @@
  * Message Controller
  *
  * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
- * @category 	Nooku
  * @package     Nooku_Server
  * @subpackage  Contacts
  */
