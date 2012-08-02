ec879133ad5609e9d05bb0a32d49ee19c07c52a5
Johan Janssens <johan@timble.net>
2012-08-02 04:44:39 +0000

    re #229 : Initial import

---
+++ code/administrator/components/com_categories/databases/rows/category.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Nooku_Server
+ * @subpackage  Categories
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Category Database Row Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Categories
+ */
+
+class ComCategoriesDatabaseRowCategory extends ComCategoriesDatabaseRowNode
+{
+
+}
