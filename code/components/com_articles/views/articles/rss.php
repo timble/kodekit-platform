c6ebc3cbe8f3d1169f0f9d17fe1e4244e7a0fcb2
Johan Janssens <johan@timble.net>
2012-08-02 04:23:03 +0000

    re #180 : Cleanup and added getCategory function.

--- code/components/com_articles/views/articles/rss.php
+++ code/components/com_articles/views/articles/rss.php
@@ -15,7 +15,38 @@
  * @package    Nooku_Server
  * @subpackage Articles
  */
-class ComArticlesViewArticlesRss extends ComArticlesViewRss
+class ComArticlesViewArticlesRss extends KViewRss
 {
+    public function display()
+    {
+        //Get the category
+        $category = $this->getCategory();
 
+        $this->assign('category'  , $category);
+        return parent::display();
+    }
+
+    public function getCategory()
+    {
+        //Get the category
+        $category = $this->getService('com://site/articles.model.categories')
+                         ->table('articles')
+                         ->id($this->getModel()->getState()->category)
+                         ->getItem();
+
+        //Set the category image
+        if (isset( $category->image ) && !empty($category->image))
+        {
+            $path = JPATH_IMAGES.'/stories/'.$category->image;
+            $size = getimagesize($path);
+
+            $category->image = (object) array(
+                'path'   => '/'.str_replace(JPATH_ROOT.DS, '', $path),
+                'width'  => $size[0],
+                'height' => $size[1]
+            );
+        }
+
+        return $category;
+    }
 }
