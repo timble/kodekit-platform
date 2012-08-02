f999c99516560825027bf66112ddb3a643f35ba6
Johan Janssens <johan@timble.net>
2012-08-02 04:23:20 +0000

    re #180 : Initial import

---
+++ code/components/com_articles/views/categories/html.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @version		$Id: html.php 3532 2012-04-02 12:00:49Z johanjanssens $
+ * @package     Nooku_Server
+ * @subpackage  Weblinks
+ * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+
+/**
+ * Categories Html View
+ *
+ * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Weblinks
+ */
+class ComArticlesViewCategoriesHtml extends ComDefaultViewHtml
+{
+	public function display()
+	{
+		$params = JFactory::getApplication()->getParams();
+		$this->assign('params', $params);
+
+		return parent::display();
+	}
+}
