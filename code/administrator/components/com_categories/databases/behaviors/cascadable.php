acac320f511cb9ea619ab65751f06385115c534c
Johan Janssens <johan@timble.net>
2012-08-02 03:49:42 +0000

    re #229 : Refactored to make implementation more generic.

--- code/administrator/components/com_categories/databases/behaviors/cascadable.php
+++ code/administrator/components/com_categories/databases/behaviors/cascadable.php
@@ -1,9 +1,8 @@
 <?php
 /**
  * @version      $Id$
- * @category	 Nooku
  * @package      Nooku_Server
- * @subpackage   Categories
+ * @subpackage   Articles
  * @copyright    Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
  * @license      GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link         http://www.nooku.org
@@ -12,53 +11,25 @@
 /**
  * Cascadable Database Behavior Class
  *
- * @author      John Bell <http://nooku.assembla.com/profile/johnbell>
- * @category	Nooku
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @package     Nooku_Server
- * @subpackage  Categories    
+ * @subpackage  Articles
  */
 class ComCategoriesDatabaseBehaviorCascadable extends KDatabaseBehaviorAbstract
 {
-    /**
-     * Deletes dependent rows.
-     *
-     * This performs an intelligent delete 
-     *
-     * @return KDatabaseRowAbstract
-     */
     protected function _beforeTableDelete(KCommandContext $context)
     {
         $result = true;
-        
-        $section = $this->section;
-        if ( is_numeric($section) || !$section) {
-            $section = 'com_articles';
-        } 
-        
-        $parts = explode('_',$section);
-            
-        //@TODO : Remove when refactoring is completed
-        switch ($parts[1])
-        {
-            case 'contact':
-                $name = 'contacts';
-                $package ='contact';
-                break;
-            default :
-                $name = KInflector::pluralize($parts[1]);
-                $package = $name;
-        }
-                 
-        $identifier = 'com://admin/'.$package.'.model.'.$name;
-        
-        $rowset = $this->getService($identifier)
-                    ->category($this->id)
-                    ->getList();
+
+        $table      = $this->table;
+        $identifier = 'com://admin/'.$table.'.database.table.'.$table;
+
+        $rowset = $this->getService($identifier)->select(array('categories_category_id' => $this->id));
 
         if($rowset->count()) {
             $result = $rowset->delete();
-        } 
-        
-        return $result; 
+        }
+
+        return $result;
     }
 }
