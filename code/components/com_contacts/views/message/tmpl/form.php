7becd80ed2a7e67d200e1757c1410041c9002823
Johan Janssens <johan@timble.net>
2012-08-02 04:28:47 +0000

    Docblocks

--- code/components/com_contacts/views/message/tmpl/form.php
+++ code/components/com_contacts/views/message/tmpl/form.php
@@ -1,7 +1,6 @@
 <?
 /**
  * @version		$Id: default.php 3537 2012-04-02 17:56:59Z johanjanssens $
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Contacts
  * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
