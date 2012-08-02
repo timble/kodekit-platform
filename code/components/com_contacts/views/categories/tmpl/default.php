b0405d6d69b82809e04c4eb93f8794e9febe8940
Johan Janssens <johan@timble.net>
2012-08-02 04:34:40 +0000

    re #229 : Added empty file. Otherwise com_menus does not recognise the layout correctly

--- code/components/com_contacts/views/categories/tmpl/default.php
+++ code/components/com_contacts/views/categories/tmpl/default.php
@@ -1,9 +1,8 @@
 <?
 /**
  * @version		$Id: default.php 3537 2012-04-02 17:56:59Z johanjanssens $
- * @category	Nooku
  * @package     Nooku_Server
- * @subpackage  Weblinks
+ * @subpackage  Contacts
  * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link		http://www.nooku.org
@@ -11,15 +10,4 @@
 
 defined('KOOWA') or die('Restricted access'); ?>
 
-<? if ( $params->def( 'show_page_title', 1 ) ) : ?>
-<h1><?= @escape($params->get('page_title')); ?></h1>
-<? endif; ?>
 
-<? foreach($categories as $category) : ?>
-<h2>
-	<a href="<?= @route('view=contacts&category='. $category->id.':'.$category->slug) ?>" class="category<?= @escape($params->get( 'pageclass_sfx' )); ?>">
-		<?= @escape($category->title);?>
-	</a>
-</h2>
-<p><?= $category->description;?></p>
-<? endforeach; ?>
