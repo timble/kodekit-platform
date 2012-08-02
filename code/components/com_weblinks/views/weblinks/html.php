49da2294c2005296c23058db8ef1a3d13d13cc28
Johan Janssens <johan@timble.net>
2012-08-02 04:43:19 +0000

    Cleanup. Added getCategory() function.

--- code/components/com_weblinks/views/weblinks/html.php
+++ code/components/com_weblinks/views/weblinks/html.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @version		$Id$
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Weblinks
  * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
@@ -13,61 +12,69 @@
  * Weblinks Html View
  *
  * @author    	Jeremy Wilken <http://nooku.assembla.com/profile/gnomeontherun>
- * @category 	Nooku
  * @package     Nooku_Server
  * @subpackage  Weblinks
  */
 class ComWeblinksViewWeblinksHtml extends ComDefaultViewHtml
 {
-	/**
-	 * Display the view
-	 *
-	 * @return	string	The output of the view
-	 */
-	public function display()
-	{
-	    //Get the parameters
-	    $params = JFactory::getApplication()->getParams();
-	    
-	    //Get the category
-	    $category = $this->getService('com://site/weblinks.model.categories')
-	                     ->id($this->getModel()->getState()->category)
-	                     ->getItem();
-	                  	
-		// Get the parameters of the active menu item
-		$menu = JFactory::getApplication()->getMenu()->getActive();
-		
-		// Set the page title
-		if (is_object( $menu ))
-		{
-		    $menu_params = new JParameter( $menu->params );
-		    if (!$menu_params->get( 'page_title')) {
-		        $params->set('page_title',	$category->title);
-		    }
-		}
-		else $params->set('page_title',	$category->title);
-		
-		JFactory::getDocument()->setTitle( $params->get( 'page_title' ) );
-		
-		//set breadcrumbs
-		JFactory::getApplication()->getPathway()->addItem($category->title, '');
+    /**
+     * Display the view
+     *
+     * @return	string	The output of the view
+     */
+    public function display()
+    {
+        //Get the parameters
+        $params = JFactory::getApplication()->getParams();
 
-		// Set up the category image
-		if (isset( $category->image ) && $category->image != '')
-		{
-			$category->image = array(
-			    'src'  		=> KRequest::base().'/'.str_replace(JPATH_ROOT.DS, '', JPATH_IMAGES.'/stories/'.$category->image),
-			    'attribs' => array(
-			    		'align'  => $category->image_position,
-			    		'hspace' => 6,
-						'title'  => JText::_('Web Links')
-			    )
-		    );
-		}
+        //Get the category
+        $category = $this->getCategory();
 
-		$this->assign('params'    , $params);
-		$this->assign('category'  , $category);
-		
-		return parent::display();
-	}
+        //Get the parameters of the active menu item
+        if ($page = JFactory::getApplication()->getMenu()->getActive())
+        {
+            $menu_params = new JParameter( $page->params );
+            if (!$menu_params->get( 'page_title')) {
+                $params->set('page_title',	$category->title);
+            }
+        }
+        else $params->set('page_title',	$category->title);
+
+        //Set the page title
+        JFactory::getDocument()->setTitle( $params->get( 'page_title' ) );
+
+        //Set the pathway
+        if($page->query['view'] == 'categories' ) {
+            JFactory::getApplication()->getPathway()->addItem($category->title, '');
+        }
+
+        $this->assign('params'    , $params);
+        $this->assign('category'  , $category);
+
+        return parent::display();
+    }
+
+    public function getCategory()
+    {
+        //Get the category
+        $category = $this->getService('com://site/weblinks.model.categories')
+            ->table('weblinks')
+            ->id($this->getModel()->getState()->category)
+            ->getItem();
+
+        //Set the category image
+        if (isset( $category->image ) && !empty($category->image))
+        {
+            $path = JPATH_IMAGES.'/stories/'.$category->image;
+            $size = getimagesize($path);
+
+            $category->image = (object) array(
+                'path'   => '/'.str_replace(JPATH_ROOT.DS, '', $path),
+                'width'  => $size[0],
+                'height' => $size[1]
+            );
+        }
+
+        return $category;
+    }
 }
