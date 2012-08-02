a97c16fb8c49385158e654930bd80c770d326e46
Johan Janssens <johan@timble.net>
2012-08-02 04:30:35 +0000

    Docblocks

--- code/components/com_contacts/views/contact/vcard.php
+++ code/components/com_contacts/views/contact/vcard.php
@@ -1,7 +1,6 @@
 <?php
 /**
 * @version		$Id$
-* @category		Nooku
 * @package     	Nooku_Server
 * @subpackage  	Contacts
 * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
@@ -13,7 +12,6 @@
  * Contact Vcard View
  *
  * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
- * @category 	Nooku
  * @package     Nooku_Server
  * @subpackage  Contacts
  */
