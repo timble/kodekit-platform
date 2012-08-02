a83de80830e8853d6ffbec4bb5327c9fe03d2e68
Johan Janssens <johan@timble.net>
2012-08-02 03:57:49 +0000

    re #229 : Moved rowset categories class from articles component.

--- code/administrator/components/com_categories/databases/rowsets/categories.php
+++ code/administrator/components/com_categories/databases/rowsets/categories.php
@@ -1,24 +1,22 @@
 <?php
 /**
  * @version     $Id$
- * @category    Nooku
  * @package     Nooku_Server
- * @subpackage  Articles
+ * @subpackage  Categories
  * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
 
 /**
- * Folders Database Rowset Class
+ * Categories Database Rowset Class
  *
  * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
- * @category    Nooku
  * @package     Nooku_Server
- * @subpackage  Articles
+ * @subpackage  Categories
  */
 
-class ComArticlesDatabaseRowsetFolders extends ComArticlesDatabaseRowsetNodes
+class ComCategoriesDatabaseRowsetCategories extends ComCategoriesDatabaseRowsetNodes
 {
     protected function _initialize(KConfig $config)
     {
