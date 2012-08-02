5da2b0b601c116d6b7ee09fdfcc18bc3a873a404
Johan Janssens <johan@timble.net>
2012-08-02 04:32:46 +0000

    re #78 : Moved details rendering into hcard layout.

--- code/components/com_contacts/views/contact/tmpl/default.php
+++ code/components/com_contacts/views/contact/tmpl/default.php
@@ -1,7 +1,6 @@
 <?
 /**
  * @version		$Id: default.php 3537 2012-04-02 17:56:59Z johanjanssens $
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Contacts
  * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
@@ -15,59 +14,11 @@ defined('KOOWA') or die('Restricted access'); ?>
 <script src="media://lib_koowa/js/koowa.js" />
 -->
 
-<? if ($contact->name && $contact->params->get('show_name')) : ?>
-<h1><?= $contact->name; ?></h1>
-<? endif;?>
+<? if ($contact->params->get('allow_vcard')) : ?>
+    <link href="<?= @route('format=vcard') ?>" rel="alternate" type="text/x-vcard; version=2.1" title="Vcard - <?= $contact->name; ?>"/>
+<? endif; ?>
 
-<?if ($contact->image && $contact->params->get('show_image')) : ?>
-<div style="float: right;">
-	<img src="images/stories/<?= $contact->image ?>" title="@text( 'Contact' )" />
-</div>
-<? endif;?>
-
-<?if ($contact->con_position && $contact->params->get('show_position')) : ?>
-<h2><?= $contact->con_position?></h2>
-<? endif;?>
-
-<p>
-    <? if ($contact->address && $contact->params->get('show_street_address')) : ?>
-    <?= $contact->address?><br />
-    <? endif; ?>
-    <? if ( $contact->suburb && $contact->params->get('show_suburb')) : ?>
-    <?= $contact->suburb?><br />
-    <? endif; ?>
-    <? if ($contact->state && $contact->params->get('show_state')) : ?>
-    <?= $contact->state?><br />
-    <? endif; ?>
-    <?if ($contact->postcode && $contact->params->get('show_postcode')) : ?>
-        <?= $contact->postcode?><br />
-    <? endif; ?>
-    <? if ($contact->country && $contact->params->get('show_country')) : ?>
-        <?= $contact->country?><br />
-    <? endif; ?>
-</p>
-
-<ul>
-    <? if ($contact->telephone && $contact->params->get('show_telephone')) :?>
-    <li><?= $contact->telephone?></li>
-    <? endif; ?>
-    <? if ($contact->fax && $contact->params->get('show_fax')) :?>
-    <li><?= $contact->fax?></li>
-    <? endif; ?>
-    <?if ($contact->mobile && $contact->params->get('show_mobile')) :?>
-    <li><?= $contact->mobile?></li>
-    <? endif; ?>
-    <?if ($contact->email_to && $contact->params->get('show_email')) :?>
-    <li><a href="mailto:<?= $contact->email_to?>"><?= $contact->email_to?></a></li>
-    <? endif; ?>
-    
-</ul>
-
-<?if ($contact->fax && $contact->params->get('show_misc')) :?>
-<p>
-    <?= $contact->misc ?>
-</p>
-<? endif; ?> 
+<?= @template('hcard') ?>
                
 <?if ($contact->params->get('allow_vcard')) :?>
 <p>
