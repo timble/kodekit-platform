d2cd03ba25d31b5f15df870417a5b9912c6f65f5
Johan Janssens <johan@timble.net>
2012-08-02 04:22:52 +0000

    re #180 : Cleanup and added getCategory function.

--- code/components/com_articles/views/articles/html.php
+++ code/components/com_articles/views/articles/html.php
@@ -19,9 +19,55 @@ class ComArticlesViewArticlesHtml extends ComArticlesViewHtml
 {
     public function display()
     {
-        $params = JComponentHelper::getParams('com_articles');
-        $this->assign('params', $params);
+        //Get the parameters
+        $params = JFactory::getApplication()->getParams();
+
+        //Get the category
+        $category = $this->getCategory();
+
+        //Get the parameters of the active menu item
+        if ($page = JFactory::getApplication()->getMenu()->getActive())
+        {
+            $menu_params = new JParameter( $page->params );
+            if (!$menu_params->get( 'page_title')) {
+                $params->set('page_title',	$category->title);
+            }
+        }
+        else $params->set('page_title',	$category->title);
+
+        //Set the pathway
+        if($page->query['view'] == 'categories' ) {
+            JFactory::getApplication()->getPathway()->addItem($category->title, '');
+        }
+
+        $this->assign('params'  , $params);
+        $this->assign('category', $category);
 
         return parent::display();
     }
+
+    public function getCategory()
+    {
+        //Get the category
+        $category = $this->getService('com://site/articles.model.categories')
+                         ->table('articles')
+                         ->id($this->getModel()->getState()->category)
+                         ->getItem();
+
+        //Set the category image
+        if (isset( $category->image ) && !empty($category->image))
+        {
+            $path = JPATH_IMAGES.'/stories/'.$category->image;
+            $size = getimagesize($path);
+
+            $category->image = (object) array(
+                'path'   => '/'.str_replace(JPATH_ROOT.DS, '', $path),
+                'width'  => $size[0],
+                'height' => $size[1]
+            );
+        }
+
+        return $category;
+    }
+
 }
