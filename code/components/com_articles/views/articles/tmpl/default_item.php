dfc926cd2379ad8d1ebe01f9970056a8c5b9a51e
Johan Janssens <johan@timble.net>
2012-08-02 04:25:30 +0000

    re #180 : Cleanup

---
+++ code/components/com_articles/views/articles/tmpl/default_item.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @version        $Id$
+ * @package        Nooku_Server
+ * @subpackage     Articles
+ * @copyright      Copyright (C) 2009 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
+ */
+
+defined('KOOWA') or die('Restricted access'); ?>
+
+<? if ($article->editable) : ?>
+<div class="edit-article">
+    <a href="<?= @helper('route.article', array('row' => $article, 'layout' => 'form')) ?>">
+        <?= @text('Edit') ?>
+    </a>
+</div>
+<? endif; ?>
+
+<article <?= !$article->state ? 'class="article-unpublished"' : '' ?>>
+
+    <h1><a href="<?= @helper('route.article', array('row' => $article)) ?>"><?= $article->title ?></a></h1>
+
+    <p class="timestamp">
+        <?= @helper('date.timestamp', array('row' => $article)); ?>
+    </p>
+
+    <? if ($article->fulltext && $params->get('show_readmore')) : ?>
+
+        <?= $article->introtext; ?>
+         <a href="<?= @helper('route.article', array('row' => $article)) ?>"><?= @text('Read more') ?></a>
+
+    <? else : ?>
+
+        <?= $article->introtext . $article->fulltext ?>
+
+    <? endif; ?>
+
+</article>
