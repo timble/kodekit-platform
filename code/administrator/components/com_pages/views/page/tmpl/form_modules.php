606499a62d5ab9477160cad6e29146f156eda5e8
Gergo Erdosi <gergo@timble.net>
2012-08-06 10:48:04 +0000

    Move input fields into labels. re #168

--- code/administrator/components/com_pages/views/page/tmpl/form_modules.php
+++ code/administrator/components/com_pages/views/page/tmpl/form_modules.php
@@ -18,12 +18,14 @@ defined('KOOWA') or die('Restricted access') ?>
         <? foreach($modules->available as $module) : ?>
             <input type="hidden" name="modules[<?= $module->id ?>][others]" value="" />
             
-            <? $checked = count($modules->assigned->find(array('modules_module_id' => $module->id))) ? 'checked="checked"' : '' ?>
-            <input type="checkbox" name="modules[<?= $module->id ?>][current]" value="1" class="module-<?= $module->id ?>" <?= $checked ?>/>
-            
             <a class="modal" href="<?= @route('option=com_pages&view=module&layout=modal&tmpl=component&module='.$module->id.'&page='.$page->id) ?>" rel="{handler: 'iframe', size: {x: 800, y: 400}}">
-                <label><?= $module->title ?></label>
-            </a><br />
+                <label>
+                    <? $checked = count($modules->assigned->find(array('modules_module_id' => $module->id))) ? 'checked="checked"' : '' ?>
+                    <input type="checkbox" name="modules[<?= $module->id ?>][current]" value="1" class="module-<?= $module->id ?>" <?= $checked ?>/>
+                    
+                    <?= $module->title ?>
+                </label>
+            </a><br>
         <? endforeach ?>
     </fieldset>
 </section>
