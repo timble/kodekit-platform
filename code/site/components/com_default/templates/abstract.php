3c0bd18c954c8f5b2a85377f21f8de4f2f51c126
Johan Janssens <johan@timble.net>
2012-08-08 22:04:18 +0000

    Replaced JPATH_THEMES with JAPPLICATION.'/templates'

--- code/site/components/com_default/templates/abstract.php
+++ code/site/components/com_default/templates/abstract.php
@@ -82,7 +82,7 @@ abstract class ComDefaultTemplateAbstract extends KTemplateAbstract
 	public function findFile($path)
 	{
 	    $template  = JFactory::getApplication()->getTemplate();
-        $override  = JPATH_THEMES.'/'.$template.'/html';
+        $override  = JPATH_APPLICATION.'/templates/'.$template.'/html';
 	    $override .= str_replace(array(JPATH_BASE.'/modules', JPATH_BASE.'/components', '/views'), '', $path);
 
 	    //Try to load the template override
