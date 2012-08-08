a12a869f118fee942c13fd1f26baacb9e37cec07
Johan Janssens <johan@timble.net>
2012-08-08 22:03:59 +0000

    Replaced JPATH_THEMES with JAPPLICATION.'/templates'

--- code/site/modules/mod_default/templates/filters/chrome.php
+++ code/site/modules/mod_default/templates/filters/chrome.php
@@ -26,10 +26,10 @@ class ModDefaultTemplateFilterChrome extends KTemplateFilterAbstract implements 
     {
         parent::__construct($config);
 
-        include_once JPATH_THEMES.'/system/html/modules.php';
+        include_once JPATH_APPLICATION.'/templates/system/html/modules.php';
 
-        if(file_exists(JPATH_THEMES.'/'.$config->template.'/html/modules.php')) {
-		    include_once JPATH_THEMES.'/'.$config->template.'/html/modules.php';
+        if(file_exists(JPATH_APPLICATION.'/templates/'.$config->template.'/html/modules.php')) {
+		    include_once JPATH_APPLICATION.'/templates/'.$config->template.'/html/modules.php';
         }
     }
 
