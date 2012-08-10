3ba8b62180a3d01b34326d4a5553853ed57d5abb
Johan Janssens <johan@timble.net>
2012-08-10 12:01:29 +0000

    re #228 : Added ComApplicationControllerError

---
+++ code/administrator/components/com_application/controllers/error.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @version		$Id$
+ * @package     Nooku_Server
+ * @subpackage  Application
+ * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+
+/**
+ * Error Controller Class
+ *   
+ * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Application
+ */
+ 
+class ComApplicationControllerError extends KControllerResource
+{
+    protected function _actionGet(KCommandContext $context)
+    {
+        $this->getModel()
+            ->getItem()
+            ->setData(KConfig::toData($context->data));
+
+        //Make sure the buffers are cleared
+        while(@ob_get_clean());
+
+        return parent::_actionGet($context);
+    }
+}
