86cb4ae843500a895f122b7da3b681b6b94f0e41
Johan Janssens <johan@timble.net>
2012-08-11 18:34:03 +0000

    Cleanup. Removed defined('KOOWA') or die('Restricted access') ?> checks

--- code/administrator/components/com_users/views/users/tmpl/default_scopebar.php
+++ code/administrator/components/com_users/views/users/tmpl/default_scopebar.php
@@ -1,4 +1,4 @@
-<?php 
+<?
 /**
  * @version     $Id: default_filter.php 3472 2012-03-17 14:57:58Z tomjanssens $
  * @category	Nooku
@@ -8,7 +8,7 @@
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
-defined('KOOWA') or die( 'Restricted access' ); ?>
+?>
 
 <div class="scopebar">
 	<div class="scopebar-group">
