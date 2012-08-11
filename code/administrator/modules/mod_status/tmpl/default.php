ee60ad9a04957a29ba5ed0329bd2b053a50ebdc2
Johan Janssens <johan@timble.net>
2012-08-11 18:35:01 +0000

    Cleanup. Removed defined('KOOWA') or die('Restricted access') ?> checks

--- code/administrator/modules/mod_status/tmpl/default.php
+++ code/administrator/modules/mod_status/tmpl/default.php
@@ -8,8 +8,7 @@
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
-
-defined('KOOWA') or die('Restricted access'); ?>
+?>
 
 <script src="media://lib_koowa/js/koowa.js" />
 
