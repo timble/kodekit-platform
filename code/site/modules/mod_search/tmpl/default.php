dced21a0de6ebf6442194afa05f392279d270226
Johan Janssens <johan@timble.net>
2012-08-11 18:40:12 +0000

    Cleanup. Removed defined('KOOWA') or die('Restricted access') ?> checks

--- code/site/modules/mod_search/tmpl/default.php
+++ code/site/modules/mod_search/tmpl/default.php
@@ -8,8 +8,7 @@
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link		http://www.nooku.org
  */
-
-defined('KOOWA') or die('Restricted access'); ?>
+?>
 
 <form action="<?= JRoute::_('index.php?option=com_search'); ?>" method="get">
 
