dbae266bbc2ce5ab72c8ceb486eac133099460d1
Johan Janssens <johan@timble.net>
2012-08-11 18:37:07 +0000

    Cleanup. Removed defined('KOOWA') or die('Restricted access') ?> checks

--- code/site/components/com_categories/views/categories/tmpl/default.php
+++ code/site/components/com_categories/views/categories/tmpl/default.php
@@ -7,8 +7,7 @@
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link		http://www.nooku.org
  */
-
-defined('KOOWA') or die('Restricted access'); ?>
+?>
 
 <? if ( $params->def( 'show_page_title', 1 ) ) : ?>
 <h1><?= @escape($params->get('page_title')); ?></h1>
