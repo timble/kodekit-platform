9865b97a18bae7c33a76ef850f15c3772479e420
Johan Janssens <johan@timble.net>
2012-08-11 18:34:11 +0000

    Cleanup. Removed defined('KOOWA') or die('Restricted access') ?> checks

--- code/administrator/components/com_versions/views/revisions/tmpl/list.php
+++ code/administrator/components/com_versions/views/revisions/tmpl/list.php
@@ -1,4 +1,4 @@
-<?php
+<?
 /**
  * @version     $Id$
  * @category	Nooku
@@ -8,8 +8,7 @@
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
-
-defined('KOOWA') or die( 'Restricted access' ); ?>
+?>
 
 <table id="revisions-list" class="adminlist">
 	<thead>
