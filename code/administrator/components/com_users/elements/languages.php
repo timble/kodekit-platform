7da56ba5c117d822394b64c7cfaf051df9dcb8d5
Johan Janssens <johan@timble.net>
2012-08-11 23:46:46 +0000

    Moved languages element to com_users component and updated xml files.

---
+++ code/administrator/components/com_users/elements/languages.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @version     $Id: article.php 4368 2012-08-05 13:04:43Z gergoerdosi $
+ * @package     Nooku_Server
+ * @subpackage  Users
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Languages Element Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ */
+
+class JElementLanguages extends JElement
+{
+	var	$_name = 'Languages';
+
+	function fetchElement($name, $value, &$node, $control_name)
+	{
+		$user = JFactory::getUser();
+
+		/*
+		 * @TODO: change to acl_check method
+		 */
+		if(!($user->get('gid') >= 23) && $node->attributes('client') == 'administrator') {
+			return JText::_('No Access');
+		}
+
+		$client = $node->attributes('client');
+
+		jimport('joomla.language.helper');
+		$languages = JLanguageHelper::createLanguageList($value, JPATH_ROOT.'/'.$client, true);
+		array_unshift($languages, JHTML::_('select.option', '', '- '.JText::_('Select Language').' -'));
+
+		return JHTML::_('select.genericlist',  $languages, ''.$control_name.'['.$name.']', 'class="inputbox"', 'value', 'text', $value, $control_name.$name );
+	}
+}
