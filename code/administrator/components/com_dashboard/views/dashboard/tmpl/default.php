18eb66c76c1501fbab4f13696da8f048e9258d30
Johan Janssens <johan@timble.net>
2012-08-11 18:31:07 +0000

    Cleanup. Removed defined('KOOWA') or die('Restricted access') ?> checks

--- code/administrator/components/com_dashboard/views/dashboard/tmpl/default.php
+++ code/administrator/components/com_dashboard/views/dashboard/tmpl/default.php
@@ -8,8 +8,7 @@
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link		http://www.nooku.org
  */
-
-defined('KOOWA') or die('Restricted access'); ?>
+?>
 
 <module position="title" content="replace">
 	<?= @helper('toolbar.title', array('toolbar' => $toolbar))?>
