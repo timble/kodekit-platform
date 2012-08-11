d9f513c6086a83a40faf9b745d41b80137e5f83d
Johan Janssens <johan@timble.net>
2012-08-11 18:30:46 +0000

    Cleanup. Removed defined('KOOWA') or die('Restricted access') ?> checks

--- code/administrator/components/com_categories/views/categories/tmpl/default.php
+++ code/administrator/components/com_categories/views/categories/tmpl/default.php
@@ -1,4 +1,4 @@
-<?php 
+<?
 /**
  * @version     $Id$
  * @package     Nooku_Server
@@ -7,7 +7,7 @@
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
-defined('KOOWA') or die( 'Restricted access' ); ?>
+?>
 
 <!--
 <script src="media://lib_koowa/js/koowa.js" />
