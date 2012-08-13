12b54d99c783229a4e87520e71266986dffc3a9a
Gergo Erdosi <gergo@timble.net>
2012-08-13 20:30:40 +0000

    Don't display hidden pages in the menu. re #83

--- code/site/modules/mod_pages/html.php
+++ code/site/modules/mod_pages/html.php
@@ -13,9 +13,13 @@ class ModPagesHtml extends ModDefaultHtml
         
         foreach($pages as $page)
         {
-            if($page->pages_menu_id != $this->params->get('menu_id') || $page->level - 1 < $start
-                || ($page->level - 1 != $start && (!$children || ($end != 0 && ($end <= $start || $page->level - 1 > $end)))))
+            if($page->pages_menu_id != $this->params->get('menu_id') || $page->hidden)
             {
+                $this->pages->extract($page);
+                continue;
+            }
+                
+            if($page->level - 1 < $start || ($page->level - 1 != $start && (!$children || ($end != 0 && ($end <= $start || $page->level - 1 > $end))))) {
                 $this->pages->extract($page);
             }
         }
