a3e23645bb628187cacde0c40dd6d602e34405ff
Gergo Erdosi <gergo@timble.net>
2012-08-14 10:33:38 +0000

    Add languages tables. re #215

--- code/installation/sql/12.3.sql
+++ code/installation/sql/12.3.sql
@@ -419,4 +419,51 @@ ALTER TABLE `#__pages` DROP COLUMN `pollid`;
 ALTER TABLE `#__pages` DROP COLUMN `browserNav`;
 ALTER TABLE `#__pages` DROP COLUMN `utaccess`;
 ALTER TABLE `#__pages` DROP COLUMN `lft`;
-ALTER TABLE `#__pages` DROP COLUMN `rgt`
+ALTER TABLE `#__pages` DROP COLUMN `rgt`;
+
+# --------------------------------------------------------
+# com_languages schema changes
+
+CREATE TABLE IF NOT EXISTS `#__languages_items` (
+    `languages_item_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `iso_code` VARCHAR(8) NOT NULL,
+    `table` VARCHAR(150) NOT NULL,
+    `row` INT UNSIGNED NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `created_on` DATETIME,
+    `created_by` INT UNSIGNED,
+    `modified_on` DATETIME,
+    `modified_by` INT UNSIGNED,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `original` BOOLEAN NOT NULL DEFAULT 0,
+    `deleted` BOOLEAN NOT NULL DEFAULT 0,
+    `params` TEXT,
+    PRIMARY KEY (`languages_item_id`)
+) ENGINE = InnoDB CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `#__languages_languages` (
+    `languages_language_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(150) NOT NULL,
+    `native_name` VARCHAR(150) NOT NULL,
+    `iso_code` VARCHAR(8) NOT NULL,
+    `slug` VARCHAR(255) NOT NULL,
+    `created_on` DATETIME,
+    `created_by` INT UNSIGNED,
+    `locked_on` DATETIME,
+    `locked_by` INT UNSIGNED,
+    `enabled` BOOLEAN NOT NULL DEFAULT 0,
+    `image` VARCHAR(255),
+    `ordering` INT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (`languages_language_id`),
+    UNIQUE KEY (`iso_code`),
+    UNIQUE KEY (`slug`)
+) ENGINE = InnoDB CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `#__languages_tables` (
+    `languages_table_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `table_name` VARCHAR(150) NOT NULL,
+    `unique_column` VARCHAR(150) NOT NULL,
+    `title_column` VARCHAR(150) NOT NULL,
+    `enabled` BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (`languages_table_id`)
+) ENGINE = InnoDB CHARSET = utf8;
--- code/installation/sql/install_schema.sql
+++ code/installation/sql/install_schema.sql
@@ -247,6 +247,68 @@ CREATE TABLE `#__core_acl_groups_aro_map` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `#__languages_items`
+--
+
+CREATE TABLE IF NOT EXISTS `#__languages_items` (
+    `languages_item_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `iso_code` VARCHAR(8) NOT NULL,
+    `table` VARCHAR(150) NOT NULL,
+    `row` INT UNSIGNED NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `created_on` DATETIME,
+    `created_by` INT UNSIGNED,
+    `modified_on` DATETIME,
+    `modified_by` INT UNSIGNED,
+    `status` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+    `original` BOOLEAN NOT NULL DEFAULT 0,
+    `deleted` BOOLEAN NOT NULL DEFAULT 0,
+    `params` TEXT,
+    PRIMARY KEY (`languages_item_id`)
+) ENGINE = InnoDB CHARSET = utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__languages_languages`
+--
+
+CREATE TABLE IF NOT EXISTS `#__languages_languages` (
+    `languages_language_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(150) NOT NULL,
+    `native_name` VARCHAR(150) NOT NULL,
+    `iso_code` VARCHAR(8) NOT NULL,
+    `slug` VARCHAR(255) NOT NULL,
+    `created_on` DATETIME,
+    `created_by` INT UNSIGNED,
+    `locked_on` DATETIME,
+    `locked_by` INT UNSIGNED,
+    `enabled` BOOLEAN NOT NULL DEFAULT 0,
+    `image` VARCHAR(255),
+    `ordering` INT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (`languages_language_id`),
+    UNIQUE KEY (`iso_code`),
+    UNIQUE KEY (`slug`)
+) ENGINE = InnoDB CHARSET = utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__languages_tables`
+--
+
+CREATE TABLE IF NOT EXISTS `#__languages_tables` (
+    `languages_table_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `table_name` VARCHAR(150) NOT NULL,
+    `unique_column` VARCHAR(150) NOT NULL,
+    `title_column` VARCHAR(150) NOT NULL,
+    `enabled` BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (`languages_table_id`)
+) ENGINE = InnoDB CHARSET = utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `#__modules`
 --
 
