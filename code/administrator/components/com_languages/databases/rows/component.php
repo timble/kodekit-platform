7904b8a2edaaf3196c48d9edbcaf8e5413eb9bb6
Gergo Erdosi <gergo@timble.net>
2012-08-18 20:56:15 +0000

    Fix table row. Clean code up. re #215

--- code/administrator/components/com_languages/databases/rows/component.php
+++ code/administrator/components/com_languages/databases/rows/component.php
@@ -3,9 +3,10 @@ class ComLanguagesDatabaseRowComponent extends KDatabaseRowDefault
 {
     public function save()
     {
-        $result = parent::save();
+        $modified = $this->isModified('enabled');
+        $result   = parent::save();
         
-        if($this->getStatus() == KDatabase::STATUS_UPDATED && $this->isModified('enabled') && $this->enabled)
+        if($this->getStatus() == KDatabase::STATUS_UPDATED && $modified && $this->enabled)
         {
             // If there aren't any tables for the component yet, add them from the manifest file.
             $tables = $this->getService('com://admin/languages.database.table.tables');
@@ -14,26 +15,22 @@ class ComLanguagesDatabaseRowComponent extends KDatabaseRowDefault
                 $component = $this->getService('com://admin/extensions.database.table.components')
                     ->select($this->id, KDatabase::FETCH_ROW);
                 
-                $file = $this->getIdentifier()->getApplication('admin').'/components/'.$component->option.'/manifest.xml';
+                $file = $this->getIdentifier()->getApplication('admin').'/components/'.$component->option.'/databases/behaviors/translatable.xml';
                 if(file_exists($file) && $xml = simplexml_load_file($file))
                 {
-                    if(isset($xml->tables))
+                    $data = array();
+                    foreach($xml as $table)
                     {
-                        $data = array();
-                        foreach($xml->tables->children() as $table)
-                        {
-                            if((string) $table['translatable'] == 'yes')
-                            {
-                                $data[] = array(
-                                    'components_component_id' => $this->id,
-                                    'table' => (string) $table
-                                );
-                            }
-                        }
-                        
-                        if($data) {
-                            $tables->getRowset()->addData($data)->save();
-                        }
+                        $data[] = array(
+                            'components_component_id' => $this->id,
+                            'name' => (string) $table->name,
+                            'unique_column' => (string) $table->columns->unique,
+                            'title_column' => (string) $table->columns->title
+                        );
+                    }
+                    
+                    if($data) {
+                        $tables->getRowset()->addData($data)->save();
                     }
                 }
             }
--- code/administrator/components/com_languages/databases/rows/table.php
+++ code/administrator/components/com_languages/databases/rows/table.php
@@ -3,29 +3,30 @@ class ComLanguagesDatabaseRowTable extends KDatabaseRowDefault
 {
     public function save()
     {
-        if($this->isNew())
+        $result = parent::save();
+        
+        if($this->getStatus() == KDatabase::STATUS_CREATED)
         {
             $database  = $this->getTable()->getDatabase();
-            $languages = $this->getService('com://admin/languages.model.languages')->getList();
-            $primary   = $this->getService('com://admin/languages.config.language')->getPrimary();
+            $prefix    = $database->getTablePrefix();
+            $languages = JFactory::getApplication()->getLanguages();
+            $primary   = $languages->getPrimary();
 
             foreach($languages as $language)
             {
-                $table_name = $this->table_name;
-                
-                if($language->iso_code != $primary->iso_code)
+                if($language->id != $primary->id)
                 {
-                    $table_name = strtolower($language->iso_code).'_'.$this->table_name;
+                    $table = strtolower($language->iso_code).'_'.$this->table;
                     
                     // Create language specific table.
-                    $query = 'CREATE TABLE '.$database->quoteIdentifier($database->getTablePrefix().$table_name).
-                        ' LIKE '.$database->quoteIdentifier($database->getTablePrefix().$this->table_name);
+                    $query = 'CREATE TABLE '.$database->quoteIdentifier($prefix.$table).
+                        ' LIKE '.$database->quoteIdentifier($prefix.$this->table);
                     $database->execute($query);
                     
                     // Copy content of original table into the language specific one.
                     $query = $this->getService('koowa:database.query.insert')
-                        ->table($table_name)
-                        ->values($this->getService('koowa:database.query.select')->table($this->table_name));
+                        ->table($table)
+                        ->values($this->getService('koowa:database.query.select')->table($this->table));
                     $database->execute($query);
                     
                     $status   = ComLanguagesDatabaseRowItem::STATUS_MISSING;
@@ -38,26 +39,24 @@ class ComLanguagesDatabaseRowTable extends KDatabaseRowDefault
                     $original = 1;
                 }
                 
-                // Add items to languages_items table.
-                $subquery = $this->getService('koowa:database.query.select')
+                // Add items to the items table.
+                $select = $this->getService('koowa:database.query.select')
                     ->columns(array(
-                        'iso_code' => ':iso_code',
-                        'table' => ':table_name',
-                        'row' => ':unique_column',
-                        'title' => ':title_column',
-                        'created_on' => 'NOW()',
-                        'created_by' => 0,
-                        'modified_on' => 'NOW()',
-                        'modified_by' => 0,
+                        'languages_language_id' => ':languages_language_id',
+                        'table' => ':table',
+                        'row' => $this->unique_column,
+                        'title' => $this->title_column,
+                        'created_on' => ':created_on',
+                        'created_by' => ':created_by',
                         'status' => ':status',
                         'original' => ':original'
                     ))
-                    ->table($table_name)
+                    ->table($this->name)
                     ->bind(array(
-                        'iso_code' => $language->iso_code,
-                        'table_name' => $this->table_name,
-                        'unique_column' => $this->unique_column,
-                        'title_column' => $this->title_column,
+                        'languages_language_id' => $language->id,
+                        'table' => $this->name,
+                        'created_on' => gmdate('Y-m-d H:i:s'),
+                        'created_by' => JFactory::getUser()->id,
                         'status' => $status,
                         'original' => $original
                     ));
@@ -65,132 +64,21 @@ class ComLanguagesDatabaseRowTable extends KDatabaseRowDefault
                 $query = $this->getService('koowa:database.query.insert')
                     ->table('languages_items')
                     ->columns(array(
-                        'iso_code',
+                        'languages_language_id',
                         'table',
                         'row',
                         'title',
                         'created_on',
                         'created_by',
-                        'modified_on',
-                        'modified_by',
                         'status',
                         'original'
                     ))
-                    ->values($subquery);
+                    ->values($select);
                 
                 $database->execute($query);
             }
         }
         
-        return parent::save();
-    }
-    
-    protected function _findUniqueColumn()
-    {
-        $result = '';
-        $schema = $this->getTable()->getDatabase()->getTableSchema($this->table_name);
-        
-        // Find the primary key.
-        foreach($schema->columns as $column)
-        {
-            if($column->primary)
-            {
-                $result = $column->name;
-                break;
-            }
-        }
-        
-        // If no primary key was found, try an autoinc column.
-        if(!$result)
-        {
-            foreach($schema->columns as $column)
-            {
-                if($column->autoinc)
-                {
-                    $result = $column->name;
-                    break;
-                }
-            }
-        }
-        
-        // If no autoinc column was found, try a unique key.
-        if(!$result)
-        {
-            foreach($schema->columns as $column)
-            {
-                if($column->unique)
-                {
-                    $result = $column->name;
-                    break;
-                }
-            }
-        }
-        
         return $result;
-    }
-    
-    protected function _findTitleColumn()
-    {
-        $result = '';
-        $schema = $this->getTable()->getDatabase()->getTableSchema($this->table_name);
-        
-        // Find the title column, based on a list of typical names.
-        $titles = array('title', 'name', 'alias', 'text', 'dmname');
-        
-        foreach($titles as $title)
-        {
-            if(array_key_exists($title, $schema->columns))
-            {
-                $result = $title;
-                break;
-            }
-        }
-        
-        // If no suitable column was found, find a char or varchar field to use as title.
-        if(!$result)
-        {
-            foreach($schema->columns as $column)
-            {
-                if(stripos($column->type, 'char'))
-                {
-                    $result = $column->name;
-                    break;
-                }
-            }
-        }
-        
-        // If still nothing was found, try text fields.
-        if(!$result)
-        {
-            foreach($schema->columns as $column)
-            {
-                if(stripos($column->type, 'text'))
-                {
-                    $result = $column->name;
-                    break;
-                }
-            }
-        }
-        
-        return $result;
-    }
-    
-    public function __get($key)
-    {
-        if(($key == 'unique_column' || $key == 'title_column') && !isset($this->data['unique_column'])) {
-            $this->unique_column = $this->_findUniqueColumn();
-        }
-        
-        if($key == 'title_column' && !isset($this->data['title_column'])) {
-            $this->title_column = $this->_findTitleColumn();
-        }
-        
-        if($key == 'description' && !isset($this->_data['description']))
-        {
-            $schema = $this->getTable()->getDatabase()->getTableSchema($this->table_name);
-            $this->description = $schema->description;
-        }
-        
-        return parent::__get($key);
     }
 }
