9007f86be8bb6b9c49c540af1e4d25e460bf04c3
Gergo Erdosi <gergo@timble.net>
2012-08-19 11:38:31 +0000

    Add application state. re #215

--- code/administrator/components/com_languages/models/languages.php
+++ code/administrator/components/com_languages/models/languages.php
@@ -8,7 +8,8 @@ class ComLanguagesModelLanguages extends KModelTable
         $this->getState()
             ->insert('primary', 'boolean', null, true)
             ->insert('iso_code', 'com://admin/languages.filter.iso', null, true)
-            ->insert('enabled', 'boolean');
+            ->insert('enabled', 'boolean')
+            ->insert('application', 'cmd');
     }
     
     protected function _buildQueryWhere(KDatabaseQuerySelect $query)
@@ -24,6 +25,10 @@ class ComLanguagesModelLanguages extends KModelTable
         
         if(is_bool($state->enabled)) {
             $query->where('tbl.enabled = :enabled')->bind(array('enabled' => (int) $state->enabled));
+        }
+        
+        if($state->application) {
+            $query->where('tbl.application = :application')->bind(array('application' => $state->application));
         }
     }
 }
