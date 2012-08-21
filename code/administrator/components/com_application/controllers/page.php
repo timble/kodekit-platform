798068050c4c0806d5b940ed96f2d77e10983923
Johan Janssens <johan@timble.net>
2012-08-21 02:15:01 +0000

    re #228 : Added page controller

---
+++ code/administrator/components/com_application/controllers/page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @version		$Id$
+ * @package     Nooku_Server
+ * @subpackage  Application
+ * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+
+/**
+ * Page Controller Class
+ *   
+ * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Application
+ */
+ 
+class ComApplicationControllerPage extends KControllerResource
+{
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'view' => 'page'
+        ));
+
+        parent::_initialize($config);
+    }
+}
