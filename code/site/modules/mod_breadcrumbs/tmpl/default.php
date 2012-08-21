984d747f4393906b64d834e757f356d276a0270d
Johan Janssens <johan@timble.net>
2012-08-21 01:46:16 +0000

    re #234 : Refactored module

--- code/site/modules/mod_breadcrumbs/tmpl/default.php
+++ code/site/modules/mod_breadcrumbs/tmpl/default.php
@@ -1,8 +1,19 @@
+<?
+/**
+ * @version		$Id: form.php 1294 2011-05-16 22:57:57Z johanjanssens $
+ * @package     Nooku_Server
+ * @subpackage  Pages
+ * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+?>
+
 <ul class="breadcrumb">
 
-	<?php for ($i = 0; $i < $count; $i ++) : ?>
+	<? for ($i = 0; $i < count($list); $i ++) : ?>
 		<? // If not the last item in the breadcrumbs add the separator ?>
-		<? if ($i < $count - 1) : ?>
+		<? if ($i < count($list) - 1) : ?>
 			<? if(!empty($list[$i]->link)) : ?>
 				<li><?= '<a href="'.$list[$i]->link.'" class="pathway">'.$list[$i]->name.'</a>'; ?></li>
 			<? else : ?>
