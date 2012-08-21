9c1f686504a592e1af17ec4a633d62e31816c29a
Johan Janssens <johan@timble.net>
2012-08-21 01:29:46 +0000

    re #234 : Refactored module

---
+++ code/site/modules/mod_syndicate/html.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @version     $Id: html.php 1481 2012-02-10 01:46:24Z johanjanssens $
+ * @package     Nooku_Server
+ * @subpackage  Default
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Syndicate Module Html View Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Modules
+ * @subpackage  Default
+ */
+ 
+class ModSyndicateHtml extends ModDefaultHtml
+{
+    public function display()
+    {
+        $this->params->set('text', 'Feed Entries');
+
+        if($link = $this->getLink($this->params))
+        {
+            $this->assign('params', $this->params);
+            $this->assign('link'  , $link);
+
+            return parent::display();
+        }
+    }
+
+    function getLink($params)
+    {
+        $document = JFactory::getDocument();
+
+        foreach($document->_links as $link)
+        {
+            if(strpos($link, 'application/rss+xml'))
+            {
+                preg_match("#href=\"(.*?)\"#s", $link, $matches);
+                return $matches[1];
+            }
+        }
+
+    }
+} 
