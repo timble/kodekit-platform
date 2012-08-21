e05050faaca0eb192e97e6da2084b5d71afa73a0
Johan Janssens <johan@timble.net>
2012-08-21 02:44:06 +0000

    re #234 : Cleanup

--- code/administrator/modules/mod_status/tmpl/default.php
+++ code/administrator/modules/mod_status/tmpl/default.php
@@ -12,21 +12,19 @@
 
 <script src="media://lib_koowa/js/koowa.js" />
 
-<? $class = JRequest::getInt('hidemainmenu') ? "class='disabled'" : ""; ?>
-
 <ul id="statusmenu">
 <li class="preview">
 	<a href="<?= JURI::root() ?>" target="_blank"><?= @text('Preview') ?></a>
 </li>
 
-<li <?= $class  ?>>
+<li>
 	<a href="<?= JRoute::_('index.php?option=com_users&view=user&id='.JFactory::getUser()->id) ?>"><?= @text('My Profile') ?></a>
 </li>
 
 <? if(!strpos(KRequest::get('server.HTTP_USER_AGENT', 'word'), 'Titanium')) : ?>
 
-    <? $json = "{method:'post', url:'index.php?option=com_users&view=session&id=".JFactory::getSession()->getId()."', params:{action:'delete', _token:'".@service('session')->getToken()."'}}"; ?>
-    <li <?= $class ?>>
+    <? $json = "{method:'post', url:'index.php?option=com_users&view=session&id=".@service('application.session')->getId()."', params:{action:'delete', _token:'".@service('application.session')->getToken()."'}}"; ?>
+    <li>
     	<a href="#" onclick="new Koowa.Form(<?= $json ?>).submit();"><?= @text('Logout') ?></a>
     </li>
 <? endif; ?>
