f2701883c45649cf7a67de35cce34ca32f1648e1
Johan Janssens <johan@timble.net>
2012-08-21 01:51:16 +0000

    re #228 : Replace JFactory::getApplication() with $this->getService('application')

--- code/site/components/com_users/router.php
+++ code/site/components/com_users/router.php
@@ -26,7 +26,7 @@ class ComUsersRouter extends ComDefaultRouter
         {
             if(!empty($query['Itemid']))
             {
-                $page = JFactory::getApplication()->getPages()->find($query['Itemid'] );
+                $page = $this->getService('application')->getPages()->find($query['Itemid'] );
                 if(!isset($page->link->query['view']) || $page->link->query['view'] != $query['view']) {
                     $segments[] = $query['view'];
                 }
