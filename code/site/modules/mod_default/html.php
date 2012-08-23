6467cdbec5ab691e05d8d22c87673d7b7b2b9e1a
Johan Janssens <johan@timble.net>
2012-08-23 22:49:59 +0000

    re #234 : Dynamically attach the chrome filter is one or more chrome styles have been defined. Removed Joomla compatibility.

--- code/site/modules/mod_default/html.php
+++ code/site/modules/mod_default/html.php
@@ -28,25 +28,22 @@ class ModDefaultHtml extends KViewHtml
     protected function _initialize(KConfig $config)
     {
         $config->append(array(
-        	'media_url'        => KRequest::root() . '/media',
-        	'template_filters' => array('chrome'),
-            'data'			   => array(
-                'styles' => array()
-            )
+            'media_url'  => KRequest::root() . '/media',
+            'data'	     => array()
         ));
 
         parent::_initialize($config);
     }
 
-	/**
-	 * Get the name
-	 *
-	 * @return 	string 	The name of the object
-	 */
-	public function getName()
-	{
-		return $this->getIdentifier()->package;
-	}
+    /**
+     * Get the name
+     *
+     * @return 	string 	The name of the object
+     */
+    public function getName()
+    {
+        return $this->getIdentifier()->package;
+    }
 
     /**
      * Renders and echo's the views output
@@ -55,75 +52,30 @@ class ModDefaultHtml extends KViewHtml
      */
     public function display()
     {
-		//Load the language files.
-		//Type only exists if the module is loaded through ComExtensionsModelsModules
-		if(isset($this->module->type)) {
-            JFactory::getLanguage()->load($this->module->type);
-		}
+        //Load the language files.
+        JFactory::getLanguage()->load($this->module->type);
+
+        //Dynamically attach the chrome filter
+        if(!empty($this->module->chrome)) {
+            $this->getTemplate()->attachFilter('chrome', array('styles' => $this->module->chrome));
+        }
 
         if(empty($this->module->content))
-		{
+        {
             $identifier = clone $this->getIdentifier();
             $identifier->name = $this->getLayout();
 
             $this->output = $this->getTemplate()
                 ->loadIdentifier($identifier, $this->_data)
                 ->render();
-		}
-		else
-		{
-		     $this->output = $this->getTemplate()
+        }
+        else
+        {
+            $this->output = $this->getTemplate()
                 ->loadString($this->module->content, $this->_data, false)
                 ->render();
-		}
+        }
 
         return $this->output;
-    }
-
-    /**
-     * Set a view properties
-     *
-     * @param   string  The property name.
-     * @param   mixed   The property value.
-     */
-    public function __set($property, $value)
-    {
-        if($property == 'module')
-        {
-            if(is_string($value->params)) {
-                $value->params = $this->_parseParams($value->params);
-            }
-        }
-
-        parent::__set($property, $value);
-    }
-
-	/**
-     * Method to extract key/value pairs out of a string
-     *
-     * @param   string  String containing the parameters
-     * @return  array   Key/Value pairs for the attributes
-     */
-    protected function _parseParams( $string )
-    {
-        $params = array();
-
-        if(!version_compare(JVERSION,'1.6.0','ge'))
-        {
-            $string = trim($string);
-
-            if(!empty($string))
-            {
-                foreach(explode("\n", $string) as $line)
-                {
-                    $param = explode("=", $line, 2);
-                    $params[$param[0]] = $param[1];
-                }
-            }
-        }
-        else $params = json_decode($string, true);
-
-        $params = new KConfig($params);
-        return $params;
     }
 }
