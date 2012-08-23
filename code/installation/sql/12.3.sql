8379b94bd45546d3b2aed37447ec464c494d0229
Johan Janssens <johan@timble.net>
2012-08-23 22:32:05 +0000

    Renamed client_id to application. Application column now holds the real application name.

--- code/installation/sql/12.3.sql
+++ code/installation/sql/12.3.sql
@@ -43,8 +43,11 @@ DROP TABLE #__plugins;
 ALTER TABLE `#__modules` DROP `iscore`;
 ALTER TABLE `#__modules` DROP `control`;
 ALTER TABLE `#__modules` DROP `numnews`;
-
 ALTER TABLE `#__components` DROP `iscore`;
+
+ALTER TABLE  `#__modules` CHANGE  `client_id`  `application` VARCHAR( 50 ) NOT NULL
+UPDATE `#__modules` SET `application` = 'admin' WHERE `application` = 1;
+UPDATE `#__modules` SET `application` = 'site' WHERE `application` = 0;
 
 # --------------------------------------------------------
 # com_contacts schema changes
@@ -60,11 +63,11 @@ UPDATE `#__categories` SET `section` = 'com_contacts' WHERE `section` = 'com_con
 UPDATE `#__users` SET `params` = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(`params`, 'timezone=-12', 'timezone=Etc/GMT-12'), 'timezone=-11', 'timezone=Pacific/Midway'), 'timezone=-10', 'timezone=Pacific/Honolulu'), 'timezone=-9.5', 'timezone=Pacific/Marquesas'), 'timezone=-9', 'timezone=US/Alaska'), 'timezone=-8', 'timezone=US/Pacific'), 'timezone=-7', 'timezone=US/Mountain'), 'timezone=-6', 'timezone=US/Central'), 'timezone=-5', 'timezone=US/Eastern'), 'timezone=-4.5', 'timezone=America/Caracas'), 'timezone=-4', 'timezone=America/Barbados'), 'timezone=-3.5', 'timezone=Canada/Newfoundland'), 'timezone=-3', 'timezone=America/Buenos_Aires'), 'timezone=-2', 'timezone=Atlantic/South_Georgia'), 'timezone=-1', 'timezone=Atlantic/Azores'), 'timezone=0', 'timezone=Europe/London'), 'timezone=1', 'timezone=Europe/Amsterdam'), 'timezone=2', 'timezone=Europe/Istanbul'), 'timezone=3', 'timezone=Asia/Riyadh'), 'timezone=3.5', 'timezone=Asia/Tehran'), 'timezone=4', 'timezone=Asia/Muscat'), 'timezone=4.5', 'timezone=Asia/Kabul'), 'timezone=5', 'timezone=Asia/Karachi'), 'timezone=5.5', 'timezone=Asia/Calcutta'), 'timezone=5.75', 'timezone=Asia/Katmandu'), 'timezone=6', 'timezone=Asia/Dhaka'), 'timezone=6.5', 'timezone=Indian/Cocos'), 'timezone=7', 'timezone=Asia/Bangkok'), 'timezone=8', 'timezone=Australia/Perth'), 'timezone=8.75', 'timezone=Australia/West'), 'timezone=9', 'timezone=Asia/Tokyo'), 'timezone=9.5', 'timezone=Australia/Adelaide'), 'timezone=10', 'timezone=Australia/Brisbane'), 'timezone=10.5', 'timezone=Australia/Lord_Howe'), 'timezone=11', 'timezone=Pacific/Kosrae'), 'timezone=11.5', 'timezone=Pacific/Norfolk'), 'timezone=12', 'timezone=Pacific/Auckland'), 'timezone=12.75', 'timezone=Pacific/Chatham'), 'timezone=13', 'timezone=Pacific/Tongatapu'), 'timezone=14', 'timezone=Pacific/Kiritimati');
 
 -- Remove unused indexes
-ALTER TABLE jos_users DROP INDEX idx_name;
-ALTER TABLE jos_users DROP INDEX gid_block;
+ALTER TABLE #__users DROP INDEX idx_name;
+ALTER TABLE #__users DROP INDEX gid_block;
 
 -- Update indexes
-ALTER TABLE  `jos_users` DROP INDEX  `email` , ADD UNIQUE  `email` (  `email` );
+ALTER TABLE  `#__users` DROP INDEX  `email` , ADD UNIQUE  `email` (  `email` );
 
 -- Remove unused columns from #__session
 RENAME TABLE`#__session` TO  `#__users_sessions`;
@@ -75,6 +78,7 @@ ALTER TABLE `#__users_sessions` DROP `gid`;
 ALTER TABLE `#__users_sessions` CHANGE  `session_id`  `users_session_id` VARCHAR( 128 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 ALTER TABLE `#__users_sessions` CHANGE  `userid`  `email` VARCHAR( 100 ) NOT NULL COMMENT  '@Filter("email")'
 ALTER TABLE `#__users_sessions` DROP INDEX  `userid`;
+ALTER TABLE `#__users_sessions` CHANGE  `client_id`  `application` VARCHAR( 50 ) NOT NULL
 
 ALTER TABLE `#__users` DROP `username`;
 
@@ -317,7 +321,7 @@ UPDATE `#__components` SET `admin_menu_link` = 'option=com_articles&view=article
 UPDATE `#__components` SET `admin_menu_link` = 'option=com_contacts&view=contacts' WHERE `admin_menu_link` = 'option=com_contacts' OR `link` = 'option=com_contacts';
 UPDATE `#__components` SET `admin_menu_link` = 'option=com_weblinks&view=weblinks' WHERE `admin_menu_link` = 'option=com_weblinks' OR `link` = 'option=com_weblinks';
 
-UPDATE `#__modules` SET `title` = 'Admin Pages', `module` = 'mod_pages' WHERE `module` = 'mod_menu' AND `client_id` = 1;
+UPDATE `#__modules` SET `title` = 'Admin Pages', `module` = 'mod_pages' WHERE `module` = 'mod_menu' AND `application` = 'admin';
 UPDATE `#__modules` SET `module` = 'mod_pages' WHERE `module` = 'mod_mainmenu';
 UPDATE `#__modules` AS `modules` SET `modules`.`params` = REPLACE(`modules`.`params`, CONCAT('menutype=', SUBSTRING_INDEX(SUBSTRING_INDEX(`modules`.`params`, 'menutype=', -1), '\n', 1)), CONCAT('menu_id=', (SELECT `id` FROM `#__pages_menus` AS `menus` WHERE `menus`.`slug` = SUBSTRING_INDEX(SUBSTRING_INDEX(`modules`.`params`, 'menutype=', -1), '\n', 1)))) WHERE `modules`.`module` = 'mod_pages';
 
