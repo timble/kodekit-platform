d8d2c0cf143575524ab0a612a78f2b3e7200d12e
Johan Janssens <johan@timble.net>
2012-08-23 22:48:57 +0000

    re #234 : Module chrome styles are now implemented as functions inside the chrome filter.

--- code/site/modules/mod_default/templates/filters/chrome.php
+++ code/site/modules/mod_default/templates/filters/chrome.php
@@ -1,23 +1,30 @@
 <?php
 /**
-* @version      $Id$
-* @package      Nooku_Modules
-* @subpackage	Default
-* @copyright    Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
-* @license      GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
-* @link 		http://www.nooku.org
-*/
+ * @version      $Id$
+ * @package      Nooku_Modules
+ * @subpackage	Default
+ * @copyright    Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license      GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link 		http://www.nooku.org
+ */
 
 /**
  * Module Chrome Filter
  *
  * @author		Johan Janssens <johan@nooku.org>
-* @package      Nooku_Module
-* @subpackage   Default
+ * @package      Nooku_Module
+ * @subpackage   Default
  */
 class ModDefaultTemplateFilterChrome extends KTemplateFilterAbstract implements KTemplateFilterWrite
 {
-  	/**
+    /**
+     * The chrome styles
+     *
+     * @var array
+     */
+    protected $_styles;
+
+    /**
      * Constructor.
      *
      * @param   object  An optional KConfig object with configuration options
@@ -26,14 +33,10 @@ class ModDefaultTemplateFilterChrome extends KTemplateFilterAbstract implements 
     {
         parent::__construct($config);
 
-        include_once JPATH_APPLICATION.'/templates/system/html/modules.php';
-
-        if(file_exists(JPATH_APPLICATION.'/templates/'.$config->template.'/html/modules.php')) {
-		    include_once JPATH_APPLICATION.'/templates/'.$config->template.'/html/modules.php';
-        }
+        $this->_styles = $config->styles;
     }
 
-	/**
+    /**
      * Initializes the options for the object
      *
      * Called from {@link __construct()} as a first step of object instantiation.
@@ -44,42 +47,153 @@ class ModDefaultTemplateFilterChrome extends KTemplateFilterAbstract implements 
     protected function _initialize(KConfig $config)
     {
         $config->append(array(
-            'priority'   => KCommand::PRIORITY_LOW,
-            'template'   => JFactory::getApplication()->getTemplate()
+            'styles'  => array(),
         ));
 
         parent::_initialize($config);
     }
 
-	/**
-	 * Render the module chrome
-	 *
-	 * @param string Block of text to parse
-	 * @return ModDefaultFilterChrome
-	 */
-	public function write(&$text)
-	{
-		$data = (object) $this->getTemplate()->getData();
+    /**
+     * Append a module chrome style
+     *
+     * @return ModDefaultTemplateFilterChrome
+     */
+    public function appendStyle($style)
+    {
+        $this->_styles[] = $style;
+        return $this;
+    }
 
-	    foreach($data->styles as $style)
-		{
-            $method = 'modChrome_'.$style;
+    /**
+     * Prepend a module chrome style
+     *
+     * @return ModDefaultTemplateFilterChrome
+     */
+    public function prependStyle($style)
+    {
+        array_unshift($this->_styles, $style);
+        return $this;
+    }
 
-			// Apply chrome and render module
-		    if (function_exists($method))
-			{
-		        $data->module->style   = implode(' ', $data->styles);
-		        $data->module->content = $text;
+    /**
+     * Get the module chrome styles
+     *
+     * @return array
+     */
+    public function getStyles()
+    {
+        return $this->_styles;
+    }
 
-				ob_start();
-				    $method($data->module, $data->module->params, $data->attribs);
-				    $data->module->content = ob_get_contents();
-				ob_end_clean();
-			}
+    /**
+     * Render the module chrome
+     *
+     * @param string Block of text to parse
+     * @return ModDefaultFilterChrome
+     */
+    public function write(&$text)
+    {
+        $data = (object) $this->getTemplate()->getData();
 
-            $text = $data->module->content;
-	    }
+        foreach($data->module->chrome as $style)
+        {
+            $method = '_style'.ucfirst($style);
 
-	    return $this;
+            // Apply chrome and render module
+            if (method_exists($this, $method))
+            {
+                $data->module->content = $text;
+                $text = $this->$method($data->module);
+            }
+        }
+
+        return $this;
+    }
+
+    protected function _styleWrapped($module)
+    {
+        $html = '';
+        if (!empty ($module->content))
+        {
+            $html .= '<div class="module">';
+            if ($module->showtitle) {
+                $html .= '<h3>'.$module->title.'</h3>';
+            }
+
+            $html .= $module->content;
+            $html .= '</div>';
+        }
+
+        return $html;
+    }
+
+    protected function _styleAccordion($module)
+    {
+        $accordion = $this->getTemplate()->getHelper('accordion');
+
+        $config = array(
+            'title'     => JText::_( $module->title ),
+            'id'        => 'module' . $module->id,
+            'translate' => false
+        );
+
+        $html = '';
+        if(isset($module->attribs['rel']) && isset($module->attribs['rel']['first'])) {
+            $html .= $accordion->startPane();
+        }
+
+        $html .= $accordion->startPanel( $config);
+        $html .= $module->content;
+        $html .= $accordion->endPanel();
+
+        if(isset($module->attribs['rel']) && isset($module->attribs['rel']['last'])) {
+            $html .= $accordion->endPane();
+        }
+
+        return $html;
+    }
+
+    protected function _styleTabs($module)
+    {
+        $tabs = $this->getTemplate()->getHelper('tabs');
+
+        $config = array(
+            'title'     => JText::_( $module->title ),
+            'id'        => 'module' . $module->id,
+            'translate' => false
+        );
+
+        $html = '';
+        if(isset($module->attribs['rel']) && isset($module->attribs['rel']['first'])) {
+            $html .= $tabs->startPane();
+        }
+
+        $html .= $tabs->startPanel( $config);
+        $html .= $module->content;
+        $html .= $tabs->endPanel();
+
+        if(isset($module->attribs['rel']) && isset($module->attribs['rel']['last'])) {
+            $html .= $tabs->endPane();
+        }
+
+        return $html;
+    }
+
+    protected function _styleOutline($module)
+    {
+        $html = '';
+
+        $html .= '<style>';
+        $html .= '.mod-preview-info { padding: 2px 4px 2px 4px; border: 1px solid black; position: absolute; background-color: white; color: red;opacity: .80; filter: alpha(opacity=80); -moz-opactiy: .80; }';
+        $html .=  '.mod-preview-wrapper { background-color:#eee;  border: 1px dotted black; color:#700; opacity: .50; filter: alpha(opacity=50); -moz-opactiy: .50;}';
+
+        $html .= '<div class="mod-preview">';
+        $html .= '<div class="mod-preview-info">'.$module->position."[".$module->style."]".'</div>';
+        $html .= '<div class="mod-preview-wrapper">';
+        $html .= $module->content;
+        $html .= '</div>';
+        $html .= '</div>';
+
+        return $html;
     }
 }
