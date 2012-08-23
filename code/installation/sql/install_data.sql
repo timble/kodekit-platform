19f81ce42ce2863560c78bfc94cdba96d5e2a041
Johan Janssens <johan@timble.net>
2012-08-23 22:32:21 +0000

    re #234 : Renamed client_id to application. Application column now holds the real application name.

--- code/installation/sql/install_data.sql
+++ code/installation/sql/install_data.sql
@@ -56,12 +56,12 @@ VALUES
 -- Dumping data for table `#__modules`
 --
 
-INSERT INTO `#__modules` (`id`, `title`, `content`, `ordering`, `position`, `checked_out`, `checked_out_time`, `published`, `module`, `access`, `showtitle`, `params`, `client_id`) VALUES
-(1, 'Main Menu', '', 2, 'left', 0, '0000-00-00 00:00:00', 1, 'mod_pages', 0, 1, 'menu_id=1\nmoduleclass_sfx=_menu\n', 0),
-(2, 'Login', '', 3, 'login', 0, '0000-00-00 00:00:00', 1, 'mod_login', 0, 1, '', 1),
-(10, 'Logged in Users', '', 13, 'cpanel', 0, '0000-00-00 00:00:00', 1, 'mod_logged', 2, 1, '', 1),
-(12, 'Admin Pages', '', 5, 'menu', 0, '0000-00-00 00:00:00', 1, 'mod_pages', 2, 1, '', 1),
-(14, 'User Status', '', 7, 'status', 0, '0000-00-00 00:00:00', 1, 'mod_status', 2, 1, '', 1);
+INSERT INTO `#__modules` (`id`, `title`, `content`, `ordering`, `position`, `checked_out`, `checked_out_time`, `published`, `module`, `access`, `showtitle`, `params`, `application`) VALUES
+(1, 'Main Menu', '', 2, 'left', 0, '0000-00-00 00:00:00', 1, 'mod_pages', 0, 1, 'menu_id=1\nmoduleclass_sfx=_menu\n', 'site'),
+(2, 'Login', '', 3, 'login', 0, '0000-00-00 00:00:00', 1, 'mod_login', 0, 1, '', 'admin'),
+(10, 'Logged in Users', '', 13, 'cpanel', 0, '0000-00-00 00:00:00', 1, 'mod_logged', 2, 1, '', 'admin'),
+(12, 'Admin Pages', '', 5, 'menu', 0, '0000-00-00 00:00:00', 1, 'mod_pages', 2, 1, '', 'admin'),
+(14, 'User Status', '', 7, 'status', 0, '0000-00-00 00:00:00', 1, 'mod_status', 2, 1, '', 'admin');
 
 --
 -- Dumping data for table `#__pages_menus`
