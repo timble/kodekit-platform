5c0763314e933edc4457b10bc271df887e4c37aa
Tom Janssens <tom@timble.net>
2012-08-24 14:19:08 +0000

    re #168 Added access filter to scopebar

--- code/administrator/components/com_pages/views/pages/tmpl/default_scopebar.php
+++ code/administrator/components/com_pages/views/pages/tmpl/default_scopebar.php
@@ -11,7 +11,7 @@
 
 <div class="scopebar">
     <div class="scopebar-group">
-            <a href="<?= @route('enabled=&deleted=' ) ?>" class="<?= !is_bool($state->enabled) && !$state->deleted ? 'active' : '' ?>">
+            <a href="<?= @route('enabled=&deleted=&access=' ) ?>" class="<?= !is_bool($state->enabled) && !$state->deleted && is_null($state->access) ? 'active' : '' ?>">
                 <?= 'All' ?>
             </a>
     </div>
@@ -25,6 +25,17 @@
         <a href="<?= @route( $state->deleted ? 'deleted=' : 'deleted=1' ) ?>" class="<?= $state->deleted ? 'active' : '' ?>">
             <?= 'Trashed' ?>
         </a>
+    </div>
+    <div class="scopebar-group">
+    	<a class="<?= $state->access === 0 ? 'active' : ''; ?>" href="<?= @route($state->access === 0 ? 'access=' : 'access=0' ) ?>">
+    	    <?= 'Public' ?>
+    	</a>
+    	<a class="<?= $state->access === 1 ? 'active' : ''; ?>" href="<?= @route($state->access === 1 ? 'access=' : 'access=1' ) ?>">
+    	    <?= 'Registered' ?>
+    	</a>
+    	<a class="<?= $state->access === 2 ? 'active' : ''; ?>" href="<?= @route($state->access === 2 ? 'access=' : 'access=2' ) ?>">
+    	    <?= 'Special' ?>
+    	</a>
     </div>
     <div class="scopebar-search">
         <?= @helper('grid.search') ?>
