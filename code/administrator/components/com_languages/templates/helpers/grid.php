4f1c4ea87aab83610b1a23f7073ba5024f5301db
Gergo Erdosi <gergo@timble.net>
2012-08-24 13:37:42 +0000

    Remove flag method. re #215

--- code/administrator/components/com_languages/templates/helpers/grid.php
+++ code/administrator/components/com_languages/templates/helpers/grid.php
@@ -1,34 +1,6 @@
 <?php
 class ComLanguagesTemplateHelperGrid extends KTemplateHelperGrid
 {
-    public function flag($config = array())
-    {
-        $config = new KConfig($config);
-        $config->append(array(
-            'iso_code' => 'en-GB',
-            'url'      => 'media://com_languages/images/flags/'
-        ));
-        
-        // Need to clone, otherwise it breaks the foreach in the layout.
-        $languages = clone $this->getTemplate()->getView()->languages;
-        $language  = $languages->find(array('iso_code' => $config->iso_code))->top();
-        
-        if($language)
-        {
-            if(!strpos($config->iso_code, '-')) {
-                $image = 'unknown.png';
-            }
-            else
-            {
-                list($language, $country) = explode('-', $config->iso_code, 2);
-                $image = $config->url.$country.'.png';
-            }
-        }
-        else $image = $config->url.$language->image;
-        
-        return '<div class="languages-flag" style="background-image: url('.$image.');" title="'.$config->iso_code.'"></div>';
-    }
-    
     public function status($config = array())
     {
         $config = new KConfig($config);
