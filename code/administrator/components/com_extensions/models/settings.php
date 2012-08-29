4267a2be112e5b02d23d323b87bd3e550da36091
Johan Janssens <johan@timble.net>
2012-08-29 18:23:42 +0000

    re #237 : Merged com_settings into com_extensions

---
+++ code/administrator/components/com_extensions/models/settings.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Nooku_Server
+ * @subpackage  Settings
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Settings Model Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Settings
+ */
+
+class ComExtensionsModelSettings extends KModelAbstract
+{
+    public function __construct(KConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->getState()
+             ->insert('name', 'cmd', null, true);        
+    }
+     
+    public function getItem()
+    {
+        if(isset($this->getList()->{$this->getState()->name})) {
+            $row = $this->getList()->{$this->getState()->name};
+        } else {
+            $row = $this->getList()->getRow();
+        }
+        
+        return $row;
+    }
+    
+    public function getList()
+    {
+        if (!isset($this->_list))
+        {
+            $rowset = $this->getService('com://admin/extensions.database.rowset.settings');
+            
+            //Insert the system configuration settings
+            $rowset->insert($this->getService('com://admin/extensions.database.row.setting_system'));
+                        
+            //Insert the component configuration settings
+            $components = $this->getService('com://admin/extensions.model.components')->enabled(1)->getList();
+            
+            foreach($components as $component)
+            {
+                $path   = JPATH_APPLICATION.'/components/'.$component->option.'/config.xml';
+                    
+                $config = array(
+                	'name' => strtolower(substr($component->option, 4)),
+                    'path' => file_exists($path) ? $path : '',
+                    'id'   => $component->id,
+                    'data' => $component->params->toArray(),
+                );
+                
+                $row = $this->getService('com://admin/extensions.database.row.setting_component', $config);
+                
+                $rowset->insert($row);
+            }
+             
+            $this->_list = $rowset;
+        }
+
+        return $this->_list;    
+    }
+}  
