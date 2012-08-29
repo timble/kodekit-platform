39cb24e4b718cc39af9aa10c5947350f4044f489
Johan Janssens <johan@timble.net>
2012-08-29 18:47:55 +0000

    re #234 : Module loader and service locator have been removed.

--- code/administrator/includes/framework.php
+++ code/administrator/includes/framework.php
@@ -48,10 +48,7 @@ Koowa::getInstance(array(
 
 unset($config);
 
-KLoader::addAdapter(new KLoaderAdapterModule(array('basepath' => JPATH_APPLICATION)));
 KLoader::addAdapter(new KLoaderAdapterComponent(array('basepath' => JPATH_APPLICATION)));
-
-KServiceIdentifier::addLocator(KService::get('koowa:service.locator.module'));
 KServiceIdentifier::addLocator(KService::get('koowa:service.locator.component'));
 		
 KServiceIdentifier::setApplication('site' , JPATH_ROOT.'/site');
