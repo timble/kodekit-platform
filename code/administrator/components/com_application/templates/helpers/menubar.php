7a27cf3dcb83b47d268bfab73ee30001d4649b60
Johan Janssens <johan@timble.net>
2012-08-29 19:24:57 +0000

    re #228 : Added specialised application menubar and toolbar template helpers.

---
+++ code/administrator/components/com_application/templates/helpers/menubar.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @version     $Id: menubar.php 4813 2012-08-21 02:25:31Z johanjanssens $
+ * @package     Nooku_Server
+ * @subpackage  Application
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Template Menubar Helper
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Application
+ */
+class ComApplicationTemplateHelperMenubar extends KTemplateHelperAbstract
+{
+ 	/**
+     * Render the menubar
+     *
+     * @param   array   An optional array with configuration options
+     * @return  string  Html
+     */
+    public function render($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+        	'menubar' => null,
+            'attribs' => array()
+        ));
+
+        $html = '<ul '.$this->_buildAttributes($config->attribs).'>';
+	    foreach ($config->menubar as $command)
+	    {
+            $name = $command->getName();
+
+            if(method_exists($this, $name)) {
+                $html .= $this->$name(array('command' => $command));
+            } else {
+                $html .= $this->command(array('command' => $command));
+            }
+        }
+        $html .= '</ul>';
+
+		return $html;
+    }
+
+    /**
+     * Render a menubar command
+     *
+     * @param   array   An optional array with configuration options
+     * @return  string  Html
+     */
+    public function command($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+        	'command' => null
+        ));
+
+        $command = $config->command;
+
+        //Add a nolink class if the command is disabled
+        if($command->disabled) {
+            $command->attribs->class->append(array('nolink'));
+        }
+
+        if($command->active) {
+             $command->attribs->class->append(array('active'));
+        }
+
+        //Create the href
+        if(!empty($command->href)) {
+            $command->attribs['href'] = $this->getTemplate()->getView()->getRoute($command->href);
+        }
+
+        $html = count($command) ? '<li class="node">' : '<li>';
+        if ($command->disabled) {
+			$html .= '<span '.$this->_buildAttributes($command->attribs).'>'.JText::_($command->label).'</span>';
+		} else {
+			$html .= '<a '.$this->_buildAttributes($command->attribs).'>'.JText::_($command->label).'</a>';
+		}
+
+        if(count($command)) {
+            $html .= $this->render(array('menubar' => $command));
+        }
+        $html .= '</li>';
+
+    	return $html;
+    }
+
+    /**
+     * Render a separator
+     *
+     * @param   array   An optional array with configuration options
+     * @return  string  Html
+     */
+    public function separator($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'command' => NULL
+        ));
+
+        $command = $config->command;
+
+        $html = '<li class="separator"><span></span></li>';
+
+        return $html;
+    }
+}
---
+++ code/administrator/components/com_application/templates/helpers/toolbar.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @version     $Id: behavior.php 3364 2011-05-25 21:07:41Z johanjanssens $
+ * @package     Nooku_Server
+ * @subpackage  Application
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Template Toolbar Helper
+ *
+ * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Application
+ */
+class ComApplicationTemplateHelperToolbar extends KTemplateHelperAbstract
+{
+    /**
+     * Render the toolbar
+     *
+     * @param   array   An optional array with configuration options
+     * @return  string  Html
+     */
+    public function render($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+        	'toolbar' => null,
+            'attribs' => array()
+        ));
+
+        $html  = '<ul '.$this->_buildAttributes($config->attribs).'>';
+	    foreach ($config->toolbar->getCommands() as $command)
+	    {
+            $name = $command->getName();
+
+            $html .= '<li>';
+	        if(method_exists($this, $name)) {
+                $html .= $this->$name(array('command' => $command));
+            } else {
+                $html .= $this->command(array('command' => $command));
+            }
+            $html .= '</li>';
+       	}
+		$html .= '</ul>';
+
+		return $html;
+    }
+
+    /**
+     * Render a toolbar command
+     *
+     * @param   array   An optional array with configuration options
+     * @return  string  Html
+     */
+    public function command($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+        	'command' => array()
+        ));
+
+        $command = $config->command;
+
+        //Create the id
+        $command->attribs['id'] = 'command-'.$command->id;
+
+        //Create the href
+        if(!empty($command->href)) {
+            $command->attribs['href'] = $this->getTemplate()->getView()->getRoute($command->href);
+        }
+
+        $html  = '<a '.$this->_buildAttributes($command->attribs).'>';
+       	$html .= JText::_($command->label);
+       	$html .= '</a>';
+
+    	return $html;
+    }
+}
