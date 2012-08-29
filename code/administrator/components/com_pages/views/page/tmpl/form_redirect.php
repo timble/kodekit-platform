c3e7eb6c452544b42b3b1ab83328781a8d7ebb8b
Johan Janssens <johan@timble.net>
2012-08-29 19:42:49 +0000

    Docblocks

--- code/administrator/components/com_pages/views/page/tmpl/form_redirect.php
+++ code/administrator/components/com_pages/views/page/tmpl/form_redirect.php
@@ -1,3 +1,13 @@
+<?
+/**
+ * @version     $Id: form_modules.php 3030 2011-10-09 13:21:09Z johanjanssens $
+ * @package     Nooku_Server
+ * @subpackage  Pages
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+?>
 <div class="control-group" id="page-link-type">
     <label class="control-label" for="parent"><?= @text('Type') ?></label>
     <div id="parent" class="controls">
