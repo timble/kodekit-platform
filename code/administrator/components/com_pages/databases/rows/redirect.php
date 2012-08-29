9768a0535a50ad80de03a3f7a639844d8b4474ac
Johan Janssens <johan@timble.net>
2012-08-29 19:44:34 +0000

    Docblocks

--- code/administrator/components/com_pages/databases/rows/redirect.php
+++ code/administrator/components/com_pages/databases/rows/redirect.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * @version     $Id: link.php 3029 2011-10-09 13:07:11Z johanjanssens $
+ * @package     Nooku_Server
+ * @subpackage  Pages
+ * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Redirecy Database Row Class
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Pages
+ */
+
 class ComPagesDatabaseRowRedirect extends ComPagesDatabaseRowPage
 {
     public function save()
