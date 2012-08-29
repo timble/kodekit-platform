4fecaf5bd23030287427774594c555a3661595ee
Johan Janssens <johan@timble.net>
2012-08-29 15:52:28 +0000

    Implemented KControllerToolbarInterface and added setParent(), getParent(), setToolbar() and getToolbar() functions to allow nesting of commands

--- code/libraries/koowa/controller/toolbar/command.php
+++ code/libraries/koowa/controller/toolbar/command.php
@@ -15,7 +15,7 @@
  * @package     Koowa_Controller
  * @subpackage 	Toolbar
  */
-class KControllerToolbarCommand extends KConfig
+class KControllerToolbarCommand extends KConfig implements KControllerToolbarInterface
 {
  	/**
      * The command name
@@ -23,6 +23,27 @@ class KControllerToolbarCommand extends KConfig
      * @var string
      */
     protected $_name;
+
+    /**
+     * The commands
+     *
+     * @var string|object
+     */
+    protected $_commands = null;
+
+    /**
+     * Toolbar command object
+     *
+     * @var object
+     */
+    protected $_parent = null;
+
+    /**
+     * Toolbar object
+     *
+     * @var object
+     */
+    protected $_toolbar = null;
 
     /**
      * Constructor.
@@ -40,10 +61,14 @@ class KControllerToolbarCommand extends KConfig
             'label'      => ucfirst($name),
             'disabled'   => false,
             'title'		 => '',
+            'href'       => null,
             'attribs'    => array(
-                'class'        => array(),
-            )
+                'class'  => array(),
+            ),
         ));
+
+        //Create the children array
+        $this->_commands = array();
 
         //Set the command name
         $this->_name = $name;
@@ -57,5 +82,135 @@ class KControllerToolbarCommand extends KConfig
     public function getName()
     {
         return $this->_name;
+    }
+
+    /**
+     * Add a command
+     *
+     * @param   string	$command The command name
+     * @param	mixed	$config  Parameters to be passed to the command
+     * @return  \KControllerToolbarCommand  The command that was added
+     */
+    public function addCommand($command, $config = array())
+    {
+        if (!($command instanceof  KControllerToolbarCommand)) {
+            $command = $this->getCommand($command, $config);
+        }
+
+        //Set the command parent
+        $command->setParent($this);
+
+        $this->_commands[] = $command;
+        return $command;
+    }
+
+    /**
+     * Get a command by name
+     *
+     * @param string $name  The command name
+     * @param array $config An optional associative array of configuration settings
+     * @return mixed KControllerToolbarCommand if found, false otherwise.
+     */
+    public function getCommand($name, $config = array())
+    {
+        return $this->getToolbar()->getCommand($name, $config);
+    }
+
+    /**
+     * Get the list of commands
+     *
+     * @return  array
+     */
+    public function getCommands()
+    {
+        return $this->_commands;
+    }
+
+    /**
+     * Get a new iterator
+     *
+     * @return  \RecursiveArrayIterator
+     */
+    public function getIterator()
+    {
+        return new RecursiveArrayIterator($this->getCommands());
+    }
+
+    /**
+     * Returns the number of elements in the collection.
+     *
+     * Required by the Countable interface
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->_commands);
+    }
+
+    /**
+     * Get the parent node
+     *
+     * @return	\KControllerToolbarCommand
+     */
+    public function getParent()
+    {
+        return $this->_parent;
+    }
+
+    /**
+     * Set the parent command
+     *
+     * @param object $node The parent command
+     * @return \KControllerToolbarCommand
+     */
+    public function setParent(KControllerToolbarCommand $command )
+    {
+        $this->_parent = $command;
+        return $this;
+    }
+
+    /**
+     * Get the toolbar object
+     *
+     * @return  \KControllerToolbarInterface
+     */
+    public function getToolbar()
+    {
+        return $this->_toolbar;
+    }
+
+    /**
+     * Set the parent node
+     *
+     * @param object $node The toolbar this command belongs too
+     * @return \KControllerToolbarCommand
+     */
+    public function setToolbar(KControllerToolbarInterface $toolbar )
+    {
+        $this->_toolbar = $toolbar;
+        return $this;
+    }
+
+    /**
+     * Add a command by it's name
+     *
+     * @param   string  Method name
+     * @param   array   Array containing all the arguments for the original call
+     * @return mixed
+     * @see addCommand()
+     */
+    public function __call($method, $args)
+    {
+        $parts = KInflector::explode($method);
+
+        if($parts[0] == 'add' && isset($parts[1]))
+        {
+            $config = isset($args[0]) ? $args[0] : array();
+            $command = $this->addCommand(strtolower($parts[1]), $config);
+            return $command;
+        }
+
+        return parent::__call($method, $args);
     }
 }
