d27637446ecba647dbe9de70ca0daae3d5dd8989
Johan Janssens <johan@timble.net>
2012-08-29 19:27:16 +0000

    ComCategoriesDatabaseRowsetNodes no longer implements RecursiveIterator

--- code/administrator/components/com_categories/databases/rowsets/nodes.php
+++ code/administrator/components/com_categories/databases/rowsets/nodes.php
@@ -16,38 +16,31 @@
  * @subpackage  Categories
  */
 
-class ComCategoriesDatabaseRowsetNodes extends KDatabaseRowsetAbstract implements RecursiveIterator
+class ComCategoriesDatabaseRowsetNodes extends KDatabaseRowsetAbstract
 {
     /**
-     * Returns if an iterator can be created for the current entry.
+     * Constructor
      *
-     * @return	boolean
+     * @param KConfig|null $config  An optional KConfig object with configuration options
+     * @return \KDatabaseRowsetAbstract
      */
-	public function hasChildren()
-	{
-		return current($this->_object_set)->hasChildren();
-	}
+    public function __construct(KConfig $config = null)
+    {
+        parent::__construct($config);
 
-	/**
-     * Returns an iterator for the current entry.
-     *
-     * @return	RecursiveIterator
-     */
-	public function getChildren()
-	{
-		return $this->current()->getChildren();
-	}
+        $this->_object_set->setIteratorClass('RecursiveArrayIterator');
+    }
 
-	/**
+    /**
      * Adds the rows as an hierarchical tree of nodes.
      *
      * This function requires each row to contain a an enumerated 'path' array containing the node
      * id's from root to the node. If no path exists or the path is empty the row will be added to 
      * the root node.
      *
-	 * @param  array  	An associative array of row data to be inserted.
-	 * @param  boolean	If TRUE, mark the row(s) as new (i.e. not in the database yet). Default TRUE
-	 * @return  KDatabaseRowsetAbstract
+	 * @param  array  	$list An associative array of row data to be inserted.
+	 * @param  boolean	$new If TRUE, mark the row(s) as new (i.e. not in the database yet). Default TRUE
+	 * @return  \KDatabaseRowsetAbstract
 	 * @see __construct
      */
 	public function addData(array $list, $new = true)
