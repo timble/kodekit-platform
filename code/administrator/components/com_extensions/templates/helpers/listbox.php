7c952f287fc194e2205212a50808d660a1bbdcff
Johan Janssens <johan@timble.net>
2012-08-29 18:28:28 +0000

    re #237 : Merged com_settings into com_extensions

--- code/administrator/components/com_extensions/templates/helpers/listbox.php
+++ code/administrator/components/com_extensions/templates/helpers/listbox.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @version     $Id$
- * @category	Nooku
  * @package     Nooku_Server
  * @subpackage  Extensions
  * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
@@ -10,55 +9,171 @@
  */
 
 /**
- * Listbox Template Helper
+ * Listbox Template Helper Class
  *
- * @author      Stian Didriksen <http://nooku.assembla.com/profile/stiandidriksen>
- * @category	Nooku
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @package     Nooku_Server
- * @subpackage  Extensions    
+ * @subpackage  Extensions
  */
 
 class ComExtensionsTemplateHelperListbox extends ComDefaultTemplateHelperListbox
 {
-	/**
-	 * Customized to add a few attributes.
-	 * 
-	 * @param 	array 	An optional array with configuration options
-	 * @return	string	Html
-	 * @see __call()
-	 */
-	protected function _listbox($config = array())
- 	{
-		$config = new KConfig($config);
-		
-		$config->append(array(
-			'filter' => array(
-				'application' => $config->application
-			)
-		));
+    public function list_limits($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'name'		=> 'list_limit',
+            'attribs'	=> array()
+        ));
 
-		return parent::_listbox($config);
- 	}
- 	
- 	public function positions($config = array())
- 	{
- 	    $config = new KConfig($config);
-		$config->append(array(
-			'model' 	=> 'positions',
-			'name'		=> 'position',
-		));
+        $options[] 	= $this->option(array('text' => '5', 'value' => '5'));
+        $options[] 	= $this->option(array('text' => '10', 'value' => '10'));
+        $options[] 	= $this->option(array('text' => '15', 'value' => '15'));
+        $options[] 	= $this->option(array('text' => '20', 'value' => '20'));
+        $options[] 	= $this->option(array('text' => '25', 'value' => '25'));
+        $options[] 	= $this->option(array('text' => '30', 'value' => '30'));
+        $options[] 	= $this->option(array('text' => '50', 'value' => '50'));
+        $options[] 	= $this->option(array('text' => '100', 'value' => '100'));
 
-		return $this->_listbox($config);
- 	}
- 	
-    public function types($config = array())
- 	{
- 	    $config = new KConfig($config);
-		$config->append(array(
-			'model' 	=> 'modules',
-			'name'		=> 'type',
-		));
+        $list = $this->optionlist(array(
+            'options'   => $options,
+            'name'      => $config->name,
+            'selected'  => $config->selected,
+            'attribs'   => $config->attribs
+        ));
 
-		return $this->_listbox($config);
- 	}
+        return $list;
+    }
+
+    public function cache_handlers($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'name'		=> 'cache_handler',
+            'attribs'	=> array()
+        ));
+
+        jimport('joomla.cache.cache');
+        $rows = JCache::getStores();
+
+        foreach($rows as $row) {
+            $options[] 	= $this->option(array('text' => JText::_(ucfirst($row)), 'value' => $row));
+        }
+
+        $list = $this->optionlist(array(
+            'options'   => $options,
+            'name'      => $config->name,
+            'selected'  => $config->selected,
+            'attribs'   => $config->attribs
+        ));
+
+        return $list;
+    }
+
+    public function timezones($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'name'		=> 'timezone',
+            'attribs'	=> array(),
+            'deselect'  => true,
+            'prompt'    => '- '.JText::_('Select Time Zone').' -',
+        ));
+
+        if ($config->deselect) {
+            $options[] = $this->option(array('text' => $config->prompt, 'value' => ''));
+        }
+
+        foreach (DateTimeZone::listIdentifiers() as $identifier)
+        {
+            if (strpos($identifier, '/')) {
+                list($group, $locale) = explode('/', $identifier, 2);
+                $groups[$group][] = str_replace('_', ' ', $locale);
+            }
+        }
+
+        $options[] = $this->option(array('text' => 'Coordinated Universal Time', 'value' => 'UTC'));
+        foreach ($groups as $group => $locales) {
+            $options[] = $this->option(array('text' => $group, 'group' => true));
+
+            foreach ($locales as $locale) {
+                $options[] = $this->option(array('text' => $locale, 'value' => str_replace(' ', '_', $group.'/'.$locale)));
+            }
+        }
+
+        $list = $this->optionlist(array(
+            'options'   => $options,
+            'name'      => $config->name,
+            'selected'  => $config->selected,
+            'attribs'   => $config->attribs
+        ));
+
+        return $list;
+    }
+
+    public function error_reportings($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'name'		=> 'error_reporting',
+            'attribs'	=> array()
+        ));
+
+        $options[] 	= $this->option(array('text' => 'Server Defaults', 'value' => 0));
+        $options[] 	= $this->option(array('text' => 'Development', 'value' => 1));
+        $options[] 	= $this->option(array('text' => 'Production' , 'value' => 2));
+
+        $list = $this->optionlist(array(
+            'options'   => $options,
+            'name'      => $config->name,
+            'selected'  => $config->selected,
+            'attribs'   => $config->attribs
+        ));
+
+        return $list;
+    }
+
+    public function mailers($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'name'		=> 'mailer',
+            'attribs'	=> array()
+        ));
+
+        $options[] 	= $this->option(array('text' => 'PHP mail function', 'value' => 'mail'));
+        $options[] 	= $this->option(array('text' => 'Sendmail', 'value' =>  'sendmail'));
+        $options[] 	= $this->option(array('text' => 'SMTP Server', 'value' => 'smtp'));
+
+        $list = $this->optionlist(array(
+            'options'   => $options,
+            'name'      => $config->name,
+            'selected'  => $config->selected,
+            'attribs'   => $config->attribs
+        ));
+
+        return $list;
+    }
+
+    public function smtpsecure($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'name'		=> 'smtpsecure',
+            'attribs'	=> array()
+        ));
+
+        $options[] 	= $this->option(array('text' => 'None', 'value' => 'none'));
+        $options[] 	= $this->option(array('text' => 'SSL', 'value' =>  'ssl'));
+        $options[] 	= $this->option(array('text' => 'TLS', 'value' => 'tls'));
+
+        $list = $this->optionlist(array(
+            'options'   => $options,
+            'name'      => $config->name,
+            'selected'  => $config->selected,
+            'attribs'   => $config->attribs
+        ));
+
+        return $list;
+    }
 }
