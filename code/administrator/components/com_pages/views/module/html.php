c23f2008ad328bfac2cb01c321940f5aa6951968
Johan Janssens <johan@timble.net>
2012-08-29 19:42:17 +0000

    re #234 : Merged module view from com_extensions. Code cleanup

--- code/administrator/components/com_pages/views/module/html.php
+++ code/administrator/components/com_pages/views/module/html.php
@@ -18,37 +18,23 @@
 
 class ComPagesViewModuleHtml extends ComDefaultViewHtml
 {
-    protected function _initialize(KConfig $config)
-    {
-        $config->append(array(
-            'auto_assign' => false
-        ));
-
-        parent::_initialize($config);
-    }
-
     public function display()
     {
-        $menus = $this->getService('com://admin/pages.model.menus')
-            ->sort('title')
-            ->getList();
+        if($this->getLayout() == 'modal')
+        {
+            $this->menu      = $this->getService('com://admin/pages.model.menus')->sort('title')->getList();
+            $this->pages     = $this->getService('com://admin/pages.model.pages')->getList();
+            $this->modules   = $this->getService('com://admin/extensions.model.modules')->application('site')->getList();
+            $this->relations = $this->getService($this->getModel()->getIdentifier())->module($this->getModel()->module)->getList();
+        }
 
-        $this->assign('menus', $menus);
+        if($this->getLayout() == 'form')
+        {
+            $module = $this->getModel()->getItem();
 
-        $pages = $this->getService('com://admin/pages.model.pages')->getList();
-        $this->assign('pages', $pages);
-
-        $modules = $this->getService('com://admin/extensions.model.modules')
-            ->application('site')
-            ->getList();
-
-        $this->assign('modules', $modules);
-
-        $relations = $this->getService($this->getModel()->getIdentifier())
-            ->module($this->getModel()->module)
-            ->getList();
-
-        $this->assign('relations', $relations);
+            $path = $this->getIdentifier()->getApplication($module->application);
+            JFactory::getLanguage()->load($module->name, $path );
+        }
 
         return parent::display();
     }
