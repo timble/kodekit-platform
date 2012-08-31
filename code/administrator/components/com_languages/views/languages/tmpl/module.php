4f97bf410f01ce8235a205c1f5df965c9e792d3f
Gergo Erdosi <gergo@timble.net>
2012-08-31 09:28:23 +0000

    Display the language selector only if component is enabled for translations. re #215

--- code/administrator/components/com_languages/views/languages/tmpl/module.php
+++ code/administrator/components/com_languages/views/languages/tmpl/module.php
@@ -1,14 +1,19 @@
 <? if(@service('application')->getCfg('multilanguage')) : ?>
-    <?= @helper('behavior.mootools') ?>
-    <script>
-    window.addEvent('domready', function(){
-        document.getElement('select[name=language]').addEvent('change', function(){
-            window.location = this.value;
-        });
-    });
-    </script>
+    <? $component = @service('com://admin/extensions.model.components')->name('com_'.$component)->getList()->top() ?>
+    <? $tables = @service('com://admin/languages.model.tables')->enabled(true)->getList() ?>
     
-    <ktml:module position="toolbar" content="append">
-        <?= @helper('com://admin/languages.template.helper.listbox.languages') ?>
-    </ktml:module>
+    <? if(count($tables->find(array('component' => $component->id)))) : ?>
+        <?= @helper('behavior.mootools') ?>
+        <script>
+        window.addEvent('domready', function(){
+            document.getElement('select[name=language]').addEvent('change', function(){
+                window.location = this.value;
+            });
+        });
+        </script>
+        
+        <ktml:module position="toolbar" content="append">
+            <?= @helper('com://admin/languages.template.helper.listbox.languages') ?>
+        </ktml:module>
+    <? endif ?>
 <? endif ?>
