77d6c4a1ea80379e223d7719fcbd8f8fda1404b2
Gergo Erdosi <gergo@timble.net>
2012-08-31 19:35:13 +0000

    Clean code up. re #215

--- code/administrator/components/com_languages/templates/helpers/listbox.php
+++ code/administrator/components/com_languages/templates/helpers/listbox.php
@@ -10,40 +10,43 @@ class ComLanguagesTemplateHelperListbox extends ComDefaultTemplateHelperListbox
 		    'component' => null
 		));
 		
+		$result      = '';
 		$application = $this->getService('application');
-		$components  = $application->getComponents();
 		
-		if($components->find(array('name' => 'com_'.$config->component))->top()->isTranslatable())
+		if($application->getCfg('multilanguage'))
 		{
-    		$options   = array();
-    		$languages = $this->getService('application')->getLanguages();
-    		$active    = $languages->getActive();
-    		
-    		foreach($languages as $language)
+    		$components  = $application->getComponents();
+    		if($components->find(array('name' => 'com_'.$config->component))->top()->isTranslatable())
     		{
-    		    $route = $this->getTemplate()->getView()->getRoute('language='.$language->slug);
-    		    $options[] = $this->option(array('text' => $language->name, 'value' => $route));
-    		    
-    		    if($language->iso_code == $active->iso_code) {
-    		        $config->selected = $route;
-    		    }
-    		}
-    
-    		$result = '
-    		    <script>
-                    window.addEvent(\'domready\', function() {
-                        document.getElement(\'select[name='.$config->name.']\').addEvent(\'change\', function(){
-                            window.location = this.value;
+    		    $result = '
+        		    <script>
+                        window.addEvent(\'domready\', function() {
+                            document.getElement(\'select[name='.$config->name.']\').addEvent(\'change\', function(){
+                                window.location = this.value;
+                            });
                         });
-                    });
-                </script>
-    		';
-    		
-    		$config->options = $options;
-    		$result .= parent::optionlist($config);
-		}
-		else $result = '';
-		
+                    </script>
+        		';
+    		    
+        		$options   = array();
+        		$languages = $application->getLanguages();
+        		$active    = $languages->getActive();
+        		
+        		foreach($languages as $language)
+        		{
+        		    $route = $this->getTemplate()->getView()->getRoute('language='.$language->slug);
+        		    $options[] = $this->option(array('text' => $language->name, 'value' => $route));
+        		    
+        		    if($language->iso_code == $active->iso_code) {
+        		        $config->selected = $route;
+        		    }
+        		}
+        		
+        		$config->options = $options;
+        		$result .= parent::optionlist($config);
+    		}
+		}	
+			
 		return $result;
     }
 }
