2a0d39540735acc95a09833f72d3f838c835b306
Johan Janssens <johan@timble.net>
2012-09-03 21:32:11 +0000

    The constructor no longer accepts null as default.

--- code/libraries/koowa/command/event.php
+++ code/libraries/koowa/command/event.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * @version		$Id$
- * @package		Koowa_Command
- * @copyright	Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link     	http://www.nooku.org
+ * @version        $Id$
+ * @package        Koowa_Command
+ * @copyright    Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link         http://www.nooku.org
  */
 
 /**
  * Event Command
- * 
- * The event commend will translate the command name to a onCommandName format 
+ *
+ * The event commend will translate the command name to a onCommandName format
  * and let the event dispatcher dispatch to any registered event handlers.
  *
  * @author      Johan Janssens <johan@nooku.org>
@@ -27,26 +27,23 @@ class KCommandEvent extends KCommand
      * @var KEventDispatcher
      */
     protected $_dispatcher;
-    
+
     /**
      * Constructor.
      *
      * @param   object  An optional KConfig object with configuration options
      */
-    public function __construct( KConfig $config = null) 
-    { 
-        //If no config is passed create it
-        if(!isset($config)) $config = new KConfig();
-        
+    public function __construct(KConfig $config)
+    {
         parent::__construct($config);
-        
-         if(is_null($config->event_dispatcher)) {
-			throw new KMixinException('event_dispatcher [KEventDispatcher] option is required');
-		}
-        
+
+        if (is_null($config->event_dispatcher)) {
+            throw new KMixinException('event_dispatcher [KEventDispatcher] option is required');
+        }
+
         $this->_event_dispatcher = $config->event_dispatcher;
     }
-    
+
     /**
      * Initializes the options for the object
      *
@@ -58,42 +55,41 @@ class KCommandEvent extends KCommand
     protected function _initialize(KConfig $config)
     {
         $config->append(array(
-            'event_dispatcher'   => null
+            'event_dispatcher' => null
         ));
 
         parent::_initialize($config);
     }
-    
+
     /**
      * Command handler
-     * 
+     *
      * @param   string      The command name
      * @param   object      The command context
      * @return  boolean     Always returns true
      */
-    public function execute( $name, KCommandContext $context) 
+    public function execute($name, KCommandContext $context)
     {
         $type = '';
-        
-        if($context->caller)
-        {   
+
+        if ($context->caller) {
             $identifier = clone $context->caller->getIdentifier();
-            
-            if($identifier->path) {
+
+            if ($identifier->path) {
                 $type = array_shift($identifier->path);
             } else {
                 $type = $identifier->name;
             }
         }
-        
-        $parts = explode('.', $name);   
-        $name  = 'on'.ucfirst(array_shift($parts)).ucfirst($type).KInflector::implode($parts);
-        
+
+        $parts = explode('.', $name);
+        $name = 'on' . ucfirst(array_shift($parts)) . ucfirst($type) . KInflector::implode($parts);
+
         $event = new KEvent(clone($context));
         $event->setPublisher($context->caller);
-        
+
         $this->_event_dispatcher->dispatchEvent($name, $event);
-        
+
         return true;
     }
 }
