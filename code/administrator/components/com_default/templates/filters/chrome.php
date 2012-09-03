82ee0540f500b973b744b54e6eff72317a3a7751
Johan Janssens <johan@timble.net>
2012-09-03 23:27:36 +0000

    The constructor no longer accepts null as default.

--- code/administrator/components/com_default/templates/filters/chrome.php
+++ code/administrator/components/com_default/templates/filters/chrome.php
@@ -29,7 +29,7 @@ class ComDefaultModuleDefaultTemplateFilterChrome extends KTemplateFilterAbstrac
      *
      * @param   object  An optional KConfig object with configuration options
      */
-    public function __construct( KConfig $config = null)
+    public function __construct( KConfig $config)
     {
         parent::__construct($config);
 
