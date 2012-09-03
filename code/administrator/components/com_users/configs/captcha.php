cf81b2b70b4bbb317a1a245e7b1569d37a0b4866
Johan Janssens <johan@timble.net>
2012-09-03 23:36:07 +0000

    Code cleanup

--- code/administrator/components/com_users/configs/captcha.php
+++ code/administrator/components/com_users/configs/captcha.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @version     $Id$
- * @category    Nooku
  * @package     Nooku_Server
  * @subpackage  Users
  * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
@@ -13,7 +12,6 @@
  * Captcha configuration class.
  *
  * @author     Arunas Mazeika <http://nooku.assembla.com/profile/arunasmazeika>
- * @category   Nooku
  * @package    Nooku_Server
  * @subpackage Users
  */
@@ -27,21 +25,22 @@ class ComUsersConfigCaptcha extends KConfig implements KServiceInstantiatable, K
     private $__service_identifier;
 
     /**
-     * The service container
+     * The service manager
      *
-     * @var KService
+     * @var KServiceManager
      */
-    private $__service_container;
+    private $__service_manager;
 
     /**
      * Constructor.
      *
      * @param   object  An optional KConfig object with configuration options
      */
-    public function __construct(KConfig $config) {
+    public function __construct(KConfig $config)
+    {
         //Set the service container
-        if (isset($config->service_container)) {
-            $this->__service_container = $config->service_container;
+        if (isset($config->service_manager)) {
+            $this->__service_container = $config->service_manager;
         }
 
         //Set the service identifier
@@ -52,41 +51,73 @@ class ComUsersConfigCaptcha extends KConfig implements KServiceInstantiatable, K
         parent::__construct($config);
     }
 
-    final public function getService($identifier, array $config = array()) {
-        if (!isset($this->__service_container)) {
-            throw new KObjectException("Failed to call " . get_class($this) . "::getService(). No service_container object defined.");
-        }
+    /**
+     * Force creation of a singleton
+     *
+     * @param 	object 	An optional KConfig object with configuration options
+     * @param 	object	A KServiceInterface object
+     * @return ComUsersConfigCaptcha
+     */
+    static public function getInstance(KConfigInterface $config, KServiceInterface $container)
+    {
+        $instance = new self($config);
 
-        return $this->__service_container->get($identifier, $config);
+        // Force singleton behavior.
+        $container->set($config->service_manager, $instance);
+
+        return $instance;
+    }
+
+    /**
+     * Get an instance of a class based on a class identifier only creating it if it does not exist yet.
+     *
+     * @param	string|object	$identifier The class identifier or identifier object
+     * @param	array  			$config     An optional associative array of configuration settings.
+     * @throws	\RuntimeException If the service manager has not been defined.
+     * @return	object  		Return object on success, throws exception on failure
+     * @see 	KObjectServiceable
+     */
+    final public function getService($identifier = null, array $config = array())
+    {
+        if(isset($identifier))
+        {
+            if(!isset($this->__service_manager))
+            {
+                throw new RuntimeException(
+                    "Failed to call ".get_class($this)."::getService(). No service_manager object defined."
+                );
+            }
+
+            $result = $this->__service_manager->get($identifier, $config);
+        }
+        else $result = $this->__service_manager;
+
+        return $result;
     }
 
     /**
      * Gets the service identifier.
      *
-     * @throws    KObjectException if the service container has not been defined.
-     * @return    KServiceIdentifier
-     * @see     KObjectServiceable
+     * @param	string|object	$identifier The class identifier or identifier object
+     * @throws	\RuntimeException If the service manager has not been defined.
+     * @return	KServiceIdentifier
+     * @see 	KObjectServiceable
      */
-    final public function getIdentifier($identifier = null) {
-        if (isset($identifier)) {
-            if (!isset($this->__service_container)) {
-                throw new KObjectException("Failed to call " . get_class($this) . "::getIdentifier(). No service_container object defined.");
+    final public function getIdentifier($identifier = null)
+    {
+        if(isset($identifier))
+        {
+            if(!isset($this->__service_manager))
+            {
+                throw new RuntimeException(
+                    "Failed to call ".get_class($this)."::getIdentifier(). No service_manager object defined."
+                );
             }
 
-            $result = $this->__service_container->getIdentifier($identifier);
+            $result = $this->__service_manager->getIdentifier($identifier);
         }
         else  $result = $this->__service_identifier;
 
         return $result;
-    }
-
-    static public function getInstance(KConfigInterface $config, KServiceInterface $container) {
-
-        $instance = new self($config);
-
-        // Force singleton behavior.
-        $container->set($config->service_identifier, $instance);
-
-        return $instance;
     }
 }
