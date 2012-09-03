5e1ef18216913ffd4a12e3769a1123144994c396
Johan Janssens <johan@timble.net>
2012-09-03 23:45:56 +0000

    re #228 : Added specialised json view. Will just return it's data.

---
+++ code/site/components/com_application/views/page/json.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @version     $Id: html.php 4781 2012-08-23 22:09:11Z johanjanssens $
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Html View Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ */
+
+class ComApplicationViewPageJson extends ComApplicationViewHtml
+{
+    public function display()
+    {
+        return $this->content;
+    }
+}
