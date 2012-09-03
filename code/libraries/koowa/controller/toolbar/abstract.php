9a41ea74bb5b9597cbf5a1c43f54ec0d04059f17
Johan Janssens <johan@timble.net>
2012-09-03 21:32:46 +0000

    The constructor no longer accepts null as default.

--- code/libraries/koowa/controller/toolbar/abstract.php
+++ code/libraries/koowa/controller/toolbar/abstract.php
@@ -1,18 +1,18 @@
 <?php
 /**
-* @version      $Id$
-* @package		Koowa_Controller
-* @subpackage 	Toolbar
-* @copyright    Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
-* @license      GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
-*/
+ * @version      $Id$
+ * @package        Koowa_Controller
+ * @subpackage     Toolbar
+ * @copyright    Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license      GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ */
 
 /**
  * Abstract Controller Toolbar Class
  *
  * @author      Johan Janssens <johan@nooku.org>
  * @package     Koowa_Controller
- * @subpackage 	Toolbar
+ * @subpackage     Toolbar
  * @uses        KInflector
  */
 abstract class KControllerToolbarAbstract extends KEventSubscriberAbstract implements KControllerToolbarInterface
@@ -23,8 +23,8 @@ abstract class KControllerToolbarAbstract extends KEventSubscriberAbstract imple
      * @var     array
      */
     protected $_controller;
-    
-    /** 
+
+    /**
      * The commands
      *
      * @var array
@@ -34,22 +34,19 @@ abstract class KControllerToolbarAbstract extends KEventSubscriberAbstract imple
     /**
      * Constructor.
      *
-     * @param  KConfig 	An associative array of configuration settings or a KConfig instance.
+     * @param  KConfig     An associative array of configuration settings or a KConfig instance.
      */
-    public function __construct(KConfig $config = null)
+    public function __construct(KConfig $config)
     {
-        //If no config is passed create it
-        if(!isset($config)) $config = new KConfig();
-        
         parent::__construct($config);
-        
-        if(is_null($config->controller)) {
-			throw new KMixinException('controller [KController] option is required');
-		}
+
+        if (is_null($config->controller)) {
+            throw new KMixinException('controller [KController] option is required');
+        }
 
         //Create the commands array
         $this->_commands = array();
-        
+
         // Set the controller
         $this->_controller = $config->controller;
     }
@@ -65,15 +62,15 @@ abstract class KControllerToolbarAbstract extends KEventSubscriberAbstract imple
     protected function _initialize(KConfig $config)
     {
         $config->append(array(
-            'controller'    => null,
+            'controller' => null,
         ));
-        
+
         parent::_initialize($config);
     }
-    
-	/**
+
+    /**
      * Get the controller object
-     * 
+     *
      * @return  \KControllerInterface
      */
     public function getController()
@@ -102,12 +99,12 @@ abstract class KControllerToolbarAbstract extends KEventSubscriberAbstract imple
         $this->_commands[] = $command;
         return $command;
     }
-     
+
     /**
      * Add a command
      *
-     * @param   string	$command The command name
-     * @param	mixed	$config  Parameters to be passed to the command
+     * @param   string    $command The command name
+     * @param    mixed    $config  Parameters to be passed to the command
      * @return  \KControllerToolbarCommand  The command that was added
      */
     public function addCommand($command, $config = array())
@@ -122,52 +119,48 @@ abstract class KControllerToolbarAbstract extends KEventSubscriberAbstract imple
         $this->_commands[] = $command;
         return $command;
     }
-    
-	/** 
+
+    /**
      * Get a command by name
-     * 
+     *
      * @param string $name  The command name
      * @param array $config An optional associative array of configuration settings
      * @return mixed KControllerToolbarCommand if found, false otherwise.
-     */ 
-    public function getCommand($name, $config = array()) 
+     */
+    public function getCommand($name, $config = array())
     {
         //Create the config object
         $command = new KControllerToolbarCommand($name, $config);
 
         //Attach the command to the toolbar
         $command->setToolbar($this);
-        
+
         //Find the command function to call
-        if(method_exists($this, '_command'.ucfirst($name)))
-        {
-            $function =  '_command'.ucfirst($name);
+        if (method_exists($this, '_command' . ucfirst($name))) {
+            $function = '_command' . ucfirst($name);
             $this->$function($command);
-        }
-        else
-        {
+        } else {
             //Don't set an action for GET commands
-            if(!isset($command->href))
-            {
+            if (!isset($command->href)) {
                 $command->append(array(
-         			'attribs'    => array(
-               			'data-action'  => $command->getName()
+                    'attribs' => array(
+                        'data-action' => $command->getName()
                     )
                 ));
             }
         }
 
         return $command;
-    } 
-    
- 	/**
+    }
+
+    /**
      * Get the list of commands
      *
      * @return  array
      */
     public function getCommands()
     {
-        return $this->_commands;   
+        return $this->_commands;
     }
 
     /**
@@ -179,7 +172,7 @@ abstract class KControllerToolbarAbstract extends KEventSubscriberAbstract imple
     {
         return new RecursiveArrayIterator($this->getCommands());
     }
- 
+
     /**
      * Reset the commands array
      *
@@ -192,25 +185,24 @@ abstract class KControllerToolbarAbstract extends KEventSubscriberAbstract imple
         return $this;
     }
 
- 	/**
+    /**
      * Add a command by it's name
-	 *
+     *
      * @param   string  Method name
      * @param   array   Array containing all the arguments for the original call
      * @return mixed
      * @see addCommand()
      */
     public function __call($method, $args)
-    {  
-		$parts = KInflector::explode($method);
+    {
+        $parts = KInflector::explode($method);
 
-		if($parts[0] == 'add' && isset($parts[1]))
-		{
-		    $config = isset($args[0]) ? $args[0] : array();	    
-		    $command = $this->addCommand(strtolower($parts[1]), $config);
-			return $command;
-		}
-        
+        if ($parts[0] == 'add' && isset($parts[1])) {
+            $config = isset($args[0]) ? $args[0] : array();
+            $command = $this->addCommand(strtolower($parts[1]), $config);
+            return $command;
+        }
+
         return parent::__call($method, $args);
     }
 }
