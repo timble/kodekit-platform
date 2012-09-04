444822ac3069c98e44c4092fe3431c43a85e1dfe
Gergo Erdosi <gergo@timble.net>
2012-09-04 13:22:35 +0000

    Add docblock. re #228

--- code/administrator/components/com_application/databases/rows/component.php
+++ code/administrator/components/com_application/databases/rows/component.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * @version     $Id$
+ * @package     Nooku_Server
+ * @subpackage  Application
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Component Database Row Class
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @package     Nooku_Server
+ * @subpackage  Application
+ */
 class ComApplicationDatabaseRowComponent extends KDatabaseRowAbstract
 {
     public function isTranslatable()
