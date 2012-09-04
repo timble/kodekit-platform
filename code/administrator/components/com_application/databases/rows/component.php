2ff4c6022f2a52277a203f9d447292cf7387362d
Gergo Erdosi <gergo@timble.net>
2012-09-04 13:51:25 +0000

    Remove dependency on application settings. re #215

--- code/administrator/components/com_application/databases/rows/component.php
+++ code/administrator/components/com_application/databases/rows/component.php
@@ -20,16 +20,13 @@ class ComApplicationDatabaseRowComponent extends KDatabaseRowAbstract
     public function isTranslatable()
     {
         $result = false;
-        if($this->getService('application')->getCfg('multilanguage'))
-        {
-            $tables = $this->getService('com://admin/languages.model.tables')
-                ->reset()
-                ->enabled(true)
-                ->getList();
-            
-            if(count($tables->find(array('extensions_component_id' => $this->id)))) {
-                $result = true;
-            }
+        $tables = $this->getService('com://admin/languages.model.tables')
+            ->reset()
+            ->enabled(true)
+            ->getList();
+        
+        if(count($tables->find(array('extensions_component_id' => $this->id)))) {
+            $result = true;
         }
         
         return $result;
