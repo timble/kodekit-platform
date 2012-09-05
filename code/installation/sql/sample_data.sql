2626cb1a0ba972e7b1c747ff87b070f9405fdd6e
Gergo Erdosi <gergo@timble.net>
2012-09-05 13:08:33 +0000

    Remove rows that are in install_data.sql. re #193

--- code/installation/sql/sample_data.sql
+++ code/installation/sql/sample_data.sql
@@ -134,7 +134,6 @@ VALUES
 
 INSERT INTO `#__pages_modules` (`id`, `title`, `content`, `ordering`, `position`, `created_by`, `created_on`, `modified_by`, `modified_on`, `locked_by`, `locked_on`, `published`, `name`, `access`, `showtitle`, `params`, `extensions_component_id`, `application`)
 VALUES
-	(1, 'Main Menu', '', 2, 'left', NULL, NULL, NULL, NULL, 0, '0000-00-00 00:00:00', 1, 'mod_menu', 0, 1, 'menu_id=1\nmoduleclass_sfx=_menu\n', 25, 'site'),
 	(17, 'User Menu', '', 18, 'left', NULL, NULL, NULL, NULL, 0, '0000-00-00 00:00:00', 1, 'mod_menu', 1, 1, 'menu_id=2\ncache=1', 25, 'site'),
 	(18, 'Login Form', '', 21, 'left', NULL, NULL, NULL, NULL, 0, '0000-00-00 00:00:00', 1, 'mod_login', 0, 1, 'greeting=1\nname=0', 31, 'site'),
 	(19, 'Latest News', '', 19, 'user1', NULL, NULL, NULL, NULL, 0, '0000-00-00 00:00:00', 1, 'mod_articles', 0, 1, 'cache=1', 20, 'site'),
@@ -246,7 +245,6 @@ VALUES
 
 INSERT INTO `#__pages` (`pages_page_id`, `pages_menu_id`, `title`, `slug`, `link_url`, `link_id`, `type`, `published`, `hidden`, `home`, `extensions_component_id`, `created_by`, `created_on`, `modified_by`, `modified_on`, `locked_by`, `locked_on`, `access`, `params`)
 VALUES
-	(1, 1, 'Home', 'home', 'index.php?option=com_articles&view=articles', NULL, 'component', 1, 0, 1, 20, NULL, NULL, NULL, NULL, 0, '0000-00-00 00:00:00', 0, 'show_featured=1\nshow_page_title=1\npage_title=Welcome to the Frontpage\nshow_description=0\nshow_description_image=0\nnum_leading_articles=1\nnum_intro_articles=4\nnum_columns=2\nnum_links=4\nshow_title=1\nmenu_image=-1\nsecure=0\norderby_pri=\norderby_sec=front\nshow_pagination=2\nshow_pagination_results=1\nshow_noauth=0\nlink_titles=0\nshow_intro=1\nshow_section=0\nlink_section=0\nshow_category=0\nlink_category=0\nshow_author=1\nshow_create_date=1\nshow_modify_date=1\nshow_item_navigation=0\nshow_readmore=1\nshow_vote=0\nshow_icons=1\nshow_pdf_icon=1\nshow_print_icon=1\nshow_email_icon=1\nshow_hits=1\n\n'),
 	(2, 1, 'Joomla! License', 'joomla-license', 'index.php?option=com_articles&view=article&id=5', NULL, 'component', 1, 0, 0, 20, NULL, NULL, NULL, NULL, 0, '0000-00-00 00:00:00', 0, 'menu_image=-1\nsecure=0\nshow_noauth=0\nlink_titles=0\nshow_intro=1\nshow_section=0\nlink_section=0\nshow_category=0\nlink_category=0\nshow_author=1\nshow_create_date=1\nshow_modify_date=1\nshow_item_navigation=0\nshow_readmore=1\nshow_vote=0\nshow_icons=1\nshow_pdf_icon=1\nshow_print_icon=1\nshow_email_icon=1\nshow_hits=1\n\n'),
 	(20, 2, 'Your Details', 'your-details', 'index.php?option=com_user&view=user&task=edit', NULL, 'component', 1, 0, 0, 14, NULL, NULL, NULL, NULL, 0, '0000-00-00 00:00:00', 1, ''),
 	(24, 2, 'Logout', 'logout', 'index.php?option=com_user&view=login', NULL, 'component', 1, 0, 0, 14, NULL, NULL, NULL, NULL, 0, '0000-00-00 00:00:00', 1, ''),
