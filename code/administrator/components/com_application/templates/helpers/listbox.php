1f5dede6dcbbda172d42efe0deba72bcf029c1c0
Gergo Erdosi <gergo@timble.net>
2012-09-11 09:30:30 +0000

    Add applications listbox helper. re #228

---
+++ code/administrator/components/com_application/templates/helpers/listbox.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Nooku_Server
+ * @subpackage  Application
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Listbox Template Helper Class
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @package     Nooku_Server
+ * @subpackage  Application
+ */
+class ComApplicationTemplateHelperListbox extends ComDefaultTemplateHelperListbox
+{
+    public function applications($config = array())
+    {
+        $config = new KConfig($config);
+        $config->append(array(
+            'deselect' => true,
+            'prompt'   => '- Select -',
+        ));
+        
+        $options = array();
+        if($config->deselect) {
+            $options[] = $this->option(array('text' => JText::_($config->prompt)));
+        }
+        
+        foreach($this->getIdentifier()->getApplications() as $application => $path) {
+            $options[] = $this->option(array('text' => $application, 'value' => $application));
+        }
+        
+        $config->options = $options;
+        
+        return $this->optionlist($config);
+    }
+}
