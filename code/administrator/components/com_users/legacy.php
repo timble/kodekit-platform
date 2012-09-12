e9500ab067abd11c989c7032312cc93ca421a9a7
Gergo Erdosi <gergo@timble.net>
2012-09-12 22:28:02 +0000

    Clean code up. re #214

--- code/administrator/components/com_users/legacy.php
+++ code/administrator/components/com_users/legacy.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @version		$Id$
- * @package		Nooku_Server
- * @subpackage	Users
- * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		http://www.nooku.org
+ * @version     $Id$
+ * @package     Nooku_Server
+ * @subpackage  Users
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
  */
 
 /**
