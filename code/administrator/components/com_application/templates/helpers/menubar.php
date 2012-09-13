35f567ee1c0ee41294b647f7abf8154e06d18e00
Gergo Erdosi <gergo@timble.net>
2012-09-13 13:11:33 +0000

    Refactor menubar template helper to use page list helper. re #228

--- code/administrator/components/com_application/templates/helpers/menubar.php
+++ code/administrator/components/com_application/templates/helpers/menubar.php
@@ -30,92 +30,21 @@ class ComApplicationTemplateHelperMenubar extends KTemplateHelperAbstract
         	'menubar' => null,
             'attribs' => array('class' => array())
         ));
+        
+        $result = '';
+        
+        $menus = $this->getService('com://admin/pages.model.menus')
+            ->application('admin')
+            ->getList();
+        
+        $menu = $menus->find(array('slug' => 'menubar'));
+        
+        if(count($menu))
+        {
+            $pages  = $this->getService('application.pages')->find(array('pages_menu_id' => $menu->top()->id));
+            $result = $this->getService('com://admin/pages.template.helper.list')->pages(array('pages' => $pages));
+        }   
 
-        if($this->getService('component')->getController()->getView()->getLayout() == 'form') {
-            $config->attribs->class->append(array('disabled'));
-        }
-
-        $html = '<ul '.$this->_buildAttributes($config->attribs).'>';
-	    foreach ($config->menubar as $command)
-	    {
-            $name = $command->getName();
-
-            if(method_exists($this, $name)) {
-                $html .= $this->$name(array('command' => $command));
-            } else {
-                $html .= $this->command(array('command' => $command));
-            }
-        }
-        $html .= '</ul>';
-
-		return $html;
-    }
-
-    /**
-     * Render a menubar command
-     *
-     * @param   array   An optional array with configuration options
-     * @return  string  Html
-     */
-    public function command($config = array())
-    {
-        $config = new KConfig($config);
-        $config->append(array(
-        	'command' => null
-        ));
-
-        $command = $config->command;
-
-        if($this->getService('component')->getController()->getView()->getLayout() == 'form') {
-            $command->disabled = true;
-        }
-
-        //Add a nolink class if the command is disabled
-        if($command->disabled) {
-            $command->attribs->class->append(array('nolink'));
-        }
-
-        if($command->active) {
-             $command->attribs->class->append(array('active'));
-        }
-
-        //Create the href
-        if(!empty($command->href) && !$command->disabled) {
-            $command->attribs['href'] = $this->getTemplate()->getView()->getRoute($command->href);
-        }
-
-        $html = count($command) ? '<li class="node">' : '<li>';
-        if ($command->disabled) {
-			$html .= '<span '.$this->_buildAttributes($command->attribs).'>'.JText::_($command->label).'</span>';
-		} else {
-			$html .= '<a '.$this->_buildAttributes($command->attribs).'>'.JText::_($command->label).'</a>';
-		}
-
-        if(count($command)) {
-            $html .= $this->render(array('menubar' => $command));
-        }
-        $html .= '</li>';
-
-    	return $html;
-    }
-
-    /**
-     * Render a separator
-     *
-     * @param   array   An optional array with configuration options
-     * @return  string  Html
-     */
-    public function separator($config = array())
-    {
-        $config = new KConfig($config);
-        $config->append(array(
-            'command' => NULL
-        ));
-
-        $command = $config->command;
-
-        $html = '<li class="separator"><span></span></li>';
-
-        return $html;
+        return $result;
     }
 }
