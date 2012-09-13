efd5b3801da44f8bd37c27db5249dd2ff8c02734
Gergo Erdosi <gergo@timble.net>
2012-09-13 13:10:25 +0000

    Add pages rowset. re #228

---
+++ code/administrator/components/com_application/databases/rowsets/pages.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Nooku_Server
+ * @subpackage  Application
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Pages Database Rowset Class
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @package     Nooku_Server
+ * @subpackage  Application
+ */
+class ComApplicationDatabaseRowsetPages extends KDatabaseRowsetAbstract implements KServiceInstantiatable
+{
+    public function __construct(KConfig $config )
+    {
+        parent::__construct($config);
+
+        //TODO : Inject raw data using $config->data
+        $pages = $this->getService('com://admin/pages.model.pages')
+            ->published(true)
+            ->getList();
+
+        $this->merge($pages);
+    }
+
+    protected function _initialize(KConfig $config)
+    {
+        $config->identity_column = 'id';
+
+        parent::_initialize($config);
+    }
+
+    public static function getInstance(KConfigInterface $config, KServiceInterface $container)
+    {
+        if(!$container->has($config->service_identifier))
+        {
+            $classname = $config->service_identifier->classname;
+            $instance  = new $classname($config);
+            $container->set($config->service_identifier, $instance);
+        }
+
+        return $container->get($config->service_identifier);
+    }
+
+    public function getPage($id)
+    {
+        $page = $this->find($id);
+        return $page;
+    }
+}
