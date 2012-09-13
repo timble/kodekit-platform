3c4bdf184d0da3f283e3391e0828f519fd943dab
Gergo Erdosi <gergo@timble.net>
2012-09-13 13:14:24 +0000

    Fix bugs. re #168

--- code/administrator/components/com_pages/templates/helpers/list.php
+++ code/administrator/components/com_pages/templates/helpers/list.php
@@ -5,15 +5,14 @@ class ComPagesTemplateHelperList extends KTemplateHelperAbstract
     {
         $config = new KConfig($config);
         $config->append(array(
-            'pages' => array()
+            'pages'  => array(),
+            'active' => null
         ));
         
-        $pages  = $this->getService('application.pages');
-        $active = $pages->getActive();
-        
-        $result     = '';
-        $first      = true;
-        $last_depth = 0;
+        $application = $this->getService('application')->getIdentifier()->application;
+        $result      = '';
+        $first       = true;
+        $last_depth  = 0;
         
         foreach($config->pages as $page)
         {
@@ -41,17 +40,18 @@ class ComPagesTemplateHelperList extends KTemplateHelperAbstract
                 $result .= '</li>';
             }
             
-            $result .= '<li '.($page->id == $active->id ? 'class="active"' : '').'>';
+            $result .= '<li '.($config->active && $page->id == $config->active->id ? 'class="active"' : '').'>';
             switch($page->type)
             {
                 case 'component':
-    				$result .= '<a href="'.$this->getTemplate()->getView()->getRoute($page->link->getQuery().'&Itemid='.$page->id).'">';
+                    $link = $this->getService('koowa:dispatcher.router.route', array('url' => 'index.php?'.$page->link->getQuery().($application == 'site' ? '&Itemid='.$page->id : ''), 'escape' => true));
+    				$result .= '<a href="'.(string) $link.'">';
                     $result .= $page->title;
                     $result .= '</a>';
     				break;
     				
     		    case 'menulink':
-    		        $page_linked = $pages->getPage($page->link->query['Itemid']);
+    		        $page_linked = $this->getService('application.pages')->getPage($page->link->query['Itemid']);
     		        $result .= '<a href="'.$page_linked->link.'">';
                     $result .= $page->title;
                     $result .= '</a>';
