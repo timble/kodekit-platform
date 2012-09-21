9acb49677e5a23dfe745842efcd5f55599cccf3b
Tom Janssens <tom@timble.net>
2012-09-21 12:09:23 +0000

    Moved category image handling to helper re #229

---
+++ code/site/components/com_categories/templates/helpers/string.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @version        $Id$
+ * @package        Nooku_Server
+ * @subpackage     Categories
+ * @copyright      Copyright (C) 2009 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
+ */
+
+/**
+ * Categories Template Helper Class
+ *
+ * @author     Tom Janssens <http://nooku.assembla.com/profile/tomjanssens>
+ * @package    Nooku_Server
+ * @subpackage Categories
+ */
+class ComCategoriesTemplateHelperString extends KTemplateHelperDefault
+{
+    public function image($config = array())
+    {
+        $config   = new KConfig($config);
+        $config->append(array(
+           'align' => 'right',
+           'class' => 'thumbnail'
+        ));
+        
+        //Set the category image
+        if (isset( $config->row->image ) && !empty($config->row->image))
+        {
+            $path = JPATH_IMAGES.'/stories/'.$config->row->image;
+            $size = getimagesize($path);
+
+            $image = (object) array(
+                'path'   => '/'.str_replace(JPATH_ROOT.DS, '', $path),
+                'width'  => $size[0],
+                'height' => $size[1]
+            );
+        }
+        
+        $html = '<img class="'.$config->class.'" align="'.$config->align.'" src="'.$image->path.'" height="'.$image->height.'" width="'.$image->width.'" />';
+
+        return $html;
+    }
+}
