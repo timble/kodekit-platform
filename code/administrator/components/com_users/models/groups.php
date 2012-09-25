f7ef186cd19f90f5d73112c6ed3dde7cfed606da
Gergo Erdosi <gergo@timble.net>
2012-09-25 09:09:21 +0000

    Add groups model. re #241

---
+++ code/administrator/components/com_users/models/groups.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Nooku_Server
+ * @subpackage  Users
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Groups Model Class
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @package     Nooku_Server
+ * @subpackage  Users
+ */
+class ComUsersModelGroups extends ComDefaultModelDefault
+{
+    public function __construct(KConfig $config)
+    {
+        parent::__construct($config);
+        
+        $this->getState()
+            ->insert('sort', 'cmd', array('type', 'name'));
+    }
+}
