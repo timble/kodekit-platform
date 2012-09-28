e8d6ede34a3755b9fb5e80288e4d18a99d6c238f
Gergo Erdosi <gergo@timble.net>
2012-09-28 12:11:30 +0000

    Drop usertype column. re #241

--- code/installation/sql/12.3.sql
+++ code/installation/sql/12.3.sql
@@ -106,6 +106,8 @@ ALTER TABLE `#__users` ADD `modified_on` DATETIME AFTER `modified_by`;
 ALTER TABLE `#__users` ADD `locked_by` INT(11) UNSIGNED AFTER `modified_on`;
 ALTER TABLE `#__users` ADD `locked_on` DATETIME AFTER `locked_by`;
 
+ALTER TABLE `#__users` DROP `usertype`;
+
 -- Remove unused columns from #__session
 RENAME TABLE`#__session` TO  `#__users_sessions`;
 
