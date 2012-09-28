867b3fac7f4f0f13c72883ec31c648baa973d66e
Johan Janssens <johan@timble.net>
2012-09-28 13:00:26 +0000

    re #231 : Use media://lib_koowa instead of media://system

--- code/administrator/components/com_default/templates/helpers/image.php
+++ code/administrator/components/com_default/templates/helpers/image.php
@@ -57,7 +57,7 @@ class ComDefaultTemplateHelperImage extends KTemplateHelperListbox
 		<script>
 		window.addEvent('domready', function(){
 			$('".$config->name."').addEvent('change', function(){
-				var value = this.value ? ('".$root."/' + this.value) : 'media://system/images/blank.png';
+				var value = this.value ? ('".$root."/' + this.value) : 'media://lib_koowa/images/blank.png';
 				$('".$config->name."-preview').src = value;
 			});
 		});
@@ -123,7 +123,7 @@ class ComDefaultTemplateHelperImage extends KTemplateHelperListbox
 
  	    $image = JURI::root(true).str_replace(JPATH_ROOT, '', $config->directory).'/'.$config->selected;
 
- 	    $path = $config->selected ? $image : 'media://system/images/blank.png';
+ 	    $path = $config->selected ? $image : 'media://lib_koowa/images/blank.png';
   		$html = '<img '.$this->_buildAttributes(array(
   			'src'		=> $path,
   			'id'		=> $config->name.'-preview',
