ea754c3c95c10b1c17a9d336139c1ba9684a7165
Gergo Erdosi <gergo@timble.net>
2012-10-01 20:06:16 +0000

    Disable orderable behavior temporarily. re #168

--- code/administrator/components/com_pages/databases/tables/pages.php
+++ code/administrator/components/com_pages/databases/tables/pages.php
@@ -24,10 +24,10 @@ class ComPagesDatabaseTablePages extends KDatabaseTableDefault
             'name' => 'pages',
             'behaviors'  => array(
                 'creatable', 'modifiable', 'lockable', 'sluggable', 'assignable',
-                'com://admin/pages.database.behavior.orderable' => array(
+                /*'com://admin/pages.database.behavior.orderable' => array(
                     'table' => 'pages_orderings',
                     'columns' => array('title', 'custom')
-                ),
+                ),*/
                 'com://admin/pages.database.behavior.closurable' => array(
                     'table' => 'pages_closures'
                 )
