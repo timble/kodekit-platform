7dd0adadaa4e7c62a4ea520dbce77a1abf655ca6
Gergo Erdosi <gergo@timble.net>
2012-10-01 20:06:48 +0000

    Refactor closurable behavior, simplify quries, improve performance. re #168

--- code/administrator/components/com_pages/databases/behaviors/closurable.php
+++ code/administrator/components/com_pages/databases/behaviors/closurable.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     $Id: pages.php 3029 2011-10-09 13:07:11Z johanjanssens $
+ * @version     $Id$
  * @package     Nooku_Server
  * @subpackage  Pages
  * @copyright   Copyright (C) 2011 Timble CVBA and Contributors. (http://www.timble.net).
@@ -11,17 +11,25 @@
 /**
  * Closurable Database Behavior Class
  *
- * This behavior is used for saving and deleting relations. The reason for using a separate behavior is to make sure that
- * other behaviors like orderable can use methods like getAncestors, getParent.
- *
  * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
  * @package     Nooku_Server
  * @subpackage  Pages
  */
 class ComPagesDatabaseBehaviorClosurable extends KDatabaseBehaviorAbstract
 {
+    /**
+     * The closure table name
+     * 
+     * @var string
+     */
     protected $_table;
     
+    /**
+     * Constructor
+     *
+     * @param  object   A KConfig object with configuration options.
+     * @return void
+     */
     public function __construct(KConfig $config)
     {
         parent::__construct($config);
@@ -31,10 +39,18 @@ class ComPagesDatabaseBehaviorClosurable extends KDatabaseBehaviorAbstract
         }
     }
     
+    /**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param  object   A KConfig object with configuration options.
+     * @return void
+     */
     protected function _initialize(KConfig $config)
     {
         $config->append(array(
-            'priority'   => KCommand::PRIORITY_HIGHEST,
+            'priority'   => KCommand::PRIORITY_HIGH,
             'auto_mixin' => true,
             'table'      => null
         ));
@@ -42,16 +58,165 @@ class ComPagesDatabaseBehaviorClosurable extends KDatabaseBehaviorAbstract
         parent::_initialize($config);
     }
     
+    /**
+     * Get parent id
+     * 
+     * @return int|null The parent id if row has a parent, null otherwise.
+     */
+    public function getParentId()
+    {
+        $id = $this->level > 1 ? end(array_values($this->getParentIds())) : null;
+        
+        return $id; 
+    }
+    
+    /**
+     * Get parent ids
+     * 
+     * @return array The parent ids.
+     */
+    public function getParentIds()
+    {
+        $ids = array_map('intval', explode('/', $this->path));
+        array_pop($ids);
+        
+        return $ids;
+    }
+    
+    /**
+     * Get closure table name
+     * 
+     * @return string
+     */
+    public function getClosureTable()
+    {
+        if(!isset($this->_table) && ($this->getMixer() instanceof KDatabaseRowTable || $this->getMixer() instanceof KDatabaseRowsetTable))
+        {
+            $name = $this->getIdentifier()->name;
+            $this->_table = $this->getTable()->getBehavior($name)->getClosureTable();
+        }
+        
+        return $this->_table;
+    }
+    
+    /**
+     * Get the siblings of the row
+     *
+     * @return KDatabaseRowsetAbstract
+     */
+    public function getSiblings()
+    {
+        
+        $table = $this->getTable();
+        $query = $this->getService('koowa:database.query.select')
+            
+            ->join(array('closures' => $this->getClosureTable()), 'closures.descendant_id = tbl.'.$table->getIdentityColumn(), 'INNER')
+            ->where('tbl.'.$table->getIdentityColumn().' <> :id')
+            ->having('COUNT(`crumbs`.`ancestor_id`) = :level')
+            ->bind(array('id' => $this->id, 'level' => $this->level));
+
+        if($this->level > 1) {
+            $query->where('closures.ancestor_id = :ancestor_id')->bind(array('ancestor_id' => $this->getParentId()));
+        }
+        
+        $result = $table->select($query);
+
+        return $result;
+    }
+    
+    /**
+     * Get the first ancestor of the row
+     *
+     * @return KDatabaseRowAbstract Parent row or empty if there is no parent.
+     */
+    public function getParent()
+    {
+        $table = $this->getTable();
+        
+        if($this->level > 1) {
+            $result = $table->select(end(array_values($this->getParentIds())), KDatabase::FETCH_ROW);
+        } else {
+            $result = $table->getRow();
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * Get ancestors of the row
+     *
+     * @return KDatabaseRowsetAbstract A rowset containing all ancestors.
+     */
+    public function getAncestors()
+    {
+        $table = $this->getTable();
+        
+        if($this->level > 1) {
+            $result = $table->select($this->getParentIds());
+        } else {
+            $result = $table->getRowset();
+        }
+        
+        return $result;
+    }
+    
+    /**
+     * Get ancestors of the row
+     *
+     * @return KDatabaseRowsetAbstract A rowset containing all ancestors.
+     */
+    public function getDescendants()
+    {
+        $table = $this->getTable();
+        $query = $this->getService('koowa:database.query.select')
+            ->join(array('closures' => $this->getClosureTable()), 'closures.descendant_id = tbl.'.$table->getIdentityColumn(), 'INNER')
+            ->where('closures.ancestor_id = :id')
+            ->where('tbl.'.$table->getIdentityColumn().' <> :id')
+            ->bind(array('id' => $this->id));
+        
+        $result = $table->select($query);
+        
+        return $result;
+    }
+    
+    /**
+     * Checks if the current row is a descendant of the given one
+     *
+     * @param  KDatabaseRowAbstract $row
+     * @return boolean
+     */
+    public function isDescendantOf(KDatabaseRowAbstract $row)
+    {
+        return in_array($row->id, $this->getParentIds());
+    }
+    
+    /**
+     * Checks if the current row is an ancestor of the given one
+     *
+     * @param  KDatabaseRowAbstract $row
+     * @return boolean
+     */
+    public function isAncestorOf(KDatabaseRowAbstract $row)
+    {
+        return in_array($this->id, $row->getParentIds());
+    }
+    
+    /**
+     * Add level and path columns to the query.
+     * 
+     * @param  KCommandContext $context A command context object.
+     * @return boolean True on success, false on failure.
+     */
     protected function _beforeTableSelect(KCommandContext $context)
     {
         if($query = $context->query)
         {
             $state     = $context->options->state;
             $id_column = $context->caller->getIdentityColumn();
-            
+
             $query->columns(array('level' => 'COUNT(crumbs.ancestor_id)'))
                 ->columns(array('path' => 'GROUP_CONCAT(crumbs.ancestor_id ORDER BY crumbs.level DESC SEPARATOR \'/\')'))
-                ->join(array('crumbs' => $this->_table), 'crumbs.descendant_id = tbl.'.$id_column, 'INNER')
+                ->join(array('crumbs' => $this->getClosureTable()), 'crumbs.descendant_id = tbl.'.$id_column, 'INNER')
                 ->group('tbl.'.$id_column);
             
             if($state)
@@ -72,12 +237,14 @@ class ComPagesDatabaseBehaviorClosurable extends KDatabaseBehaviorAbstract
                 }
             }
         }
+
+        return true;
     }
     
     /**
-     * Inserts relations into the relation table
+     * Insert relations into the relation table
      * 
-     * @param KCommandContext $context A command context object.
+     * @param  KCommandContext $context A command context object.
      * @return boolean True on success, false on failure.
      */
     protected function _afterTableInsert(KCommandContext $context)
@@ -90,7 +257,7 @@ class ComPagesDatabaseBehaviorClosurable extends KDatabaseBehaviorAbstract
             // Set path and level for the current row.
             if($data->parent_id)
             {
-                $parent = $table->select($data->parant_id, KDatabase::FETCH_ROW);
+                $parent = $table->select($data->parent_id, KDatabase::FETCH_ROW);
                 
                 if(!$parent->isNew()) {
                     $data->setData(array('level' => $parent->level + 1, 'path' => $parent->path.'/'.$data->id), false);
@@ -100,39 +267,39 @@ class ComPagesDatabaseBehaviorClosurable extends KDatabaseBehaviorAbstract
             
             // Insert the self relation.
             $query = $this->getService('koowa:database.query.insert')
-                ->table($this->_table)
+                ->table($this->getClosureTable())
                 ->columns(array('ancestor_id', 'descendant_id', 'level'))
                 ->values(array($data->id, $data->id, 0));
             
-            $result = $table->getDatabase()->insert($query);
+            $table->getDatabase()->insert($query);
             
             // Insert child relations.
-            if($result && $data->parent_id)
+            if($data->parent_id)
             {
                 $select = $this->getService('koowa:database.query.select')
                     ->columns(array('ancestor_id', $data->id, 'level + 1'))
-                    ->table($this->_table)
+                    ->table($this->getClosureTable())
                     ->where('descendant_id = :descendant_id')
-                    ->bind(array('descendant_id' => $data->parent_id));
+                    ->bind(array('descendant_id' => $data->getParentId()));
                 
                 $query = $this->getService('koowa:database.query.insert')
-                    ->table($this->_table)
+                    ->table($this->getClosureTable())
                     ->columns(array('ancestor_id', 'descendant_id', 'level'))
                     ->values($select);
                 
-                $result = $table->getDatabase()->insert($query);
+                $table->getDatabase()->insert($query);
             }
-            
-            return $result === false ? false : true;
         }
+        
+        return true;
     }
     
     /**
-     * Updates relations if parent has changes
+     * Update relations if parent has changed
      * 
      * @link http://www.mysqlperformanceblog.com/2011/02/14/moving-subtrees-in-closure-table/
      * 
-     * @param KCommandContext $context A command context object.
+     * @param  KCommandContext $context A command context object.
      * @return boolean True on success, false on failure. 
      */
     protected function _afterTableUpdate(KCommandContext $context)
@@ -140,12 +307,13 @@ class ComPagesDatabaseBehaviorClosurable extends KDatabaseBehaviorAbstract
         if($context->affected !== false)
         {
             $data = $context->data;
-            if((int) $data->parent_id != (int) end(array_values($data->parent_ids)))
+            if((int) $data->parent_id != (int) $data->getParentId())
             {
                 $table = $this->getTable();
+                
                 if($data->parent_id)
                 {
-                    $parent = $table->select((int) $this->parent_id, KDatabase::FETCH_ROW);
+                    $parent = $table->select((int) $data->parent_id, KDatabase::FETCH_ROW);
                     if($parent->isDescendantOf($data))
                     {
                         $this->setStatusMessage(JText::_('You cannot move a node under one of its descendants'));
@@ -157,19 +325,19 @@ class ComPagesDatabaseBehaviorClosurable extends KDatabaseBehaviorAbstract
                 // Delete the outdated paths for the old location.
                 $query_descendant = $this->getService('koowa:database.query.select')
                     ->columns('descendant_id')
-                    ->table($this->_table)
+                    ->table($this->getClosureTable())
                     ->where('ancestor_id = :ancestor_id')
                     ->bind(array('ancestor_id' => $data->id));
                 
                 $query_ancestor = $this->getService('koowa:database.query.select')
                     ->columns('ancestor_id')
-                    ->table($this->_table)
+                    ->table($this->getClosureTable())
                     ->where('descendant_id = :descendant_id')
                     ->where('ancestor_id != descdendant_id')
                     ->bind(array('descendant_id' => $data->id));
                  
                 $query = $this->getService('koowa:database.query.delete')
-                    ->table($this->_table)
+                    ->table($this->getClosureTable())
                     ->where('descendant_id IN :descendant_id')
                     ->where('ancestor_id IN :ancestor_id')
                     ->bind(array('descendant_id' => $query_descendant, 'ancestor_id' => $query_ancestor));
@@ -186,38 +354,47 @@ class ComPagesDatabaseBehaviorClosurable extends KDatabaseBehaviorAbstract
                     ->bind(array('ancestor_id' => $data->id, 'descendant_id' => (int) $data->parent_id));
                     
                 $query = $this->getService('koowa:database.query.insert')
-                    ->table($this->_table)
+                    ->table($this->getClosureTable())
                     ->columns(array('ancestor_id', 'descendant_id', 'level'))
                     ->values($select);
                 
-                $result = $table->getDatabase()->insert($query);
+                $table->getDatabase()->insert($query);
                 
-                if($result !== false)
-                {
-                    $data->path = ($data->parent_id ? $parent->path.'/' : '').$data->id;
-                    $this->path = $data->path;
-                }
-                
-                return $result === false ? false : true;
+                $data->path = ($data->parent_id ? $parent->path.'/' : '').$data->id;
             }
         }
+        
+        return true;
     }
     
     /**
-     * Deletes the row and its children
+     * Delete the row and its children
      *
-     * @param KCommandContext $context A command context object.
+     * @param  KCommandContext $context A command context object.
      * @return boolean True on success, false on failure. 
      */
     protected function _afterTableDelete(KCommandContext $context)
     {
         if($context->affected !== false)
         {
-            $this->getTable()->getCommandChain()->disable();
-            $result = $context->data->getDescendants()->delete();
-            $this->getTable()->getCommandChain()->enable();
+            $table = $context->caller;
             
-            return $result === false ? false : true;
+            $query_descendants = $this->getService('koowa:database.query.select')
+                ->columns($table->getIdentityColumn())
+                ->table($table->getName())
+                ->join(array('closures' => $table->getClosureTable()), 'closures.descendant_id = tbl.'.$table->getIdentityColumn(), 'INNER')
+                ->where('closures.ancestor_id = :id')
+                ->where($table->getIdentityColumn().' <> :id')
+                ->bind(array('id' => $context->data->id));
+            
+            $query = $this->getService('koowa:database.query.delete')
+                ->table($table->getName())
+                ->where($table->getIdentityColumn().' IN :descendants')
+                ->bind(array('descendants' => $query_descendants));
+
+            $table->getDatabase()->delete($query);
         }
+        
+        return true;
     }
 }
