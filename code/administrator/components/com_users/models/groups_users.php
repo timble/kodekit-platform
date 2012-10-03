7c5ed2e16e22e7d7d0cabeeb54133d6a19d9b4e6
Tom Janssens <tom@timble.net>
2012-10-03 13:46:35 +0000

    Added user and group state to groups_user model re #241

--- code/administrator/components/com_users/models/groups_users.php
+++ code/administrator/components/com_users/models/groups_users.php
@@ -2,6 +2,15 @@
 
 class ComUsersModelGroups_users extends ComDefaultModelDefault
 {	
+	public function __construct(KConfig $config)
+	{
+	    parent::__construct($config);
+	    
+	    $this->getState()
+	        ->insert('user' , 'int')
+	        ->insert('group' , 'int');
+	}
+	
 	protected function _buildQueryColumns(KDatabaseQuerySelect $query)
 	{
 		parent::_buildQueryColumns($query);
@@ -14,5 +23,19 @@ class ComUsersModelGroups_users extends ComDefaultModelDefault
 	protected function _buildQueryJoins(KDatabaseQuerySelect $query)
 	{
 		$query->join(array('group' => 'users_groups'), 'group.users_group_id = tbl.users_group_id');
+	}
+	
+	protected function _buildQueryWhere(KDatabaseQuerySelect $query)
+	{
+	    parent::_buildQueryWhere($query);
+		$state = $this->getState();
+		
+		if ($state->user) {
+			$query->where('tbl.users_user_id = :user')->bind(array('user' => $state->user));
+		}
+		
+		if ($state->group) {
+			$query->where('tbl.users_group_id = :group')->bind(array('group' => $state->group));
+		}
 	}
 }
