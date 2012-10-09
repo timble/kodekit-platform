528718059eb60564bb1744c9b65788e6c98e5e57
Johan Janssens <johan@timble.net>
2012-10-09 14:37:20 +0000

    Allow to retrieve the service manager object using $this->getService()

--- code/libraries/koowa/object/serviceable.php
+++ code/libraries/koowa/object/serviceable.php
@@ -15,15 +15,14 @@
 interface KObjectServiceable
 {
 	/**
-	 * Get an instance of a class based on a class identifier only creating it
-	 * if it doesn't exist yet.
+	 * Get an instance of a class based on a class identifier only creating it if it doesn't exist yet.
 	 *
 	 * @param	string|object	$identifier The class identifier or identifier object
 	 * @param	array  			$config     An optional associative array of configuration settings.
 	 * @throws	KObjectException
 	 * @return	object  		Return object on success, throws exception on failure
 	 */
-	public function getService($identifier, array $config = array());
+	public function getService($identifier = null, array $config = array());
 
 	/**
 	 * Get a service identifier.
