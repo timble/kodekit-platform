fc1f733800fa7bdbde7ad63deb5b8d3d8001ba9a
Johan Janssens <johan@timble.net>
2012-10-23 15:03:24 +0000

    Code cleanup

--- code/libraries/koowa/dispatcher/session/handler/database.php
+++ code/libraries/koowa/dispatcher/session/handler/database.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @version        $Id$
- * @package        Koowa_Dispatcher
+ * @version     $Id$
+ * @package     Koowa_Dispatcher
  * @subpackage  Session
- * @copyright    Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
- * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link         http://www.nooku.org
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
  */
 
 /**
