595019a5b846d10f27e56e4fd9a72ff72e5a6d41
Johan Janssens <johan@timble.net>
2012-10-23 20:03:10 +0000

    Fixed typo

--- code/administrator/components/com_debug/views/debug/tmpl/default_profiles.php
+++ code/administrator/components/com_debug/views/debug/tmpl/default_profiles.php
@@ -22,7 +22,8 @@
   	<tbody>
   		<? foreach ( $events as $event ) : ?>
   		<tr>  
-			<td class="-koowa-sortable"><?= $event['target'] ?></div>
+			<td class="-koowa-sortable"><?= $event['target'] ?></td>
+			<td class="-koowa-sortable"><?= $event['target'] ?></td>
             <td class="-koowa-sortable"><?= $event['message'] ?></td>
             <td class="-koowa-sortable" data-comparable="<?= $event['time'] ?>"><?= sprintf('%.3f', $event['time']).' seconds' ?></td>
             <td class="-koowa-sortable"><?= $event['memory'] ?></td>
