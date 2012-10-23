50196e61213c8593aea837d293197fdac93793a9
Johan Janssens <johan@timble.net>
2012-10-23 17:54:46 +0000

    Docblocks

--- code/administrator/components/com_application/controllers/toolbars/menubar.php
+++ code/administrator/components/com_application/controllers/toolbars/menubar.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * @version   	$Id: menubar.php 4774 2012-08-08 22:13:02Z johanjanssens $
+ * @version		$Id: dispatcher.php 5146 2012-09-03 23:25:47Z johanjanssens $
+ * @category	Nooku
  * @package     Nooku_Server
- * @subpackage  Application
- * @copyright  	Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
- * @license   	GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link        http://www.nooku.org
+ * @subpackage  Dashboard
+ * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
  */
 
 /**
