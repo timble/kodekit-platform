40205f5fb067e4038653c84ed68b71d8e65e3af2
Johan Janssens <johan@timble.net>
2012-10-23 15:32:19 +0000

    Added special error event which implement KException interface

---
+++ code/libraries/koowa/event/error.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @version     $Id: event.php 4996 2012-10-09 14:54:46Z johanjanssens $
+ * @package     Koowa_Event
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Error Event Class
+ *
+ * @author      Johan Janssens <johan@nooku.org>
+ * @package     Koowa_Event
+ */
+class KEventError extends KEvent implements KExceptionInterface
+{
+    /**
+     * Set the exception
+     *
+     * @param Exception $exception
+     */
+    public function setException(Exception $exception)
+    {
+        $this->exception = $exception;
+    }
+
+    /**
+     * Get the exception
+     *
+     * @return Exception
+     */
+    public function getException()
+    {
+        return $this->exception;
+    }
+
+    /**
+     * Return the error message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->exception->getMessage();
+    }
+
+    /**
+     * Return the error code
+     *
+     * @return integer
+     */
+    public function getCode()
+    {
+        return $this->exception->getCode();
+    }
+
+    /**
+     * Return the source filename
+     *
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->exception->getFile();
+    }
+
+    /**
+     * Return the source line number
+     *
+     * @return integer
+     */
+    public function getLine()
+    {
+        return $this->exception->getLine();
+    }
+
+    /**
+     * Return the backtrace information
+     *
+     * @return array
+     */
+    public function getTrace()
+    {
+        return $this->exception->getTrace();
+    }
+
+    /**
+     * Return the backtrace as a string
+     *
+     * @return string
+     */
+    public function getTraceAsString()
+    {
+        return $this->exception->getTraceAsString();
+    }
+
+    /**
+     * Format the error for display
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->exception;
+    }
+}
