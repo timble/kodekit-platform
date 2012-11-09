3137dab23af40b4d9f32e002951218a30c1cf2af
Arunas Mazeika <arunas@timble.net>
2012-11-09 12:43:00 +0000

    Updated model with latest table changes.
    re #246.

--- code/administrator/components/com_users/models/groups_users.php
+++ code/administrator/components/com_users/models/groups_users.php
@@ -1,16 +1,24 @@
 <?php
+/**
+ * @version     $Id$
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Users
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
 
+/**
+ * Group Users Model Class
+ *
+ * @author      Tom Janssens <http://nooku.assembla.com/profile/tomjanssens>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Users
+ */
 class ComUsersModelGroups_users extends ComDefaultModelDefault
-{	
-	public function __construct(KConfig $config)
-	{
-	    parent::__construct($config);
-	    
-	    $this->getState()
-	        ->insert('user' , 'int')
-	        ->insert('group' , 'int');
-	}
-	
+{
 	protected function _buildQueryColumns(KDatabaseQuerySelect $query)
 	{
 		parent::_buildQueryColumns($query);
@@ -30,12 +38,12 @@ class ComUsersModelGroups_users extends ComDefaultModelDefault
 	    parent::_buildQueryWhere($query);
 		$state = $this->getState();
 		
-		if ($state->user) {
-			$query->where('tbl.users_user_id = :user')->bind(array('user' => $state->user));
+		if ($user_id = $state->user_id) {
+			$query->where('tbl.users_user_id = :user_id')->bind(array('user_id' => $user_id));
 		}
 		
-		if ($state->group) {
-			$query->where('tbl.users_group_id = :group')->bind(array('group' => $state->group));
+		if ($group_id = $state->group_id) {
+			$query->where('tbl.users_group_id = :group_id')->bind(array('group_id' => $group_id));
 		}
 	}
 }
