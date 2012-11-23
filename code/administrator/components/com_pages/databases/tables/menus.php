55c78beef9cfde555e4e2031f9b7250d05340a6c
Tom Janssens <tom@timble.net>
2012-11-23 08:43:13 +0000

    Added Menus Database Table Class re #168

---
+++ code/administrator/components/com_pages/databases/tables/menus.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Nooku_Server
+ * @subpackage  Pages
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Menus Database Table Class
+ *
+ * @author      Tom Janssens <http://nooku.assembla.com/profile/tomjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Pages
+ */
+
+class ComPagesDatabaseTableMenus extends KDatabaseTableDefault
+{
+    public function  _initialize(KConfig $config) 
+    {		
+        $config->append(array(
+            'behaviors'  => array(
+                'creatable', 'modifiable', 'lockable', 'sluggable'
+            )
+            ));
+     
+        parent::_initialize($config);
+    }
+}
