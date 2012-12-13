ac3f15009e01b67b312ae0cb0ef74d4054aefa96
Gergo Erdosi <gergo@timble.net>
2012-12-13 16:31:58 +0000

    Add pathway config class. re #253

---
+++ code/site/components/com_application/configs/pathway.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Nooku_Server
+ * @subpackage  Application
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Application Pathway Config
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @package     Nooku_Server
+ * @subpackage  Application
+ */
+class ComApplicationConfigPathway extends KConfig
+{
+    /**
+     * Add item to pathway
+     *
+     * @param $name
+     * @param null $link
+     * @return ComApplicationConfigPathway
+     */
+    public function addItem($name, $link = null)
+    {
+        $item = new stdClass();
+        $item->name = html_entity_decode($name, ENT_COMPAT, 'UTF-8');
+        $item->link = $link;
+
+        $this->_data['items'][] = $item;
+
+        return $this;
+    }
+}
