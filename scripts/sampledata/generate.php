c60fadb657b75630e21dd33aaa2842b4961636e5
Gergo Erdosi <gergo@timble.net>
2012-12-25 13:55:57 +0000

    Improve sample data generator, fix bugs. re #252

--- scripts/sampledata/generate.php
+++ scripts/sampledata/generate.php
@@ -16,22 +16,23 @@
  * @subpackage  Sample_Data
  */
 
+set_time_limit(0);
+
 // Exit if script is not called from command line.
 if(PHP_SAPI != 'cli')
 {
-    echo 'This script is only meant to run at the command line.';
+    print 'This script is only meant to run at the command line.';
     exit(1);
 }
 
-set_time_limit(0);
-
+// Define custom functions.
 function pick_random(array $array)
 {
     $key = array_rand($array);
     return !is_null($key) ? $array[$key] : null;
 }
 
-// Load Koowa libraries.
+// Load application.
 define('_JEXEC', 1);
 define('JPATH_APPLICATION', realpath(__DIR__.'/../../code/administrator'));
 define('JPATH_BASE'       , JPATH_APPLICATION);
@@ -40,10 +41,12 @@ define('JPATH_LIBRARIES'  , JPATH_ROOT.'/libraries' );
 define('JPATH_SITES'      , JPATH_ROOT.'/sites');
 define('DS', DIRECTORY_SEPARATOR);
 
-require_once(JPATH_APPLICATION.'/bootstrap.php');
+require_once JPATH_APPLICATION.'/bootstrap.php';
 
 KLoader::loadIdentifier('com://admin/application.aliases');
-KService::get('com://admin/application.dispatcher')->loadConfig(new KCommandContext());
+$application = KService::get('com://admin/application.dispatcher');
+$application->loadConfig(new KCommandContext());
+$application->loadSession(new KCommandContext());
 
 // Load Lorem Ipsum generator class.
 require_once __DIR__.'/libraries/loremipsum.php';
@@ -52,93 +55,165 @@ $generator = new LoremIpsum();
 // Load progress bar class.
 require_once __DIR__.'/libraries/progressbar.php';
 
-// Define row numbers.
-$numbers = array(
-    'articles'   => 100000,
-    'categories' => 1000,
-    'users'      => 1000
-);
+// Prompt the user for the number of rows.
+$numbers = array();
+$types   = array('articles', 'categories', 'users');
 
-// Generate users.
-ProgressBar::start($numbers['users']);
-ProgressBar::setMessage('Adding users...');
-
-for($i = 0; $i < $numbers['users']; $i++)
+print "Specify the number of rows to insert:\r\n";
+foreach($types as $type)
 {
-    $row = KService::get('com://admin/users.model.users')->getItem();
-    $row->name = $generator->words(pick_random(range(1, 3)));
-    $row->email = $i.'@example.'.pick_random(array('com', 'net', 'org'));
-    $row->enabled = (int) rand(0, 50) > 2;
-    $row->password = (string) rand(100000, 1000000);
-
-    $groups = array_merge(array_fill(0, 100, 18), array(19, 20, 21, 23, 24, 25));
-    $row->role_id = pick_random($groups);
-
-    $row->save();
-
-    echo ProgressBar::next();
-}
-echo ProgressBar::finish();
-echo "DONE\r\n\r\n";
-
-// Generate categories.
-$categories = array('articles' => array(), 'contacts' => array(), 'weblinks' => array());
-
-ProgressBar::start($numbers['categories'] * count($categories));
-ProgressBar::setMessage('Adding categories...');
-
-foreach(array_keys($categories) as $table)
-{
-    $levels = array(1 => array(), 2 => array());
-
-    for($i = 0; $i < $numbers['categories']; $i++)
+    while(!isset($numbers[$type]))
     {
-        $row = KService::get('com://admin/categories.model.categories')->getItem();
-        $row->title = $generator->words(pick_random(range(1, 5)));
-        $row->description = $generator->sentences(pick_random(range(0, 3)));
-        $row->table = $table;
-        $row->published = (int) (rand(0, 20) > 0);
+        printf("  %s: ", $type);
+        $input = trim(fgets(STDIN));
 
-        $level = pick_random(range(1, 3));
-        if($level > 1)
-        {
-            $parent_id = pick_random($levels[$level-1]);
-            $row->parent_id = (int) $parent_id;
+        if(strlen($input) && !is_numeric($input)) {
+            print "Error: Value has to be an integer.\r\n";
+        } else {
+            $numbers[$type] = (int) $input;
         }
-
-        $row->save();
-
-        $levels[$level + 1][] = $row->id;
-        $categories[$table][] = $row->id;
-
-        echo ProgressBar::next();
     }
 }
 
-unset($levels);
+if(!$numbers['users'] && ($numbers['articles'] || $numbers['categories'])) {
+    $numbers['users'] = 1;
+}
 
-echo ProgressBar::finish();
-echo "DONE\r\n\r\n";
+if(!$numbers['categories'] && $numbers['articles']) {
+    $numbers['categories'] = 1;
+}
+
+print "\r\n";
+unset($types, $type, $input);
+
+// Generate users.
+if($numbers['users'])
+{
+    ProgressBar::start($numbers['users']);
+    ProgressBar::setMessage('Adding users...');
+
+    $date_min = new DateTime('-3 years');
+    $date_max = new DateTime();
+    $autoinc  = KService::get('com://admin/users.database.table.users')->getSchema()->autoinc;
+
+    for($i = 0; $i < $numbers['users']; $i++)
+    {
+        $row = KService::get('com://admin/users.model.users')->getItem();
+        $row->name = $generator->words(pick_random(range(1, 3)));
+        $row->email = ($autoinc + $i).'@example.'.pick_random(array('com', 'net', 'org'));
+        $row->enabled = (int) rand(0, 50) > 2;
+        $row->password = $row->email;
+
+        $groups = array_merge(array_fill(0, 100, 18), array(19, 20, 21, 23, 24, 25));
+        $row->role_id = pick_random($groups);
+
+        $created = new DateTime('@'.rand($date_min->format('U'), $date_max->format('U')));
+        $row->created_on = $created->format('Y-m-d H:i:s');
+        $row->created_by = $autoinc + $i;
+
+        $row->save();
+        $users[] = $row->id;
+
+        print ProgressBar::next();
+    }
+
+    unset($date_min, $date_max, $autoinc, $i, $row, $groups);
+
+    print ProgressBar::finish();
+    print "DONE\r\n\r\n";
+}
+
+// Generate categories.
+if($numbers['categories'])
+{
+    $categories = array_fill_keys(array('articles'/*, 'contacts', 'weblinks'*/), array());
+
+    foreach($categories as $table => $values)
+    {
+        if(!$numbers[$table]) {
+            unset($categories[$table]);
+        }
+    }
+
+    if($categories)
+    {
+        ProgressBar::start($numbers['categories'] * count($categories));
+        ProgressBar::setMessage('Adding categories...');
+
+        foreach(array_keys($categories) as $table)
+        {
+            $levels   = array_fill_keys(array(1, 2, 3), array());
+            $date_min = new DateTime('-3 years');
+            $date_max = new DateTime();
+
+            for($i = 0; $i < $numbers['categories']; $i++)
+            {
+                $row = KService::get('com://admin/categories.model.categories')->getItem();
+                $row->title = $generator->words(pick_random(range(1, 5)));
+                $row->description = $generator->sentences(pick_random(range(0, 3)));
+                $row->table = $table;
+                $row->published = (int) (rand(0, 20) > 0);
+
+                $created = new DateTime('@'.rand($date_min->format('U'), $date_max->format('U')));
+                $row->created_on = $created->format('Y-m-d H:i:s');
+                $row->created_by = pick_random($users);
+
+                $level = pick_random(range(1, 3));
+                if($level > 1)
+                {
+                    if($parent_id = pick_random($levels[$level-1])) {
+                        $row->parent_id = $parent_id;
+                    }
+                }
+
+                $row->save();
+
+                $levels[$level + 1][] = $row->id;
+                $categories[$table][] = $row->id;
+
+                print ProgressBar::next();
+            }
+        }
+
+        unset($levels, $date_min, $date_max, $i, $row, $created, $level, $parent_id);
+
+        print ProgressBar::finish();
+        print "DONE\r\n\n";
+    }
+}
 
 // Generate articles.
-ProgressBar::start($numbers['articles']);
-ProgressBar::setMessage('Adding articles...');
-
-for($i = 0; $i < $numbers['articles']; $i++)
+if($numbers['articles'])
 {
-    $row = KService::get('com://admin/articles.model.articles')->getItem();
-    $row->title = $generator->words(pick_random(range(1, 5)));
-    $row->introtext = $generator->sentences(pick_random(range(1, 3)));
-    $row->fulltext = $generator->sentences(pick_random(range(0, 5)));
-    $row->published = (int) (rand(0, 20) > 0);
-    $row->featured = pick_random(array_merge(array_fill(0, 5, 0), array(1)));
-    $row->categories_category_id = pick_random($categories['articles']);
+    ProgressBar::start($numbers['articles']);
+    ProgressBar::setMessage('Adding articles...');
 
-    $row->save();
+    $date_min = new DateTime('-3 years');
+    $date_max = new DateTime();
 
-    echo ProgressBar::next();
+    for($i = 0; $i < $numbers['articles']; $i++)
+    {
+        $row = KService::get('com://admin/articles.model.articles')->getItem();
+        $row->title = $generator->words(pick_random(range(1, 5)));
+        $row->introtext = $generator->sentences(pick_random(range(1, 3)));
+        $row->fulltext = $generator->sentences(pick_random(range(0, 5)));
+        $row->published = (int) (rand(0, 20) > 0);
+        $row->featured = pick_random(array_merge(array_fill(0, 5, 0), array(1)));
+        $row->categories_category_id = pick_random($categories['articles']);
+
+        $created = new DateTime('@'.rand($date_min->format('U'), $date_max->format('U')));
+        $row->created_on = $created->format('Y-m-d H:i:s');
+        $row->created_by = pick_random($users);
+
+        $row->save();
+
+        print ProgressBar::next();
+    }
+
+    unset($date_min, $date_max, $i, $row, $created);
+
+    print ProgressBar::finish();
+    print "DONE\r\n\n";
 }
-echo ProgressBar::finish();
-echo "DONE\r\n\r\n";
 
 exit(0);
