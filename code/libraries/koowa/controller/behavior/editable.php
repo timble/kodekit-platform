9cccdf127dcb00b9ce25159a5a2d8d75b54c8a36
Johan Janssens <johan@timble.net>
2012-12-27 17:02:02 +0000

    Split KControllerBehaviorEditable. Moved entity locking into new KControllerLockable behavior.

--- code/libraries/koowa/controller/behavior/editable.php
+++ code/libraries/koowa/controller/behavior/editable.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * @version        $Id$
- * @package        Koowa_Controller
- * @subpackage    Command
- * @copyright    Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
- * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link         http://www.nooku.org
+ * @version     $Id$
+ * @package     Koowa_Controller
+ * @subpackage  Behavior
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
  */
 
 /**
  * Editable Controller Behavior Class
  *
- * @author        Johan Janssens <johan@nooku.org>
+ * @author      Johan Janssens <johan@nooku.org>
  * @package     Koowa_Controller
- * @subpackage    Behavior
+ * @subpackage  Behavior
  */
 class KControllerBehaviorEditable extends KControllerBehaviorAbstract
 {
@@ -26,18 +26,11 @@ class KControllerBehaviorEditable extends KControllerBehaviorAbstract
     {
         parent::__construct($config);
 
-        if ($this->isDispatched() && KRequest::type() == 'HTTP')
-        {
-            $this->registerCallback('before.read' , array($this, 'setReferrer'));
-            $this->registerCallback('after.apply' , array($this, 'lockReferrer'));
-            $this->registerCallback('after.read'  , array($this, 'unlockReferrer'));
-            $this->registerCallback('after.save'  , array($this, 'unsetReferrer'));
-            $this->registerCallback('after.cancel', array($this, 'unsetReferrer'));
-        }
-
-        $this->registerCallback('after.read'  , array($this, 'lockResource'));
-        $this->registerCallback('after.save'  , array($this, 'unlockResource'));
-        $this->registerCallback('after.cancel', array($this, 'unlockResource'));
+        $this->registerCallback('before.read' , array($this, 'setReferrer'));
+        $this->registerCallback('after.apply' , array($this, 'lockReferrer'));
+        $this->registerCallback('after.read'  , array($this, 'unlockReferrer'));
+        $this->registerCallback('after.save'  , array($this, 'unsetReferrer'));
+        $this->registerCallback('after.cancel', array($this, 'unsetReferrer'));
     }
 
     /**
@@ -79,7 +72,7 @@ class KControllerBehaviorEditable extends KControllerBehaviorAbstract
         $identifier = $this->getMixer()->getIdentifier();
 
         $referrer = $this->getService('koowa:http.url',
-            array('url' => KRequest::get('cookie.referrer', 'url'))
+            array('url' => $context->request->cookies->get('referrer', 'url'))
         );
 
         return $referrer;
@@ -93,10 +86,10 @@ class KControllerBehaviorEditable extends KControllerBehaviorAbstract
      */
     public function setReferrer(KCommandContext $context)
     {
-        if (!KRequest::has('cookie.referrer_locked'))
+        if (!$context->request->cookies->has('referrer_locked'))
         {
-            $request  = KRequest::url();
-            $referrer = KRequest::referrer();
+            $request  = $context->request->getUrl();
+            $referrer = $context->request->getReferrer();
 
             //Compare request url and referrer
             if (!isset($referrer) || ((string)$referrer == (string)$request))
@@ -169,32 +162,30 @@ class KControllerBehaviorEditable extends KControllerBehaviorAbstract
     /**
      * Save action
      *
-     * This function wraps around the edit or add action. If the model state is
-     * unique a edit action will be executed, if not unique an add action will be
-     * executed.
+     * This function wraps around the edit or add action. If the model state is unique a edit action will be
+     * executed, if not unique an add action will be executed.
      *
      * This function also sets the redirect to the referrer.
      *
-     * @param   KCommandContext    A command context object
-     * @return     KDatabaseRow     A row object containing the saved data
+     * @param   KCommandContext  A command context object
+     * @return  KDatabaseRow     A row object containing the saved data
      */
     protected function _actionSave(KCommandContext $context)
     {
         $action = $this->getModel()->getState()->isUnique() ? 'edit' : 'add';
-        $data = $context->getSubject()->execute($action, $context);
+        $entity = $context->getSubject()->execute($action, $context);
 
         //Create the redirect
         $context->response->setRedirect($this->getReferrer($context));
 
-        return $data;
+        return $entity;
     }
 
     /**
      * Apply action
      *
-     * This function wraps around the edit or add action. If the model state is
-     * unique a edit action will be executed, if not unique an add action will be
-     * executed.
+     * This function wraps around the edit or add action. If the model state is unique a edit action will be
+     * executed, if not unique an add action will be executed.
      *
      * This function also sets the redirect to the current url
      *
@@ -204,29 +195,29 @@ class KControllerBehaviorEditable extends KControllerBehaviorAbstract
     protected function _actionApply(KCommandContext $context)
     {
         $action = $this->getModel()->getState()->isUnique() ? 'edit' : 'add';
-        $data = $context->getSubject()->execute($action, $context);
+        $entity = $context->getSubject()->execute($action, $context);
 
         //Create the redirect
         $url = $this->getReferrer($context);
 
-        if ($data instanceof KDatabaseRowAbstract)
+        if ($entity instanceof KDatabaseRowAbstract)
         {
-            $url = clone KRequest::url();
+            $url = clone $context->request->getUrl();
 
             if ($this->getModel()->getState()->isUnique())
             {
-                $states = $this->getModel()->getState()->getData(true);
+                $states = $this->getModel()->getState()->toArray(true);
 
                 foreach ($states as $key => $value) {
-                    $url->query[$key] = $data->get($key);
+                    $url->query[$key] = $entity->get($key);
                 }
             }
-            else $url->query[$data->getIdentityColumn()] = $data->get($data->getIdentityColumn());
+            else $url->query[$entity->getIdentityColumn()] = $entity->get($entity->getIdentityColumn());
         }
 
         $context->response->setRedirect($url);
 
-        return $data;
+        return $entity;
     }
 
     /**
@@ -242,8 +233,7 @@ class KControllerBehaviorEditable extends KControllerBehaviorAbstract
         //Create the redirect
         $context->response->setRedirect($this->getReferrer($context));
 
-        $data = $context->getSubject()->execute('read', $context);
-
-        return $data;
+        $entity = $context->getSubject()->execute('read', $context);
+        return $entity;
     }
 }
