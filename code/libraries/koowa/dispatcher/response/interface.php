821f47b2ceb40b5bbf8105705989cb8259a872e7
Johan Janssens <johan@timble.net>
2012-12-27 17:39:14 +0000

    re #252 : Removed KDispatcherResponseInterface now extends from KControllerResponseInterface

--- code/libraries/koowa/dispatcher/response/interface.php
+++ code/libraries/koowa/dispatcher/response/interface.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @version		$Id: abstract.php 4948 2012-09-03 23:05:48Z johanjanssens $
- * @package		Koowa_Controller
+ * @package		Koowa_Dispatcher
  * @subpackage  Response
  * @copyright	Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
@@ -9,37 +9,13 @@
  */
 
 /**
- * Controller Response Interface
+ * Dispatcher Response Interface
  *
  * @author		Johan Janssens <johan@nooku.org>
- * @package     Koowa_Controller
+ * @package     Koowa_Dispatcher
  * @subpackage  Response
  */
-interface KControllerResponseInterface extends KHttpResponseInterface
+interface KDispatcherResponseInterface extends KControllerResponseInterface, KDispatcherResponseTransportInterface, KServiceInstantiatable
 {
-    /**
-     * Send HTTP headers
-     *
-     * @return KControllerResponseAbstract
-     */
-    public function sendHeaders();
 
-    /**
-     * Sends content for the current web response.
-     *
-     * @return KControllerResponseAbstract
-     */
-    public function sendContent();
-
-    /**
-     * Send HTTP response
-     *
-     * Prepares the Response before it is sent to the client. This method tweaks the headers to ensure that
-     * it is compliant with RFC 2616 and calculates or modifies the cache-control header to a sensible and
-     * conservative value
-     *
-     * @see http://tools.ietf.org/html/rfc2616
-     * @return KControllerResponseAbstract
-     */
-    public function send();
 }
