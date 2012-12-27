b2c7264d4de0b5d9b57706c8af80fe8e918a2226
Johan Janssens <johan@timble.net>
2012-12-27 17:18:18 +0000

    re #281 : Added KControllerExceptionForbidden exception class

--- code/libraries/koowa/controller/exception/forbidden.php
+++ code/libraries/koowa/controller/exception/forbidden.php
@@ -2,15 +2,17 @@
 /**
  * @version     $Id$
  * @package     Koowa_Controller
+ * @subpackage  Exception
  * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link        http://www.nooku.org
  */
 
 /**
- * Koowa Controller Exception class
+ * Controller Exception Forbidden Class
  *
  * @author      Johan Janssens <johan@nooku.org>
  * @package     Koowa_Controller
+ * @subpackage  Exception
  */
-class KControllerException extends KException {}
+class KControllerExceptionForbidden extends KHttpExceptionForbidden implements KControllerException {}
