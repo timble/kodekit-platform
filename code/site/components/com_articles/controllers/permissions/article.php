2fb1a8931a3feec9e5a7022b11e76cb8a6a82fe4
Johan Janssens <johan@timble.net>
2012-12-28 17:06:26 +0000

    Moved executable behavior into controller/permission package. This allows for per controller permission handlers.

---
+++ code/site/components/com_articles/controllers/permissions/article.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @version        $Id: executable.php 5655 2012-10-25 13:37:47Z johanjanssens $
+ * @package        Nooku_Server
+ * @subpackage     Articles
+ * @copyright      Copyright (C) 2009 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
+ */
+
+/**
+ * Article Controller Permission Class
+ *
+ * @author     Arunas Mazeika <http://nooku.assembla.com/profile/arunasmazeika>
+ * @package    Nooku_Server
+ * @subpackage Articles
+ */
+class ComArticlesControllerPermissionArticle extends ComDefaultControllerPermissionDefault
+{
+
+}
