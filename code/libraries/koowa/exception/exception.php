31b801651d397ee6f4ffad286bd79e37495cb572
Johan Janssens <johan@timble.net>
2012-12-28 01:31:29 +0000

    re #281 : Improved handler of exceptions during executing of __call() functions.

--- code/libraries/koowa/exception/exception.php
+++ code/libraries/koowa/exception/exception.php
@@ -10,13 +10,13 @@
 /**
  * Exception Class
  *
- * KException is the base class for all koowa related exceptions and provides an additional method for printing up a
+ * KException is the base class for all related exceptions and provides an additional method for printing up a
  * detailed view of an exception.
  *
  * @author      Johan Janssens <johan@nooku.org>
  * @package     Koowa_Exception
  */
-class KException extends Exception implements KExceptionInterface
+class KException extends \Exception implements KExceptionInterface
 {
     /**
      * Constructor
@@ -25,13 +25,26 @@ class KException extends Exception implements KExceptionInterface
      * @param integer The exception code
      * @param object  The previous exception
      */
-    public function __construct($message = null, $code = KHttpResponse::INTERNAL_SERVER_ERROR, Exception $previous = null)
+    public function __construct($message = '', $code = KHttpResponse::INTERNAL_SERVER_ERROR, \Exception $previous = null)
     {
-        if (!$message) {
-            throw new $this('Unknown '. get_class($this));
-        }
-
         parent::__construct($message, (int) $code, $previous);
+
+        $traces = $this->getTrace();
+
+        //Traverse up the trace stack to find the actual function that was not found
+        if($traces[0]['function'] == '__call')
+        {
+            foreach($traces as $trace)
+            {
+                if($trace['function'] != '__call')
+                {
+                    $this->message = "Call to undefined method : ".$trace['class'].$trace['type'].$trace['function'];
+                    $this->file    = $trace['file'];
+                    $this->line    = $trace['line'];
+                    break;
+                }
+            }
+        }
     }
 
     /**
