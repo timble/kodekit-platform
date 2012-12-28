75353150e17411b481737b3cd8c29b8ddc08a986
Johan Janssens <johan@timble.net>
2012-12-28 02:30:15 +0000

    Added KServiceContainerInterface

---
+++ code/libraries/koowa/service/container/interface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @version 	$Id: container.php 4629 2012-05-06 22:11:00Z johanjanssens $
+ * @package		Koowa_Service
+ * @subpackage  Container
+ * @copyright	Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ */
+
+/**
+ * Service Container Interface
+ *
+ * @author      Johan Janssens <johan@nooku.org>
+ * @package     Koowa_Service
+ * @subpackage  Container
+ */
+interface KServiceContainerInterface
+{
+
+}
