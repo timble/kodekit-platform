536104c58caec55be5257dc33c61e83a16870eea
Johan Janssens <johan@timble.net>
2012-12-28 21:11:08 +0000

    KModel::getList() had been renamed to KModel::getRowset()

--- code/administrator/components/com_pages/views/pages/tmpl/list.php
+++ code/administrator/components/com_pages/views/pages/tmpl/list.php
@@ -2,8 +2,8 @@
 
 <nav class="scrollable">
 <ul class="nav nav-list">
-<? foreach($this->getService('com://admin/pages.model.menus')->sort('title')->getList() as $menu) : ?>
-    <? $menu_pages = $this->getService('com://admin/pages.model.pages')->getList()->find(array('pages_menu_id' => $menu->id)) ?>
+<? foreach($this->getService('com://admin/pages.model.menus')->sort('title')->getRowset() as $menu) : ?>
+    <? $menu_pages = $this->getService('com://admin/pages.model.pages')->getRowset()->find(array('pages_menu_id' => $menu->id)) ?>
     <? if(count($menu_pages)) : ?>
         <li class="nav-header"><?= $menu->title ?></li>
 		<? $first = true; $last_depth = 0; ?>
