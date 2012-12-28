a255a6579d274f571d7b3a0293a93288bb6a2f98
Johan Janssens <johan@timble.net>
2012-12-28 02:29:52 +0000

    Moved KServiceContainer into it's own subpackage.

--- code/libraries/koowa/service/container/container.php
+++ code/libraries/koowa/service/container/container.php
@@ -2,6 +2,7 @@
 /**
  * @version 	$Id$
  * @package		Koowa_Service
+ * @subpackage  Container
  * @copyright	Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  */
@@ -12,8 +13,9 @@
  * @author      Johan Janssens <johan@nooku.org>
  * @category    Koowa
  * @package     Koowa_Service
+ * @subpackage  Container
  */
-class KServiceContainer extends ArrayObject
+class KServiceContainer extends ArrayObject implements KServiceContainerInterface
 {
 
 }
