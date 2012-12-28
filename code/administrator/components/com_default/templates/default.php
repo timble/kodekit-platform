63863af14d0f1ceba1e9c69a32c5055a6e10a550
Johan Janssens <johan@timble.net>
2012-12-28 20:13:10 +0000

    KModelState::getData() function has been renamed to toArray();

--- code/administrator/components/com_default/templates/default.php
+++ code/administrator/components/com_default/templates/default.php
@@ -37,13 +37,13 @@ class ComDefaultTemplateDefault extends ComDefaultTemplateAbstract
         if(KInflector::isPlural($view->getName()))
         {
             if($state = $view->getModel()->getState()) {
-                $params = array_merge( $state->getData(), $params);
+                $params = array_merge( $state->toArray(), $params);
             }
         }
         else
         {
-            if($item = $view->getModel()->getItem()) {
-                $params = array_merge( $item->getData(), $params);
+            if($item = $view->getModel()->getRow()) {
+                $params = array_merge( $item->toArray(), $params);
             }
         }
 
