24c973c6c75f6726ef40d6503c696d89748021a5
Johan Janssens <johan@timble.net>
2012-12-28 02:01:26 +0000

    re #281 : Added KHttpExceptionMethodNotAllowed exception

---
+++ code/libraries/koowa/http/exception/methodnotallowed.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version     $Id: exception.php 4629 2012-05-06 22:11:00Z johanjanssens $
+ * @package     Koowa_Http
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Http Exception Bad Request Class
+ *
+ * @author      Johan Janssens <johan@nooku.org>
+ * @package     Koowa_Http
+ */
+class KHttpExceptionMethodNotAllowed extends KException implements KHttpException
+{
+    /**
+     * Constructor
+     *
+     * @param string  $message  The exception message
+     * @param integer $code     The exception code
+     * @param object  $previous The previous exception
+     */
+    public function __construct($message = null, $code = 0, Exception $previous = null)
+    {
+        $code = KHttpResponse::METHOD_NOT_ALLOWED;
+
+        if(!$message) {
+            $message = KHttpResponse::$status_messages[$code];
+        }
+
+        parent::__construct($message, $code, $previous);
+    }
+}
