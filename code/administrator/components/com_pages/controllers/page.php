719289892ab2ee227a448686ac13c2c0011457b2
Johan Janssens <johan@timble.net>
2012-12-30 03:04:31 +0000

    Don't extend from default controller.

--- code/administrator/components/com_pages/controllers/page.php
+++ code/administrator/components/com_pages/controllers/page.php
@@ -1,5 +1,22 @@
 <?php
-class ComPagesControllerPage extends ComDefaultControllerDefault
+/**
+ * @version     $Id: module.php 4997 2012-08-29 19:39:25Z johanjanssens $
+ * @package     Nooku_Server
+ * @subpackage  Pages
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Page Controller Class
+ *
+ * @author      Gergo Erdosi <http://nooku.assembla.com/profile/gergoerdosi>
+ * @package     Nooku_Server
+ * @subpackage  Pages
+ */
+
+class ComPagesControllerPage extends ComDefaultControllerModel
 {
     protected function _initialize(KConfig $config)
     {
