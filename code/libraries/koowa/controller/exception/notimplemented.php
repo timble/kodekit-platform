1405b61f9349c5c9ef551dd68ad37825e5db35e2
Johan Janssens <johan@timble.net>
2012-12-30 01:27:50 +0000

    Renamed and moved KDispatcherExceptionActionNotImplemented to KControllerExceptionActionNotImplemented

--- code/libraries/koowa/controller/exception/notimplemented.php
+++ code/libraries/koowa/controller/exception/notimplemented.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @version     $Id: exception.php 4629 2012-05-06 22:11:00Z johanjanssens $
- * @package     Koowa_Dispatcher
+ * @package     Koowa_Controller
  * @subpackage  Exception
  * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
@@ -9,10 +9,10 @@
  */
 
 /**
- * Dispatcher Exception Not Implemented Class
+ * Controller Exception Not Implemented Class
  *
  * @author      Johan Janssens <johan@nooku.org>
  * @package     Koowa_Dispatcher
  * @subpackage  Exception
  */
-class KDispatcherExceptionActionNotImplemented extends KHttpExceptionNotImplemented implements KDispatcherException {}
+class KControllerExceptionNotImplemented extends KHttpExceptionNotImplemented implements KControllerException {}
