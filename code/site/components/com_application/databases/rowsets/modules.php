5f069a9317c034e5c13da6cc933d6190fd11eff5
Johan Janssens <johan@timble.net>
2013-01-25 16:24:46 +0100

    re #291 : Added application modules rowset to load application modules.

---
+++ code/site/components/com_application/databases/rowsets/modules.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @version     $Id: components.php 5058 2012-08-31 09:30:32Z gergoerdosi $
+ * @package     Nooku_Server
+ * @subpackage  Application
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Modules Database Rowset Class
+ *
+ * @author      Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Application
+ */
+class ComApplicationDatabaseRowsetModules extends KDatabaseRowsetAbstract implements KServiceInstantiatable
+{
+    public function __construct(KConfig $config )
+    {
+        parent::__construct($config);
+
+        //TODO : Inject raw data using $config->data
+        $page = $this->getService('application.pages')->getActive();
+
+        $modules = $this->getService('com://admin/pages.model.modules')
+            ->application('site')
+            ->published(true)
+            ->access((int) $this->getService('user')->isAuthentic())
+            ->page($page->id)
+            ->getRowset();
+
+        $this->merge($modules);
+    }
+
+    protected function _initialize(KConfig $config)
+    {
+        $config->identity_column = 'id';
+        parent::_initialize($config);
+    }
+
+    public static function getInstance(KConfigInterface $config, KServiceInterface $container)
+    {
+        if (!$container->has($config->service_identifier))
+        {
+            //Create the singleton
+            $classname = $config->service_identifier->classname;
+            $instance  = new $classname($config);
+            $container->set($config->service_identifier, $instance);
+        }
+
+        return $container->get($config->service_identifier);
+    }
+}
