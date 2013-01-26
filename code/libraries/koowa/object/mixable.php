1bc1280707545f9390957c5fb0c6b6b3bff43315
Johan Janssens <johan@timble.net>
2013-01-26 18:35:41 +0100

    Added KObjectMixable interface

---
+++ code/libraries/koowa/object/mixable.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Koowa_Object
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+/**
+ * Object Mixable Interface
+ *
+ * @author      Johan Janssens <johan@nooku.org>
+ * @package     Koowa_Object
+ */
+interface KObjectMixable
+{
+    /**
+     * Mixin an object
+     *
+     * When using mixin(), the calling object inherits the methods of the mixed in objects, in a LIFO order.
+     *
+     * @@param   mixed  $mixin  An object that implements KMixinInterface, KServiceIdentifier object
+     *                          or valid identifier string
+     * @param    array $config  An optional associative array of configuration options
+     * @return  KObjectInterface
+     */
+    public function mixin($mixin, $config = array());
+
+    /**
+     * Checks if the object or one of it's mixin's inherits from a class.
+     *
+     * @param   string|object   $class The class to check
+     * @return  bool Returns TRUE if the object inherits from the class
+     */
+    public function inherits($class);
+
+    /**
+     * Get a list of all the available methods
+     *
+     * This function returns an array of all the methods, both native and mixed in
+     *
+     * @return array An array
+     */
+    public function getMethods();
+}
--- code/libraries/koowa/object/object.php
+++ code/libraries/koowa/object/object.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @version        $Id$
- * @package        Koowa_Object
- * @copyright    Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
- * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link         http://www.nooku.org
+ * @version     $Id$
+ * @package     Koowa_Object
+ * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
  */
 
 /**
@@ -14,7 +14,7 @@
  * @category    Koowa
  * @package     Koowa_Object
  */
-class KObject implements KObjectHandlable, KObjectServiceable
+class KObject implements KObjectHandlable, KObjectServiceable, KObjectMixable
 {
     /**
      * Class methods
@@ -98,10 +98,10 @@ class KObject implements KObjectHandlable, KObjectServiceable
      *
      * When using mixin(), the calling object inherits the methods of the mixed in objects, in a LIFO order.
      *
-     * @@param   mixed    An object that implements KMixinInterface, KServiceIdentifier object
-     *                     or valid identifier string
-     * @param    array An optional associative array of configuration options
-     * @return  KObject
+     * @@param   mixed  $mixin  An object that implements KMixinInterface, KServiceIdentifier object
+     *                          or valid identifier string
+     * @param    array $config  An optional associative array of configuration options
+     * @return  KObjectInterface
      */
     public function mixin($mixin, $config = array())
     {
