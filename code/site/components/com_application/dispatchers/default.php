4d07d2d95705e0d245a9ef87e70117c316c79bde
Gergo Erdosi <gergo@timble.net>
2013-02-03 00:12:57 +0100

    Change link to getLink(). re #289

--- code/site/components/com_application/dispatchers/default.php
+++ code/site/components/com_application/dispatchers/default.php
@@ -171,7 +171,7 @@ class ComApplicationDispatcherDefault extends KDispatcherApplication
             //Redirect to the default menu item if the route is empty
             if(empty($route))
             {
-                $url = $pages->getHome()->link;
+                $url = $pages->getHome()->getLink();
                 $url->query['Itemid'] = $pages->getHome()->id;
 
                 $this->getRouter()->build($url);
@@ -418,7 +418,7 @@ class ComApplicationDispatcherDefault extends KDispatcherApplication
                         {
                             case 'pagelink':
                             case 'url' :
-                                $url = $page->link;
+                                $url = $page->getLink();
                                 break;
 
                             case 'separator':
@@ -426,7 +426,7 @@ class ComApplicationDispatcherDefault extends KDispatcherApplication
                                 break;
 
                             default:
-                                $url = $page->link;
+                                $url = $page->getLink();
                                 $url->query['Itemid'] = $page->id;
                                 $url = $this->getRouter()->build($url);
                                 break;
