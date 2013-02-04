baec501dc221acbe5341a6dfb95de0976bb25d75
Arunas Mazeika <arunas@timble.net>
2013-02-04 16:44:58 +0100

    Updated search module.
    re #301

--- code/site/components/com_articles/modules/search/html.php
+++ code/site/components/com_articles/modules/search/html.php
@@ -1,28 +1,30 @@
 <?php
 /**
- * @version		$Id: weblinks.php 1291 2011-05-16 22:13:45Z johanjanssens $
- * @package     Nooku_Server
- * @subpackage  Search
- * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		http://www.nooku.org
+ * @version        $Id$
+ * @package        Nooku_Server
+ * @subpackage     Search
+ * @copyright      Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
  */
 
 /**
- * Search Module Html Class
+ * Articles Search Module Html Class
  *
- * @author    	Arunas Mazeika <http://nooku.assembla.com/profile/amazeika>
- * @package     Nooku_Server
- * @subpackage  Search
+ * @author        Arunas Mazeika <http://nooku.assembla.com/profile/arunasmazeika>
+ * @package       Nooku_Server
+ * @subpackage    Search
  */
-class ComSearchModuleSearchHtml extends ComDefaultModuleDefaultHtml
+class ComArticlesModuleSearchHtml extends ComDefaultModuleDefaultHtml
 {
-	public function display()
-	{
-		$this->form_class  = $this->module->params->get('form_class');
-		$this->input_class = $this->module->params->get('input_class');
-		$this->placeholder = $this->module->params->get('placeholder', JText::_('search'));
+    public function display()
+    {
+        $this->form_class   = $this->module->params->get('form_class', 'form-search');
+        $this->input_class  = $this->module->params->get('input_class', 'span2 search-query');
+        $this->button_class = $this->module->params->get('button_class', 'btn');
+        $this->placeholder  = $this->module->params->get('placeholder', 'Search articles');
+        $this->item_id      = $this->module->params->get('item_id', null);
 
-		return parent::display();
-	}
+        return parent::display();
+    }
 }
--- code/site/components/com_articles/modules/search/tmpl/default.php
+++ code/site/components/com_articles/modules/search/tmpl/default.php
@@ -1,14 +1,20 @@
 <?
 /**
- * @version		$Id: form.php 1294 2011-05-16 22:57:57Z johanjanssens $
- * @package     Nooku_Server
- * @subpackage  Search
- * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		http://www.nooku.org
+ * @version        $Id$
+ * @package        Nooku_Server
+ * @subpackage     Search
+ * @copyright      Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
  */
 ?>
 
-<form action="<?= @route('option=com_search'); ?>" method="get" <?= $form_class ? 'class="'.$form_class.'"' : '' ?>>
-	<input name="term" <?= $input_class ? 'class="'.$input_class.'"' : '' ?> type="text" placeholder="<?= $placeholder ?>" />
+<? if ($item_id): ?>
+<form action="<?=@route('option=com_articles&view=articles&Itemid=' . $item_id)?>" method="get" class="<?=$form_class?>">
+    <div class="input-append">
+        <input id="searchword" name="searchword" class="<?=$input_class?>" type="text" value=""
+               placeholder="<?=@text($placeholder)?>"/>
+        <button type="submit" class="<?=$button_class?>"><i class="icon-search"></i></button>
+    </div>
 </form>
+<? endif; ?>
