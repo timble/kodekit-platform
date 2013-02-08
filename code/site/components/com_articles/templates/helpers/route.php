a72addf7fd645aa9750f920baf0386c3ceb223e8
Johan Janssens <johan@timble.net>
2013-02-08 01:12:07 +0100

    KView::getRoute() now accepts arrays too.

--- code/site/components/com_articles/templates/helpers/route.php
+++ code/site/components/com_articles/templates/helpers/route.php
@@ -43,7 +43,7 @@ class ComArticlesTemplateHelperRoute extends ComDefaultTemplateHelperRoute
             $route['Itemid'] = $page->id;
         }
 
-		return $this->getTemplate()->getView()->getRoute(http_build_query($route, '', '&'));
+		return $this->getTemplate()->getView()->getRoute($route);
 	}
 
     public function category($config = array())
@@ -75,6 +75,6 @@ class ComArticlesTemplateHelperRoute extends ComDefaultTemplateHelperRoute
             $route['Itemid'] = $item->id;
         };
 
-        return $this->getTemplate()->getView()->getRoute(http_build_query($route, '', '&'));
+        return $this->getTemplate()->getView()->getRoute($route);
     }
 }
