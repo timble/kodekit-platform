6f7b946d1af38f1f2e00df1e229bcb5f3c777b72
Johan Janssens <johan@timble.net>
2013-02-20 00:24:29 +0100

    Docblocks

--- code/site/component/pages/modules/custom/tmpl/default.php
+++ code/site/component/pages/modules/custom/tmpl/default.php
@@ -1,3 +1,13 @@
+<?
+/**
+ * @package     Nooku_Server
+ * @subpackage  Pages
+ * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
+?>
+
 <? if($show_title) : ?>
 <h3><?= $module->title ?></h3>
 <? endif ?>
