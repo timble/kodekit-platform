036302bf7f22d349748faee3d35b4a29bf4e0ec1
Stian Didriksen <stian@timble.net>
2013-02-20 15:53:40 +0100

    working on overlay fix

--- code/media/koowa/js/koowa.js
+++ code/media/koowa/js/koowa.js
@@ -578,27 +578,45 @@ Koowa.Overlay = new Class({
             if (this.options.evalScripts) {
             	scripts = element.getElements('script[type=text/javascript]');
                 scripts = scripts.filter(function(script) {
-                    if(!script.src) return false;
-                    if(document.head.getElement('script[src$='+script.src.replace(location.origin, '')+']')) return false;
+                    var src = script.src.replace(location.origin, '');
+                    if(script.src && document.head.getElement('script[src$='+src+']')) return false;
                     return true;
                 });
                 if(scripts.length) {
+                    //Remove existing domready events as they've fired by now anyway
+                    delete window.retrieve('events').domready;
+console.warn(scripts.length);
                     var self = this, script = scripts.shift(), loadScript = function(script){
-                        new Asset.javascript(script.src, {id: script.id, onload: function(){
-                            if(scripts.length) {
-                                script = scripts.shift();
-                                loadScript(script);
-                            } else {
-                                //Remove existing domready events as they've fired by now anyway
-                                delete window.retrieve('events').domready;
-
-                                if(self._tmp_scripts) {
-                                    $exec(self._tmp_scripts);
+                        var options = {
+                            id: script.id,
+                            type: script.type,
+                            //text: script.innerText,
+                            onLoad: function(){
+                                console.error(scripts.length, script.src, script.innerText);
+                                if(scripts.length) {
+                                    tmp = scripts.shift();
+                                    loadScript(tmp);
+                                } else {
+                                    window.fireEvent('domready');
                                 }
-
-                                window.fireEvent('domready');
                             }
-                        }});
+                        };
+                        console.warn(options);
+                        if(script.innerText) {
+                            //$exec(script.innerText.trim());
+                        }
+                        if(script.src) {
+                            console.log('got src');
+                            try {
+                                new Asset.javascript(script.src, options);
+                            } catch(error)
+                            {
+                                console.error(error);
+                            }
+                        } else {
+                            options.onLoad();
+                        }
+
                     };
                     loadScript(script);
                 };
@@ -662,8 +680,9 @@ Koowa.Overlay = new Class({
     
     processScripts: function(text){
         if(this.options.evalScripts) {
-            var scripts, text = text.replace(/<script[^>]*>([\s\S]*?)<\/script>/gi, function(){
+            var i = 0, scripts, text = text.replace(/<script[^>]*>([\s\S]*?)<\/script>/gi, function(){
     			scripts += arguments[1] + '\n';
+                i++;
     			return '';
     		});
     		this._tmp_scripts = scripts;
