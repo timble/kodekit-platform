0931f9276846220150aa0426e0099901646c6377
Tom Janssens <tom@timble.net>
2013-02-26 16:13:00 +0100

    Extended the list view to show images as thumbnails and files as unordered list re #330

--- code/application/admin/component/attachments/views/attachments/templates/list.html.php
+++ code/application/admin/component/attachments/views/attachments/templates/list.html.php
@@ -1,11 +1,23 @@
 <? $list = (isset($row) && isset($table)) ? $attachments->find(array('row' => $row, 'table' => $table)) : $attachments ?>
 
 <? if(count($list)) : ?>
-<div id="attachments-attachments-list">
-    <ul class="attachments">
+    <ul class="thumbnails" style="padding-left:0;">
     <? foreach($list as $item) : ?>
-        <li><a href="<?= @route('option=com_attachments&view=attachment&format=file&id='.$item->id) ?>"><?= @escape($item->name) ?></a></li>
+    	<? if($item->file->isImage()) : ?>
+    	<li style="width: 100px;">
+	    	<a class="modal thumbnail" href="<?= @route('view=attachment&format=file&id='.$item->id) ?>" rel="{handler: 'image'}">
+	    	   <img src="<?= $item->thumbnail->thumbnail ?>" />
+	    	</a>
+    	</li>
+    	<? endif ?>
+    <? endforeach ?>
+	</ul>
+    
+    <ul>
+    <? foreach($list as $item) : ?>        
+    	<? if(!$item->file->isImage()) : ?>
+    	<li><a href="<?= @route('view=attachment&format=file&id='.$item->id) ?>"><?= @escape($item->name) ?></a> </li>
+    	<? endif ?>
     <? endforeach ?>
     </ul>
-   </div>
 <? endif ?>
