ee258225d9aca5675d71251321369d0aebb4907a
Johan Janssens <johan@timble.net>
2013-03-01 16:46:06 +0100

    Use @url() template alias.

--- application/admin/component/application/views/page/templates/default_head.html.php
+++ application/admin/component/application/views/page/templates/default_head.html.php
@@ -9,7 +9,7 @@
 ?>
 
 <head>
-    <base href="<?= @service('request')->getUrl(); ?>" />
+    <base href="<?= @url(); ?>" />
     <title><?= @escape(@service('application')->getCfg('sitename' )). ' - ' .@text( 'Administration')  ?></title>
     <meta content="text/html; charset=utf-8" http-equiv="content-type"  />
     <meta content="chrome=1" http-equiv="X-UA-Compatible" />
--- application/site/component/application/views/page/templates/default_head.html.php
+++ application/site/component/application/views/page/templates/default_head.html.php
@@ -9,7 +9,7 @@
 ?>
 
 <head>
-<base href="<?= @service('request')->getUrl(); ?>" />
+<base href="<?= @url(); ?>" />
 <title><?= @escape(@service('application')->getCfg('sitename' )). ' - ' .@text( 'Administration')  ?></title>
 <meta content="text/html; charset=utf-8" http-equiv="content-type"  />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
