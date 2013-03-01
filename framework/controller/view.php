87a78a1ef3c3ef0746aa572d2a70daac63e7cf6a
Johan Janssens <johan@timble.net>
2013-03-01 17:30:24 +0100

    re #474 : Remove USER and PASS from the base view URL.

--- framework/controller/view.php
+++ framework/controller/view.php
@@ -86,7 +86,7 @@ abstract class KControllerView extends KControllerAbstract
 			//Create the view
 			$config = array(
                 'media_url' => $this->getService('request')->getBaseUrl()->getPath().'/media',
-			    'base_url'	=> $this->getService('request')->getUrl()->toString(KHttpUrl::BASE | KHttpUrl::BASE),
+			    'base_url'	=> $this->getService('request')->getUrl()->toString(KHttpUrl::BASE ^ KHttpUrl::USER ^ KHttpUrl::PASS),
                 'layout'    => $this->getRequest()->getQuery()->get('layout', 'alpha')
 			);
 
