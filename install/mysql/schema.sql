9b4c0f85940a8aa5bc4c63413c68640433eb1d1c
Tom Janssens <tom@timble.net>
2013-03-04 17:52:48 +0100

    Added table column to #__terms re #333

--- install/mysql/schema.sql
+++ install/mysql/schema.sql
@@ -421,19 +421,21 @@ CREATE TABLE `#__pages_modules` (
 --
 
 CREATE TABLE `#__terms` (
-	`terms_term_id` bigint(20) unsigned NOT NULL auto_increment,
-	`title` VARCHAR( 255 ) NOT NULL,
-	`slug` VARCHAR( 255 ) NOT NULL,
-	`params` text NOT NULL,
-	`created_by` int(10) unsigned DEFAULT NULL,
-    `created_on` datetime DEFAULT NULL,
-    `modified_by` int(10) unsigned DEFAULT NULL,
-    `modified_on` datetime DEFAULT NULL,
-    `locked_by` int(10) unsigned DEFAULT NULL,
-    `locked_on` datetime DEFAULT NULL,
-	PRIMARY KEY ( `terms_term_id` ) ,
-	UNIQUE KEY ( `slug` ),
-	UNIQUE KEY ( `title` )
+  `terms_term_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `table` varchar(50) NOT NULL,
+  `created_by` int(10) unsigned DEFAULT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `modified_by` int(10) unsigned DEFAULT NULL,
+  `modified_on` datetime DEFAULT NULL,
+  `locked_by` int(10) unsigned DEFAULT NULL,
+  `locked_on` datetime DEFAULT NULL,
+  `params` text NOT NULL,
+  PRIMARY KEY (`terms_term_id`),
+  UNIQUE KEY `slug` (`slug`),
+  UNIQUE KEY `title` (`title`),
+  KEY `table` (`table`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
