ff37143b2630b6eae11f4cefe09f08362139ecab
Johan Janssens <johan@timble.net>
2013-03-26 04:17:25 +0100

    re #284 : Moved BaseControllerPermissionDefault to ApplicationControllerPermissionDefault

--- application/site/component/contacts/controllers/permissions/message.php
+++ application/site/component/contacts/controllers/permissions/message.php
@@ -16,7 +16,7 @@ use Nooku\Framework;
  * @package     Nooku_Server
  * @subpackage  Contacts
  */
-class ContactsControllerPermissionMessage extends BaseControllerPermissionDefault
+class ContactsControllerPermissionMessage extends ApplicationControllerPermissionDefault
 {
     public function canAdd()
     {
