3bae5e4e8d98d39294d36f9643a915432c3d88c4
Tom Janssens <tom@timble.net>
2013-03-31 23:58:03 +0200

    Improved markup and styling for comments rendering re #347

--- application/site/component/comments/view/comment/templates/form.html.php
+++ application/site/component/comments/view/comment/templates/form.html.php
@@ -2,7 +2,6 @@
     <input type="hidden" name="row" value="<?= $state->row ?>" />
     <input type="hidden" name="table" value="<?= $state->table ?>" />
 
-    <textarea type="text" name="text" placeholder="<?= @text('Add new comment here ...') ?>" id="new-comment-text"></textarea>
-    <br />
-    <input class="button" type="submit" value="<?= @text('Submit') ?>"/>
+    <textarea type="text" name="text" placeholder="<?= @text('Add new comment here ...') ?>" id="new-comment-text" class="input-block-level"></textarea>
+    <input class="btn btn-primary" type="submit" value="<?= @text('Comment') ?>"/>
 </form>
