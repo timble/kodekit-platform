a5abea3356f6fc10de9c0b67d4698c8e56fd5f02
Johan Janssens <johan@timble.net>
2013-04-26 03:08:14 +0200

    Fixed typo

--- component/files/dispatcher.php
+++ component/files/dispatcher.php
@@ -21,7 +21,7 @@ use Nooku\Library;
 
 class Dispatcher extends Library\DispatcherComponent
 {
-	public function __construct(Library\Config $config)
+	public function __construct(Library\ObjectConfig $config)
 	{
 		parent::__construct($config);
 	
