f3a173f9d054ecfbd4c4ceb20626a3fb5a236f9e
Johan Janssens <johan@timble.net>
2013-04-29 12:32:15 +0200

    Docblocks

--- application/site/component/users/controller/behavior/resettable.php
+++ application/site/component/users/controller/behavior/resettable.php
@@ -13,9 +13,11 @@ class UsersControllerBehaviorResettable extends Users\ControllerBehaviorResettab
 {
     protected function _afterControllerToken(Library\CommandContext $context)
     {
-        if ($context->result) {
+        if ($context->result)
+        {
             $url = $this->getObject('application.pages')->getHome()->getLink();
             $this->getObject('application')->getRouter()->build($url);
+
             $context->response->setRedirect($url);
             //@TODO : Set message in session
             //$context->response->setRedirect($url, \JText::_('CONFIRMATION_EMAIL_SENT'));
