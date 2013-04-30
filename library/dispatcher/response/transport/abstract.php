d3698edaba5aae446d8e3a8137e911af1c7d6354
Johan Janssens <johan@timble.net>
2013-04-30 18:07:58 +0200

    Docblocks

--- library/dispatcher/response/transport/abstract.php
+++ library/dispatcher/response/transport/abstract.php
@@ -28,7 +28,7 @@ abstract class DispatcherResponseTransportAbstract extends Object implements Dis
     /**
      * Constructor.
      *
-     * @param 	object 	An optional ObjectConfig object with configuration options.
+     * @param ObjectConfig $config 	An optional ObjectConfig object with configuration options.
      */
     public function __construct(ObjectConfig $config)
     {
@@ -57,7 +57,7 @@ abstract class DispatcherResponseTransportAbstract extends Object implements Dis
      *
      * Called from {@link __construct()} as a first step of object instantiation.
      *
-     * @param 	object 	An optional ObjectConfig object with configuration options.
+     * @param 	ObjectConfig $config 	An optional ObjectConfig object with configuration options.
      * @return 	void
      */
     protected function _initialize(ObjectConfig $config)
