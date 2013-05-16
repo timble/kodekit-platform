fb0bf9d30a2fb770e1b9bc0981c653babfa369ea
Arunas Mazeika <arunas@timble.net>
2013-05-16 11:14:30 +0200

    Behaviors cleanup. Also added/improved user error reporting and redirects.
    re #352

--- application/site/component/users/controller/behavior/resettable.php
+++ application/site/component/users/controller/behavior/resettable.php
@@ -11,15 +11,45 @@ use Nooku\Library, Nooku\Component\Users;
 
 class UsersControllerBehaviorResettable extends Users\ControllerBehaviorResettable
 {
+    protected function _beforeControllerRead(Library\CommandContext $context)
+    {
+        if ($token = $context->request->query->get('token', $this->_filter))
+        {
+            // Push the token to the view.
+            $this->getView()->token = $token;
+        }
+    }
+
     protected function _afterControllerToken(Library\CommandContext $context)
+    {
+        $user = $context->user;
+        if (!$context->result)
+        {
+            $user->addFlashMessage(\JText::_('ERROR_SENDING_CONFIRMATION_EMAIL'), 'error');
+            $url = $context->request->getReferrer();
+        }
+        else
+        {
+            $user->addFlashMessage(\JText::_('CONFIRMATION_EMAIL_SUCCESS'));
+            $url = $this->getObject('application.pages')->getHome()->getLink();
+            $this->getObject('application')->getRouter()->build($url);
+        }
+        $context->response->setRedirect($url);
+    }
+
+    protected function _afterControllerReset(Library\CommandContext $context)
     {
         if ($context->result)
         {
+            $context->user->addFlashMessage(JText::_('PASSWORD_RESET_SUCCESS'));
             $url = $this->getObject('application.pages')->getHome()->getLink();
             $this->getObject('application')->getRouter()->build($url);
-
-            $context->user->addFlashMessage(\JText::_('CONFIRMATION_EMAIL_SENT'));
             $context->response->setRedirect($url);
+        }
+        else
+        {
+            $context->user->addFlashMessage($context->error, 'error');
+            $context->response->setRedirect($context->request->getReferrer());
         }
     }
 }
