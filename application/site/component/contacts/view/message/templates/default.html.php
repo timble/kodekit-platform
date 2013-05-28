1bc3723018a02113536b8fa6ba509f34a560335c
Arunas Mazeika <arunas@timble.net>
2013-05-28 17:22:01 +0200

    Added captcha widget. Form fields are populated if captcha data is made available.
    re #346

--- application/site/component/contacts/view/message/templates/default.html.php
+++ application/site/component/contacts/view/message/templates/default.html.php
@@ -11,6 +11,12 @@
 <?=@helper('behavior.mootools')?>
 <?=@helper('behavior.validator')?>
 
+<script type="text/javascript">
+    window.onDomReady(function() {
+        $('recaptcha_response_field').addClass('required');
+    });
+</script>
+
 <form action="<?=  @helper('route.message', array('row' => $contact, 'category' => $category->getSlug())) ?>" method="post" name="emailForm" class="-koowa-form form-horizontal">
     <input type="hidden" name="_action" value="add" />
     <div class="control-group">
@@ -18,7 +24,7 @@
             <?= @text( 'Enter your name' );?>:
         </label>
         <div class="controls">
-            <input type="text" name="name" value="" class="required" required />
+            <input type="text" name="name" value="<?=isset($captcha_data)?@escape($captcha_data->name):''?>" class="required" required />
         </div>
     </div>
     <div class="control-group">
@@ -26,7 +32,7 @@
             <?= @text( 'Email address' );?>:
         </label>
         <div class="controls">
-            <input type="email" name="email" value="" class="required validate-email" maxlength="100" required />
+            <input type="email" name="email" value="<?=isset($captcha_data)?@escape($captcha_data->email):''?>" class="required validate-email" maxlength="100" required />
         </div>
     </div>
     <div class="control-group">
@@ -34,7 +40,7 @@
             <?= @text( 'Message subject' );?>:
         </label>
         <div class="controls">
-            <input type="text" name="subject" value="" required />
+            <input type="text" name="subject" value="<?=isset($captcha_data)?@escape($captcha_data->subject):''?>" required />
         </div>
     </div>
     <div class="control-group">
@@ -42,14 +48,23 @@
             <?= @text( 'Enter your message' );?>:
         </label>
         <div class="controls">
-            <textarea rows="10" name="text" class="required" required></textarea>
+            <textarea rows="10" name="text" class="required" required><?=isset($captcha_data)?$captcha_data->text:''?></textarea>
         </div>
     </div>
     <div class="control-group">
         <div class="controls">
             <label class="checkbox" for="email_copy">
-                <input type="checkbox" name="email_copy" value="1" /> <?= @text( 'EMAIL_A_COPY' ); ?>
+                <input <?=isset($captcha_data) && $captcha_data->email_copy?'checked':''?> type="checkbox" name="email_copy" value="1" /> <?= @text( 'EMAIL_A_COPY' ); ?>
             </label>
+        </div>
+    </div>
+
+    <div class="control-group">
+        <label class="control-label" for="recaptcha_challenge_field">
+            <?=@text('Verification Code')?>
+        </label>
+        <div class="controls">
+            <?=@helper('com:users.captcha.render', array('attribs' => array('class' => 'required')))?>
         </div>
     </div>
     
