5ef0a8efdc8ce5ec585f9ef1c97d017c00a0e913
Johan Janssens <johan@timble.net>
2013-06-22 00:47:10 +0200

    Code cleanup and docblocks

--- application/admin/component/articles/controller/attachment.php
+++ application/admin/component/articles/controller/attachment.php
@@ -1,7 +1,21 @@
 <?php
+/**
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
 
 use Nooku\Library;
 
+/**
+ * Attachment Controller Class
+ *
+ * @author    	Tom Janssens <http://nooku.assembla.com/profile/tomjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ */
 class ArticlesControllerAttachment extends AttachmentsControllerAttachment
 {
     public function __construct(Library\ObjectConfig $config)
@@ -9,17 +23,5 @@ class ArticlesControllerAttachment extends AttachmentsControllerAttachment
         parent::__construct($config);
 
         $this->getModel()->getTable()->attachBehavior('com:articles.database.behavior.assignable');
-    }
-
-    protected function _initialize(Library\ObjectConfig $config)
-    {
-        $config->append(array(
-            'model'   => 'com:attachments.model.attachments',
-            'request' => array(
-                'view' => 'attachment'
-            )
-        ));
-
-        parent::_initialize($config);
     }
 }
