89e108e1cbec5ff038d0307c31e23c9dd269a3d7
Johan Janssens <johan@timble.net>
2013-06-25 03:54:17 +0200

    re #378 : Removed unneeded hidden properties.

--- application/admin/component/terms/view/term/templates/default.html.php
+++ application/admin/component/terms/view/term/templates/default.html.php
@@ -18,8 +18,6 @@
 </ktml:module>
 
 <form action="" method="post" class="-koowa-form" id="term-form">
-    <input type="hidden" name="table" value="<?= $state->table ?>" />
-    
     <div class="main">
 		<div class="title">
 			<input class="required" type="text" name="title" maxlength="255" value="<?= $term->title; ?>" placeholder="<?= @text( 'Title' ); ?>" />
