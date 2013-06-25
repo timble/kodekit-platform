4c1610aa65abc7adade901fd94d38c3b6b2c52c1
Tom Janssens <tom@timble.net>
2013-06-25 17:16:52 +0200

    Customised standard toolbar re #386

--- component/wysiwyg/resources/media/ckeditor/config.js
+++ component/wysiwyg/resources/media/ckeditor/config.js
@@ -60,23 +60,18 @@ CKEDITOR.editorConfig = function( config ) {
 
     config.toolbar_standard =
         [
-            { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-            { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Scayt' ] },
-            { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
-            { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar' ] },
-            { name: 'tools', items: [ 'Maximize' ] },
-            { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source' ] },
-            { name: 'others', items: [ '-' ] },
-            '/',
+            { name: 'styles', items: [ 'Format' ] },
+            { name: 'others', items: [ 'readmore' ] },
             { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
-            { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ] },
-            { name: 'styles', items: [ 'Styles', 'Format' ] },
-            { name: 'about', items: [ 'readmore' ] }
+            { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent' ] },
+            { name: 'links', items: [ 'Link', 'Unlink' ] },
+            { name: 'insert', items: [ 'Image', 'Table' ] },
+            { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'PasteText', '-', 'Undo', 'Redo' ] },
+            { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Scayt' ] },
+            { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source' ] }
         ];
     config.toolbar_title =
         [
             ['Undo','Redo']
         ];
-
-
 };
