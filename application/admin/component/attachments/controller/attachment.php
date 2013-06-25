2d51e6381d5cc892ca372553abd7335b06cd0629
Johan Janssens <johan@timble.net>
2013-06-25 03:45:13 +0200

    re #378 : Moved generic attachement controller code to Attachments\ControllerAttachment.

--- application/admin/component/attachments/controller/attachment.php
+++ application/admin/component/attachments/controller/attachment.php
@@ -1,32 +1,23 @@
 <?php
 /**
  * @package     Nooku_Server
- * @subpackage  Attachments
+ * @subpackage  Categories
  * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
  * @link		http://www.nooku.org
  */
 
 use Nooku\Library;
+use Nooku\Component\Attachments;
 
 /**
- * Attachment Controller Class
+ * Attachments Controller Class
  *
  * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @package     Nooku_Server
  * @subpackage  Attachments
  */
-class AttachmentsControllerAttachment extends Library\ControllerModel
+class AttachmentsControllerAttachment extends Attachments\ControllerAttachment
 {
-	protected function _initialize(Library\ObjectConfig $config)
-	{
-		$config->append(array(
-            'model'   => 'com:attachments.model.attachments',
-			'request' => array(
-				'view' => 'attachments'
-			)
-		));
-		
-		parent::_initialize($config);
-	}
+
 }
--- application/site/component/attachments/controller/attachment.php
+++ application/site/component/attachments/controller/attachment.php
@@ -1,20 +1,23 @@
 <?php
+/**
+ * @package     Nooku_Server
+ * @subpackage  Categories
+ * @copyright	Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.nooku.org
+ */
 
 use Nooku\Library;
+use Nooku\Component\Attachments;
 
-class AttachmentsControllerAttachment extends Library\ControllerModel
+/**
+ * Attachments Controller Class
+ *
+ * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package     Nooku_Server
+ * @subpackage  Attachments
+ */
+abstract class AttachmentsControllerAttachment extends Attachments\ControllerAttachment
 {
-	protected function _initialize(Library\ObjectConfig $config)
-	{
-		$config->append(array(
-		    'model'   => 'com:attachments.model.attachments',
-			'request' => array(
-				'view' => 'attachment'
-			)
-		));
-		
-		parent::_initialize($config);
-		
-		$config->view = 'com:attachments.view.'.$config->request->view.'.'.$config->request->format;
-	}
+
 }
