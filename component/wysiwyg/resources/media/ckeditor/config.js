6a9aad51f83e8a7baaa783081f35346c41fdc2c8
Terry Visser <terry@timble.net>
2013-06-25 10:51:41 +0200

    Codemirror was not working. Fixed this re #386

--- component/wysiwyg/resources/media/ckeditor/config.js
+++ component/wysiwyg/resources/media/ckeditor/config.js
@@ -2,6 +2,10 @@
  * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
+(function() {
+    CKEDITOR.plugins.addExternal('readmore','nookuplugins/readmore/', 'plugin.js');
+})();
+
 
 CKEDITOR.editorConfig = function( config ) {
 
@@ -9,9 +13,26 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 
-    // Loading addition plugins
-    config.extraPlugins = 'codemirror';
-    config.customConfig = "nookuconfig.js";
+    config.extraPlugins = 'codemirror,readmore';
+    config.codemirror = {
+        theme: 'default',
+        lineNumbers: true,
+        lineWrapping: true,
+        matchBrackets: true,
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        enableSearchTools: true, // Whether or not to enable search tools, CTRL+F (Find), CTRL+SHIFT+F (Replace), CTRL+SHIFT+R (Replace All), CTRL+G (Find Next), CTRL+SHIFT+G (Find Previous)
+        enableCodeFolding: true,
+        enableCodeFormatting: true,
+        autoFormatOnStart: true,
+        autoFormatOnUncomment: true,
+        highlightActiveLine: true,
+        highlightMatches: true,
+        showFormatButton: true,
+        showCommentButton: true,
+        showUncommentButton: true
+    };
+
     config.allowedContent = true;
     config.toolbar_full =
         [
@@ -52,66 +73,6 @@ CKEDITOR.editorConfig = function( config ) {
             { name: 'about', items: [ 'readmore' ] }
         ];
 
-    // Default CodeMirror settings
-    config.codemirror = {
-
-        // Set this to the theme you wish to use (codemirror themes)
-        theme: 'default',
-
-        // Whether or not you want to show line numbers
-        lineNumbers: true,
-
-        // Whether or not you want to use line wrapping
-        lineWrapping: true,
-
-        // Whether or not you want to highlight matching braces
-        matchBrackets: true,
-
-        // Whether or not you want tags to automatically close themselves
-        autoCloseTags: true,
-
-        // Whether or not you want Brackets to automatically close themselves
-        autoCloseBrackets: true,
-
-        // Whether or not to enable search tools, CTRL+F (Find), CTRL+SHIFT+F (Replace), CTRL+SHIFT+R (Replace All), CTRL+G (Find Next), CTRL+SHIFT+G (Find Previous)
-        enableSearchTools: true,
-
-        // Whether or not you wish to enable code folding (requires 'lineNumbers' to be set to 'true')
-        enableCodeFolding: true,
-
-        // Whether or not to enable code formatting
-        enableCodeFormatting: true,
-
-        // Whether or not to automatically format code should be done when the editor is loaded
-        autoFormatOnStart: true,
-
-        // Whether or not to automatically format code should be done every time the source view is opened
-        autoFormatOnModeChange: true,
-
-        // Whether or not to automatically format code which has just been uncommented
-        autoFormatOnUncomment: true,
-
-        // Whether or not to highlight the currently active line
-        highlightActiveLine: true,
-
-        // Whether or not to show the search Code button on the toolbar
-        showSearchButton: true,
-
-        // Whether or not to highlight all matches of current word/selection
-        highlightMatches: true,
-
-        // Whether or not to show the format button on the toolbar
-        showFormatButton: true,
-
-        // Whether or not to show the comment button on the toolbar
-        showCommentButton: true,
-
-        // Whether or not to show the uncomment button on the toolbar
-        showUncommentButton: true,
-        // Whether or not to show the showAutoCompleteButton button on the toolbar
-        showAutoCompleteButton: true
-
-    };
 
 
 };
