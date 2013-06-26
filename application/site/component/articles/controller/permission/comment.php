9834ee8e3ebe3cf6ab5ac37c228e6e8a4f8d7bc4
Terry Visser <terry@timble.net>
2013-06-26 18:52:12 +0200

    Added permission for adding editing and deleting of comments re #389

--- application/site/component/articles/controller/permission/comment.php
+++ application/site/component/articles/controller/permission/comment.php
@@ -1,8 +1,22 @@
 <?php
 /**
- * User: terryvisser
- * Date: 26-06-13
- * Time: 15:59
- * File: comment.php
- **/
- defined( 'KOOWA' ) or die( 'Restricted Access' );
+ * @package        Nooku_Server
+ * @subpackage     Article
+ * @copyright      Copyright (C) 2009 - 2012 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           http://www.nooku.org
+ */
+
+use Nooku\Library;
+
+/**
+ * Comment Controller Permission Class
+ *
+ * @author     Terry Visser <http://nooku.assembla.com/profile/terryvisser>
+ * @package    Nooku_Server
+ * @subpackage Article
+ */
+class ArticlesControllerPermissionComment extends CommentsControllerPermissionComment
+{
+
+}
