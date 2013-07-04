557124bb74725b950ab3fad7db2955121fc28008
Tom Janssens <tom@timble.net>
2013-07-04 12:30:23 +0200

    Added date helper to com:articles to generate type:datetime input fields re #367

---
+++ application/admin/component/articles/template/helper/date.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ * @copyright   Copyright (C) 2011 - 2012 Timble CVBA and Contributors. (http://www.timble.net).
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        http://www.nooku.org
+ */
+
+use Nooku\Library;
+
+/**
+ * Date Template Helper
+ *
+ * @author      Tom Janssens <http://nooku.assembla.com/profile/tomjanssens>
+ * @category    Nooku
+ * @package     Nooku_Server
+ * @subpackage  Articles
+ */
+
+class ArticlesTemplateHelperDate extends Library\TemplateHelperDate
+{
+    /**
+     * Render a HTML5 date type field
+     *
+     * @param 	array 	$config An optional array with configuration options
+     * @return	string	Html
+     */
+    public function datetime($config = array())
+    {
+        $config = new Library\ObjectConfig($config);
+        $config->append(array(
+            'name'   => 'date',
+            'type'   => 'datetime-local'
+        ));
+
+        $value = null;
+        if($value = $config->row->{$config->name}) {
+            $value = gmdate('Y-m-d\TH:i:s', strtotime($value));
+        }
+
+        $html = '<input type="'.$config->type.'" name="'.$config->name.'" value="'.$value.'" />';
+
+        return $html;
+    }
+}
