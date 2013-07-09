92a545655bfc851285535f65293d0a298b1533d6
Tom Janssens <tom@timble.net>
2013-07-09 15:48:55 +0200

    Moved inline editing to form layout, otherwise you're not able to change metadata (published state, category, …) re #386

--- application/site/component/articles/view/article/templates/default.html.php
+++ application/site/component/articles/view/article/templates/default.html.php
@@ -9,7 +9,14 @@
 ?>
 <article <?= !$article->published ? 'class="article-unpublished"' : '' ?>>
     <div class="page-header">
-	    <h1 id="title" contenteditable="<?= $article->editable ? 'true':'false';?>"><?= $article->title ?></h1>
+	    <h1>
+            <?= $article->title ?>
+            <? if ($article->editable) : ?>
+                <a style="float: right;" class="btn btn-mini" href="<?= @helper('route.article', array('row' => $article, 'layout' => 'form')) ?>">
+                    <i class="icon-edit"></i>
+                </a>
+            <? endif; ?>
+        </h1>
 	    <?= @helper('date.timestamp', array('row' => $article, 'show_modify_date' => false)); ?>
 	    <? if (!$article->published) : ?>
 	    <span class="label label-info"><?= @text('Unpublished') ?></span>
@@ -23,54 +30,9 @@
         <img class="thumbnail" src="<?= $article->thumbnail ?>" align="right" style="margin:0 0 20px 20px;" />
     <? endif; ?>
 
-    <? if($article->fulltext) : ?>
-    <div id="introtext" class="article_introtext" contenteditable="<?= $article->editable ? 'true':'false';?>">
-        <?= $article->introtext ?>
-    </div>
-    <? else : ?>
-    <div id="introtext" contenteditable="<?= $article->editable ? 'true':'false';?>" >
-        <?= $article->introtext ?>
-    </div>
-    <? endif ?>
-
-    <div id="fulltext" contenteditable="<?= $article->editable?  'true':'false';?>">
+    <?= $article->introtext ?>
     <?= $article->fulltext ?>
-    </div>
-
 
     <?= @template('com:tags.view.tags.default.html') ?>
     <?= @template('com:attachments.view.attachments.default.html', array('attachments' => $attachments, 'exclude' => array($article->attachments_attachment_id))) ?>
 </article>
-
-<? if ($article->editable) : ?>
-    <script src="media://application/js/jquery.js" /></script>
-
-    <script src="media://ckeditor/ckeditor/ckeditor.js" />
-    <script type='text/javascript' language='javascript'>
-
-        CKEDITOR.on( 'instanceCreated', function( event ) {
-            var editor = event.editor,
-                element = editor.element;
-
-            if ( element.is( 'h1', 'h2', 'h3' ) || element.getAttribute( 'id' ) == 'taglist' ) {
-                editor.on( 'configLoaded', function() {
-                    editor.config.toolbar = 'title';
-                });
-            }else{
-                editor.on( 'configLoaded', function() {
-                    editor.config.toolbar = 'standard';
-                });
-            }
-            editor.on('blur', function (ev) {
-                jQuery.post('<?=@route();?>', {
-                    id: <?=$article->id;?>,
-                    introtext : CKEDITOR.instances.introtext.getData(),
-                    fulltext : CKEDITOR.instances.fulltext.getData(),
-                    title : CKEDITOR.instances.title.getData(),
-                    _token:'<?= @object('user')->getSession()->getToken() ?>'
-                });
-            });
-        });
-
-    </script>
-<? endif;?>
--- application/site/component/articles/view/article/templates/form.html.php
+++ application/site/component/articles/view/article/templates/form.html.php
@@ -19,14 +19,45 @@
     <?= @helper('com:base.toolbar.render', array('toolbar' => $toolbar));?>
 </div>
 
+<article <?= !$article->published ? 'class="article-unpublished"' : '' ?>>
+    <div class="page-header">
+        <h1 id="title" contenteditable="<?= $article->editable ? 'true':'false';?>"><?= $article->title ?></h1>
+        <?= @helper('date.timestamp', array('row' => $article, 'show_modify_date' => false)); ?>
+        <? if (!$article->published) : ?>
+            <span class="label label-info"><?= @text('Unpublished') ?></span>
+        <? endif ?>
+        <? if ($article->access) : ?>
+            <span class="label label-important"><?= @text('Registered') ?></span>
+        <? endif ?>
+    </div>
+
+    <? if($article->thumbnail): ?>
+        <img class="thumbnail" src="<?= $article->thumbnail ?>" align="right" style="margin:0 0 20px 20px;" />
+    <? endif; ?>
+
+    <? if($article->fulltext) : ?>
+        <div id="introtext" class="article_introtext" contenteditable="<?= $article->editable ? 'true':'false';?>">
+            <?= $article->introtext ?>
+        </div>
+    <? else : ?>
+        <div id="introtext" contenteditable="<?= $article->editable ? 'true':'false';?>" >
+            <?= $article->introtext ?>
+        </div>
+    <? endif ?>
+
+    <div id="fulltext" contenteditable="<?= $article->editable?  'true':'false';?>">
+        <?= $article->fulltext ?>
+    </div>
+
+    <?= @template('com:tags.view.tags.default.html') ?>
+    <?= @template('com:attachments.view.attachments.default.html', array('attachments' => $attachments, 'exclude' => array($article->image))) ?>
+</article>
+
+
 <form method="post" action="" class="-koowa-form form-horizontal">
     <input type="hidden" name="published" value="0" />
     <input type="hidden" name="access" value="0" />
-    
-    <fieldset>
-        <input class="input-block-level" type="text" name="title" maxlength="100" value="<? echo @escape($article->title); ?>" style="margin-bottom: 10px"/>
-        <?= @object('com:ckeditor.controller.editor')->render(array('name' => 'text', 'text' => $article->text,'toolbar' => 'standard')) ?>
-    </fieldset>
+
     <fieldset>
         <legend><?= @text('Publishing'); ?></legend>
         <div class="control-group">
@@ -61,3 +92,36 @@
         </div>
     </fieldset>
 </form>
+
+<? if ($article->editable) : ?>
+    <script src="media://application/js/jquery.js" /></script>
+
+    <script src="media://ckeditor/ckeditor/ckeditor.js" />
+        <script type='text/javascript' language='javascript'>
+
+        CKEDITOR.on( 'instanceCreated', function( event ) {
+            var editor = event.editor,
+                element = editor.element;
+
+            if ( element.is( 'h1', 'h2', 'h3' ) || element.getAttribute( 'id' ) == 'taglist' ) {
+                editor.on( 'configLoaded', function() {
+                    editor.config.toolbar = 'title';
+                });
+            }else{
+                editor.on( 'configLoaded', function() {
+                    editor.config.toolbar = 'standard';
+                });
+            }
+            editor.on('blur', function (ev) {
+                jQuery.post('<?=@route();?>', {
+                    id: <?=$article->id;?>,
+                    introtext : CKEDITOR.instances.introtext.getData(),
+                    fulltext : CKEDITOR.instances.fulltext.getData(),
+                    title : CKEDITOR.instances.title.getData(),
+                    _token:'<?= @object('user')->getSession()->getToken() ?>'
+                });
+            });
+        });
+
+    </script>
+<? endif;?>
