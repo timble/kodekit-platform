be8202b55801bc1c06ef847cfceb0d0216889c21
Tom Janssens <tom@timble.net>
2013-08-10 18:05:28 +0200

    Cleanup markup

--- application/site/component/categories/view/categories/templates/default.html.php
+++ application/site/component/categories/view/categories/templates/default.html.php
@@ -10,26 +10,24 @@
 
 <? foreach($categories as $category) : ?>
 <article>
-<div class="page-header">
-    <h1>
-        <a href="<?= @helper('route.category', array('row' => $category)) ?>">
-        	<?= @escape($category->title);?>
-        </a>
-    </h1>
-</div>
+    <div class="page-header">
+        <h1>
+            <a href="<?= @helper('route.category', array('row' => $category)) ?>">
+                <?= @escape($category->title);?>
+            </a>
+        </h1>
+    </div>
 
-<div class="clearfix">
     <? if($category->thumbnail) : ?>
         <a href="<?= @helper('route.category', array('row' => $category)) ?>">
             <img class="article__thumbnail" src="<?= $category->thumbnail ?>" />
         </a>
     <? endif ?>
-	
-	<? if ($category->description) : ?>
-	<p><?= $category->description; ?></p>
-	<? endif; ?>
-</div>
 
-<a href="<?= @helper('route.category', array('row' => $category)) ?>"><?= @text('Read more') ?></a>
+    <? if ($category->description) : ?>
+    <p><?= $category->description; ?></p>
+    <? endif; ?>
+
+    <a href="<?= @helper('route.category', array('row' => $category)) ?>"><?= @text('Read more') ?></a>
 </article>
 <? endforeach; ?>
