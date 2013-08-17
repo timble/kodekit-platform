162e3f53c50c4cedbba85a12dca27677d79b99c4
Tom Janssens <tom@timble.net>
2013-08-17 15:55:13 +0200

    Added HTML <header> tag re #367

--- application/site/component/articles/view/article/templates/default.html.php
+++ application/site/component/articles/view/article/templates/default.html.php
@@ -8,7 +8,7 @@
  */
 ?>
 <article <?= !$article->published ? 'class="article-unpublished"' : '' ?>>
-    <div class="page-header">
+    <header>
 	    <? if (@object('component')->getController()->canEdit()) : ?>
 	    <a style="float: right;" class="btn" href="<?= @helper('route.article', array('row' => $article, 'layout' => 'form')) ?>">
 	        <i class="icon-edit"></i>
@@ -22,7 +22,7 @@
 	    <? if ($article->access) : ?>
 	    <span class="label label-important"><?= @text('Registered') ?></span>
 	    <? endif ?>
-	</div>
+	</header>
 
     <?= @helper('com:attachments.image.thumbnail', array('row' => $article)) ?>
 
