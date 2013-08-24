f273fdd8e8b7835d66d87ae0b5b173a0252750b8
Johan Janssens <johan@timble.net>
2013-08-25 00:41:18 +0200

    Implemented escape() and translate() methods. Translate acts as a adapter for JText for now.

--- library/template/abstract.php
+++ library/template/abstract.php
@@ -119,6 +119,31 @@ abstract class TemplateAbstract extends Object implements TemplateInterface
     }
 
     /**
+     * Escape a string
+     *
+     * By default the function uses htmlspecialchars to escape the string
+     *
+     * @param string $string String to to be escape
+     * @return string Escaped string
+     */
+    public function escape($string)
+    {
+        return htmlspecialchars($string);
+    }
+
+    /**
+     * Translates a string and handles parameter replacements
+     *
+     * @param string $string String to translate
+     * @param array  $parameters An array of parameters
+     * @return string Translated string
+     */
+    public function translate($string, array $parameters = array())
+    {
+        return \JText::_($string);
+    }
+
+    /**
      * Get the template data
      *
      * @return  mixed
