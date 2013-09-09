c893f936a0c9949575ca8c8b01ca1408741970a4
Tom Janssens <tom@timble.net>
2013-09-10 00:12:29 +0200

    Added editor re #347

--- application/admin/component/comments/view/comment/templates/default.html.php
+++ application/admin/component/comments/view/comment/templates/default.html.php
@@ -8,13 +8,15 @@
  */
 ?>
 
-<div id="comments-comment-form">
-	<form action="<?= route('row='.@$state->row.'&table='.$state->table) ?>" method="post">
-        <input type="hidden" name="row" value="<?= $state->row ?>" />
-        <input type="hidden" name="table" value="<?= $state->table ?>" />
+<script src="assets://js/koowa.js" />
 
-        <textarea type="text" name="text" placeholder="<?= translate('Add new comment here ...') ?>" id="new-comment-text"></textarea>
-        <br />
-        <input class="button" type="submit" value="<?= translate('Submit') ?>"/>
-    </form>
-</div>
+<ktml:module position="actionbar">
+    <ktml:toolbar type="actionbar">
+</ktml:module>
+
+<form action="" method="post" id="comment-form" class="-koowa-form">
+    <input type="hidden" name="row" value="<?= $state->row ?>" />
+    <input type="hidden" name="table" value="<?= $state->table ?>" />
+
+    <?= object('com:ckeditor.controller.editor')->render(array('name' => 'text', 'toolbar' => 'basic', 'text' => $comment->text)) ?>
+</form>
