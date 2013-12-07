f205643325cfba6b4d89b703cf9fd22ff707857c
Johan Janssens <johan@timble.net>
2013-12-07 20:11:47 +0100

    Fixed typo

--- component/categories/database/behavior/cascadable.php
+++ component/categories/database/behavior/cascadable.php
@@ -19,7 +19,7 @@ use Nooku\Library;
  */
 class DatabaseBehaviorCascadable extends Library\DatabaseBehaviorAbstract
 {
-    protected function _beforeDelete(Library\ControllerContext $context)
+    protected function _beforeDelete(Library\DatabaseContextInterface $context)
     {
         $result = true;
 
