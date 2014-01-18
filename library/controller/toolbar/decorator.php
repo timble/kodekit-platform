e60a5ecf93dc24e401aa30a989d42ac5fe6aa484
Johan Janssens <johan@timble.net>
2014-01-18 04:23:30 +0100

    Renamed ControllerToolbarInterface::attachToolbar() to addToolbar() and detachToolbar() to removeToolbar().  CommandInvokerInterface::execute() function now accepts as CommandInterface object as it's first argument. The command name is passed in the command object and can be retrieved using the KCommandInterface::getName() function.

--- library/controller/toolbar/decorator.php
+++ library/controller/toolbar/decorator.php
@@ -23,22 +23,18 @@ abstract class ControllerToolbarDecorator extends ObjectDecorator implements Con
      * This function translates the command name to a command handler function of the format '_before[Command]'
      * or '_after[Command]. Command handler functions should be declared protected.
      *
-     * @param 	string           $name	    The command name
      * @param 	Command  $context 	The command context
      * @return 	boolean Always returns TRUE
      */
-    final public function execute($name, Command $context)
+    final public function execute(CommandInterface $command)
     {
-        $identifier = $context->getSubject()->getIdentifier()->toArray();
-        $type = array_shift($identifier['path']);
-
-        $parts  = explode('.', $name);
+        $parts  = explode('.', $command->getName());
         $method = '_'.$parts[0].ucfirst($parts[1]);
 
         if(method_exists($this, $method)) {
-            $this->$method($context);
+            $this->$method($command);
         } else {
-            $this->getDelegate()->execute($name, $context);
+            $this->getDelegate()->execute($command);
         }
 
         return true;
@@ -63,8 +59,8 @@ abstract class ControllerToolbarDecorator extends ObjectDecorator implements Con
         {
             if($controller->hasToolbar($delegate->getType()))
             {
-                $controller->detachToolbar($delegate);
-                $controller->attachToolbar($this);
+                $controller->removeToolbar($delegate);
+                $controller->addToolbar($this);
             }
         }
 
