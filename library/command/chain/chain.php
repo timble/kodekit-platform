badb3dd0476284fb5cda59ffa925743b0e2981db
Johan Janssens <johan@timble.net>
2014-01-20 00:52:59 +0100

    Refactored CommandChain. The command chain no longer extends from ObjectQueue but instead composes it. Renamed run() method to invokeCommand(). The invokation break condition is no longer passed through the invokeCommand method but is set in the command chain through the constructor. By default the break condition is 'false'. If no break condition is reached the invokeCommand method will return an array of the results of each command invoker called.

--- library/command/chain/chain.php
+++ library/command/chain/chain.php
@@ -12,21 +12,14 @@ namespace Nooku\Library;
 /**
  * Command Chain
  *
- * The command chain implements a double linked list. The command handle is used as the key. Each command can have a
- * priority, default priority is 3 The queue is ordered by priority, commands with a higher priority are called first.
+ * The command chain implements a queue. The command handle is used as the key. Each command can have a priority, default
+ * priority is 3 The queue is ordered by priority, commands with a higher priority are called first.
  *
  * @author  Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @package Nooku\Library\Command
  */
-class CommandChain extends ObjectQueue implements CommandChainInterface
+class CommandChain extends Object implements CommandChainInterface
 {
-    /**
-     * Enabled status of the chain
-     *
-     * @var boolean
-     */
-    protected $_enabled;
-
     /**
      * The chain stack
      *
@@ -37,17 +30,44 @@ class CommandChain extends ObjectQueue implements CommandChainInterface
     private $__stack;
 
     /**
+     * The invoker queue
+     *
+     * @var ObjectQueue
+     */
+    private $__queue;
+
+    /**
+     * Enabled status of the chain
+     *
+     * @var boolean
+     */
+    private $__enabled;
+
+    /**
+     * The chain break condition
+     *
+     * @var boolean
+     */
+    protected $_condition;
+
+    /**
      * Constructor
      *
-     * @param ObjectConfig|null $config  An optional ObjectConfig object with configuration options
+     * @param ObjectConfig  $config  An optional KObjectConfig object with configuration options
      * @return CommandChain
      */
     public function __construct(ObjectConfig $config)
     {
         parent::__construct($config);
 
-        $this->_enabled = (boolean) $config->enabled;
-        $this->__stack = $this->getObject('lib:object.stack');
+        //Set the chain enabled state
+        $this->__enabled = (boolean) $config->enabled;
+
+        //Set the chain break condition
+        $this->_condition = $config->command_condition;
+
+        $this->__stack = $this->getObject('koowa:object.stack');
+        $this->__queue = $this->getObject('koowa:object.queue');
     }
 
     /**
@@ -55,127 +75,27 @@ class CommandChain extends ObjectQueue implements CommandChainInterface
      *
      * Called from {@link __construct()} as a first step of object instantiation.
      *
-     * @param   ObjectConfig $object An optional ObjectConfig object with configuration options
-     * @return  void
+     * @param  ObjectConfig $config Configuration options
+     * @return void
      */
     protected function _initialize(ObjectConfig $config)
     {
         $config->append(array(
-            'enabled' => true,
+            'command_condition' => false,
+            'enabled'           => true
         ));
 
         parent::_initialize($config);
     }
 
     /**
-     * Attach a command invoker to the chain
-     *
-     * The priority parameter can be used to override the command priority while enqueueing the command.
-     *
-     * @param   CommandInvokerInterface  $invoker
-     * @param   integer                  $priority The command priority, usually between 1 (high priority) and 5 (lowest),
-     *                                        default is 3. If no priority is set, the command priority will be used
-     *                                        instead.
-     * @return CommandChain
-     * @throws \InvalidArgumentException if the object doesn't implement CommandInvokerInterface
-     */
-    public function enqueue(ObjectHandlable $invoker, $priority = null)
-    {
-        if (!$invoker instanceof CommandInvokerInterface) {
-            throw new \InvalidArgumentException('Invoker needs to implement CommandInvokerInterface');
-        }
-
-        $priority = is_int($priority) ? $priority : $invoker->getPriority();
-        return parent::enqueue($invoker, $priority);
-    }
-
-    /**
-     * Removes a command invoker from the queue
-     *
-     * @param   CommandInvokerInterface $invoker
-     * @return  boolean    TRUE on success FALSE on failure
-     * @throws  \InvalidArgumentException if the object implement CommandInvokerInterface
-     */
-    public function dequeue(ObjectHandlable $invoker)
-    {
-        if (!$invoker instanceof CommandInvokerInterface) {
-            throw new \InvalidArgumentException('Invoker needs to implement CommandInvokerInterface');
-        }
-
-        return parent::dequeue($invoker);
-    }
-
-    /**
-     * Check if the queue does contain a given command invoker
-     *
-     * @param  CommandInvokerInterface $invoker
-     * @return bool
-     * @throws  \InvalidArgumentException if the object implement CommandInvokerInterface
-     */
-    public function contains(ObjectHandlable $invoker)
-    {
-        if (!$invoker instanceof CommandInvokerInterface) {
-            throw new \InvalidArgumentException('Invoker needs to implement CommandInvokerInterface');
-        }
-
-        return parent::contains($invoker);
-    }
-
-    /**
-     * Run the commands in the chain
-     *
-     * If a command returns the 'break condition' the executing is halted. If no break condition is specified the
-     * command chain will pass the command invokers, regardless of the invoker result returned.
-     *
-     * @param   string  $name
-     * @param   Command $command
-     * @param   mixed   $condition The break condition
-     * @return  void|mixed If the chain breaks, returns the break condition. If the chain is not enabled will void
-     */
-    public function run($name, Command $command, $condition = null)
-    {
-        if ($this->isEnabled())
-        {
-            $this->__stack->push(clone $this);
-
-            foreach ($this->__stack->top() as $invoker)
-            {
-                if($condition === self::CONDITION_EXCEPTION)
-                {
-                    try
-                    {
-                        $invoker->execute($name, $command);
-                    }
-                    catch (CommandExceptionInvoker $e)
-                    {
-                        $this->__stack->pop();
-                        return $e;
-                    }
-                }
-                else
-                {
-                    $result = $invoker->execute($name, $command);
-
-                    if($condition && $result === $condition)
-                    {
-                        $this->__stack->pop();
-                        return $condition;
-                    }
-                }
-            }
-
-            $this->__stack->pop();
-        }
-    }
-
-    /**
      * Enable the chain
      *
-     * @return  CommandChain
+     * @return  $this
      */
     public function enable()
     {
-        $this->_enabled = true;
+        $this->__enabled = true;
         return $this;
     }
 
@@ -184,45 +104,124 @@ class CommandChain extends ObjectQueue implements CommandChainInterface
      *
      * If the chain is disabled running the chain will always return TRUE
      *
-     * @return  CommandChain
+     * @return  $this
      */
     public function disable()
     {
-        $this->_enabled = false;
+        $this->__enabled = false;
         return $this;
     }
 
     /**
-     * Set the priority of a command invoker
+     * Invoke a command by calling all registered invokers
      *
-     * @param CommandInvokerInterface $invoker
-     * @param integer $priority
-     * @return CommandChain
-     * @throws \InvalidArgumentException if the object doesn't implement CommandInvokerInterface
+     * If a command invoker returns the 'break condition' the executing is halted. If no break condition is specified the
+     * the command chain will execute all command invokers, regardless of the invoker result returned.
+     *
+     * @param  string|CommandInterface  $command    The command name or a KCommandInterface object
+     * @param  array|\Traversable       $attributes An associative array or a Traversable object
+     * @param  ObjectInterface          $subject    The command subject
+     * @return array|mixed Returns an array of the command results in FIFO order. If the chain breaks, and the break
+     *                     condition is not NULL returns the break condition instead.
      */
-    public function setPriority(ObjectHandlable $invoker, $priority)
+    public function invokeCommand($command, $attributes = null, $subject = null)
     {
-        if (!$invoker instanceof CommandInvokerInterface) {
-            throw new \InvalidArgumentException('Invoker needs to implement CommandInvokerInterface');
+        $result = array();
+
+        if ($this->isEnabled())
+        {
+            $this->__stack->push(clone $this->__queue);
+
+            //Make sure we have an command object
+            if (!$command instanceof CommandInterface)
+            {
+                if($attributes instanceof CommandInterface)
+                {
+                    $name    = $command;
+                    $command = $attributes;
+
+                    $command->setName($name);
+                }
+                else $command = new Command($command, $attributes, $subject);
+            }
+
+            foreach ($this->__stack->peek() as $invoker)
+            {
+                try {
+                    $result[] = $invoker->executeCommand($command, $this->_condition);
+                } catch (CommandExceptionInvoker $e) {
+                    $result[] = $e;
+                }
+
+                if($this->_condition !== null && current($result) === $this->_condition)
+                {
+                    $result = current($result);
+                    break;
+                }
+            }
+
+            $this->__stack->pop();
         }
 
-        return parent::setPriority($invoker, $priority);
+        return $result;
     }
 
     /**
-     * Get the priority of a command invoker
+     * Attach a command to the chain
      *
-     * @param  CommandInvokerInterface $invoker
-     * @return integer The command priority
-     * @throws \InvalidArgumentException if the object doesn't implement CommandInvokerInterface
+     * @param  CommandInvokerInterface  $invoker  The command invoker
+     * @return CommandChain
      */
-    public function getPriority(ObjectHandlable $invoker)
+    public function addInvoker(CommandInvokerInterface $invoker)
     {
-        if (!$invoker instanceof CommandInvokerInterface) {
-            throw new \InvalidArgumentException('Invoker needs to implement CommandInvokerInterface');
-        }
+        $this->__queue->enqueue($invoker, $invoker->getPriority());
+        return $this;
+    }
 
-        return parent::getPriority($invoker);
+    /**
+     * Removes a command from the chain
+     *
+     * @param CommandInvokerInterface  $invoker  The command invoker
+     * @return CommandChain
+     */
+    public function removeInvoker(CommandInvokerInterface $invoker)
+    {
+        $this->__queue->dequeue($invoker);
+        return $this;
+    }
+
+    /**
+     * Get the list of invokers enqueue in the chain
+     *
+     * @return ObjectQueue   An object queue containing the invokers
+     */
+    public function getInvokers()
+    {
+        return $this->__queue;
+    }
+
+    /**
+     * Set the priority of a command
+     *
+     * @param  CommandInvokerInterface $invoker   A command invoker
+     * @param integer                  $priority  The command priority
+     * @return CommandChain
+     */
+    public function setInvokerPriority(CommandInvokerInterface $invoker, $priority)
+    {
+        $this->__queue->setPriority($invoker, $priority);
+        return $this;
+    }
+
+    /**
+     * Get the priority of a command
+     *
+     * @param  CommandInvokerInterface $invoker A command invoker
+     * @return integer The command priority
+     */
+    public function getInvokerPriority(CommandInvokerInterface $invoker)
+    {
+        return $this->__queue->getPriority($invoker);
     }
 
     /**
@@ -232,6 +231,6 @@ class CommandChain extends ObjectQueue implements CommandChainInterface
      */
     public function isEnabled()
     {
-        return $this->_enabled;
+        return $this->__enabled;
     }
 }
--- library/command/chain/interface.php
+++ library/command/chain/interface.php
@@ -12,69 +12,19 @@ namespace Nooku\Library;
 /**
  * Command Chain Interface
  *
+ * The command chain implements a queue. The command handle is used as the key. Each command can have a priority, default
+ * priority is 3 The queue is ordered by priority, commands with a higher priority are called first.
+ *
  * @author  Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @package Nooku\Library\Command
  */
 interface CommandChainInterface
 {
     /**
-     * Break conditions
+     * Enable the chain
+     *
+     * @return  $this
      */
-    const CONDITION_FALSE     = false; //Stop when first invoker indicates that it has failed
-    const CONDITION_TRUE      = true;  //Stop when first invoker indicates that it has succeeded
-    const CONDITION_EXCEPTION = -1;    //Break when an invoker throws an CommandExceptionInvokerFailed exception
-
-    /**
-     * Run the commands in the chain
-     *
-     * If a command returns the 'break condition' the executing is halted. If no break condition is specified the
-     * command chain will pass the command invokers, regardless of the invoker result returned.
-     *
-     * @param   string  $name
-     * @param   Command $command
-     * @param   mixed   $condition The break condition
-     * @return  void|mixed If the chain breaks, returns the break condition. If the chain is not enabled will void
-     */
-    public function run($name, Command $command, $condition = null);
-
-    /**
-     * Attach a command invoker to the chain
-     *
-     * The priority parameter can be used to override the command priority while enqueueing the command.
-     *
-     * @param   CommandInvokerInterface   $invoker
-     * @param   integer   $priority The command priority, usually between 1 (high priority) and 5 (lowest),
-     *                              default is 3. If no priority is set, the command priority will be used
-     *                              instead.
-     * @return CommandChain
-     * @throws \InvalidArgumentException if the object doesn't implement CommandInvokerInterface
-     */
-    public function enqueue(ObjectHandlable $invoker, $priority = null);
-
-    /**
-     * Removes a command invoker from the chain
-     *
-     * @param   CommandInvokerInterface $invoker
-     * @return  boolean    TRUE on success FALSE on failure
-     * @throws  \InvalidArgumentException if the object implement CommandInvokerInterface
-     */
-    public function dequeue(ObjectHandlable $invoker);
-
-
-    /**
-     * Check if the queue does contain a given command invoker
-     *
-     * @param  CommandInvokerInterface $invoker
-     * @return bool
-     * @throws  \InvalidArgumentException if the object implement CommandInvokerInterface
-     */
-    public function contains(ObjectHandlable $invoker);
-
-   /**
-    * Enable the chain
-    *
-    * @return  CommandChain
-    */
     public function enable();
 
     /**
@@ -82,28 +32,55 @@ interface CommandChainInterface
      *
      * If the chain is disabled running the chain will always return TRUE
      *
-     * @return  CommandChain
+     * @return  $this
      */
     public function disable();
 
     /**
-     * Set the priority of a command
+     * Invoke a command by calling all registered invokers
      *
-     * @param CommandInvokerInterface $invoker
-     * @param integer           $priority
-     * @return CommandChain
-     * @throws \InvalidArgumentException if the object doesn't implement CommandInvokerInterface
+     * If a command invoker returns the 'break condition' the executing is halted. If no break condition is specified the
+     * the command chain will execute all command invokers, regardless of the invoker result returned.
+     *
+     * @param  string|CommandInterface  $command    The command name or a KCommandInterface object
+     * @param  array|\Traversable       $attributes An associative array or a Traversable object
+     * @param  ObjectInterface          $subject    The command subject
+     * @return array|mixed Returns an array of the command results in FIFO order. If the chain breaks, and the break
+     *                     condition is not NULL returns the break condition instead.
      */
-    public function setPriority(ObjectHandlable $invoker, $priority);
+    public function invokeCommand($command, $attributes = array(), $subject = null);
 
     /**
-     * Get the priority of a command
+     * Attach a command to the chain
      *
-     * @param  CommandInvokerInterface $invoker
-     * @return integer The command priority
-     * @throws \InvalidArgumentException if the object doesn't implement CommandInvokerInterface
+     * @param  CommandInvokerInterface  $invoker  The command invoker
+     * @return CommandChainInterface
      */
-    public function getPriority(ObjectHandlable $invoker);
+    public function addInvoker(CommandInvokerInterface $invoker);
+
+    /**
+     * Get the list of invokers enqueue in the chain
+     *
+     * @return ObjectQueue An object queue containing the invokers
+     */
+    public function getInvokers();
+
+    /**
+     * Set the priority of a command invoker
+     *
+     * @param CommandInvokerInterface $invoker   A command invoker
+     * @param integer                   $priority  The command priority
+     * @return CommandChain
+     */
+    public function setInvokerPriority(CommandInvokerInterface $invoker, $priority);
+
+    /**
+     * Get the priority of a command invoker
+     *
+     * @param  CommandInvokerInterface $invoker A command invoker
+     * @return integer The command priority
+     */
+    public function getInvokerPriority(CommandInvokerInterface $invoker);
 
     /**
      * Check of the command chain is enabled
