322df0e1a44278cc4e78a77dc430523c827b683d
Johan Janssens <johan@timble.net>
2014-01-20 01:52:13 +0100

    Refactored CommandInvoker. Renamed execute() method to executeCommand() and merged callback functionality now called command handlers. A command invoker now keeps a list of command handlers to call when it's being executed. Command handlers can be invoker methods (public and registered) or Closures. The chain's break condition is now also being passed when executing the invoker allowing to break execution of the different handlers if a handler returns the break condition. The invoker will return a list of the handler results if the execution was not halted, otherwise it will return the break condition.

--- library/command/invoker/interface.php
+++ library/command/invoker/interface.php
@@ -27,18 +27,51 @@ interface CommandInvokerInterface extends ObjectHandlable
     const PRIORITY_LOWEST  = 5;
 
     /**
-	 * Generic Command handler
-	 *
-	 * @param 	string 	       $name     The command name
-	 * @param 	Command $context  The command context
-	 * @return	boolean
-	 */
-	public function execute( $name, Command $context);
+     * Command handler
+     *
+     * @param  CommandInterface $command    The command
+     * @param  mixed            $condition  The break condition
+     * @return array|mixed Returns an array of the handler results in FIFO order. If a handler breaks and the break
+     *                     condition is not NULL returns the break condition.
+     */
+    public function executeCommand(CommandInterface $command, $condition = null);
 
-	/**
-	 * Get the priority of the command
-	 *
-	 * @return	integer The command priority
-	 */
-  	public function getPriority();
+    /**
+     * Add a command handler
+     *
+     * If the handler has already been added. It will not be re-added but parameters will be merged. This allows to
+     * change or add parameters for existing handlers.
+     *
+     * @param  	string           $command  The command name to register the handler for
+     * @param 	string|\Closure  $method   The name of the method or a Closure object
+     * @param   array|object     $params    An associative array of config parameters or a KObjectConfig object
+     * @throws  \InvalidArgumentException If the callback is not a callable
+     * @return  CommandInvokerAbstract
+     */
+    public function addCommandHandler($command, $method, $params = array());
+
+    /**
+     * Remove a command handler
+     *
+     * @param  	string	$command  The command to unregister the handler from
+     * @param 	string	$method   The name of the method to unregister
+     * @return  CommandInvokerAbstract
+     */
+    public function removeCommandHandler($command, $method);
+
+    /**
+     * Get the handlers for a command
+     *
+     * @param string $command   The command
+     * @return  array An array of command handlers
+     */
+    public function getCommandHandlers($command);
+
+    /**
+     * Get the priority of the invoker
+     *
+     * @return	integer The invoker priority
+     */
+    public function getPriority();
+
 }
