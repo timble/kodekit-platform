d9ddc5414cc8585458f84bd54e70d2424df45d39
Johan Janssens <johan@timble.net>
2014-01-21 05:37:35 +0100

    Fixed merge issue.

--- library/event/mixin.php
+++ library/event/mixin.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Framework - http://www.nooku.org
  *
- * @copyright	Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @copyright   Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        git://git.assembla.com/nooku-framework.git for the canonical source repository
  */
 
 namespace Nooku\Library;
@@ -12,20 +12,20 @@ namespace Nooku\Library;
 /**
  * Event Mixin
  *
- * Class can be used as a mixin in classes that want to implement a an event dispatcher and allow adding and removing
- * listeners.
+ * Class can be used as a mixin in classes that want to implement a an event publisher and allow adding and removing
+ * event listeners and subscribers.
  *
  * @author  Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
  * @package Nooku\Library\Event
  */
-class EventMixin extends ObjectMixinAbstract
+class EventMixin extends ObjectMixinAbstract implements EventMixinInterface
 {
     /**
-     * Event dispatcher object
+     * Event publisher object
      *
-     * @var EventDispatcherInterface
+     * @var EventPublisherInterface
      */
-    protected $_event_dispatcher;
+    private $__event_publisher;
 
     /**
      * List of event subscribers
@@ -33,32 +33,33 @@ class EventMixin extends ObjectMixinAbstract
      * Associative array of event subscribers, where key holds the subscriber identifier string
      * and the value is an identifier object.
      *
-     * @var    array
+     * @var  array
      */
-    protected $_event_subscribers = array();
+    private $__event_subscribers = array();
 
     /**
      * Object constructor
      *
-     * @param ObjectConfig $config  An optional ObjectConfig object with configuration options
+     * @param ObjectConfig $config An optional ObjectConfig object with configuration options
+     * @throws \InvalidArgumentException
      */
     public function __construct(ObjectConfig $config)
     {
         parent::__construct($config);
 
-        if (is_null($config->event_dispatcher)) {
-            throw new \InvalidArgumentException('event_dispatcher [EventDispatcherInterface] config option is required');
+        if (is_null($config->event_publisher)) {
+            throw new \InvalidArgumentException('event_publisher [EventPublisherInterface] config option is required');
         }
 
         //Set the event dispatcher
-        $this->_event_dispatcher = $config->event_dispatcher;
+        $this->__event_publisher = $config->event_publisher;
 
         //Add the event listeners
         foreach ($config->event_listeners as $event => $listener) {
             $this->addEventListener($event, $listener);
         }
 
-        //Add the event handlers
+        //Add the event subscribers
         $subscribers = (array) ObjectConfig::unbox($config->event_subscribers);
 
         foreach ($subscribers as $key => $value)
@@ -82,7 +83,7 @@ class EventMixin extends ObjectMixinAbstract
     protected function _initialize(ObjectConfig $config)
     {
         $config->append(array(
-            'event_dispatcher'  => 'event.dispatcher',
+            'event_publisher'   => 'event.publisher',
             'event_subscribers' => array(),
             'event_listeners'   => array(),
         ));
@@ -91,116 +92,126 @@ class EventMixin extends ObjectMixinAbstract
     }
 
     /**
-     * Get the event dispatcher
+     * Publish an event by calling all listeners that have registered to receive it.
      *
-     * @return  EventDispatcherInterface
+     * @param  string|EventInterface              $event      The event name or a KEventInterface object
+     * @param  array|\Traversable|EventInterface  $attributes An associative array, an object implementing the
+     *                                                        EventInterface or a Traversable object
+     * @param  mixed                              $target     The event target
+     * @throws \InvalidArgumentException  If the event is not a string or does not implement the KEventInterface
+     * @return null|EventInterface Returns the event object. If the chain is not enabled will return NULL.
      */
-    public function getEventDispatcher()
+    public function publishEvent($event, $attributes = array(), $target = null)
     {
-        if(!$this->_event_dispatcher instanceof EventDispatcherInterface)
-        {
-            $this->_event_dispatcher = $this->getObject($this->_event_dispatcher);
+        return $this->getEventPublisher()->publishEvent($event, $attributes, $target);
+    }
 
-            //Make sure the request implements ControllerRequestInterface
-            if(!$this->_event_dispatcher instanceof EventDispatcherInterface)
+    /**
+     * Get the event publisher
+     *
+     * @throws \UnexpectedValueException
+     * @return EventPublisherInterface
+     */
+    public function getEventPublisher()
+    {
+        if(!$this->__event_publisher instanceof EventPublisherInterface)
+        {
+            $this->__event_publisher = $this->getObject($this->__event_publisher);
+
+            if(!$this->__event_publisher instanceof EventPublisherInterface)
             {
                 throw new \UnexpectedValueException(
-                    'EventDispatcher: '.get_class($this->_event_dispatcher).' does not implement EventDispatcherInterface'
+                    'EventPublisher: '.get_class($this->__event_publisher).' does not implement KEventPublisherInterface'
                 );
             }
         }
 
-        return $this->_event_dispatcher;
+        return $this->__event_publisher;
     }
 
     /**
-     * Set the chain of command object
+     * Set the event publisher
      *
-     * @param   EventDispatcherInterface  $dispatcher An event dispatcher object
-     * @return  Object  The mixer object
+     * @param   EventPublisherInterface  $publisher An event publisher object
+     * @return  ObjectInterface  The mixer
      */
-    public function setEventDispatcher(EventDispatcherInterface $dispatcher)
+    public function setEventPublisher(EventPublisherInterface $publisher)
     {
-        $this->_event_dispatcher = $dispatcher;
+        $this->__event_publisher = $publisher;
         return $this->getMixer();
     }
 
     /**
      * Add an event listener
      *
-     * @param  string   $event     The event name
-     * @param  callable $listener  The listener
-     * @param  integer $priority   The event priority, usually between 1 (high priority) and 5 (lowest),
-     *                             default is 3. If no priority is set, the command priority will be used
-     *                             instead.
-     * @return  Object The mixer objects
+     * @param string|EventInterface  $event     The event name or a KEventInterface object
+     * @param callable               $listener  The listener
+     * @param integer                $priority  The event priority, usually between 1 (high priority) and 5 (lowest),
+     *                                           default is 3 (normal)
+     * @throws \InvalidArgumentException If the listener is not a callable
+     * @throws \InvalidArgumentException  If the event is not a string or does not implement the KEventInterface
+     * @return ObjectInterface The mixer
      */
-    public function addEventListener($event, $listener, $priority = Event::PRIORITY_NORMAL)
+    public function addEventListener($event, $listener, $priority = EventInterface::PRIORITY_NORMAL)
     {
-        $this->getEventDispatcher()->addEventListener($event, $listener, $priority);
+        $this->getEventPublisher()->addListener($event, $listener, $priority);
         return $this->getMixer();
     }
 
     /**
      * Remove an event listener
      *
-     * @param   string   $event     The event name
-     * @param   callable $listener  The listener
-     * @return  Object  The mixer object
+     * @param string|EventInterface  $event      The event name or a KEventInterface object
+     * @param callable                $listener  The listener
+     * @throws \InvalidArgumentException If the listener is not a callable
+     * @throws \InvalidArgumentException  If the event is not a string or does not implement the KEventInterface
+     * @return ObjectInterface The mixer
      */
     public function removeEventListener($event, $listener)
     {
-        $this->getEventDispatcher()->removeEventListener($event, $listener);
+        $this->getEventPublisher()->removeListener($event, $listener);
         return $this->getMixer();
     }
 
     /**
      * Add an event subscriber
      *
-     * @param   mixed  $subscriber An object that implements ObjectInterface, ObjectIdentifier object
-     *                            or valid identifier string
-     * @param  array  $config   An optional associative array of configuration settings
-     * @param  integer $priority The event priority, usually between 1 (high priority) and 5 (lowest),
-     *                 default is 3. If no priority is set, the command priority will be used
-     *                 instead.
-     * @return  Object    The mixer object
+     * @param mixed  $subscriber An object that implements ObjectInterface, ObjectIdentifier object
+     *                           or valid identifier string
+     * @param array  $config     An optional associative array of configuration options
+     * @return ObjectInterface The mixer
      */
-    public function addEventSubscriber($subscriber, $config = array(), $priority = null)
+    public function addEventSubscriber($subscriber, $config = array())
     {
         if (!($subscriber instanceof EventSubscriberInterface)) {
             $subscriber = $this->getEventSubscriber($subscriber, $config);
         }
 
-        $priority = is_int($priority) ? $priority : $subscriber->getPriority();
-        $this->getEventDispatcher()->addEventSubscriber($subscriber, $priority);
-
+        $subscriber->subscribe($this->getEventPublisher());
         return $this;
     }
 
     /**
      * Remove an event subscriber
      *
-     * @param   mixed  $subscriber An object that implements ObjectInterface, ObjectIdentifier object
-     *                             or valid identifier string
-     * @return  Object  The mixer object
+     * @param  EventSubscriberInterface  $subscriber An event subscriber
+     * @return ObjectInterface The mixer
      */
-    public function removeEventSubscriber($subscriber)
+    public function removeEventSubscriber(EventSubscriberInterface $subscriber)
     {
-        if (!($subscriber instanceof EventSubscriberInterface)) {
-            $subscriber = $this->getEventSubscriber($subscriber);
-        }
-
-        $this->getEventDispatcher()->removeEventSubscriber($subscriber);
+        $subscriber->unsubscribe($this->getEventPublisher());
         return $this->getMixer();
     }
 
     /**
      * Get a event subscriber by identifier
      *
+     * The subscriber will be created if does not exist yet, otherwise the existing subscriber will be returned.
+     *
      * @param  mixed $subscriber An object that implements ObjectInterface, ObjectIdentifier object
      *                          or valid identifier string
      * @param  array  $config   An optional associative array of configuration settings
-     * @throws \DomainException    If the subscriber is not implementing the EventSubscriberInterface
+     * @throws \UnexpectedValueException    If the subscriber is not implementing the EventSubscriberInterface
      * @return EventSubscriberInterface
      */
     public function getEventSubscriber($subscriber, $config = array())
@@ -210,29 +221,40 @@ class EventMixin extends ObjectMixinAbstract
             //Create the complete identifier if a partial identifier was passed
             if (is_string($subscriber) && strpos($subscriber, '.') === false)
             {
-                $identifier = clone $this->getIdentifier();
-                $identifier->path = array('event', 'subscriber');
-                $identifier->name = $subscriber;
+                $identifier = $this->getIdentifier()->toArray();
+                $identifier['path'] = array('event', 'subscriber');
+                $identifier['name'] = $subscriber;
+
+                $identifier = $this->getIdentifier($identifier);
             }
             else $identifier = $this->getIdentifier($subscriber);
         }
         else $identifier = $subscriber;
 
-        if (!isset($this->_event_subscribers[(string)$identifier]))
+        if (!isset($this->__event_subscribers[(string)$identifier]))
         {
-            $config['event_dispatcher'] = $this->getEventDispatcher();
             $subscriber = $this->getObject($identifier, $config);
 
             //Check the event subscriber interface
             if (!($subscriber instanceof EventSubscriberInterface))
             {
                 throw new \UnexpectedValueException(
-                    "Event Subscriber $identifier does not implement EventSubscriberInterface"
+                    "Event Subscriber $identifier does not implement KEventSubscriberInterface"
                 );
             }
         }
-        else $subscriber = $this->_event_subscribers[(string)$identifier];
+        else $subscriber = $this->__event_subscribers[(string)$identifier];
 
         return $subscriber;
+    }
+
+    /**
+     * Gets the event subscribers
+     *
+     * @return array An array of event subscribers
+     */
+    public function getEventSubscribers()
+    {
+        return array_values($this->__event_subscribers);
     }
 }
