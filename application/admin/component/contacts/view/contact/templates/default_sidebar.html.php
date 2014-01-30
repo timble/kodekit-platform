cb00a9945ea63afc8bf9ea046770a3b982d2d9bb
Tom Janssens <tom@timble.net>
2014-01-30 14:19:44 +0100

    Set default value for categories radiolist when creating a new contact

--- application/admin/component/contacts/view/contact/templates/default_sidebar.html.php
+++ application/admin/component/contacts/view/contact/templates/default_sidebar.html.php
@@ -8,6 +8,17 @@
  */
 ?>
 
+<? if(!$contact->categories_category_id) : ?>
+<script>
+    // Set default value for categories radiolist
+    $jQuery(document).ready(
+        function(){
+            $jQuery('fieldset[name=categories_category_id] label:first-of-type input:radio').prop('checked', true);
+        }
+    );
+</script>
+<? endif ?>
+
 <fieldset>
     <legend><?= translate('Publish'); ?></legend>
     <div>
