7b54ab4fa0d1d4e86f2d469aa6431326758d51b7
Ercan Ozkaya <ercan@timble.net>
2014-03-13 14:35:36 +0200

    test #172: Refactor JSON output

--- component/files/view/json.php
+++ component/files/view/json.php
@@ -19,17 +19,22 @@ use Nooku\Library;
  */
 class ViewJson extends Library\ViewJson
 {
-    protected function _getRow()
+    protected function _renderData()
     {
-        $row  = $this->getModel()->getRow();
-        $data = parent::_getRow();
+        $output = parent::_renderData();
 
-        $status = $row->getStatus() !== Library\Database::STATUS_FAILED;
-		$data['status'] = $status;
-        if ($data === false){
-            $data['error'] = $row->getStatusMessage();
+        if (!$this->_plural)
+        {
+            $row    = $this->getModel()->getRow();
+            $status = $row->getStatus() !== Library\Database::STATUS_FAILED;
+
+            $output['status'] = $status;
+
+            if ($status === false){
+                $output['error'] = $row->getStatusMessage();
+            }
         }
 
-        return $data;
+        return $output;
     }
 }
