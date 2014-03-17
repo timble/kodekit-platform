be44ab02d3c224fd39f69342e2ad0598ffb11c27
Johan Janssens <johan@timble.net>
2014-03-17 17:08:07 +0100

    Code cleanup

--- library/behavior/mixin/mixin.php
+++ library/behavior/mixin/mixin.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Framework - http://www.nooku.org
  *
- * @copyright	Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @copyright      Copyright (C) 2007 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           git://git.assembla.com/nooku-framework.git for the canonical source repository
  */
 
 namespace Nooku\Library;
@@ -39,10 +39,9 @@ class BehaviorMixin extends CommandMixin implements BehaviorMixinInterface
         parent::__construct($config);
 
         //Add the behaviors in FIFO order
-        $behaviors = (array) ObjectConfig::unbox($config->behaviors);
+        $behaviors = (array)ObjectConfig::unbox($config->behaviors);
 
-        foreach ($behaviors as $key => $value)
-        {
+        foreach ($behaviors as $key => $value) {
             if (is_numeric($key)) {
                 $this->addBehavior($value);
             } else {
@@ -56,7 +55,8 @@ class BehaviorMixin extends CommandMixin implements BehaviorMixinInterface
      *
      * Called from {@link __construct()} as a first step of object instantiation.
      *
-     * @param  ObjectConfig $config   An optional ObjectConfig object with configuration options.
+     * @param  ObjectConfig $config An optional ObjectConfig object with configuration options.
+     *
      * @return void
      */
     protected function _initialize(ObjectConfig $config)
@@ -64,7 +64,7 @@ class BehaviorMixin extends CommandMixin implements BehaviorMixinInterface
         parent::_initialize($config);
 
         $config->append(array(
-            'behaviors'  => array(),
+            'behaviors' => array(),
         ));
     }
 
@@ -73,24 +73,21 @@ class BehaviorMixin extends CommandMixin implements BehaviorMixinInterface
      *
      * @param   mixed $behavior   An object that implements BehaviorInterface, an ObjectIdentifier
      *                            or valid identifier string
-     * @param   array $config    An optional associative array of configuration settings
+     * @param   array $config     An optional associative array of configuration settings
+     *
      * @return  Object The mixer object
      */
     public function addBehavior($behavior, $config = array())
     {
-
         //Create the complete identifier if a partial identifier was passed
-        if (is_string($behavior) && strpos($behavior, '.') === false)
-        {
-            $identifier = $this->getIdentifier()->toArray();
-            $identifier['path'] =  array($identifier['path'][0], 'behavior');
+        if (is_string($behavior) && strpos($behavior, '.') === false) {
+            $identifier         = $this->getIdentifier()->toArray();
+            $identifier['path'] = array($identifier['path'][0], 'behavior');
             $identifier['name'] = $behavior;
 
             $identifier = $this->getIdentifier($identifier);
-        }
-        else
-        {
-            if($behavior instanceof BehaviorInterface) {
+        } else {
+            if ($behavior instanceof BehaviorInterface) {
                 $identifier = $behavior->getIdentifier();
             } else {
                 $identifier = $this->getIdentifier($behavior);
@@ -98,13 +95,11 @@ class BehaviorMixin extends CommandMixin implements BehaviorMixinInterface
         }
 
         //Attach the behavior if it doesn't exist yet
-        if(!$this->hasBehavior($identifier->name))
-        {
+        if (!$this->hasBehavior($identifier->name)) {
             //Create the behavior object
-            if (!($behavior instanceof BehaviorInterface))
-            {
+            if (!($behavior instanceof BehaviorInterface)) {
                 $config['mixer'] = $this->getMixer();
-                $behavior = $this->getObject($identifier, $config);
+                $behavior        = $this->getObject($identifier, $config);
             }
 
             if (!($behavior instanceof BehaviorInterface)) {
@@ -130,7 +125,8 @@ class BehaviorMixin extends CommandMixin implements BehaviorMixinInterface
     /**
      * Check if a behavior exists
      *
-     * @param   string  $name The name of the behavior
+     * @param   string $name The name of the behavior
+     *
      * @return  boolean TRUE if the behavior exists, FALSE otherwise
      */
     public function hasBehavior($name)
@@ -141,14 +137,15 @@ class BehaviorMixin extends CommandMixin implements BehaviorMixinInterface
     /**
      * Get a behavior by name
      *
-     * @param  string  $name   The behavior name
+     * @param  string $name The behavior name
+     *
      * @return BehaviorInterface
      */
     public function getBehavior($name)
     {
         $result = null;
 
-        if(isset($this->__behaviors[$name])) {
+        if (isset($this->__behaviors[$name])) {
             $result = $this->__behaviors[$name];
         }
 
