ab9cae3a10540a9572bb39c9212802af8b1054d3
Johan Janssens <johan@timble.net>
2014-03-17 17:16:42 +0100

    Code cleanup

--- application/admin/component/application/dispatcher/http.php
+++ application/admin/component/application/dispatcher/http.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Framework - http://www.nooku.org
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           git://git.assembla.com/nooku-framework.git for the canonical source repository
  */
 
 use Nooku\Library;
@@ -28,14 +28,14 @@ class ApplicationDispatcherHttp extends Application\DispatcherHttp
     /**
      * Constructor.
      *
-     * @param Library\ObjectConfig $config	An optional Library\ObjectConfig object with configuration options.
+     * @param Library\ObjectConfig $config An optional Library\ObjectConfig object with configuration options.
      */
     public function __construct(Library\ObjectConfig $config)
     {
         parent::__construct($config);
 
         //Set the site name
-        if(empty($config->site)) {
+        if (empty($config->site)) {
             $this->_site = $this->getSite();
         } else {
             $this->_site = $config->site;
@@ -51,8 +51,9 @@ class ApplicationDispatcherHttp extends Application\DispatcherHttp
      *
      * Called from {@link __construct()} as a first step of object instantiation.
      *
-     * @param 	Library\ObjectConfig    $config  An optional Library\ObjectConfig object with configuration options.
-     * @return 	void
+     * @param    Library\ObjectConfig $config An optional Library\ObjectConfig object with configuration options.
+     *
+     * @return    void
      */
     protected function _initialize(Library\ObjectConfig $config)
     {
@@ -60,9 +61,9 @@ class ApplicationDispatcherHttp extends Application\DispatcherHttp
             'base_url' => '/administrator',
             'site'     => null,
             'options'  => array(
-                'config_file'  => JPATH_ROOT.'/config/config.php',
-                'language'     => null,
-                'theme'        => 'default'
+                'config_file' => JPATH_ROOT . '/config/config.php',
+                'language'    => null,
+                'theme'       => 'default'
             ),
         ));
 
@@ -72,22 +73,19 @@ class ApplicationDispatcherHttp extends Application\DispatcherHttp
     /**
      * Run the application
      *
-     * @param Library\DispatcherContextInterface $context	A dispatcher context object
+     * @param Library\DispatcherContextInterface $context A dispatcher context object
      */
     protected function _actionRun(Library\DispatcherContextInterface $context)
     {
         //Set the site error reporting
         $this->getObject('exception.handler')->setErrorLevel($this->getCfg('debug_mode'));
 
-        //Set the paths
-        $params = $this->getObject('application.extensions')->getExtension('files')->params;
-
-        define('JPATH_FILES'  , JPATH_SITES.'/'.$this->getSite().'/files');
-        define('JPATH_CACHE'  , $this->getCfg('cache_path', JPATH_ROOT.'/cache'));
+        define('JPATH_FILES', JPATH_SITES . '/' . $this->getSite() . '/files');
+        define('JPATH_CACHE', $this->getCfg('cache_path', JPATH_ROOT . '/cache'));
 
         // Set timezone to user's setting, falling back to global configuration.
         $timezone = new \DateTimeZone($context->user->get('timezone', $this->getCfg('timezone')));
-		date_default_timezone_set($timezone->getName());
+        date_default_timezone_set($timezone->getName());
 
         //Route the request
         $this->route();
@@ -96,7 +94,7 @@ class ApplicationDispatcherHttp extends Application\DispatcherHttp
     /**
      * Route the request
      *
-     * @param Library\DispatcherContextInterface $context	A dispatcher context object
+     * @param Library\DispatcherContextInterface $context A dispatcher context object
      */
     protected function _actionRoute(Library\DispatcherContextInterface $context)
     {
@@ -109,50 +107,46 @@ class ApplicationDispatcherHttp extends Application\DispatcherHttp
         $context->request->query->add($url->query);
 
         //Forward the request
-        $component = substr( $context->request->query->get('option', 'cmd', 'com_dashboard'), 4);
+        $component = substr($context->request->query->get('option', 'cmd', 'com_dashboard'), 4);
         $this->forward($component);
     }
 
     /**
      * Load the configuration
      *
-     * @return	void
+     * @return    void
      */
     public function loadConfig()
     {
         // Check if the site exists
-        if($this->getObject('com:sites.model.sites')->fetch()->find($this->getSite()))
-        {
+        if ($this->getObject('com:sites.model.sites')->fetch()->find($this->getSite())) {
             //Load the application config settings
             JFactory::getConfig()->loadArray($this->getConfig()->options->toArray());
 
             //Load the global config settings
-            require_once( $this->getConfig()->options->config_file );
+            require_once($this->getConfig()->options->config_file);
             JFactory::getConfig()->loadObject(new JConfig());
 
             //Load the site config settings
-            require_once( JPATH_SITES.'/'.$this->getSite().'/config/config.php');
+            require_once(JPATH_SITES . '/' . $this->getSite() . '/config/config.php');
             JFactory::getConfig()->loadObject(new JSiteConfig());
 
-        }
-        else throw new Library\ControllerExceptionResourceNotFound('Site :'.$this->getSite().' not found');
+        } else throw new Library\ControllerExceptionResourceNotFound('Site :' . $this->getSite() . ' not found');
     }
 
     /**
      * Load the user session or create a new one
      *
-     * @return	void
+     * @return    void
      */
     public function getUser()
     {
-        if(!$this->_user instanceof Library\UserInterface)
-        {
-            $user    =  parent::getUser();
-            $session =  $user->getSession();
+        if (!$this->_user instanceof Library\UserInterface) {
+            $user    = parent::getUser();
+            $session = $user->getSession();
 
             //Re-create the session if we changed sites
-            if($user->isAuthentic() && ($session->site != $this->getSite()))
-            {
+            if ($user->isAuthentic() && ($session->site != $this->getSite())) {
                 //@TODO : Fix this
                 //if(!$this->getObject('com:users.controller.session')->add()) {
                 //    $session->destroy();
@@ -171,28 +165,26 @@ class ApplicationDispatcherHttp extends Application\DispatcherHttp
     public function loadLanguage(Library\DispatcherContextInterface $context)
     {
         $languages = $this->getObject('application.languages');
-        $language = null;
+        $language  = null;
 
         // If a language was specified it has priority.
-        if($iso_code = $this->getConfig()->options->language)
-        {
+        if ($iso_code = $this->getConfig()->options->language) {
             $result = $languages->find(array('iso_code' => $iso_code));
-            if(count($result) == 1) {
+            if (count($result) == 1) {
                 $language = $result->top();
             }
         }
 
         // Otherwise use user language setting.
-        if(!$language && $iso_code = $context->user->get('language'))
-        {
+        if (!$language && $iso_code = $context->user->get('language')) {
             $result = $languages->find(array('iso_code' => $iso_code));
-            if(count($result) == 1) {
+            if (count($result) == 1) {
                 $language = $result->top();
             }
         }
 
         // If language still not set, use the primary.
-        if(!$language) {
+        if (!$language) {
             $language = $languages->getPrimary();
         }
 
@@ -205,23 +197,26 @@ class ApplicationDispatcherHttp extends Application\DispatcherHttp
     /**
      * Get the application router.
      *
-     * @param  array $options 	An optional associative array of configuration options.
-     * @return	\ApplicationRouter
+     * @param  array $options An optional associative array of configuration options.
+     *
+     * @return    \ApplicationRouter
      */
     public function getRouter(array $options = array())
     {
         $router = $this->getObject('com:application.router', $options);
+
         return $router;
     }
 
     /**
      * Gets a configuration value.
      *
-     * @param	string	$name    The name of the value to get.
-     * @param	mixed	$default The default value
-     * @return	mixed	The user state.
+     * @param    string $name    The name of the value to get.
+     * @param    mixed  $default The default value
+     *
+     * @return    mixed    The user state.
      */
-    public function getCfg( $name, $default = null )
+    public function getCfg($name, $default = null)
     {
         return JFactory::getConfig()->getValue('config.' . $name, $default);
     }
@@ -243,29 +238,28 @@ class ApplicationDispatcherHttp extends Application\DispatcherHttp
      * it will return 'default'.
      *
      * @param  boolean $reparse Reparse the site name from the request url
+     *
      * @return string  The site name
      */
     public function getSite($reparse = false)
     {
-        if(!$this->_site || $reparse)
-        {
+        if (!$this->_site || $reparse) {
             // Check URL host
-            $uri  = clone($this->getRequest()->getUrl());
+            $uri = clone($this->getRequest()->getUrl());
 
             $host = $uri->getHost();
-            if(!$this->getObject('com:sites.model.sites')->fetch()->find($host))
-            {
+            if (!$this->getObject('com:sites.model.sites')->fetch()->find($host)) {
                 // Check folder
                 $base = $this->getRequest()->getBaseUrl()->getPath();
                 $path = trim(str_replace($base, '', $uri->getPath()), '/');
-                if(!empty($path)) {
+                if (!empty($path)) {
                     $site = array_shift(explode('/', $path));
                 } else {
                     $site = 'default';
                 }
 
                 //Check if the site can be found, otherwise use 'default'
-                if(!$this->getObject('com:sites.model.sites')->fetch()->find($site)) {
+                if (!$this->getObject('com:sites.model.sites')->fetch()->find($site)) {
                     $site = 'default';
                 }
 
