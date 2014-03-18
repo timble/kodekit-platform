b774d65bbc19acf291dd973fa6e5b53ec7dd3afa
Johan Janssens <johan@timble.net>
2014-03-18 03:27:08 +0100

    re #378 : Code cleanup

--- component/files/controller/thumbnail.php
+++ component/files/controller/thumbnail.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Framework - http://www.nooku.org
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           git://git.assembla.com/nooku-framework.git for the canonical source repository
  */
 
 namespace Nooku\Component\Files;
@@ -22,63 +22,54 @@ class ControllerThumbnail extends ControllerAbstract
 {
     protected function _actionBrowse(Library\ControllerContextInterface $context)
     {
-    	// Clone to make cacheable work since we change model states
+        // Clone to make cacheable work since we change model states
         $model = clone $this->getModel();
 
-    	// Save state data for later
+        // Save state data for later
         $state_data = $model->getState()->getValues();
-        
+
         $nodes = $this->getObject('com:files.model.nodes')->setState($state_data)->fetch();
 
-        if (!$model->getState()->files && !$model->getState()->filename) 
-        {
-        	$needed  = array();
-        	foreach ($nodes as $row)
-        	{
-        		if ($row instanceof Files\DatabaseRowFile && $row->isImage()) {
-        			$needed[] = $row->name;
-        		}
-        	}
-        } 
-        else $needed = $model->getState()->files ? $model->getState()->files : $model->getState()->filename;
-        
-		$model->reset()
-		      ->setState($state_data)
-		      ->files($needed);
+        if (!$model->getState()->filename) {
+            $needed = array();
+            foreach ($nodes as $row) {
+                if ($row instanceof Files\DatabaseRowFile && $row->isImage()) {
+                    $needed[] = $row->name;
+                }
+            }
+        } else $needed = $model->getState()->filename;
 
-		$list = $model->fetch();
+        $model->setState($state_data)
+            ->filename($needed);
 
-    	$found = array();
+        $list = $model->fetch();
+
+        $found = array();
         foreach ($list as $row) {
-        	$found[] = $row->filename;
+            $found[] = $row->filename;
         }
 
-        if (count($found) !== count($needed))
-        {
-        	$new = array();
-        	foreach ($nodes as $row)
-        	{
-        		if ($row instanceof Files\DatabaseRowFile && $row->isImage() && !in_array($row->name, $found))
-        		{
-	        		$result = $row->saveThumbnail();
-	        		if ($result) {
-	        			$new[] = $row->name;
-	        		}
-        		}
-        	}
-        	
-        	if (count($new))
-        	{
-				$model->reset()
-				    ->getState()->setValues($state_data)
-				    ->set('files', $new);
-				
-				$additional = $model->fetch();
-				
-				foreach ($additional as $row) {
-					$list->insert($row);
-				}
-        	}
+        if (count($found) !== count($needed)) {
+            $new = array();
+            foreach ($nodes as $row) {
+                if ($row instanceof Files\DatabaseRowFile && $row->isImage() && !in_array($row->name, $found)) {
+                    $result = $row->saveThumbnail();
+                    if ($result) {
+                        $new[] = $row->name;
+                    }
+                }
+            }
+
+            if (count($new)) {
+                $model->getState()->setValues($state_data)
+                    ->set('filename', $new);
+
+                $additional = $model->fetch();
+
+                foreach ($additional as $row) {
+                    $list->insert($row);
+                }
+            }
         }
 
         return $list;
