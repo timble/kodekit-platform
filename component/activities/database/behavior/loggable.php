25a0fed04dfd0d973fa25be8e888bf9983ed96a4
Johan Janssens <johan@timble.net>
2014-03-18 03:32:16 +0100

    re #378 : Add loggable database behavior to be able to retrieve a list of activities for a row.

---
+++ component/activities/database/behavior/loggable.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Nooku Framework - http://www.nooku.org
+ *
+ * @copyright      Copyright (C) 2011 - 2013 Timble CVBA and Contributors. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           git://git.assembla.com/nooku-framework.git
+ */
+
+namespace Nooku\Component\Activities;
+
+use Nooku\Library;
+
+/**
+ * Loggable Database Behavior
+ *
+ * @author  Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package Nooku\Component\Activities
+ */
+class DatabaseBehaviorLoggable extends Library\DatabaseBehaviorAbstract
+{
+    /**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param  Library\ObjectConfig $config A ObjectConfig object with configuration options
+     *
+     * @return void
+     */
+    protected function _initialize(Library\ObjectConfig $config)
+    {
+        $config->append(array(
+            'auto_mixin' => true
+        ));
+
+        parent::_initialize($config);
+    }
+
+    /**
+     * Get a list of activities
+     *
+     * @return Library\DatabaseRowsetInterface
+     */
+    public function getActivities()
+    {
+        $activities = $this->getObject('com:activities.model.activities')
+            ->row($this->id)
+            ->package($this->getTable()->getIdentifier()->package)
+            ->name($this->getTable()->getName())
+            ->fetch();
+
+        return $activities;
+    }
+}
