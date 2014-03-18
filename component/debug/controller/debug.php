3862f8c8e528082b5291034198052566446b0136
Johan Janssens <johan@timble.net>
2014-03-18 03:27:20 +0100

    re #378 : Code cleanup

--- component/debug/controller/debug.php
+++ component/debug/controller/debug.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Framework - http://www.nooku.org
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           git://git.assembla.com/nooku-framework.git for the canonical source repository
  */
 
 namespace Nooku\Component\Debug;
@@ -19,12 +19,5 @@ use Nooku\Library;
  */
 class ControllerDebug extends Library\ControllerView
 {
-    protected function _initialize(Library\ObjectConfig $config)
-    {
-        parent::_initialize($config);
 
-        //Don't dispatch event or allow callbacks
-        $config->dispatch_events  = false;
-        $config->enable_callbacks = false;
-    }
 }
