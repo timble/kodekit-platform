7b7fb563698cd1a410366dbfcab6a8641f195624
Johan Janssens <johan@timble.net>
2014-03-18 03:23:58 +0100

    re #378 : Model state is no longer injected, use the query params instead.

--- component/languages/database/behavior/translatable.php
+++ component/languages/database/behavior/translatable.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Framework - http://www.nooku.org
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           git://git.assembla.com/nooku-framework.git for the canonical source repository
  */
 
 namespace Nooku\Component\Languages;
@@ -20,11 +20,11 @@ use Nooku\Library;
 class DatabaseBehaviorTranslatable extends Library\DatabaseBehaviorAbstract implements Library\ObjectMultiton
 {
     protected $_tables;
-    
+
     public function __construct(Library\ObjectConfig $config)
     {
         parent::__construct($config);
-        
+
         $this->_tables = $this->getObject('com:languages.model.tables')
             ->enabled(true)
             ->fetch();
@@ -33,12 +33,12 @@ class DatabaseBehaviorTranslatable extends Library\DatabaseBehaviorAbstract impl
     public function isSupported()
     {
         // If table is not enabled, return null to prevent enqueueing.
-        $table = $this->getMixer() instanceof Library\DatabaseTableInterface ? $this->getMixer() : $this->getMixer()->getTable();
+        $table  = $this->getMixer() instanceof Library\DatabaseTableInterface ? $this->getMixer() : $this->getMixer()->getTable();
         $needle = array(
             'name'           => $table->getBase(),
-            'component_name' => 'com_'.$table->getIdentifier()->package
+            'component_name' => 'com_' . $table->getIdentifier()->package
         );
-        
+
         return count($this->_tables->find($needle)) ? true : false;
     }
 
@@ -46,133 +46,126 @@ class DatabaseBehaviorTranslatable extends Library\DatabaseBehaviorAbstract impl
     {
         $methods = parent::getMixableMethods($exclude);
         $mixer   = $this->getMixer();
-        
-        if(!is_null($mixer))
-        {
+
+        if (!is_null($mixer)) {
             // If table is not enabled, don't mix the methods.
             $table  = $mixer instanceof Library\DatabaseTableInterface ? $mixer : $mixer->getTable();
             $needle = array(
                 'name'           => $table->getBase(),
-                'component_name' => 'com_'.$table->getIdentifier()->package
+                'component_name' => 'com_' . $table->getIdentifier()->package
             );
-            
-            if(!count($this->_tables->find($needle)))
-            {
+
+            if (!count($this->_tables->find($needle))) {
                 $methods['isTranslatable'] = false;
-                
+
                 unset($methods['getLanguages']);
                 unset($methods['getTranslations']);
             }
         }
-        
+
         return $methods;
     }
-    
+
     public function getLanguages()
     {
         return $this->getObject('application.languages');
     }
-    
+
     public function getTranslations()
     {
         $translations = $this->getObject('com:languages.model.translations')
             ->table($this->getMixer()->getIdentifier()->package)
             ->row($this->id)
             ->fetch();
-        
+
         return $translations;
     }
-    
+
     protected function _beforeSelect(Library\DatabaseContext $context)
     {
-        if($query = $context->query)
-        {
+        if ($query = $context->query) {
             $table     = $this->_tables->find(array('name' => $context->table))->top();
             $languages = $this->getObject('application.languages');
             $active    = $languages->getActive();
             $primary   = $languages->getPrimary();
-            
+
             // Join translation to add status to rows.
-            $state = $context->options->state;
-            if(!$query->isCountQuery() && $state && !$state->isUnique() && isset($state->translated))
-            {
+            $params = $context->query->params;
+            if (!$query->isCountQuery() && $params->has('translated')) {
+                $translated = $params->has('translated');
+
                 $query->columns(array(
-                        'translation_status'    => 'translations.status',
-                        'translation_original'  => 'translations.original',
-                        'translation_deleted'   => 'translations.deleted'))
+                    'translation_status'   => 'translations.status',
+                    'translation_original' => 'translations.original',
+                    'translation_deleted'  => 'translations.deleted'))
                     ->join(array('translations' => 'languages_translations'),
-                        'translations.table = :translation_table'.
-                        ' AND translations.row = tbl.'.$table->unique_column.
+                        'translations.table = :translation_table' .
+                        ' AND translations.row = tbl.' . $table->unique_column .
                         ' AND translations.iso_code = :translation_iso_code')
                     ->bind(array(
                         'translation_iso_code' => $active->iso_code,
                         'translation_table'    => $table->name
                     ));
-                
-                if(!is_null($state->translated))
-                {
-                    $status = $state->translated ? LanguagesDatabaseRowTranslation::STATUS_COMPLETED : array(
+
+                if (!is_null($translated)) {
+                    $status = $translated ? LanguagesDatabaseRowTranslation::STATUS_COMPLETED : array(
                         LanguagesDatabaseRowTranslation::STATUS_MISSING,
                         LanguagesDatabaseRowTranslation::STATUS_OUTDATED
                     );
-                    
+
                     $query->where('translations.status IN :translation_status')
-                        ->bind(array('translation_status' => (array) $status));
+                        ->bind(array('translation_status' => (array)$status));
                 }
             }
-            
+
             // Modify table in the query if active language is not the primary.
-            if($active->iso_code != $primary->iso_code) {
-                $context->query->table[key($query->table)] = strtolower($active->iso_code).'_'.$table->name;
+            if ($active->iso_code != $primary->iso_code) {
+                $context->query->table[key($query->table)] = strtolower($active->iso_code) . '_' . $table->name;
             }
         }
     }
-    
+
     protected function _afterInsert(Library\DatabaseContext $context)
     {
-        if($context->affected)
-        {
+        if ($context->affected) {
             $languages = $this->getObject('application.languages');
             $active    = $languages->getActive();
             $primary   = $languages->getPrimary();
-            
+
             $translation = array(
-                'iso_code'   => $active->iso_code,
-                'table'      => $context->table,
-                'row'        => $context->data->id,
-                'status'     => DatabaseRowTranslation::STATUS_COMPLETED,
-                'original'   => 1
+                'iso_code' => $active->iso_code,
+                'table'    => $context->table,
+                'row'      => $context->data->id,
+                'status'   => DatabaseRowTranslation::STATUS_COMPLETED,
+                'original' => 1
             );
-            
+
             // Insert item into the translations table.
             $this->getObject('com:languages.database.row.translation')
                 ->setProperties($translation)
                 ->save();
-            
+
             // Insert item into language specific tables.
             $table = $this->_tables->find(array('name' => $context->table))->top();
-            
-            foreach($languages as $language)
-            {
-                if($language->iso_code != $primary->iso_code)
-                {
+
+            foreach ($languages as $language) {
+                if ($language->iso_code != $primary->iso_code) {
                     $query = clone $context->query;
-                    $query->table(strtolower($language->iso_code).'_'.$query->table);
-                    
-                    if(($key = array_search($table->unique_column, $query->columns)) !== false) {
+                    $query->table(strtolower($language->iso_code) . '_' . $query->table);
+
+                    if (($key = array_search($table->unique_column, $query->columns)) !== false) {
                         $query->values[0][$key] = $context->data->id;
                     }
-                    
+
                     $this->getTable()->getAdapter()->insert($query);
                 }
-                
-                if($language->iso_code != $active->iso_code)
-                {
+
+                if ($language->iso_code != $active->iso_code) {
                     // Insert item into translations table.
                     $translation['iso_code'] = $language->iso_code;
                     $translation['status']   = DatabaseRowTranslation::STATUS_MISSING;
                     $translation['original'] = 0;
-                    
+
                     $this->getObject('com:languages.database.row.translation')
                         ->setProperties($translation)
                         ->save();
@@ -180,37 +173,37 @@ class DatabaseBehaviorTranslatable extends Library\DatabaseBehaviorAbstract impl
             }
         }
     }
-    
+
     protected function _beforeUpdate(Library\DatabaseContext $context)
     {
         $languages = $this->getObject('application.languages');
         $active    = $languages->getActive();
         $primary   = $languages->getPrimary();
-        
-        if($active->iso_code != $primary->iso_code) {
-            $context->query->table(strtolower($active->iso_code).'_'.$context->query->table);
+
+        if ($active->iso_code != $primary->iso_code) {
+            $context->query->table(strtolower($active->iso_code) . '_' . $context->query->table);
         }
     }
-    
+
     protected function _afterUpdate(Library\DatabaseContext $context)
     {
         $languages = $this->getObject('application.languages');
         $primary   = $languages->getPrimary();
         $active    = $languages->getActive();
-        
+
         // Update item in the translations table.
-        $table = $this->_tables->find(array('name' => $context->table))->top();
-        $translation  = $this->getObject('com:languages.database.table.translations')
+        $table       = $this->_tables->find(array('name' => $context->table))->top();
+        $translation = $this->getObject('com:languages.database.table.translations')
             ->select(array(
                 'iso_code' => $active->iso_code,
                 'table'    => $context->table,
                 'row'      => $context->data->id
             ), Library\Database::FETCH_ROW);
-        
+
         $translation->setProperties(array(
             'status' => DatabaseRowTranslation::STATUS_COMPLETED
         ))->save();
-        
+
         // Set the other items to outdated if they were completed before.
         $query = $this->getObject('lib:database.query.select')
             ->where('iso_code <> :iso_code')
@@ -219,75 +212,71 @@ class DatabaseBehaviorTranslatable extends Library\DatabaseBehaviorAbstract impl
             ->where('status = :status')
             ->bind(array(
                 'iso_code' => $active->iso_code,
-                'table' => $context->table,
-                'row' => $context->data->id,
-                'status' => DatabaseRowTranslation::STATUS_COMPLETED
+                'table'    => $context->table,
+                'row'      => $context->data->id,
+                'status'   => DatabaseRowTranslation::STATUS_COMPLETED
             ));
-        
+
         $translations = $this->getObject('com:languages.database.table.translations')
             ->select($query);
-        
+
         $translations->status = DatabaseRowTranslation::STATUS_OUTDATED;
         $translations->save();
-        
+
         // Copy the item's data to all missing translations.
         $database = $this->getTable()->getAdapter();
-        $prefix = $active->iso_code != $primary->iso_code ? strtolower($active->iso_code.'_') : '';
-        $select = $this->getObject('lib:database.query.select')
-            ->table($prefix.$table->name)
-            ->where($table->unique_column.' = :unique')
+        $prefix   = $active->iso_code != $primary->iso_code ? strtolower($active->iso_code . '_') : '';
+        $select   = $this->getObject('lib:database.query.select')
+            ->table($prefix . $table->name)
+            ->where($table->unique_column . ' = :unique')
             ->bind(array('unique' => $context->data->id));
-        
+
         $query->bind(array('status' => DatabaseRowTranslation::STATUS_MISSING));
         $translations = $this->getObject('com:languages.database.table.translations')
             ->select($query);
-        
-        foreach($translations as $translation)
-        {
-            $prefix = $translation->iso_code != $primary->iso_code ? strtolower($translation->iso_code.'_') : '';
-            $query = 'REPLACE INTO '.$database->quoteIdentifier($prefix.$table->name).' '.$select;
+
+        foreach ($translations as $translation) {
+            $prefix = $translation->iso_code != $primary->iso_code ? strtolower($translation->iso_code . '_') : '';
+            $query  = 'REPLACE INTO ' . $database->quoteIdentifier($prefix . $table->name) . ' ' . $select;
             $database->execute($query);
         }
     }
-    
+
     protected function _beforeDelete(Library\DatabaseContext $context)
     {
         $languages = $this->getObject('application.languages');
         $active    = $languages->getActive();
         $primary   = $languages->getPrimary();
-        
-        if($active->iso_code != $primary->iso_code) {
-            $context->query->table(strtolower($active->iso_code).'_'.$context->table);
+
+        if ($active->iso_code != $primary->iso_code) {
+            $context->query->table(strtolower($active->iso_code) . '_' . $context->table);
         }
     }
-    
+
     protected function _afterDelete(Library\DatabaseContext $context)
     {
-        if($context->data->getStatus() == Library\Database::STATUS_DELETED)
-        {
+        if ($context->data->getStatus() == Library\Database::STATUS_DELETED) {
             $languages = $this->getObject('application.languages');
             $primary   = $languages->getPrimary();
             $active    = $languages->getActive();
-            
+
             // Remove item from other tables too.
             $database = $this->getTable()->getAdapter();
             $query    = clone $context->query;
-            
-            foreach($languages as $language)
-            {
-                if($language->iso_code != $active->iso_code)
-                {
-                    $prefix = $language->iso_code != $primary->iso_code ? strtolower($language->iso_code.'_') : ''; 
-                    $query->table($prefix.$context->table);
+
+            foreach ($languages as $language) {
+                if ($language->iso_code != $active->iso_code) {
+                    $prefix = $language->iso_code != $primary->iso_code ? strtolower($language->iso_code . '_') : '';
+                    $query->table($prefix . $context->table);
                     $database->delete($query);
                 }
             }
-            
+
             // Mark item as deleted in translations table.
             $translations = $this->getObject('com:languages.database.table.translations')
-                                 ->select(array('table' => $context->table, 'row' => $context->data->id));
+                ->select(array('table' => $context->table, 'row' => $context->data->id));
 
-            foreach($translations as $translation) {
+            foreach ($translations as $translation) {
                 $translation->setProperties(array('deleted' => 1));
             }
 
