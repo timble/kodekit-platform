9ed12c0b8f280eb42f47f74581bf9794f49bd882
Johan Janssens <johan@timble.net>
2014-03-18 03:27:54 +0100

    re #378 : Fixed typo

--- component/comments/controller/comment.php
+++ component/comments/controller/comment.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Framework - http://www.nooku.org
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           git://git.assembla.com/nooku-framework.git for the canonical source repository
  */
 
 namespace Nooku\Component\Comments;
@@ -14,11 +14,11 @@ use Nooku\Library;
 /**
  * Comment Controller
  *
- * @author    	Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
- * @package     Nooku\Component\Comments
+ * @author        Johan Janssens <http://nooku.assembla.com/profile/johanjanssens>
+ * @package       Nooku\Component\Comments
  */
 abstract class ControllerComment extends Library\ControllerModel
-{ 
+{
     protected function _initialize(Library\ObjectConfig $config)
     {
         $config->append(array(
@@ -26,44 +26,40 @@ abstract class ControllerComment extends Library\ControllerModel
         ));
 
         //Alias the permission
-        $permission         = $this->getIdentifier();
+        $permission         = $this->getIdentifier()->toArray();
         $permission['path'] = array('controller', 'permission');
 
-        $this->getObject('manager')->registerAlias(
-            'com:comments.controller.permission.comment',
-            'com:'.$permission->package.'controller.permission.comment'
-        );
+        $this->getObject('manager')->registerAlias('com:comments.controller.permission.comment', $permission);
 
         parent::_initialize($config);
     }
-    
+
     protected function _actionRender(Library\ControllerContextInterface $context)
     {
         $view = $this->getView();
 
-	    //Alias the view layout
-        if($view instanceof Library\ViewTemplate)
-	    {
-	        $layout = $view->getIdentifier()->toArray();
-            $layout['name']  = $view->getLayout();
+        //Alias the view layout
+        if ($view instanceof Library\ViewTemplate) {
+            $layout         = $view->getIdentifier()->toArray();
+            $layout['name'] = $view->getLayout();
 
-            $alias = $layout;
+            $alias            = $layout;
             $alias['package'] = 'comments';
 
-	        $this->getObject('manager')->registerAlias($layout, $alias);
-	    }
+            $this->getObject('manager')->registerAlia($alias, $layout);
+        }
 
         return parent::_actionRender($context);
     }
 
     public function getRequest()
-	{
-		$request = parent::getRequest();
+    {
+        $request = parent::getRequest();
 
         //Force set the 'table' in the request
-        $request->query->table  = $this->getIdentifier()->package;
-        $request->data->table   = $this->getIdentifier()->package;
+        $request->query->table = $this->getIdentifier()->package;
+        $request->data->table  = $this->getIdentifier()->package;
 
-	    return $request;
-	}
+        return $request;
+    }
 }
