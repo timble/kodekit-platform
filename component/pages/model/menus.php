d4c4811c533de7d570fc9523f582bf050725cfa6
Johan Janssens <johan@timble.net>
2014-03-18 03:20:15 +0100

    re #378 : Use searchable model behavior

--- component/pages/model/menus.php
+++ component/pages/model/menus.php
@@ -2,9 +2,9 @@
 /**
  * Nooku Framework - http://www.nooku.org
  *
- * @copyright	Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link           git://git.assembla.com/nooku-framework.git for the canonical source repository
  */
 
 namespace Nooku\Component\Pages;
@@ -28,11 +28,20 @@ class ModelMenus extends Library\ModelDatabase
             ->insert('application', 'word');
     }
 
+    protected function _initialize(Library\ObjectConfig $config)
+    {
+        $config->append(array(
+            'behaviors' => array('searchable'),
+        ));
+
+        parent::_initialize($config);
+    }
+
     protected function _buildQueryColumns(Library\DatabaseQuerySelect $query)
     {
         parent::_buildQueryColumns($query);
 
-        if(!$query->isCountQuery()) {
+        if (!$query->isCountQuery()) {
             $query->columns(array('page_count' => 'COUNT(pages.pages_page_id)'));
         }
     }
@@ -41,7 +50,7 @@ class ModelMenus extends Library\ModelDatabase
     {
         parent::_buildQueryJoins($query);
 
-        if(!$query->isCountQuery()) {
+        if (!$query->isCountQuery()) {
             $query->join(array('pages' => 'pages'), 'tbl.pages_menu_id = pages.pages_menu_id');
         }
     }
@@ -50,7 +59,7 @@ class ModelMenus extends Library\ModelDatabase
     {
         parent::_buildQueryGroup($query);
 
-        if(!$query->isCountQuery()) {
+        if (!$query->isCountQuery()) {
             $query->group('pages.pages_menu_id');
         }
     }
@@ -60,15 +69,8 @@ class ModelMenus extends Library\ModelDatabase
         parent::_buildQueryWhere($query);
         $state = $this->getState();
 
-        if(!$state->isUnique())
-        {
-            if($state->search)
-            {
-                $query->where('tbl.title LIKE :search')
-                    ->bind(array('search' => '%'.$state->search.'%'));;
-            }
-            
-            if($state->application) {
+        if (!$state->isUnique()) {
+            if ($state->application) {
                 $query->where('tbl.application = :application')->bind(array('application' => $state->application));
             }
         }
