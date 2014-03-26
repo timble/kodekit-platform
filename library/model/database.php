d1f0de210acdf4d1654bdfbdd0ce047f1206e2c1
Johan Janssens <johan@timble.net>
2014-03-26 03:35:43 +0100

    re #378 : Reverted previous commit.

--- library/model/database.php
+++ library/model/database.php
@@ -84,7 +84,7 @@ class ModelDatabase extends ModelAbstract
      */
     protected function _actionCreate(ModelContext $context)
     {
-        return $this->getTable()->createRowset();
+        return $this->getTable()->createRow();
     }
 
     /**
