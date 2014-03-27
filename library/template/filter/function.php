fa4b601924b9f01bf26496292e596735d98f3370
Arunas Mazeika <arunas@timble.net>
2014-03-27 21:33:22 +0100

    re #302 Delegate choose call to template object.

--- library/template/filter/function.php
+++ library/template/filter/function.php
@@ -29,7 +29,7 @@ class TemplateFilterFunction extends TemplateFilterAbstract implements TemplateF
     	'object('    => '$this->getObject(',
         'date('      => '$this->renderHelper(\'date.format\',',
         'overlay('   => '$this->renderHelper(\'behavior.overlay\', ',
-        'choose'     => '$this->choose(',
+        'choose'     => '$this->getTemplate()->choose(',
         'translate(' => '$this->translate(',
         'import('    => '$this->load(',
         'route('     => '$this->getView()->getRoute(',
