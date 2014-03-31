5f496250e83c58822debca6d79f5d7814f9850b9
Arunas Mazeika <arunas@timble.net>
2014-03-31 15:50:26 +0200

    Made regexp more readable.

--- library/date/date.php
+++ library/date/date.php
@@ -77,7 +77,7 @@ class Date extends Object implements DateInterface
      */
     public function format($format)
     {
-        $format = preg_replace_callback('/(?<!\\\)[DlFM]/', array($this, '_translate'), $format);
+        $format = preg_replace_callback('/(?<!\\\\)[DlFM]/', array($this, '_translate'), $format);
         return $this->_date->format($format);
     }
 
