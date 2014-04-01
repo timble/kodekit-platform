f19df9be6d0c4e6146912974a567c3bf0cebc95c
Arunas Mazeika <arunas@timble.net>
2014-04-01 16:28:32 +0200

    re #352 Set the error message in the context.

--- component/users/controller/behavior/activatable.php
+++ component/users/controller/behavior/activatable.php
@@ -72,7 +72,8 @@ class ControllerBehaviorActivatable extends Library\ControllerBehaviorAbstract
         $row->setData(array('activation' => '', 'enabled' => 1));
 
         if (!$row->save()) {
-            $result = false;
+            $context->error = $row->getStatusMessage();
+            $result         = false;
         }
 
         return $result;
