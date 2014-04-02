757eb6f49002c9d41a2afef2bef8c334fea470d0
Arunas Mazeika <arunas@timble.net>
2014-04-02 15:51:18 +0200

    Call getLength.

--- component/users/model/entity/password.php
+++ component/users/model/entity/password.php
@@ -72,8 +72,9 @@ class ModelEntityPassword extends Library\ModelEntityRow
             if (strlen($password) < $this->getLength())
             {
                 $this->setStatus(self::STATUS_FAILED);
-                $this->setStatusMessage($translator->translate('You need to provide a password with at least {number} characters.',
-                    array('number' => $length)));
+                $this->setStatusMessage($this->getObject('translator')
+                                             ->translate('You need to provide a password with at least {number} characters.',
+                    array('number' => $this->getLength())));
                 return false;
             }
 
