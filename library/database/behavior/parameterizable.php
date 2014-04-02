2ec41bb4c8e8d84d50ec494af6355af2bd05917a
Johan Janssens <johan@timble.net>
2014-04-02 13:55:56 +0200

    Only merge the parameters if they are not passed in as a string.

--- library/database/behavior/parameterizable.php
+++ library/database/behavior/parameterizable.php
@@ -77,14 +77,12 @@ class DatabaseBehaviorParameterizable extends DatabaseBehaviorAbstract
     {
         if(!empty($value))
         {
-            if(is_string($value)) {
-                $this->getParameters()->fromString(trim($value));
-            } else {
-                $this->getParameters()->add($value);
+            if(!is_string($value)) {
+                $value = $this->getParameters()->add($value)->toString();
             }
         }
 
-        return $this->getParameters()->toString();
+        return $value;
     }
 
     /**
@@ -114,8 +112,8 @@ class DatabaseBehaviorParameterizable extends DatabaseBehaviorAbstract
      */
     protected function _beforeInsert(DatabaseContext $context)
     {
-        if($this->getParameters() instanceof ObjectConfigInterface) {
-            $context->data->setProperty('parameters', $this->getParameters()->toString());
+        if($context->data->getParameters() instanceof ObjectConfigInterface) {
+            $context->data->setProperty('parameters', $context->data->getParameters()->toString());
         }
     }
 
@@ -127,8 +125,8 @@ class DatabaseBehaviorParameterizable extends DatabaseBehaviorAbstract
      */
     protected function _beforeUpdate(DatabaseContext $context)
     {
-        if($this->getParameters() instanceof ObjectConfigInterface) {
-            $context->data->setProperty('parameters', $this->getParameters()->toString());
+        if($context->data->getParameters() instanceof ObjectConfigInterface) {
+            $context->data->setProperty('parameters', $context->data->getParameters()->toString());
         }
     }
 }
