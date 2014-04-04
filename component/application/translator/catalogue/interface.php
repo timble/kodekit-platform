f1cfa0c56e3a39d288f2e82bbd120a10d25c682f
Arunas Mazeika <arunas@timble.net>
2014-04-04 18:09:53 +0200

    re #302 Re-factored the application catalogue interface.
    This allows for querying the catalogue for loaded translations.

--- component/application/translator/catalogue/interface.php
+++ component/application/translator/catalogue/interface.php
@@ -1,37 +1,39 @@
 <?php
 /**
- * Nooku Framework - http://www.nooku.org
- *
- * @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
- * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link           git://git.assembla.com/nooku-framework.git for the canonical source repository
- */
+* Nooku Framework - http://www.nooku.org
+*
+* @copyright      Copyright (C) 2011 - 2013 Johan Janssens and Timble CVBA. (http://www.timble.net)
+* @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+* @link           git://git.assembla.com/nooku-framework.git for the canonical source repository
+*/
 
 namespace Nooku\Component\Application;
 
 use Nooku\Library;
 
 /**
- * Translator Catalogue Interface
- *
- * @author  Arunas Mazeika <https://github.com/amazeika>
- * @package Nooku\Component\Koowa
- */
+* Translator Catalogue Interface
+*
+* @author  Arunas Mazeika <https://github.com/amazeika>
+* @package Nooku\Component\Koowa
+*/
 interface TranslatorCatalogueInterface extends Library\TranslatorCatalogueInterface
 {
     /**
-     * Import a translations into the catalogue.
+     * Sets a source as loaded.
      *
-     * @param array $translations     Associative array containing translations.
-     * @param bool  $override         Whether or not existing translations can be overridden during import.
+     * @param string $source The source.
+     *
+     * @return TranslatorCatalogueInterface
      */
-    public function import($translations, $override = false);
+    public function setLoaded($source);
 
     /**
-     * Tells if the catalogue contains a given string.
+     * Tells if translations from a given source are already loaded.
      *
-     * @param string $string The string.
-     * @return bool True if found, false otherwise.
+     * @param string $bundle The source to check against.
+     *
+     * @return bool True if loaded, false otherwise.
      */
-    public function hasString($string);
+    public function isLoaded($source);
 }
