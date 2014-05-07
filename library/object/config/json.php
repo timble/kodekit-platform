7caac89011fc75d961c0447d2355950c912e3cfe
Johan Janssens <johan@timble.net>
2014-05-07 22:11:49 +0200

    Throw DomainException if the json cannot be encoded or decoded.

--- library/object/config/json.php
+++ library/object/config/json.php
@@ -21,8 +21,8 @@ class ObjectConfigJson extends ObjectConfigFormat
      * Read from a string and create an array
      *
      * @param  string $string
-     * @return ObjectConfigJson
-     * @throws \RuntimeException
+     * @return $this
+     * @throws \DomainException  If the JSON cannot be decoded or if the encoded data is deeper than the recursion limit.
      */
     public function fromString($string)
     {
@@ -33,7 +33,7 @@ class ObjectConfigJson extends ObjectConfigFormat
             $data = json_decode($string, true);
 
             if($data === null) {
-                throw new \RuntimeException('Cannot decode JSON string');
+                throw new \DomainException('Cannot decode data from JSON string');
             }
         }
 
@@ -45,13 +45,18 @@ class ObjectConfigJson extends ObjectConfigFormat
     /**
      * Write a config object to a string.
      *
-     * @param  ObjectConfig $config
-     * @return string|false     Returns a JSON encoded string on success. False on failure.
+     * @return string|false    Returns a JSON encoded string on success. False on failure.
+     * @throws \DomainException Object could not be encoded to valid JSON.
      */
     public function toString()
     {
         $data = $this->toArray();
+        $data = json_encode($data);
 
-        return json_encode($data);
+        if($data === false) {
+            throw new \DomainException('Cannot encode data to JSON string');
+        }
+
+        return $data;
     }
 }
