ebbfd858b6a4f617e51e9bf9e92cdfe59e7bd9f8
Tom Janssens <tom@timble.net>
2014-05-12 12:49:36 +0200

    re #440 - Fixed typo

--- component/ckeditor/resources/assets/ckeditor/config.js
+++ component/ckeditor/resources/assets/ckeditor/config.js
@@ -41,7 +41,7 @@ CKEDITOR.editorConfig = function( config ) {
     };
 
     // Pasting operations will loose all formatting information
-    config.forcePasteAsPlainText = true,
+    config.forcePasteAsPlainText = true;
 
     config.allowedContent = true;
     config.toolbar_full =
