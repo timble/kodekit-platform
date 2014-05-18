9e2a64f8e1b7e4fbf26584aa66a169827802c8ac
Johan Janssens <johan@timble.net>
2014-05-19 00:59:11 +0200

    Fix - Passing a tags entity to the listbox does not work.

--- application/admin/component/articles/view/article/templates/default_sidebar.html.php
+++ application/admin/component/articles/view/article/templates/default_sidebar.html.php
@@ -47,7 +47,7 @@
             <? if($article->isTaggable()) : ?>
                 <fieldset>
                     <legend><?= translate('Tags') ?></legend>
-                    <?= helper('com:tags.listbox.tags', array('name' => 'tags[]', 'selected' => $article->getTags()->tags_tag_id, 'filter' => array('table' => 'articles'), 'attribs' => array('class' => 'select-tags', 'multiple' => 'multiple', 'style' => 'width:220px'))) ?>
+                    <?= helper('com:tags.listbox.tags', array('name' => 'tags[]', 'selected' => $article->getTags(), 'filter' => array('table' => 'articles'), 'attribs' => array('class' => 'select-tags', 'multiple' => 'multiple', 'style' => 'width:220px'))) ?>
                 </fieldset>
             <? endif ?>
         </div>
