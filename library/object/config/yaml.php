bd15b984b5387aa9d5d9b056d55fac7359ad92f5
Johan Janssens <johan@timble.net>
2014-07-22 02:51:23 +0200

    Allow to set the yaml encoder and decoder
    - Add ObjectConfigYaml::setDecoder()
    - Add ObjectConfigYaml::setEncoder()

--- library/object/config/yaml.php
+++ library/object/config/yaml.php
@@ -17,31 +17,109 @@ namespace Nooku\Library;
 class ObjectConfigYaml extends ObjectConfigFormat
 {
     /**
+     * YAML encoder callback
+     *
+     * @var callable
+     */
+    protected $_encoder;
+
+    /**
+     * YAML decoder callback
+     *
+     * @var callable
+     */
+    protected $_decoder;
+
+    /**
+     * Constructor.
+     *
+     * @param   array|ObjectConfig An associative array of configuration options or a KObjectConfig instance.
+     */
+    public function __construct( $options = array() )
+    {
+        parent::__construct($options);
+
+        if (function_exists('yaml_emit')) {
+            $this->setEncoder('yaml_emit');
+        }
+
+        if (function_exists('yaml_parse')) {
+            $this->setDecoder('yaml_parse');
+        }
+    }
+
+    /**
+     * Get callback for encoding YAML
+     *
+     * @return callable
+     */
+    public function getEncoder()
+    {
+        return $this->_ecncoder;
+    }
+
+    /**
+     * Set callback for encoding YAML
+     *
+     * @param  callable $encoder the encoder to set
+     * @throws \InvalidArgumentException
+     * @return ObjectConfigYaml
+     */
+    public function setEncoder(callable $encoder)
+    {
+        $this->_encoder = $encoder;
+        return $this;
+    }
+
+    /**
+     * Get callback for decoding YAML
+     *
+     * @return callable
+     */
+    public function getDecoder()
+    {
+        return $this->_decoder;
+    }
+
+    /**
+     * Set callback for decoding YAML
+     *
+     * @param  callable $decoder the decoder to set
+     * @throws \InvalidArgumentException
+     * @return ObjectConfigYaml
+     */
+    public function setDecoder(callable $decoder)
+    {
+        $this->_decoder = $decoder;
+        return $this;
+    }
+
+    /**
      * Read from a YAML string and create a config object
      *
      * @param  string $string
-     * @return ObjectConfigYaml
+     * @throws \DomainException
      * @throws \RuntimeException
+     * @return ObjectConfigYaml
      */
     public function fromString($string)
     {
-        $config = false;
-
-        if(function_exists('yaml_parse'))
+        if ($decoder = $this->getDecoder())
         {
             $data = array();
 
             if(!empty($string))
             {
-                $data = yaml_parse($string);
+                $data = call_user_func($decoder, $string);
 
                 if($data === false) {
-                    throw new \RuntimeException('Cannot parse YAML string');
+                    throw new \DomainException('Cannot parse YAML string');
                 }
             }
 
             $this->add($data);
         }
+        else throw new \RuntimeException("No Yaml decoder specified");
 
         return $this;
     }
@@ -49,18 +127,18 @@ class ObjectConfigYaml extends ObjectConfigFormat
     /**
      * Write a config object to a YAML string.
      *
-     * @param  ObjectConfig $config
      * @return string|false     Returns a YAML encoded string on success. False on failure.
      */
     public function toString()
     {
         $result = false;
 
-        if(function_exists('yaml_emit'))
+        if ($encoder = $this->getEncoder())
         {
             $data   = $this->toArray();
-            $result = yaml_emit($data, \YAML_UTF8_ENCODING);
+            $result = call_user_func($encoder, $data);
         }
+        else throw new \RuntimeException("No Yaml encoder specified");
 
         return $result;
     }
