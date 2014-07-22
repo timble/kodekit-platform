d48335681bc708fd603d7a290e4c4d17b6dc6ac5
Johan Janssens <johan@timble.net>
2014-07-23 00:14:57 +0200

    re #302 : Add legacy support for json_last_error_msg()

--- library/legacy.php
+++ library/legacy.php
@@ -15,7 +15,7 @@ if(extension_loaded('apc') && !function_exists('apc_exists'))
     /**
      * Check if an APC key exists
      *
-     * @param  mixed  A string, or an array of strings, that contain keys.
+     * @param  mixed  $keys A string, or an array of strings, that contain keys.
      * @return boolean Returns TRUE if the key exists, otherwise FALSE
      */
     function apc_exists($keys)
@@ -23,5 +23,37 @@ if(extension_loaded('apc') && !function_exists('apc_exists'))
 		$r;
 		apc_fetch($keys,$r);
 		return $r;
+    }
+}
+
+/**
+ * PHP 5.5 compatibility
+ */
+if (!function_exists('json_last_error_msg'))
+{
+    function json_last_error_msg()
+    {
+        switch (json_last_error())
+        {
+            case JSON_ERROR_DEPTH:
+                $error = 'Maximum stack depth exceeded';
+                break;
+            case JSON_ERROR_STATE_MISMATCH:
+                $error = 'Underflow or the modes mismatch';
+                break;
+            case JSON_ERROR_CTRL_CHAR:
+                $error = 'Unexpected control character found';
+                break;
+            case JSON_ERROR_SYNTAX:
+                $error = 'Syntax error, malformed JSON';
+                break;
+            case JSON_ERROR_UTF8:
+                $error = 'Malformed UTF-8 characters, possibly incorrectly encoded';
+                break;
+            default:
+                $error = 'Unknown Error';
+        }
+
+        return $error;
     }
 }
