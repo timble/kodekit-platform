00981727058db6d94931f1a04dd3add5849b7e5b
Johan Janssens <johan@timble.net>
2014-08-05 02:57:50 +0200

    Add additional check. Context can be NULL.

--- library/filesystem/stream/factory.php
+++ library/filesystem/stream/factory.php
@@ -76,7 +76,7 @@ class FilesystemStreamFactory extends Object implements ObjectSingleton
         $name   = !empty($scheme) ? $scheme : 'file';
 
         //Invalid context
-        if (!is_array($context) && !is_resource($context) && !get_resource_type($context) == 'stream-context')
+        if (!is_null($context) && !is_array($context) && !is_resource($context) && !get_resource_type($context) == 'stream-context')
         {
             throw new \InvalidArgumentException(sprintf(
                 'Context must be an array or a resource of type stream-context; received "%s"', gettype($context)
@@ -95,7 +95,7 @@ class FilesystemStreamFactory extends Object implements ObjectSingleton
         if (is_resource($context)) {
             $options = stream_context_get_options($context);
         } else {
-            $options = $context;
+            $options = (array) $context;
         }
 
         //Create the stream
