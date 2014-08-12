461203cac41205f9f8a4006ffb8b6a46408e2bd1
Johan Janssens <johan@timble.net>
2014-08-12 19:47:54 +0200

    Make getConfig() final.

--- library/object/object.php
+++ library/object/object.php
@@ -323,7 +323,7 @@ class Object implements ObjectInterface, ObjectHandlable, ObjectMixable, ObjectD
      * @param  mixed $identifier An ObjectIdentifier, identifier string or object implementing ObjectInterface
      * @return ObjectConfig
      */
-    public function getConfig($identifier = null)
+    final public function getConfig($identifier = null)
     {
         if (isset($identifier)) {
             $result = $this->__object_manager->getIdentifier($identifier)->getConfig();
