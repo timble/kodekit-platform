d33cea264444f50a62d4b825b24c7dc8f38239d7
Terry Visser <sittcomm@gmail.com>
2014-11-07 11:55:15 +0100

    Typo in modules model

--- component/pages/model/modules.php
+++ component/pages/model/modules.php
@@ -170,7 +170,7 @@ class ModelModules extends Library\ModelTable
                 foreach($extension as $extension)
                 {
                     $path  = Library\ClassLoader::getInstance()->getApplication('site');
-                    $path .= '/component/'.substr($extension->name, 4).'/modules';
+                    $path .= '/component/'.substr($extension->name, 4).'/module';
 
                     if(!is_dir($path)) {
                         continue;
