6b7512dfa355e3a8aac5c4e7b8e10051acb3fd9c
Arunas Mazeika <arunas@timble.net>
2014-11-26 16:43:02 +0100

    re #435 Added groups table object.

---
+++ component/users/database/table/groups.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Nooku Platform - http://www.nooku.org/platform
+ *
+ * @copyright	Copyright (C) 2011 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/nooku/nooku-platform for the canonical source repository
+ */
+
+namespace Nooku\Component\Users;
+
+use Nooku\Library;
+
+/**
+ * Groups Database Table
+ *
+ * @author  Arunas Mazeika <http://github.com/amazeika>
+ * @package Nooku\Component\Users
+ */
+class DatabaseTableGroups extends Library\DatabaseTableAbstract
+{
+    protected function _initialize(Library\ObjectConfig $config)
+    {
+        $config->append(array(
+            'behaviors' => array(
+                'identifiable',
+                'relatable' => array(
+                    'values'  => 'users',
+                    'columns' => array(
+                        'collection' => 'users_user_id',
+                        'item'       => 'users_group_id'
+                    )
+                )
+            )
+        ));
+
+        parent::_initialize($config);
+    }
+}
