075cb6731bdbf468bc692806e63b71e61eca46f1
Arunas Mazeika <arunas@timble.net>
2014-12-11 14:27:05 +0100

    re #448 Added missing docblocks.

--- application/admin/component/activities/model/entity/activity.php
+++ application/admin/component/activities/model/entity/activity.php
@@ -1,13 +1,20 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: arunasmazeika
- * Date: 04/12/2014
- * Time: 16:36
+ * Nooku Platform - http://www.nooku.org/platform
+ *
+ * @copyright	Copyright (C) 2011 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/nooku/nooku-platform for the canonical source repository
  */
 
 use Nooku\Library, Nooku\Component\Activities;
 
+/**
+ * Activity Model Entity.
+ *
+ * @author  Arunas Mazeika <http://github.com/amazeika>
+ * @package Nooku\Component\Activities
+ */
 class ActivitiesModelEntityActivity extends Activities\ModelEntityActivity implements \Nooku\Library\ObjectInstantiable
 {
     public static function getInstance(Library\ObjectConfig $config, Library\ObjectManagerInterface $manager)
