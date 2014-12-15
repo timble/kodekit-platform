6511288190dfbe685d403d4601a147320738d4a3
Arunas Mazeika <arunas@timble.net>
2014-12-15 12:15:31 +0100

    re #448 Added tag and category Articles activity entities.

---
+++ application/admin/component/articles/activity/tag.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Nooku Platform - http://www.nooku.org/platform
+ *
+ * @copyright	Copyright (C) 2011 - 2014 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/nooku/nooku-platform for the canonical source repository
+ */
+
+use Nooku\Library;
+
+/**
+ * Articles Tag Activity
+ *
+ * @author  Arunas Mazeika <http://github.com/amazeika>
+ * @package Component\Users
+ */
+class ArticlesActivityTag extends ActivitiesModelEntityActivity
+{
+    protected function _initialize(Library\ObjectConfig $config)
+    {
+        $config->append(array('object_table' => 'tags', 'object_column' => 'tags_tag_id'));
+        parent::_initialize($config);
+    }
+}
