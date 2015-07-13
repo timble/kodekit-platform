31b933ff4887a44bba8fb90df1e894eefa277e34
Oli Griffiths <oli@tumblr.com>
2015-07-13 10:17:12 -0400

    Added JsonSerializable to ObjectConfig

--- library/object/config/config.php
+++ library/object/config/config.php
@@ -18,7 +18,7 @@ namespace Nooku\Library;
  * @author  Johan Janssens <http://github.com/johanjanssens>
  * @package Nooku\Library\Object
  */
-class ObjectConfig implements ObjectConfigInterface
+class ObjectConfig implements ObjectConfigInterface, \JsonSerializable
 {
     /**
      * The configuration options
@@ -445,5 +445,17 @@ class ObjectConfig implements ObjectConfigInterface
         }
 
         $this->__options = $array;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.4.0)<br/>
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
     }
 }
