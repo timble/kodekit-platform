7c5cbe533a7521c8941584faec105664dd825ed5
Ercan Ozkaya <ercan@timble.net>
2015-08-05 17:05:42 +0300

    Add DispatcherRequestTransport package

--- library/dispatcher/request/abstract.php
+++ library/dispatcher/request/abstract.php
@@ -95,14 +95,42 @@ class DispatcherRequestAbstract extends ControllerRequest implements DispatcherR
     protected static $_formats;
 
     /**
+     * The transport queue
+     *
+     * @var	ObjectQueue
+     */
+    protected $_queue;
+
+    /**
+     * List of request transports
+     *
+     * @var array
+     */
+    protected $_transports;
+
+    /**
      * Constructor
      *
      * @param ObjectConfig $config  An optional ObjectConfig object with configuration options
-     * @return DispatcherRequest
      */
     public function __construct(ObjectConfig $config)
     {
         parent::__construct($config);
+
+        //Create the transport queue
+        $this->_queue = $this->getObject('lib:object.queue');
+
+        //Attach the request transport handlers
+        $transports = (array) ObjectConfig::unbox($config->transports);
+
+        foreach ($transports as $key => $value)
+        {
+            if (is_numeric($key)) {
+                $this->attachTransport($value);
+            } else {
+                $this->attachTransport($key, $value);
+            }
+        }
 
         //Set the trusted proxies
         $this->setProxies(ObjectConfig::unbox($config->proxies));
@@ -124,102 +152,7 @@ class DispatcherRequestAbstract extends ControllerRequest implements DispatcherR
             $this->addFormat($format, $mimetypes);
         }
 
-        //Set document root for IIS
-        if(!isset($_SERVER['DOCUMENT_ROOT']))
-        {
-            if(isset($_SERVER['SCRIPT_FILENAME'])) {
-                $_SERVER['DOCUMENT_ROOT'] = str_replace( '\\', '/', substr($_SERVER['SCRIPT_FILENAME'], 0, 0 - strlen($_SERVER['PHP_SELF'])));
-            }
-
-            if(isset($_SERVER['PATH_TRANSLATED'])) {
-                $_SERVER['DOCUMENT_ROOT'] = str_replace( '\\', '/', substr(str_replace('\\\\', '\\', $_SERVER['PATH_TRANSLATED']), 0, 0 - strlen($_SERVER['PHP_SELF'])));
-            }
-         }
-
-        //Set the authorization
-        if (!isset($_SERVER['PHP_AUTH_USER']))
-        {
-            /*
-             * If you are running PHP as CGI. Apache does not pass HTTP Basic user/pass to PHP by default.
-             * To fix this add these lines to your .htaccess file:
-             *
-             * RewriteCond %{HTTP:Authorization} ^(.+)$
-             * RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
-             */
-
-            //When using PHP-FPM HTTP_AUTHORIZATION is called REDIRECT_HTTP_AUTHORIZATION
-            if(isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
-                $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
-            }
-
-            // Decode AUTHORIZATION header into PHP_AUTH_USER and PHP_AUTH_PW when authorization header is basic
-            if (isset($_SERVER['HTTP_AUTHORIZATION']) && stripos($_SERVER['HTTP_AUTHORIZATION'], 'basic') === 0)
-            {
-                $exploded = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'] , 6)));
-                if (count($exploded) == 2) {
-                    list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = $exploded;
-                }
-            }
-        }
-
-        //Set the headers
-        $headers = array();
-        foreach ($_SERVER as $key => $value)
-        {
-            if ($value && strpos($key, 'HTTP_') === 0)
-            {
-                // Cookies are handled using the $_COOKIE superglobal
-                if (strpos($key, 'HTTP_COOKIE') === 0) {
-                    continue;
-                }
-
-                $headers[substr($key, 5)] = $value;
-            }
-            elseif ($value && strpos($key, 'CONTENT_') === 0)
-            {
-                $name = substr($key, 8); // Content-
-                $name = 'Content-' . (($name == 'MD5') ? $name : ucfirst(strtolower($name)));
-
-                $headers[$name] = $value;
-            }
-        }
-
-        if(isset($_SERVER['PHP_AUTH_USER']))
-        {
-            $headers['PHP_AUTH_USER'] = $_SERVER['PHP_AUTH_USER'];
-            $headers['PHP_AUTH_PW']   = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
-        }
-
-        $this->_headers->add($headers);
-
-        //Set the version
-        if (isset($_SERVER['SERVER_PROTOCOL']) && strpos($_SERVER['SERVER_PROTOCOL'], '1.0') !== false) {
-            $this->setVersion('1.0');
-        }
-
-        //Set request data
-        if($this->getContentType() == 'application/x-www-form-urlencoded')
-        {
-            if (in_array($this->getMethod(), array('PUT', 'DELETE', 'PATCH')))
-            {
-                parse_str($this->getContent(), $data);
-                $this->data->add($data);
-            }
-        }
-
-        if($this->getContentType() == 'application/json')
-        {
-            if(in_array($this->getMethod(), array('POST', 'PUT', 'DELETE', 'PATCH')))
-            {
-                $data = array();
-
-                if ($content = $this->getContent()) {
-                    $data = json_decode($content, true);
-                }
-
-                $this->data->add($data);
-            }
-        }
+        $this->receive();
     }
 
     /**
@@ -233,6 +166,7 @@ class DispatcherRequestAbstract extends ControllerRequest implements DispatcherR
     protected function _initialize(ObjectConfig $config)
     {
         $config->append(array(
+            'transports'  => array('server', 'headers', 'data'),
             'base_url'  => '/',
             'base_path' => null,
             'method'    => null,
@@ -258,6 +192,84 @@ class DispatcherRequestAbstract extends ControllerRequest implements DispatcherR
         ));
 
         parent::_initialize($config);
+    }
+
+    /**
+     * Receive the request by passing it through transports
+     */
+    public function receive()
+    {
+        foreach($this->_queue as $transport)
+        {
+            if($transport instanceof DispatcherRequestTransportInterface) {
+                $transport->receive($this);
+            }
+        }
+    }
+
+    /**
+     * Get a transport handler by identifier
+     *
+     * @param   mixed $transport An object that implements ObjectInterface, ObjectIdentifier object
+     *                                 or valid identifier string
+     * @param   array $config An optional associative array of configuration settings
+     * @throws \UnexpectedValueException
+     * @return DispatcherRequestTransportInterface
+     */
+    public function getTransport($transport, $config = array())
+    {
+        //Create the complete identifier if a partial identifier was passed
+        if (is_string($transport) && strpos($transport, '.') === false)
+        {
+            $identifier = $this->getIdentifier()->toArray();
+
+            if($identifier['package'] != 'dispatcher') {
+                $identifier['path'] = array('dispatcher', 'request', 'transport');
+            } else {
+                $identifier['path'] = array('request', 'transport');
+            }
+
+            $identifier['name'] = $transport;
+            $identifier = $this->getIdentifier($identifier);
+        }
+        else $identifier = $this->getIdentifier($transport);
+
+        if (!isset($this->_transports[$identifier->name]))
+        {
+            $transport = $this->getObject($identifier, array_merge($config, array('request' => $this)));
+
+            if (!($transport instanceof DispatcherRequestTransportInterface))
+            {
+                throw new \UnexpectedValueException(
+                    "Transport handler $identifier does not implement DispatcherRequestTransportInterface"
+                );
+            }
+
+            $this->_transports[$transport->getIdentifier()->name] = $transport;
+        }
+        else $transport = $this->_transports[$identifier->name];
+
+        return $transport;
+    }
+
+    /**
+     * Attach a transport handler
+     *
+     * @param   mixed  $transport An object that implements ObjectInterface, ObjectIdentifier object
+     *                            or valid identifier string
+     * @param   array $config  An optional associative array of configuration settings
+     * @return  DispatcherRequestAbstract
+     */
+    public function attachTransport($transport, $config = array())
+    {
+        if (!($transport instanceof DispatcherRequestTransportInterface)) {
+            $transport = $this->getTransport($transport, $config);
+        }
+
+        //Enqueue the transport handler in the command chain
+        $this->_queue->enqueue($transport, $transport->getPriority());
+
+        return $this;
     }
 
     /**
---
+++ library/dispatcher/request/transport/abstract.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Nooku Platform - http://www.nooku.org/platform
+ *
+ * @copyright	Copyright (C) 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/nooku/nooku-platform for the canonical source repository
+ */
+
+namespace Nooku\Library;
+
+/**
+ * Abstract Dispatcher Request Transport
+ *
+ * @author  Ercan Ozkaya <https://github.com/ercanozkaya>
+ * @package Nooku\Library\Dispatcher\Request\Transport
+ */
+abstract class DispatcherRequestTransportAbstract extends Object implements DispatcherRequestTransportInterface
+{
+    /**
+     * The filter priority
+     *
+     * @var integer
+     */
+    protected $_priority;
+
+    /**
+     * Constructor.
+     *
+     * @param ObjectConfig $config 	An optional KObjectConfig object with configuration options.
+     */
+    public function __construct(ObjectConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->_priority = $config->priority;
+    }
+
+    /**
+     * Initializes the options for the object
+     *
+     * Called from {@link __construct()} as a first step of object instantiation.
+     *
+     * @param 	ObjectConfig $config 	An optional ObjectConfig object with configuration options.
+     * @return 	void
+     */
+    protected function _initialize(ObjectConfig $config)
+    {
+        $config->append(array(
+            'priority' => self::PRIORITY_NORMAL,
+        ));
+
+        parent::_initialize($config);
+    }
+
+    /**
+     * Get the priority of a behavior
+     *
+     * @return  integer The command priority
+     */
+    public function getPriority()
+    {
+        return $this->_priority;
+    }
+}
