63b995b7258cf1eb853d273c8b81f78a3d11acf0
Johan Janssens <johan@timble.net>
2015-10-13 03:46:43 +0200

    re #449 : Move ApplicationModelEntityPages into Nook\Component\Pages\ModelEntityPages

--- component/pages/model/entity/pages.php
+++ component/pages/model/entity/pages.php
@@ -19,6 +19,36 @@ use Nooku\Library;
  */
 class ModelEntityPages extends Library\ModelEntityRowset
 {
+    /**
+     * The active page
+     *
+     * @var ModelEntityPage
+     */
+    protected $_active;
+
+    /**
+     * The primary language
+     *
+     * @var ModelEntityPage
+     */
+    protected $_primary;
+
+    public function __construct(Library\ObjectConfig $config )
+    {
+        parent::__construct($config);
+
+        //Calculate the page routes
+        foreach($this as $page)
+        {
+            $path = array();
+            foreach(explode('/', $page->path) as $id) {
+                $path[] = $this->find($id)->slug;
+            }
+
+            $page->route = implode('/', $path);
+        }
+    }
+
     public function find($needle)
     {
         $result = null;
@@ -52,6 +82,64 @@ class ModelEntityPages extends Library\ModelEntityRowset
             }
         }
         else $result = parent::find($needle);
+
+        return $result;
+    }
+
+    public function getPage($id)
+    {
+        $page = $this->find($id);
+        return $page;
+    }
+
+    public function setActive($active)
+    {
+        if(is_numeric($active)) {
+            $this->_active = $this->find($active);
+        } else {
+            $this->_active = $active;
+        }
+
+        return $this;
+    }
+
+    public function getActive()
+    {
+        return $this->_active;
+    }
+
+    public function getPrimary()
+    {
+        if(!isset($this->_primary)) {
+            $this->_primary = $this->find(array('home' => 1));
+        }
+
+        return $this->_primary;
+    }
+
+    public function isAuthorized($id, Library\UserInterface $user)
+    {
+        $result = true;
+        $page   = $this->find($id);
+
+        // Return false if page not found.
+        if(!is_null($page))
+        {
+            if($page->access || $page->users_group_id > 0)
+            {
+                // Return false if page has access set, but user is a guest.
+                if($user->isAuthentic())
+                {
+                    // Return false if page has group set, but user is not in that group.
+                    if($page->users_group_id && !in_array($page->users_group_id, $user->getGroups()))
+                    {
+                        $result = false;
+                    }
+                }
+                else $result = false;
+            }
+        }
+        else $result = false;
 
         return $result;
     }
