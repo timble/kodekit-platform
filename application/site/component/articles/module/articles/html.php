f6432502b7fa427e464d45ae52b314c3b5831946
Johan Janssens <johan@timble.net>
2015-10-13 04:26:25 +0200

    re #444 : Refactor articles module
    - Extend from Pages\ModuleAbstract
    - Render articles through articles controller

--- application/site/component/articles/module/articles/html.php
+++ application/site/component/articles/module/articles/html.php
@@ -8,6 +8,7 @@
  */
 
 use Nooku\Library;
+use Nooku\Component\Pages;
 
 /**
  * Articles Module Html View
@@ -15,13 +16,31 @@ use Nooku\Library;
  * @author  Arunas Mazeika <http://github.com/amazeika>
  * @package Component\Articles
  */
-class ArticlesModuleArticlesHtml extends PagesModuleDefaultHtml
+class ArticlesModuleArticlesHtml extends Pages\ModuleAbstract
 {
-    protected function _fetchData(Library\ViewContext $context)
+    /**
+     * Renders and echo's the views output
+     *
+     * @return ArticlesModuleArticlesHtml
+     */
+    protected function _actionRender(Library\ViewContext $context)
     {
-        $params = $this->module->getParameters();
+        //Force layout type to 'mod' to force using the module locator for partial layouts
+        $layout = $context->layout;
+
+        if (is_string($layout) && strpos($layout, '.') === false)
+        {
+            $identifier = $this->getIdentifier()->toArray();
+            $identifier['type'] = 'mod';
+            $identifier['name'] = $layout;
+            unset($identifier['path'][0]);
+
+            $context->layout = $this->getIdentifier($identifier);
+        }
 
         // Preparing the sort and direction model states.
+        $params = $context->data->module->getParameters();
+
         switch ($params->get('sort_by', 'newest'))
         {
             default:
@@ -45,31 +64,20 @@ class ArticlesModuleArticlesHtml extends PagesModuleDefaultHtml
             $category = explode(',', $category);
         }
 
-        // Prepare section state.
-        $section = str_replace(' ', '', $params->get('section', ''));
-        if ($section) {
-            $section = explode(',', $section);
-        }
-
-        // Get access id.
+        //Get user
         $user = $this->getObject('user');
 
-        $articles = $this->getObject('com:articles.model.articles')
-            ->set(array(
-            'access'    => $user->isAuthentic(),
-            'published' => 1,
-            'limit'     => $params->get('count', 5),
-            'sort'      => $sort,
-            'direction' => $direction,
-            'section'   => $section,
-            'category'  => $category))
-            ->fetch();
+        //Render the articles through the article controller
+        $result = $this->getObject('com:articles.controller.article')
+            ->access($user->isAuthentic())
+            ->published(1)
+            ->limit($params->get('count', 5))
+            ->sort($sort)
+            ->direction($direction)
+            ->category($category)
+            ->layout($context->layout)
+            ->render();
 
-        $context->data->articles = $articles;
-
-        // Set layout based on params.
-        $this->setLayout($params->get('show_content', 0) ? 'articles' : 'links');
-
-        parent::_fetchData($context);
+        return $result;
     }
 }
