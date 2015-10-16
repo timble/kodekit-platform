1c5aee58d62d4195405dce930faeb61eec78a57a
Johan Janssens <johan@timble.net>
2015-10-16 02:41:59 +0200

    re #449 : Remove support for page types

--- application/admin/component/pages/view/page/templates/default_page.html.php
+++ application/admin/component/pages/view/page/templates/default_page.html.php
@@ -8,8 +8,6 @@
  */
 ?>
 
-<? if(parameters()->type['name'] == 'component') : ?>
-
 <?= $page->getParams('url')->render('urlparams') ?>
 
 <? if($rendered_params = $page->getParams('layout')->render('params')) : ?>
@@ -17,13 +15,3 @@
 <? endif ?>
 
 <?= $page->getParams('page')->render('params'); ?>
-<? endif ?>
-
-<? if(parameters()->type['name'] == 'pagelink') : ?>
-    <div>
-        <label for="parent"><?= translate('Page') ?></label>
-        <div id="parent" class="controls">
-            <?= helper('listbox.pages', array('name' => 'link_id', 'selected' => $page->link_id)) ?>
-        </div>
-    </div>
-<? endif ?>
