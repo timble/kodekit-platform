568153bead2ee9a4b5b5f3aa7ba896e92464fe91
Johan Janssens <johan@timble.net>
2015-11-27 02:46:05 +0100

    re #449 : Rename 'wrapped' to 'wrapper'

--- application/site/component/application/view/document/templates/default.html.php
+++ application/site/component/application/view/document/templates/default.html.php
@@ -26,7 +26,7 @@
 <div class="container">
     <div class="row">
         <aside class="sidebar col-md-3">
-            <ktml:block name="left" decorator="wrapped">
+            <ktml:block name="left" decorator="wrapper">
         </aside>
         <div class="col-md-9">
             <ktml:block name="breadcrumb">
