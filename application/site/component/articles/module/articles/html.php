b2cdab8dae7fb2fcdb65c0971b2e45052b7e9baa
Johan Janssens <johan@timble.net>
2015-11-28 03:21:49 +0100

    re #449 : Extend module from Pages\ModuleEntity

--- application/site/component/articles/module/articles/html.php
+++ application/site/component/articles/module/articles/html.php
@@ -16,68 +16,21 @@ use Nooku\Component\Pages;
  * @author  Arunas Mazeika <http://github.com/amazeika>
  * @package Component\Articles
  */
-class ArticlesModuleArticlesHtml extends Pages\ModuleAbstract
+class ArticlesModuleArticlesHtml extends Pages\ModuleEntity
 {
-    /**
-     * Renders and echo's the views output
-     *
-     * @return ArticlesModuleArticlesHtml
-     */
-    protected function _actionRender(Library\ViewContext $context)
+    protected function _initialize(Library\ObjectConfig $config)
     {
-        //Force layout type to 'mod' to force using the module locator for partial layouts
-        $layout = $context->layout;
+        $config->append(array(
+            'parameters' => array(
+                'access'    => $this->getObject('user')->isAuthentic(),
+                'limit'     => 5,
+                'sort'      => 'created_on',
+                'direction' => 'DESC',
+                'published' => 1,
+                'category'  => null
+            ),
+        ));
 
-        if (is_string($layout) && strpos($layout, '.') === false)
-        {
-            $identifier = $this->getIdentifier()->toArray();
-            $identifier['type'] = 'mod';
-            $identifier['name'] = $layout;
-            unset($identifier['path'][0]);
-
-            $context->layout = $this->getIdentifier($identifier);
-        }
-
-        // Preparing the sort and direction model states.
-        $params = $context->data->module->getParameters();
-
-        switch ($params->get('sort_by', 'newest'))
-        {
-            default:
-            case 'newest':
-                $sort      = 'created_on';
-                $direction = 'DESC';
-                break;
-            case 'oldest':
-                $sort      = 'created_on';
-                $direction = 'ASC';
-                break;
-            case 'ordering':
-                $sort      = 'ordering';
-                $direction = 'ASC';
-                break;
-        }
-
-        // Prepare category state.
-        $category = str_replace(' ', '', $params->get('category', ''));
-        if ($category) {
-            $category = explode(',', $category);
-        }
-
-        //Get user
-        $user = $this->getObject('user');
-
-        //Render the articles through the article controller
-        $result = $this->getObject('com:articles.controller.article')
-            ->access($user->isAuthentic())
-            ->published(1)
-            ->limit($params->get('count', 5))
-            ->sort($sort)
-            ->direction($direction)
-            ->category($category)
-            ->layout($context->layout)
-            ->render();
-
-        return $result;
+        parent::_initialize($config);
     }
 }
