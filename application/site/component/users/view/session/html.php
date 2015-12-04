1d6d980e9a899966d9e9c44bfbdd80ee4aed2687
Johan Janssens <johan@timble.net>
2015-12-04 15:44:36 +0100

    re #449 - Fixed typo

--- application/site/component/users/view/session/html.php
+++ application/site/component/users/view/session/html.php
@@ -19,7 +19,7 @@ class UsersViewSessionHtml extends Library\ViewHtml
 {
     protected function _actionRender(Library\ViewContext $context)
     {
-        $pathway   = $this->getObject('pages')->getPatwhay();
+        $pathway   = $this->getObject('pages')->getPathway();
         $pathway[] = array('title' => $this->getObject('translator')->translate('Login'));
 
         return parent::_actionRender($context);
