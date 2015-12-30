766477802561ec698624e62d4fbb0981771965d1
Johan Janssens <johan@timble.net>
2015-12-30 03:40:00 +0100

    re #449 - Render menu module through pages controller using Pages\ModuleEntity

--- application/site/component/pages/module/menu/html.php
+++ application/site/component/pages/module/menu/html.php
@@ -11,60 +11,25 @@ use Nooku\Library;
 use Nooku\Component\Pages;
 
 /**
- * Menu Module Html View
+ * Pages Module Html View
  *
- * @author  Gergo Erdosi <http://github.com/gergoerdosi>
+ * @author  Johan Janssens <http://github.com/johanjanssens>
  * @package Component\Pages
  */
-class PagesModuleMenuHtml extends Pages\ModuleAbstract
+class PagesModuleMenuHtml extends Pages\ModuleEntity
 {
-    protected function _fetchData(Library\ViewContext $context)
+    protected function _initialize(Library\ObjectConfig $config)
     {
-        $params = $context->data->module->getParameters();
+        $config->append(array(
+            'controller' => 'com:pages.controller.pages',
+            'parameters' => array(
+                'menu'      => 1,
+                'hidden'    => 0,
+                'published' => true,
+            ),
+        ));
 
-        $start    = $params->get('start_level');
-        $end      = $params->get('end_level');
-        $children = $params->get('show_children', 'active');
-        $pages    = $this->getObject('pages');
-        $groups   = $this->getObject('user')->getGroups();
-
-        // Make sure that pages without an assigned group are also included.
-        $groups[] = 0;
-
-        $context->data->active = $pages->getActive();
-        $context->data->pages  = $pages->find(array(
-                'pages_menu_id'  => $params->get('menu_id'),
-                'hidden'         => 0,
-                'users_group_id' => $groups)
-        );
-
-        foreach(clone $context->data->pages as $page)
-        {
-            $extract = false;
-
-            // Extract if level is lower than start.
-            if($page->level < $start) {
-                $extract = true;
-            }
-
-            // Extract if level is higher than end.
-            if(!$extract && $end > $start && $page->level > $end) {
-                $extract = true;
-            }
-
-            // Extract if path is not in the active branch.
-            if(!$extract && $children == 'active' && $page->level > 1)
-            {
-                if(implode('/', $page->getParentIds()) != implode('/', array_slice(explode('/', $context->data->active->path), 0, count($page->getParentIds())))) {
-                    $extract = true;
-                }
-            }
-
-            if($extract) {
-                $context->data->pages->remove($page);
-            }
-        }
-
-        parent::_fetchData($context);
+        parent::_initialize($config);
     }
 }
+
