fb0d4004a54e4a4639ac2c91745cb1cb28ef6c93
Johan Janssens <johan@timble.net>
2016-03-06 23:25:48 +0100

    Refactor application dispatcher
    - Remove getTitle() method
    - Remove getComponent() method
    - Remove setComponent() method

--- component/application/dispatcher/dispatcher.php
+++ component/application/dispatcher/dispatcher.php
@@ -27,26 +27,6 @@ class Dispatcher extends Library\DispatcherAbstract implements Library\ObjectIns
     private $__site;
 
     /**
-     * Component name or identifier
-     *
-     * @var	string|object
-     */
-    protected $_component;
-
-    /**
-     * Constructor.
-     *
-     * @param Library\ObjectConfig $config	An optional Library\ObjectConfig object with configuration options.
-     */
-    public function __construct(Library\ObjectConfig $config)
-    {
-        parent::__construct($config);
-
-        //Set the component
-        $this->_component = $config->component;
-    }
-
-    /**
      * Initializes the options for the object
      *
      * Called from {@link __construct()} as a first step of object instantiation.
@@ -58,12 +38,10 @@ class Dispatcher extends Library\DispatcherAbstract implements Library\ObjectIns
     {
         $config->append(array(
             'dispatched' => true,
-            'controller' => 'document',
-            'component'  => '',
+            'controller' => '',
             'request'    => array('
                 base_url'   => '/'
             ),
-            'title'      => 'Application',
         ));
 
         parent::_initialize($config);
@@ -89,16 +67,6 @@ class Dispatcher extends Library\DispatcherAbstract implements Library\ObjectIns
         }
 
         return $manager->getObject('application');
-    }
-
-    /**
-     * Get the application title
-     *
-     * @return string
-     */
-    public function getTitle()
-    {
-        return $this->getConfig()->title;
     }
 
     /**
@@ -178,74 +146,6 @@ class Dispatcher extends Library\DispatcherAbstract implements Library\ObjectIns
     }
 
     /**
-     * Method to get a component dispatcher object
-     *
-     * @throws	\UnexpectedValueException	If the dispatcher doesn't implement the DispatcherInterface
-     * @return	Library\DispatcherInterface
-     */
-    public function getComponent()
-    {
-        if(!($this->_component instanceof Library\DispatcherInterface))
-        {
-            //Make sure we have a controller identifier
-            if(!($this->_component instanceof Library\ObjectIdentifier)) {
-                $this->setComponent($this->_component);
-            }
-
-            $config = array(
-                'request' 	 => $this->getRequest(),
-                'user'       => $this->getUser(),
-                'response'   => $this->getResponse(),
-                'dispatched' => true
-            );
-
-            $this->_component = $this->getObject($this->_component, $config);
-
-            //Make sure the controller implements ControllerInterface
-            if(!$this->_component instanceof Library\DispatcherInterface)
-            {
-                throw new \UnexpectedValueException(
-                    'Dispatcher: '.get_class($this->_controller).' does not implement DispatcherInterface'
-                );
-            }
-        }
-
-        return $this->_component;
-    }
-
-    /**
-     * Method to set a component object attached to the dispatcher
-     *
-     * @param	mixed	$component An object that implements DispatcherInterface, ObjectIdentifier object
-     * 					            or valid identifier string
-     * @param  array  $config  An optional associative array of configuration options
-     * @return Dispatcher
-     */
-    public function setComponent($component, $config = array())
-    {
-        if(!($component instanceof Library\DispatcherInterface))
-        {
-            if(is_string($component) && strpos($component, '.') === false )
-            {
-                $identifier			   = $this->getIdentifier()->toArray();!
-                $identifier['package'] = $component;
-
-                $identifier = $this->getIdentifier($identifier);
-            }
-            else $identifier = $this->getIdentifier($component);
-
-            //Set the configuration
-            $identifier->getConfig()->append($config);
-
-            $component = $identifier;
-        }
-
-        $this->_component = $component;
-
-        return $this;
-    }
-
-    /**
      * Resolve the request
      *
      * @param Library\DispatcherContextInterface $context A dispatcher context object
@@ -262,54 +162,47 @@ class Dispatcher extends Library\DispatcherAbstract implements Library\ObjectIns
         //Set the request
         $context->request->query->add($url->query);
 
-        //Resolve the controller
-        if($context->request->query->has('component')) {
-            $this->setComponent($context->request->query->get('component', 'cmd'));
+        //Resolve the component
+        if($context->request->query->has('component'))
+        {
+            $identifier  = $this->getIdentifier()->toArray();!
+            $identifier['package'] = $context->request->query->get('component', 'cmd');;
+
+            $this->setController($identifier);
         }
     }
 
     /**
-     * Forward to the component
+     * Forward the request
      *
      * @param Library\DispatcherContextInterface $context A dispatcher context object
      */
     protected function _actionDispatch(Library\DispatcherContextInterface $context)
     {
-        $component = $this->getComponent();
-
         //Execute the component and pass along the context
-        $component->dispatch($context);
+        $this->getController()->dispatch($context);
     }
 
     /**
-     * Forward to the component
+     * Forward the request
      *
      * @throws \InvalidArgumentException If the action parameter is not an instance of Exception or ExceptionError
      * @param Library\DispatcherContextInterface $context	A dispatcher context object
      */
     protected function _actionFail(Library\DispatcherContextInterface $context)
     {
-        //Forward to the component
-        $component = $this->getComponent();
-
-        //Execute the component and pass along the context
-        $component->fail($context);
+        //Execute the component and pass along the contex
+        $this->getController()->fail($context);
     }
 
     /**
-     * Forward to the component
-     *
-     * Redirect to a URL externally. Method performs a 301 (permanent) redirect. Method should be used to immediately
-     * redirect the dispatcher to another URL after a GET request.
+     * Forward the request
      *
      * @param Library\DispatcherContextInterface $context	A dispatcher context object
      */
     protected function _actionRedirect(Library\DispatcherContextInterface $context)
     {
-        //Forward to the component
-        $component = $this->getComponent();
-
         //Execute the component and pass along the context
-        $component->redirect($context);
+        $this->getController()->redirect($context);
     }
 }
