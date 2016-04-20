ab988fb0cd902e45793245444e5ab9ab4f271ff7
Johan Janssens <johan@timble.net>
2016-04-20 04:51:56 +0200

    Set path templates in com:theme bootstrapper

--- application/admin/component/pages/resources/config/bootstrapper.php
+++ application/admin/component/pages/resources/config/bootstrapper.php
@@ -7,8 +7,6 @@
  * @link		https://github.com/timble/kodekit-platform for the canonical source repository
  */
 
-$theme_path = APPLICATION_BASE.'/public/theme/default';
-
 return array(
 
     'identifiers' => array(
@@ -23,20 +21,6 @@ return array(
 
         'com:pages.controller.page'  => array(
             'behaviors'  => array('com:varnish.controller.behavior.cachable'),
-        ),
-
-        'com:pages.template.locator.module'  => array(
-            'path_templates' => array($theme_path.'/templates/modules')
-        ),
-
-        'com:pages.template.filter.asset'         => array(
-            'schemes' => array(
-                'assets://theme/' => '/administrator/theme/default/'
-            )
-        ),
-
-        'lib:template.locator.component'  => array(
-            'path_templates' => array($theme_path.'/templates/view/<Package>/<Path>/<File>.<Format>.<Type>')
         ),
     )
 );
