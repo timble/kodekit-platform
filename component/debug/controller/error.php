d73652f9f5e17d91760eb7abf2868949d8fdf584
Johan Janssens <johan@timble.net>
2016-04-24 21:21:59 +0200

    Docblocks

--- component/debug/controller/error.php
+++ component/debug/controller/error.php
@@ -4,7 +4,7 @@
  *
  * @copyright	Copyright (C) 2011 - 2016 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license		MPL v2.0 <https://www.mozilla.org/en-US/MPL/2.0>
- * @link		https://github.com/timble/kodekit-deugger for the canonical source repository
+ * @link		https://github.com/timble/kodekit-debugger for the canonical source repository
  */
 
 namespace Kodekit\Component\Debug;
