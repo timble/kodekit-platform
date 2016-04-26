037ff2112a0d7a3db367f4a40826b0110c98d0cb
Johan Janssens <johan@timble.net>
2016-04-26 07:16:25 +0200

    Convert tabs to spaces

--- component/tags/database/behavior/taggable.php
+++ component/tags/database/behavior/taggable.php
@@ -20,12 +20,12 @@ use Kodekit\Library;
 class DatabaseBehaviorTaggable extends Library\DatabaseBehaviorAbstract
 {
     /**
-	 * Get a list of tags
-	 *
-	 * @return Library\DatabaseRowsetInterface
-	 */
-	public function getTags()
-	{
+     * Get a list of tags
+     *
+     * @return Library\DatabaseRowsetInterface
+     */
+    public function getTags()
+    {
         $package = $this->getMixer()->getIdentifier()->package;
         $model   = $this->getObject('com:tags.model.tags', array('table' => $package.'_tags'));
 
@@ -36,25 +36,25 @@ class DatabaseBehaviorTaggable extends Library\DatabaseBehaviorAbstract
         }
 
         return $tags;
-	}
+    }
 
     /**
-	 * Modify the select query
-	 *
-	 * If the query's where information includes a tag property, auto-join the tags tables with the query and select
+     * Modify the select query
+     *
+     * If the query's where information includes a tag property, auto-join the tags tables with the query and select
      * all the rows that are tagged with a term.
-	 */
-	protected function _beforeSelect(Library\DatabaseContext $context)
-	{
-		$query = $context->query;
+     */
+    protected function _beforeSelect(Library\DatabaseContext $context)
+    {
+        $query = $context->query;
 
         if($context->query->params->has('tag'))
-		{
+        {
             $package = $this->getMixer()->getIdentifier()->package;
 
             $query->where('tags.slug = :tag');
             $query->join('LEFT', $package.'_tags_relations AS tags_relations', 'tags_relations.row = tbl.uuid');
             $query->join('LEFT', $package.'_tags AS tags', 'tags.tag_id = tags_relations.tag_id');
-		}
-	}
+        }
+    }
 }
