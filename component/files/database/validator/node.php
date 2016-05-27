fc38969cbd69ccf5283900e93a9b3e35bf667518
Johan Janssens <johan@timble.net>
2016-05-27 23:28:35 +0200

    Code cleanup

--- component/files/database/validator/node.php
+++ component/files/database/validator/node.php
@@ -19,59 +19,59 @@ use Kodekit\Library;
  */
 class DatabaseValidatorNode extends Library\CommandHandlerAbstract
 {
-	protected function _beforeSave(Library\DatabaseContext $context)
-	{
-		$entity = $context->getSubject();
+    protected function _beforeSave(Library\DatabaseContext $context)
+    {
+        $entity = $context->getSubject();
 
-		if (!$entity->isNew() && !$entity->overwrite)
+        if (!$entity->isNew() && !$entity->overwrite)
         {
             $translator = $this->getObject('translator');
             $entity->setStatusMessage($translator('Resource already exists and overwrite switch is not present.'));
-			return false;
-		}
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	protected function _beforeCopy(Library\DatabaseContext $context)
-	{
-		$entity = $context->getSubject();
+    protected function _beforeCopy(Library\DatabaseContext $context)
+    {
+        $entity = $context->getSubject();
 
         $translator = $this->getObject('translator');
 
-		if (!$entity->isModified('destination_folder') && !$entity->isModified('destination_name'))
+        if (!$entity->isModified('destination_folder') && !$entity->isModified('destination_name'))
         {
             $entity->setStatusMessage($translator('Please supply a destination.'));
-			return false;
-		}
+            return false;
+        }
 
-		if ($entity->fullpath === $entity->destination_fullpath)
+        if ($entity->fullpath === $entity->destination_fullpath)
         {
             $entity->setStatusMessage($translator('Source and destination are the same.'));
-			return false;
-		}
+            return false;
+        }
 
-		$dest_adapter = $entity->getContainer()->getAdapter($entity->getIdentifier()->name, array(
-			'path' => $entity->destination_fullpath
-		));
+        $dest_adapter = $entity->getContainer()->getAdapter($entity->getIdentifier()->name, array(
+            'path' => $entity->destination_fullpath
+        ));
 
-		$exists = $dest_adapter->exists();
+        $exists = $dest_adapter->exists();
 
-		if ($exists)
-		{
-			if (!$entity->overwrite)
+        if ($exists)
+        {
+            if (!$entity->overwrite)
             {
                 $entity->setStatusMessage($translator('Destination resource already exists.'));
-				return false;
-			}
+                return false;
+            }
             else $entity->overwritten = true;
-		}
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	protected function _beforeMove(Library\DatabaseContext $context)
-	{
-		return $this->_beforeCopy($context);
-	}
+    protected function _beforeMove(Library\DatabaseContext $context)
+    {
+        return $this->_beforeCopy($context);
+    }
 }
