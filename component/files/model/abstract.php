fc29f0241a2fefb57e2c9b74795321f9a982ad1d
Johan Janssens <johan@timble.net>
2016-05-27 23:28:54 +0200

    Code cleanup

--- component/files/model/abstract.php
+++ component/files/model/abstract.php
@@ -19,24 +19,23 @@ use Kodekit\Library;
  */
 abstract class ModelAbstract extends Library\ModelAbstract
 {
-	public function __construct(Library\ObjectConfig $config)
-	{
-		parent::__construct($config);
+    public function __construct(Library\ObjectConfig $config)
+    {
+        parent::__construct($config);
 
         $this->getState()
             ->insert('limit'    , 'int')
             ->insert('offset'   , 'int')
             ->insert('sort'     , 'cmd')
-            ->insert('direction', 'word', 'asc')
             ->insert('search'   , 'string')
 
-			->insert('container', 'com:files.filter.container', null)
-			->insert('folder'	, 'com:files.filter.path', '')
-			->insert('name'		, 'com:files.filter.path', '', true)
+            ->insert('container', 'com:files.filter.container', null)
+            ->insert('folder'	, 'com:files.filter.path', '')
+            ->insert('name'		, 'com:files.filter.path', '', true)
 
-			->insert('types'	, 'cmd', '')
-			->insert('editor'   , 'string', '') // used in modal windows
-			->insert('config'   , 'json', '')   // used to pass options to the JS application in HMVC
-			;
-	}
+            ->insert('types'	, 'cmd', '')
+            ->insert('editor'   , 'string', '') // used in modal windows
+            ->insert('config'   , 'json', '')   // used to pass options to the JS application in HMVC
+            ;
+    }
 }
