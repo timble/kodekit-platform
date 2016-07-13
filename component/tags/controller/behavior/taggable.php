2ce6bcd881bda18c87433fb22397293c4be247e5
Ercan Özkaya <ercan@timble.net>
2016-07-13 19:44:23 +0300

    Add support for different tag operations
    See: joomlatools/joomlatools-framework-tags#13

--- component/tags/controller/behavior/taggable.php
+++ component/tags/controller/behavior/taggable.php
@@ -25,7 +25,7 @@ class ControllerBehaviorTaggable extends Library\BehaviorAbstract
 
         $this->addCommandCallback('after.add'    , '_setTags');
         $this->addCommandCallback('after.edit'   , '_setTags');
-        $this->addCommandCallback('after.delete' , '_removeTags');
+        $this->addCommandCallback('before.delete' , '_removeTags');
     }
     
     /**
@@ -34,56 +34,120 @@ class ControllerBehaviorTaggable extends Library\BehaviorAbstract
      * If the request data contains a tags array, it will be used as the new tag list.
      * If the tags field is an empty string, all entity tags are deleted and no new ones are added.
      *
+     * Operation mode can be controlled by the tags_operation in the request data.
+     * Possible values are append|remove|replace, replace being the default.
+     * 
      * @param Library\ControllerContextModel $context
      * @return bool
      */
     protected function _setTags(Library\ControllerContextModel $context)
     {
-        $entity = $context->result;
-        $data   = $context->getRequest()->getData();
+        $entities = $context->result;
+        $data     = $context->getRequest()->getData();
 
-        if ($data->has('tags'))
+        if ($data->has('tags') && !$context->response->isError())
         {
-            if ($entity->isIdentifiable() && !$context->response->isError())
+            foreach($entities as $entity)
             {
-                $tags = $entity->getTags();
-    
-                $package = $this->getMixer()->getIdentifier()->package;
-                if(!$this->getObject('com:'.$package.'.controller.tag')->canAdd()) {
-                    $status  = Library\Database::STATUS_FETCHED;
-                } else {
-                    $status = null;
-                }
-    
-                //Delete tags
-                if(count($tags))
+                if ($entity->isIdentifiable())
                 {
-                    $tags->delete();
-                    $tags->clear();
-                }
-    
-                //Create tags
-                if($entity->tags)
-                {
-                    foreach ($entity->tags as $tag)
-                    {
-                        $properties = array(
-                            'title' => $tag,
-                            'row'   => $entity->uuid,
-                        );
-    
-                        $tags->insert($properties, $status);
+                    $operation = $entity->tags_operation;
+
+                    if ($operation === 'remove') {
+                        $this->_removeTags($entity);
+                    } else if ($operation === 'append') {
+                        $this->_appendTags($entity);
+                    } else {
+                        $this->_replaceTags($entity);
                     }
                 }
+            }
+        }
+    }
     
+    
+    /**
+     * Replaces the entity tags with the ones sent in the request
+     *
+     * @param Library\ModelEntityInterface $entity
+     */
+    protected function _replaceTags(Library\ModelEntityInterface $entity)
+    {
+        $tags = $entity->getTags();
+
+        //Delete tags
+        if(count($tags))
+        {
+            $tags->delete();
+            $tags->reset();
+        }
+
+        $this->_appendTags($entity);
+    }
+
+    /**
+     * Appends the tags sent in the request to the entity
+     *
+     * @param Library\ModelEntityInterface $entity
+     */
+    protected function _appendTags(Library\ModelEntityInterface $entity)
+    {
+        $package = $this->getMixer()->getIdentifier()->package;
+        if(!$this->getObject('com:'.$package.'.controller.tag')->canAdd()) {
+            $status  = Library\Database::STATUS_FETCHED;
+        } else {
+            $status = null;
+        }
+
+        $tags     = $entity->getTags();
+        $existing = array();
+
+        foreach ($tags as $tag) {
+            $existing[] = $tag->title;
+        }
+
+        //Create tags
+        if($entity->tags)
+        {
+            foreach ($entity->tags as $tag)
+            {
+                if (in_array($tag, $existing)) {
+                    continue;
+                }
+
+                $config = array(
+                    'data' => array(
+                        'title' => $tag,
+                        'row'   => $entity->uuid,
+                    ),
+                    'status' => $status,
+                );
+
+                $row = $tags->getTable()->createRow($config);
+
+                $tags->insert($row);
                 $tags->save();
-    
-                return true;
             }
         }
     }
 
-    protected function _removeTags(Library\ControllerContextModel $context)
+    /**
+     * Removes the tags sent in the request from the entity
+     * @param Library\ModelEntityInterface $entity
+     */
+    protected function _removeTags(Library\ModelEntityInterface $entity)
+    {
+        $tags = $entity->getTags();
+
+        foreach ($tags as $tag)
+        {
+            if (in_array($tag->title, $entity->tags)) {
+                $tag->delete();
+            }
+        }
+    }
+    
+    protected function _deleteTags(Library\ControllerContextModel $context)
     {
         $collection = $context->entity;
 
